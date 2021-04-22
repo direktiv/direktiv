0b67669a1b3050587f67fba2b5755852e0d8e9b1
James <james.murtagh@vorteil.io>
2021-04-22 14:43:30 +1000

    Fix kubernetes configuration

--- scripts/push/pushSecrets.sh
+++ scripts/push/pushSecrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-make docker-api && docker tag direktiv-api localhost:5000/secrets
+make docker-secrets && docker tag direktiv-secrets localhost:5000/secrets
 
 docker push localhost:5000/secrets
