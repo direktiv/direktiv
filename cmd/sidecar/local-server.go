d38fb32cc206b8b43c0ef5ea06f5c216c74e0380
Alan Murtagh <alan.murtagh@vorteil.io>
2021-05-21 12:53:41 +1000

    Improved handling of sidecar var POST requests.

--- cmd/sidecar/local-server.go
+++ cmd/sidecar/local-server.go
@@ -1074,7 +1074,7 @@ func (srv *LocalServer) setVar(ctx context.Context, ir *isolateRequest, totalSiz
 	}
 
 	chunkSize := int64(0x200000) // 2 MiB
-	if totalSize == 0 {
+	if totalSize <= 0 {
 		buf := new(bytes.Buffer)
 		_, err := io.CopyN(buf, r, chunkSize+1)
 		if err == nil {
