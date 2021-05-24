2c9a1b35d084397fad341ae9f98f6a874bbc999a
trent <trent.hilliam@vorteil.io>
2021-05-24 11:37:35 +1000

    Removed debug
    Signed-off-by: trent <trent.hilliam@vorteil.io>

--- pkg/dlog/db/handler.go
+++ pkg/dlog/db/handler.go
@@ -106,7 +106,6 @@ func (h *Handler) dispatcher() {
 			goto nextIter
 		}
 
-		fmt.Printf("logs: %+v\n", h.queuedLogs)
 		for i, msg := range h.queuedLogs {
 
 			ctxMap := make(map[string]interface{}, 0)
