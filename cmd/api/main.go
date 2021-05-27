ba75ce18637ef0e27ba313d99708b50095949b53
Jon Alfaro <45082495+jalfvort@users.noreply.github.com>
2021-05-28 08:40:12 +1000

    apiKey added to api and helm chart (#53)
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strings"
 
 	"github.com/vorteil/direktiv/pkg/api"
 )
+
+// Envrioment Value Name for Manual apikey
+const API_KEY_ENV = "DIREKTIV_API_KEY"
+
+// apiKey to be used for simple apikey auth
+var apiKey = ""
 
 func main() {
 
@@ -18,9 +28,45 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	// Enabled simple apikey auth if env is set
+	apiKey = os.Getenv(API_KEY_ENV)
+	if len(apiKey) > 0 {
+		fmt.Println("apiKey auth mode enabled")
+		s.Router().Use(APIAuthMiddleware)
+	}
+
 	err = s.Start()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+}
+
+// API Auth Middleware
+func APIAuthMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Allow preflight Options
+		if r.Method == "OPTIONS" {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		// Get Auth token
+		reqToken := r.Header.Get("Authorization")
+		splitToken := strings.Split(reqToken, "apikey")
+		if len(splitToken) != 2 {
+			w.WriteHeader(401)
+			w.Write([]byte("apikey Token is malformed"))
+			return
+		}
+
+		reqToken = strings.TrimSpace(splitToken[1])
+
+		if reqToken != apiKey {
+			w.WriteHeader(401)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
