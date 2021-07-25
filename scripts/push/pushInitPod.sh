ede9f438f473f045f8eb556c8729b04184cdb537
Jens Gerke <jens.gerke@vorteil.io>
2021-07-25 14:06:17 +1000

    added orphan jobs cleanup
    Signed-off-by: Jens Gerke <jens.gerke@vorteil.io>

--- scripts/push/pushInitPod.sh
+++ scripts/push/pushInitPod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-make docker-init-pod  && docker tag init-pod localhost:5000/init-pod
+make docker-init-pod  && docker tag direktiv-init-pod localhost:5000/init-pod
 
 docker push localhost:5000/init-pod
