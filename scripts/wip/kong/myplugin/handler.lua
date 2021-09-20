55f48663679491c7f734305d56256858dd5bed9b
Jon Alfaro <jon.alfaro@vorteil.io>
2021-09-20 10:51:37 +1000

    lua handler ...
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- scripts/wip/kong/myplugin/handler.lua
+++ scripts/wip/kong/myplugin/handler.lua
@@ -1,81 +1,20 @@
--- If you're not sure your plugin is executing, uncomment the line below and restart Kong
--- then it will throw an error which indicates the plugin is being loaded at least.
-
---assert(ngx.get_phase() == "timer", "The world is coming to an end!")
-
----------------------------------------------------------------------------------------------
--- In the code below, just remove the opening brackets; `[[` to enable a specific handler
---
--- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
--- on when exactly they are invoked and what limitations each handler has.
----------------------------------------------------------------------------------------------
-
-
-
 local plugin = {
-  PRIORITY = 500, -- set the plugin priority, which determines plugin execution order
+  PRIORITY = 500,
   VERSION = "0.1",
 }
 
-
-
--- do initialization here, any module level code runs in the 'init_by_lua_block',
--- before worker processes are forked. So anything you add here will run once,
--- but be available in all workers.
-
-
-
--- handles more initialization, but AFTER the worker process has been forked/created.
--- It runs in the 'init_worker_by_lua_block'
 function plugin:init_worker()
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'init_worker' handler")
-
-end --]]
-
-
-
---[[ runs in the 'ssl_certificate_by_lua_block'
--- IMPORTANT: during the `certificate` phase neither `route`, `service`, nor `consumer`
--- will have been identified, hence this handler will only be executed if the plugin is
--- configured as a global plugin!
-function plugin:certificate(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'certificate' handler")
-
-end --]]
-
-
-
---[[ runs in the 'rewrite_by_lua_block'
--- IMPORTANT: during the `rewrite` phase neither `route`, `service`, nor `consumer`
--- will have been identified, hence this handler will only be executed if the plugin is
--- configured as a global plugin!
-function plugin:rewrite(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'rewrite' handler")
-
-end --]]
-
+end
 
 
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
-
-  -- your custom code here
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-
-end --]]
+end
 
 
 -- runs in the 'header_filter_by_lua_block'
 function plugin:header_filter(plugin_conf)
-
-  -- your custom code here, for example;
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
   kong.response.set_header("Access-Control-Allow-Origin", "*")
   kong.response.set_header("Access-Control-Allow-Headers", "Content-Type")
   kong.response.set_header("Content-Type",  "text/event-stream")
@@ -96,13 +35,16 @@ function plugin:body_filter(conf)
 
   kong.log.debug("!!!! GOT CHUNK === ", chunk or "NO Chunk!")
 
-
+  -- Check if response is bonked 
+  kong.log.debug("!!!! response === ", kong.response.get_status())
+  kong.log.debug("!!!! CONTENT TYPE === ", kong.response.get_header("Content-Type"))
+  kong.log.debug("!!!! grpc-message === ", kong.response.get_header("grpc-message") or "Internal Error")
+    
   if eof then
     local body = concat(ctx.rt_body_chunks)
     -- kong.log.debug("body === ", body)
     -- ngx.arg[1] = upper(body)
     ngx.arg[1] = "error: eof\n\n"
-
   else
     ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
     ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
@@ -114,24 +56,4 @@ function plugin:body_filter(conf)
   end
 end
 
-
---[[ runs in the 'body_filter_by_lua_block'
-function plugin:body_filter(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'body_filter' handler")
-
-end --]]
-
-
---[[ runs in the 'log_by_lua_block'
-function plugin:log(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'log' handler")
-
-end --]]
-
-
--- return our plugin object
 return plugin
