c93e1d2f131f08c596d92265406b356dbab3502a
Jens Gerke <jens.gerke@vorteil.io>
2021-09-22 07:36:56 +1000

    telemetry config
    Signed-off-by: Jens Gerke <jens.gerke@vorteil.io>

--- pkg/flow/server.go
+++ pkg/flow/server.go
@@ -20,33 +20,6 @@ import (
 
 const parcelSize = 0x100000
 
-// type Config struct {
-// 	FunctionsService string `yaml:"functions-service"`
-// 	FlowService      string `yaml:"flow-service"`
-//
-// 	PrometheusBackend string `yaml:"prometheus-backend"`
-// 	RedisBackend      string `yaml:"redis-backend"`
-// }
-//
-// func ReadConfig(file string) (*Config, error) {
-//
-// 	c := new(Config)
-//
-// 	/* #nosec */
-// 	data, err := ioutil.ReadFile(file)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	err = yaml.Unmarshal(data, c)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	return c, nil
-//
-// }
-
 type server struct {
 	ID uuid.UUID
 
