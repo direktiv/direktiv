cd7adeeacf2a6871d839ae3763fa067401804cf3
trent <trent.hilliam@vorteil.io>
2021-09-22 10:24:40 +1000

    changed workflow variable apis to read from query params

--- pkg/api/flow.go
+++ pkg/api/flow.go
@@ -2295,7 +2295,7 @@ func (h *flowHandler) WorkflowVariable(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	namespace := mux.Vars(r)["ns"]
 	path, _ := pathAndRef(r)
-	key := mux.Vars(r)["var"]
+	key := r.URL.Query().Get("var")
 
 	in := &grpc.WorkflowVariableRequest{
 		Namespace: namespace,
@@ -2343,7 +2343,7 @@ func (h *flowHandler) SetWorkflowVariable(w http.ResponseWriter, r *http.Request
 	ctx := r.Context()
 	namespace := mux.Vars(r)["ns"]
 	path, _ := pathAndRef(r)
-	key := mux.Vars(r)["var"]
+	key := r.URL.Query().Get("var")
 
 	var rdr io.Reader
 	rdr = r.Body
@@ -2405,7 +2405,7 @@ func (h *flowHandler) DeleteWorkflowVariable(w http.ResponseWriter, r *http.Requ
 	ctx := r.Context()
 	namespace := mux.Vars(r)["ns"]
 	path, _ := pathAndRef(r)
-	key := mux.Vars(r)["var"]
+	key := r.URL.Query().Get("var")
 
 	in := &grpc.DeleteWorkflowVariableRequest{
 		Namespace: namespace,
