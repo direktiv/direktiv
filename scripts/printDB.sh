3dd2f595582055f6fc2f9e445f1512bdbd8b5fda
Jon Alfaro <jon.alfaro@vorteil.io>
2021-10-07 12:09:41 +1000

    updated scripts to get database secrets again
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- scripts/printDB.sh
+++ scripts/printDB.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "database:
-  host: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "pgbouncer-host"}}' | base64 --decode)\"
-  port: $(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "pgbouncer-port"}}' | base64 --decode)
+  host: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "host"}}' | base64 --decode)\"
+  port: $(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "port"}}' | base64 --decode)
   user: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "user"}}' | base64 --decode)\"
   password: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "password"}}' | base64 --decode)\"
   name: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "dbname"}}' | base64 --decode)\"
--- scripts/resetAll.sh
+++ scripts/resetAll.sh
@@ -86,6 +86,7 @@ helm repo add direktiv https://charts.direktiv.io
 
 helm repo update
 helm search repo direktiv
+helm dependency update $dir/../kubernetes/charts/direktiv
 helm install -n postgres --set singleNamespace=true postgres direktiv/pgo --wait
 kubectl apply -f $dir/../kubernetes/install/db/pg.yaml
 
@@ -95,8 +96,8 @@ countdown
 
 echo ""
 echo "database:
-  host: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "pgbouncer-host"}}' | base64 --decode)\"
-  port: $(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "pgbouncer-port"}}' | base64 --decode)
+  host: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "host"}}' | base64 --decode)\"
+  port: $(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "port"}}' | base64 --decode)
   user: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "user"}}' | base64 --decode)\"
   password: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "password"}}' | base64 --decode)\"
   name: \"$(kubectl get secrets -n postgres direktiv-pguser-direktiv -o 'go-template={{index .data "dbname"}}' | base64 --decode)\"
