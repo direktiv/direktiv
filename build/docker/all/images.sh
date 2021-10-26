06bbd5f29117688cf761668f9998a61cd19f1341
trent <trent.hilliam@vorteil.io>
2021-10-27 08:37:51 +1000

    Updated version hashes

--- build/docker/all/images.sh
+++ build/docker/all/images.sh
@@ -52,7 +52,7 @@ docker save --output=postgres.tar postgres:13.4
 # direktiv
 
 PREFIX="localhost:5000"
-VERSION="v0.5.5"
+VERSION="v0.5.6"
 
 docker pull $PREFIX/flow
 docker tag $PREFIX/flow vorteil/flow:$VERSION
