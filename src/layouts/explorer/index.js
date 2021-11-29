b4f6cd2d19cf8bec054584896aee83b10f02f3ef
James Murtagh <james.murtagh@vorteil.io>
2021-11-29 11:21:21 +1000

    Adding more to explorer

--- src/layouts/explorer/index.js
+++ src/layouts/explorer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
-import { IoAdd, IoChevronDown, IoChevronDownSharp, IoFolderOpen, IoSearch } from 'react-icons/io5';
+import { IoAdd, IoFolderOpen, IoSearch } from 'react-icons/io5';
 import ContentPanel, { ContentPanelBody, ContentPanelHeaderButton, ContentPanelHeaderButtonIcon, ContentPanelTitle, ContentPanelTitleIcon } from '../../components/content-panel';
 import FlexBox from '../../components/flexbox';
-import { BsChevronBarDown } from 'react-icons/bs';
-import { FaChevronDown } from 'react-icons/fa';
 import { VscTriangleDown } from 'react-icons/vsc';
+import { GenerateRandomKey } from '../../util';
+import { FiEdit, FiFolder } from 'react-icons/fi';
+import { FcWorkflow } from 'react-icons/fc';
+import { HiOutlineTrash } from 'react-icons/hi';
 
 function Explorer(props) {
     return(
@@ -33,15 +35,15 @@ function Explorer(props) {
                                 </FlexBox>
                             </div>
                         </div>
-                        <ContentPanelHeaderButton style={{ maxWidth: "150px", width: "150px", minWidth: "150px" }}>
+                        <ContentPanelHeaderButton style={{ maxWidth: "170px", width: "170px", minWidth: "170px" }}>
                             <ContentPanelHeaderButtonIcon>
                                 <IoAdd/>
                             </ContentPanelHeaderButtonIcon>
-                            Create workflow
+                            New Folder/Workflow
                         </ContentPanelHeaderButton>
                     </ContentPanelTitle>
                     <ContentPanelBody>
-                        Hello world (Explorer)
+                        <ExplorerList />
                     </ContentPanelBody>
                 </ContentPanel>
             </FlexBox>
@@ -60,4 +62,80 @@ function SearchBar(props) {
             </FlexBox>
         </div>
     );
+}
+
+function ExplorerList(props) {
+
+    let tmp = [{
+        "type": "dir",
+        "name": "important"
+    },{
+        "type": "wf",
+        "name": "example"
+    }]
+
+    return(
+        <FlexBox className="col">
+            {tmp.map((obj) => {
+                console.log(obj);
+                if (obj.type === "dir") {
+                    return (<DirListItem key={GenerateRandomKey("explorer-item-")} name={obj.name} />)
+                } else if (obj.type === "wf") {
+                    return (<WorkflowListItem key={GenerateRandomKey("explorer-item-")} name={obj.name} />)
+                }
+            })}
+        </FlexBox>
+    )
+}
+
+function DirListItem(props) {
+
+    let {name} = props;
+
+    return(
+        <div className="explorer-item">
+            <FlexBox className="explorer-item-container">
+                <FlexBox className="explorer-item-icon">
+                    <FiFolder className="auto-margin" />
+                </FlexBox>
+                <FlexBox className="explorer-item-name">
+                    {name}
+                </FlexBox>
+                <FlexBox className="explorer-item-actions">
+                    <FlexBox>
+                        <FiEdit />
+                    </FlexBox>
+                    <FlexBox>
+                        <HiOutlineTrash />
+                    </FlexBox>
+                </FlexBox>
+            </FlexBox>
+        </div>
+    )
+}
+
+function WorkflowListItem(props) {
+
+    let {name} = props;
+
+    return(
+        <div className="explorer-item">
+            <FlexBox className="explorer-item-container">
+                <FlexBox className="explorer-item-icon">
+                    <FcWorkflow className="auto-margin" />
+                </FlexBox>
+                <FlexBox className="explorer-item-name">
+                    {name}
+                </FlexBox>
+                <FlexBox className="explorer-item-actions">
+                    <FlexBox>
+                        <FiEdit />
+                    </FlexBox>
+                    <FlexBox>
+                        <HiOutlineTrash />
+                    </FlexBox>
+                </FlexBox>
+            </FlexBox>
+        </div>
+    )
 }
