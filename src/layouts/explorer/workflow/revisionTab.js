7376684cc230cc5ff12ad45dd36e8b6ca723b360
Olexandr Olexenko <ooleksenko4work@gmail.com>
2022-01-17 15:04:14 +0200

    make disable button if invalid sections

--- src/layouts/explorer/workflow/revisionTab.js
+++ src/layouts/explorer/workflow/revisionTab.js
@@ -724,7 +724,7 @@ export function RevisionTrafficShaper(props) {
                         }
                         await editWorkflowRouter(arr, router.live)
                         setRouter(await getWorkflowRouter())
-                    }} className="small">
+                    }} className={`small ${rev2 && rev1 ? "" : "disabled"}`}>
                         Save
                     </Button>
                 </FlexBox>
