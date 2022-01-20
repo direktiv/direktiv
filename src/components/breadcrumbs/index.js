4ffcf13058e6bb44b75b8c607c75f8081cb210ac
alex-olexenko <94471043+alex-olexenko@users.noreply.github.com>
2022-01-20 02:47:42 +0200

    #121 optimize long folder path (#182)
    * optimize long folder path
    
    * interm fix
    
    Signed-off-by: jalfvort <jon.alfaro@direktiv.io>
    
    * small css fix
    
    Signed-off-by: jalfvort <jon.alfaro@direktiv.io>
    
    Co-authored-by: jalfvort <jon.alfaro@direktiv.io>

--- src/components/breadcrumbs/index.js
+++ src/components/breadcrumbs/index.js
@@ -16,6 +16,7 @@ function Breadcrumbs(props) {
     const {namespace} = props
     const breadcrumbs = useBreadcrumbs(routes)
     const [searchParams] = useSearchParams() // removed 'setSearchParams' from square brackets (this should not affect anything: search 'destructuring assignment')
+    
     if (!namespace){
         return <></>
     }
@@ -23,13 +24,27 @@ function Breadcrumbs(props) {
     return(
         <FlexBox>
             <ul>
-                {breadcrumbs.map((obj)=>{
+                {breadcrumbs.length < 9 ? breadcrumbs.map((obj)=>{
                     // ignore breadcrumbs for dividers
                     if(obj.key === "/g" || obj.key === "/n" || obj.key === "/" || obj.key === `/n/${namespace}/explorer` ) {
                         return ""
                     }
                     let key = GenerateRandomKey("crumb-");
-
+               
+                    return(
+                        <li id={key} key={key}>
+                            <Link to={obj.key}>
+                                {obj.breadcrumb}
+                            </Link>
+                        </li>
+                    )
+                }) : 
+               <>
+               {breadcrumbs.slice(0, 6).map((obj)=>{
+                    if(obj.key === "/g" || obj.key === "/n" || obj.key === "/" || obj.key === `/n/${namespace}/explorer` ) {
+                        return ""
+                    }
+                    let key = GenerateRandomKey("crumb-");
                
                     return(
                         <li id={key} key={key}>
@@ -39,6 +54,25 @@ function Breadcrumbs(props) {
                         </li>
                     )
                 })}
+                <li id={"crumb-divider"} key={"crumb-divider"}>
+                    <span>{". . . "}</span>
+                </li>
+                {breadcrumbs.slice(-3).map((obj)=>{
+                    let key = GenerateRandomKey("crumb-");
+               
+                    return(
+                        <li id={key} key={key}>
+                            <Link to={obj.key}>
+                                {obj.breadcrumb}
+                            </Link>
+                        </li>
+                    )
+                })
+                }
+               </>
+                }
+                
+                
                 {searchParams.get("function") && searchParams.get("version") ? 
                     <li id={`${searchParams.get("function")}-${searchParams.get("version")}`} key={`${searchParams.get("function")}-${searchParams.get("version")}`}>
                         <Link to={`${window.location.pathname}?function=${searchParams.get("function")}&version=${searchParams.get("version")}`}>
