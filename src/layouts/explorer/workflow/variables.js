18e7042631d4fa3e3ed6eae4e4ef434a9ebe398f
Jens Gerke <jens.gerke@direktiv.io>
2022-01-28 09:06:40 +1000

    Fixes (#263)
    * fixed issue with variable length 0
    
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>
    
    * app name, remove compare warning
    
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>
    
    * fixed variable warnings
    
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>
    
    * fixed 0 value on workflow attributes
    
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- src/layouts/explorer/workflow/variables.js
+++ src/layouts/explorer/workflow/variables.js
@@ -393,6 +393,9 @@ function VariablesDeleteButton() {
 }
 
 function fileSize(size) {
+    if (size <= 0) {
+        return "0 B"
+    }
     var i = Math.floor(Math.log(size) / Math.log(1024));
     return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 }
--- src/layouts/settings/variables-panel/index.js
+++ src/layouts/settings/variables-panel/index.js
@@ -72,7 +72,7 @@ function VariablesPanel(props){
                                         throw new Error("Variable key name needs to be provided.")
                                     }
                                     if(!file) {
-                                        throw new Error("Variable key name needs to be provided.")
+                                        throw new Error("Variable file needs to be provided.")
                                     }
                                     await setNamespaceVariable(encodeURIComponent(keyValue), file, mimeType)
                                 } else {
@@ -80,9 +80,11 @@ function VariablesPanel(props){
                                         throw new Error("Variable key name needs to be provided.")
                                     }
                                     if(dValue.trim() === "") {
+                                        throw new Error("Variable value needs to be provided.")
+                                    }
+                                    if (mimeType === "") {
                                         throw new Error("Variable mimetype needs to be provided.")
                                     }
-
                                     await setNamespaceVariable(encodeURIComponent(keyValue), dValue, mimeType)
                                 }
                             }, uploadingBtn, ()=>{setUploading(false)}, true, false, true),
@@ -441,6 +443,9 @@ function VariablesDeleteButton() {
 }
 
 function fileSize(size) {
+    if (size <= 0) {
+        return "0 B"
+    }
     var i = Math.floor(Math.log(size) / Math.log(1024));
     return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 }
