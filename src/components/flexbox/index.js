e6b48a59825f93841652606207fe20d339c56119
jalfvort <jon.alfaro@direktiv.io>
2022-06-21 16:02:38 +1000

    Typescript added to project and New Logs Component added
    Signed-off-by: jalfvort <jon.alfaro@direktiv.io>

--- src/components/flexbox/index.js
+++ src/components/flexbox/index.js
@@ -3,13 +3,13 @@ import './style.css';
 
 function FlexBox(props){
 
-    let {id, children, onClick, style, className, ref, key} = props;
+    let {id, children, onClick, style, className, ref, key, onWheel, onMouseDown} = props;
     if (!className) {
         className = ""
     }
 
     return(
-        <div ref={ref} key={key} id={id} onClick={onClick} style={style} className={"flex-box " + className}>
+        <div ref={ref} key={key} id={id} onClick={onClick} style={style} className={"flex-box " + className} onWheel={onWheel} onMouseDown={onMouseDown}>
             {children}
         </div>
     );
