d32c5503bc3e9b236f6302fa2081244cdebcd052
Stefan Kracht <stefan.kracht@direktiv.io>
2023-01-25 10:58:50 +0100

    fix issue with bpfrpt_proptype_WindowScroller

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import path from "path";
+import fs from "fs";
+
+//  fix https://github.com/uber/baseweb/issues/4129
+const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`;
+export function reactVirtualized() {
+  return {
+    name: "my:react-virtualized",
+    configResolved() {
+      const file = require
+        .resolve("react-virtualized")
+        .replace(
+          path.join("dist", "commonjs", "index.js"),
+          path.join("dist", "es", "WindowScroller", "utils", "onScroll.js")
+        );
+      const code = fs.readFileSync(file, "utf-8");
+      const modified = code.replace(WRONG_CODE, "");
+      fs.writeFileSync(file, modified);
+    },
+  };
+}
 
 export default defineConfig({
   optimizeDeps: {
@@ -11,5 +32,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), reactVirtualized()],
 });
