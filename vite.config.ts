205d417ea4bdc339dc7acbc80f807dde8c21cd15
Stefan Kracht <stefan.kracht@direktiv.io>
2023-01-25 10:17:23 +0100

    add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+});
