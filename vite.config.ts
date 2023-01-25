305ad9eb17288209d0049e3219746d9610855f2b
Stefan Kracht <stefan.kracht@direktiv.io>
2023-01-25 14:19:55 +0100

    configure VITE_DEV_API_DOMAIN and add example .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
@@ -24,21 +24,31 @@ export function reactVirtualized() {
   };
 }
 
-export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: process.env.DIREKTIV_API_HOST,
+export default ({ mode }) => {
+  const { VITE_DEV_API_DOMAIN } = loadEnv(mode, process.cwd());
+
+  if (VITE_DEV_API_DOMAIN) {
+    console.warn("VITE_DEV_API_DOMAIN is not set, no API proxy will be used");
+  }
+
+  return defineConfig({
+    server: {
+      port: 3000,
+      proxy: VITE_DEV_API_DOMAIN
+        ? {
+            "/api": {
+              target: VITE_DEV_API_DOMAIN,
+            },
+          }
+        : {},
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+        },
       },
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-      },
-    },
-  },
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), reactVirtualized()],
-});
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin(), reactVirtualized()],
+  });
+};
