496258e35a2c6cf8b8887b995ca1c08ef176eecb
Stefan Kracht <stefan.kracht@direktiv.io>
2023-01-31 11:06:18 +0100

    remove @storybook/addon-links

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,6 @@ module.exports = {
   },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-react-router-v6",
