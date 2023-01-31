ac48b3b6f80bd7e786a20f83c7dc70f1eef38f0c
Stefan Kracht <stefan.kracht@direktiv.io>
2023-01-31 11:12:41 +0100

    upgrade storybook and add eslint-plugin-storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "plugin:import/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
     // Make sure it's always the last config, so it gets the chance to override other configs.
     "eslint-config-prettier",
