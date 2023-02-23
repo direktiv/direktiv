0f6f2f69b031ff835d4c97521b1bdf6f66e7b496
Stefan Kracht <stefan.kracht@direktiv.io>
2023-02-23 14:02:44 +0100

    update children

--- src/design-system/button/index.stories.tsx
+++ src/design-system/button/index.stories.tsx
@@ -12,8 +12,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: (args) => {
-    return <Button {...args}>{args.children}</Button>;
+  render: ({ children, ...args }) => {
+    return <Button {...args}>{children}</Button>;
   },
   args: {
     children: "Hello",
