3c6c7b54a2d417669daab8ad7472bb927576398b
Stefan Kracht <stefan.kracht@direktiv.io>
2023-02-23 15:38:42 +0100

    simplify button component with clsx

--- src/design-system/button/index.tsx
+++ src/design-system/button/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import clsx from "clsx";
 
 const Button: FC<{
   size?: "xs" | "sm" | "lg";
@@ -28,66 +29,32 @@ const Button: FC<{
   className,
   children,
   ...props
-}) => {
-  let sizeClass;
-  switch (size) {
-    case "lg":
-      sizeClass = "btn-lg";
-      break;
-    case "sm":
-      sizeClass = "btn-sm";
-      break;
-    case "xs":
-      sizeClass = "btn-xs";
-      break;
-  }
-
-  let colorClass;
-  switch (color) {
-    case "primary":
-      colorClass = "btn-primary";
-      break;
-    case "secondary":
-      colorClass = "btn-secondary";
-      break;
-    case "accent":
-      colorClass = "btn-accent";
-      break;
-    case "ghost":
-      colorClass = "btn-ghost";
-      break;
-    case "link":
-      colorClass = "btn-link";
-      break;
-    case "info":
-      colorClass = "btn-info";
-      break;
-    case "success":
-      colorClass = "btn-success";
-      break;
-    case "warning":
-      colorClass = "btn-warning";
-      break;
-    case "error":
-      colorClass = "btn-error";
-      break;
-  }
-
-  const activeClass = active ? "btn-active" : "";
-  const outlineClass = outline ? "btn-outline" : "";
-  const loadingClass = loading ? "loading" : "";
-  const circleClass = circle ? "btn-circle" : "";
-
-  return (
-    <button
-      className={`btn ${sizeClass} ${colorClass} ${activeClass} ${outlineClass} ${loadingClass} ${circleClass} ${
-        className ?? ""
-      }`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+}) => (
+  <button
+    className={clsx(
+      className,
+      "btn",
+      size === "lg" && "btn-lg",
+      size === "sm" && "btn-sm",
+      size === "xs" && "btn-xs",
+      color === "primary" && "btn-primary",
+      color === "secondary" && "btn-secondary",
+      color === "accent" && "btn-accent",
+      color === "ghost" && "btn-ghost",
+      color === "link" && "btn-link",
+      color === "info" && "btn-info",
+      color === "success" && "btn-success",
+      color === "warning" && "btn-warning",
+      color === "error" && "btn-error",
+      active && "btn-active",
+      outline && "btn-outline",
+      loading && "loading",
+      circle && "btn-circle"
+    )}
+    {...props}
+  >
+    {children}
+  </button>
+);
 
 export default Button;
