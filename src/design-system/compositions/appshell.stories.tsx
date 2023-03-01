9374780a7d3cf0cecf00f94dfdaa976f416077a7
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-01 14:07:09 +0100

    add theme toggle

--- src/design-system/compositions/appshell.stories.tsx
+++ src/design-system/compositions/appshell.stories.tsx
@@ -8,11 +8,12 @@ import {
   Github,
   Home,
   Layers,
+  Moon,
   Network,
   Play,
   PlusCircle,
   Settings,
-  Settings2,
+  Sun,
   Users,
 } from "lucide-react";
 import {
@@ -33,6 +34,7 @@ import {
   Sidebar,
   SidebarNavigationItem,
 } from "./AppShell";
+import { useEffect, useState } from "react";
 
 import Button from "../button";
 
@@ -52,84 +54,107 @@ const navigation = [
   { name: "Settings", href: "#", icon: Settings, current: false },
 ];
 
-export const Default = () => (
-  <Root>
-    <Sidebar version="Version: 78c688e">
-      {navigation.map((item) => (
-        <SidebarNavigationItem
-          key={item.name}
-          href={item.href}
-          active={item.current}
-        >
-          <item.icon aria-hidden="true" />
-          {item.name}
-        </SidebarNavigationItem>
-      ))}
-    </Sidebar>
-    <Main>
-      <MainTopBar>
-        <MainTopLeft>
-          <div className="text-sm breadcrumbs">
-            <ul>
-              <li>
-                <a className="gap-2">
-                  <Home className="h-4 w-auto" />
-                  My-namespace
-                </a>
-                &nbsp;
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button size="xs" color="ghost" circle>
-                      <ChevronsUpDown />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-56">
-                    <DropdownMenuLabel>Namespaces</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      My-namespace
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      second-namespace
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      another-namespace
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <PlusCircle className="mr-2 h-4 w-4" />
-                      <span>Create new namespace</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </li>
-              <li>
-                <a className="gap-2">
-                  <Github className="h-4 w-auto" />
-                  Example Mirror
-                </a>
-              </li>
-              <li>
-                <a className="gap-2">
-                  <FolderOpen className="h-4 w-auto" />
-                  Folder
-                </a>
-              </li>
-              <li>
-                <a className="gap-2">
-                  <Play className="h-4 w-auto" />
-                  workflow.yml
-                </a>
-              </li>
-            </ul>
-          </div>
-        </MainTopLeft>
-        <MainTopRight>
-          <Button color="ghost" className="px-1">
-            <Settings2 className="w-5 h-auto" />
-          </Button>
-        </MainTopRight>
-      </MainTopBar>
-    </Main>
-  </Root>
-);
+export const Default = () => {
+  const [theme, setTheme] = useState<"light" | "dark" | undefined>();
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }, [theme]);
+
+  return (
+    <Root>
+      {theme}
+      <Sidebar version="Version: 78c688e">
+        {navigation.map((item) => (
+          <SidebarNavigationItem
+            key={item.name}
+            href={item.href}
+            active={item.current}
+          >
+            <item.icon aria-hidden="true" />
+            {item.name}
+          </SidebarNavigationItem>
+        ))}
+      </Sidebar>
+      <Main>
+        <MainTopBar>
+          <MainTopLeft>
+            <div className="text-sm breadcrumbs">
+              <ul>
+                <li>
+                  <a className="gap-2">
+                    <Home className="h-4 w-auto" />
+                    My-namespace
+                  </a>
+                  &nbsp;
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button size="xs" color="ghost" circle>
+                        <ChevronsUpDown />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuLabel>Namespaces</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuCheckboxItem checked>
+                        My-namespace
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>
+                        second-namespace
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>
+                        another-namespace
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <PlusCircle className="mr-2 h-4 w-4" />
+                        <span>Create new namespace</span>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </li>
+                <li>
+                  <a className="gap-2">
+                    <Github className="h-4 w-auto" />
+                    Example Mirror
+                  </a>
+                </li>
+                <li>
+                  <a className="gap-2">
+                    <FolderOpen className="h-4 w-auto" />
+                    Folder
+                  </a>
+                </li>
+                <li>
+                  <a className="gap-2">
+                    <Play className="h-4 w-auto" />
+                    workflow.yml
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </MainTopLeft>
+          <MainTopRight>
+            <Button
+              color="ghost"
+              className="px-1"
+              onClick={() =>
+                setTheme((old) => (old === "light" ? "dark" : "light"))
+              }
+            >
+              {theme === "dark" ? (
+                <Sun className="w-5 h-auto" />
+              ) : (
+                <Moon className="w-5 h-auto" />
+              )}
+            </Button>
+          </MainTopRight>
+        </MainTopBar>
+      </Main>
+    </Root>
+  );
+};
