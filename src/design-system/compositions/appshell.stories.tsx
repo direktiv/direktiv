1f4e7606dcadb8a0071464917cd4ddd706e7bc48
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-01 11:37:17 +0100

    update shell

--- src/design-system/compositions/appshell.stories.tsx
+++ src/design-system/compositions/appshell.stories.tsx
@@ -24,7 +24,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../dropdown";
-import { Root, Sidebar } from "./AppShell";
+import { Main, MainTopBar, Root, Sidebar } from "./AppShell";
 
 import Button from "../button";
 import clsx from "clsx";
@@ -50,7 +50,7 @@ const enterprise = [
 
 export const Default = () => (
   <Root>
-    <Sidebar>
+    <Sidebar version="Version: 78c688e">
       <nav className="mt-6 px-3 space-y-3">
         <div className="space-y-1">
           {navigation.map((item) => (
@@ -107,74 +107,72 @@ export const Default = () => (
         </div>
       </nav>
     </Sidebar>
-    <div className="flex flex-col lg:pl-64">
-      <main className="flex-1">
-        <div className="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between">
-          <div className="min-w-0 flex-1">
-            <h1 className="text-lg font-medium leading-6 text-gray-900 sm:truncate">
-              <div className="text-sm breadcrumbs">
-                <ul>
-                  <li>
-                    <a className="gap-2">
-                      <Home className="h-4 w-auto" />
-                      My-namespace
-                    </a>
-                    &nbsp;
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button size="xs" color="ghost" circle>
-                          <ChevronsUpDown />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent className="w-56">
-                        <DropdownMenuLabel>Namespaces</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuCheckboxItem checked>
-                          My-namespace
-                        </DropdownMenuCheckboxItem>
-                        <DropdownMenuCheckboxItem>
-                          second-namespace
-                        </DropdownMenuCheckboxItem>
-                        <DropdownMenuCheckboxItem>
-                          another-namespace
-                        </DropdownMenuCheckboxItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                          <PlusCircle className="mr-2 h-4 w-4" />
-                          <span>Create new namespace</span>
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </li>
-                  <li>
-                    <a className="gap-2">
-                      <Github className="h-4 w-auto" />
-                      Example Mirror
-                    </a>
-                  </li>
-                  <li>
-                    <a className="gap-2">
-                      <FolderOpen className="h-4 w-auto" />
-                      Folder
-                    </a>
-                  </li>
-                  <li>
-                    <a className="gap-2">
-                      <Play className="h-4 w-auto" />
-                      workflow.yml
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </h1>
-          </div>
-          <div className="mt-4 flex sm:mt-0 sm:ml-4">
-            <Button color="ghost" className="px-1">
-              <Settings2 className="w-5 h-auto" />
-            </Button>
-          </div>
+    <Main>
+      <MainTopBar>
+        <div className="min-w-0 flex-1">
+          <h1 className="text-lg font-medium leading-6 text-gray-900 sm:truncate">
+            <div className="text-sm breadcrumbs">
+              <ul>
+                <li>
+                  <a className="gap-2">
+                    <Home className="h-4 w-auto" />
+                    My-namespace
+                  </a>
+                  &nbsp;
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button size="xs" color="ghost" circle>
+                        <ChevronsUpDown />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuLabel>Namespaces</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuCheckboxItem checked>
+                        My-namespace
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>
+                        second-namespace
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuCheckboxItem>
+                        another-namespace
+                      </DropdownMenuCheckboxItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <PlusCircle className="mr-2 h-4 w-4" />
+                        <span>Create new namespace</span>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </li>
+                <li>
+                  <a className="gap-2">
+                    <Github className="h-4 w-auto" />
+                    Example Mirror
+                  </a>
+                </li>
+                <li>
+                  <a className="gap-2">
+                    <FolderOpen className="h-4 w-auto" />
+                    Folder
+                  </a>
+                </li>
+                <li>
+                  <a className="gap-2">
+                    <Play className="h-4 w-auto" />
+                    workflow.yml
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </h1>
         </div>
-      </main>
-    </div>
+        <div className="mt-4 flex sm:mt-0 sm:ml-4">
+          <Button color="ghost" className="px-1">
+            <Settings2 className="w-5 h-auto" />
+          </Button>
+        </div>
+      </MainTopBar>
+    </Main>
   </Root>
 );
