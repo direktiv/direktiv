0a6efc86abbd4140e211fb430384f9af122464f2
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-03 16:51:16 +0100

    add drawer

--- src/design-system/compositions/AppShell/index.tsx
+++ src/design-system/compositions/AppShell/index.tsx
@@ -5,7 +5,28 @@ import { Menu } from "lucide-react";
 import clsx from "clsx";
 
 export const Root: FC = ({ children }) => (
-  <div className="min-h-full">{children}</div>
+  <div className="min-h-full">
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+          Open drawer
+        </label>
+        {children}
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 );
 
 export const Sidebar: FC<{ version: string }> = ({ children, version }) => (
