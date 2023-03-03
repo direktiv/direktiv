fd81a8e0c33ba5971357acb746b6867f05d8a527
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-03 16:04:53 +0100

    make sidebar 100 % big

--- src/design-system/compositions/AppShell/index.tsx
+++ src/design-system/compositions/AppShell/index.tsx
@@ -9,7 +9,7 @@ export const Root: FC = ({ children }) => (
 );
 
 export const Sidebar: FC<{ version: string }> = ({ children, version }) => (
-  <div className="lg:fixed lg:flex lg:w-52 lg:flex-col lg:border-r lg:border-gray-gray5 dark:lg:border-grayDark-gray5">
+  <div className="lg:fixed lg:inset-y-0 lg:flex lg:w-52 lg:flex-col lg:border-r lg:border-gray-gray5 dark:lg:border-grayDark-gray5">
     <div className="grid max-lg:space-x-5 max-lg:grid-cols-3 items-center lg:block border-b border-gray-gray5 dark:border-grayDark-gray5 px-6 py-5">
       <Button color="ghost" className="lg:hidden px-1 justify-self-start">
         <Menu />
