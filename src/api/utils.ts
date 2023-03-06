c616a218754d8c14b619b1b3efae10e6411322d0
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-06 15:06:10 +0100

    add apiFactory

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,3 +1,36 @@
+import { z } from "zod";
+
 export const getApiHeaders = (apiKey: string) => ({
   "direktiv-token": apiKey,
 });
+
+export function apiFactory<T>({
+  path,
+  method,
+  schema,
+}: {
+  path: string;
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  schema: z.ZodSchema<T>;
+}): ({ apiKey }: { apiKey: string }) => Promise<T> {
+  return async ({ apiKey }: { apiKey: string }): Promise<T> => {
+    const res = await fetch(path, {
+      method,
+      headers: {
+        "direktiv-token": `${apiKey}`,
+      },
+    });
+
+    if (res.ok) {
+      try {
+        const data = await res.json();
+        return schema.parse(data);
+      } catch (error) {
+        return Promise.reject(
+          `could not format response for ${method} ${path}`
+        );
+      }
+    }
+    return Promise.reject(`error ${res.status} for ${method} ${path}`);
+  };
+}
