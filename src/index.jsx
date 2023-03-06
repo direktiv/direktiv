5c5b0d0184930b92ff0a22e0a95394796edf4da0
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-06 13:30:09 +0100

    conditionally render old or new design

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
-import App from "./AppLegacy";
+import App from "./App";
+import AppLegacy from "./AppLegacy";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {`${import.meta.env.VITE_LEGACY_DESIGN}`.toLowerCase() === "true" ? (
+      <AppLegacy />
+    ) : (
+      <App />
+    )}
   </React.StrictMode>,
   document.getElementById("root")
 );
