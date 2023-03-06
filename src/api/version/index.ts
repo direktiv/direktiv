9b493be5891fac91fcbfd00140e76da3833f035a
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-06 15:09:30 +0100

    use apiFactory

--- src/api/version/index.ts
+++ src/api/version/index.ts
@@ -1,23 +1,8 @@
-import { CommonApiParams } from "../types";
+import { VersionSchema } from "./schema";
+import { apiFactory } from "../utils";
 
-export const getVersion = async ({
-  apiKey,
-}: CommonApiParams): Promise<GatewayListItem[]> => {
-  const res = await fetch(`/api/version`, {
-    method: "GET",
-    headers: {
-      "direktiv-token": `${apiKey}`,
-    },
-  });
-
-  // return gatewayMocks;
-
-  if (res.ok) {
-    try {
-      return (await res.json()) as Promise<GatewayListItem[]>;
-    } catch (error) {
-      return Promise.reject("could not format response");
-    }
-  }
-  return Promise.reject(`${res.status}`);
-};
+export const getVersion = apiFactory({
+  path: `/api/version`,
+  method: "GET",
+  schema: VersionSchema,
+});
