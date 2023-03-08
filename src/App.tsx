c90b7ba94648df21f228169fef95b28b7a75915c
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-08 19:59:37 +0100

    add useVersionHook

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,12 @@ import {
 } from "@tanstack/react-query";
 
 import { getNamespaces } from "./api/namespaces";
-import { getVersion } from "./api/version";
+import { useVersion } from "./api/version";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const { data: version } = useQuery({
-    queryKey: ["version"],
-    queryFn: () =>
-      getVersion({
-        apiKey: "password",
-        params: undefined,
-      }),
-    networkMode: "always",
-    staleTime: Infinity,
-  });
-
+  const { data: version } = useVersion();
   const { data: namespaces } = useQuery({
     queryKey: ["namespaces"],
     queryFn: () =>
--- src/api/version/index.ts
+++ src/api/version/index.ts
@@ -1,8 +1,31 @@
+import type { QueryFunctionContext } from "@tanstack/react-query";
 import { VersionSchema } from "./schema";
 import { apiFactory } from "../utils";
+import { useQuery } from "@tanstack/react-query";
 
-export const getVersion = apiFactory({
+const getVersion = apiFactory({
   path: `/api/version`,
   method: "GET",
   schema: VersionSchema,
 });
+
+const fetchVersions = async ({
+  queryKey: [{ apiKey }],
+}: QueryFunctionContext<ReturnType<(typeof versionKeys)["list"]>>) =>
+  getVersion({
+    apiKey: apiKey,
+    params: undefined,
+  });
+
+const versionKeys = {
+  all: [{ scope: "versions" }] as const,
+  list: (apiKey: string) => [{ ...versionKeys.all[0], apiKey }] as const,
+};
+
+export const useVersion = () =>
+  useQuery({
+    queryKey: versionKeys.list("password"),
+    queryFn: fetchVersions,
+    networkMode: "always",
+    staleTime: Infinity,
+  });
