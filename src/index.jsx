7d16f585f18480d4960575bde1c48802e7ace815
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-09 18:24:30 +0100

    lazy load app app components to avoid gobal css polution

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,22 @@
-import App from "./App";
-import AppLegacy from "./AppLegacy";
-import React from "react";
+import React, { lazy } from "react";
+
 import { createRoot } from "react-dom/client";
+
 const app = document.getElementById("root");
+
 const root = createRoot(app);
+
+const App = lazy(() => {
+  // lazy load is important here, if would just conditionally render one
+  // or the other, the css imports of both modules would be loaded in eather case
+  if (`${import.meta.env.VITE_LEGACY_DESIGN}`.toLowerCase() === "true") {
+    return import("./AppLegacy");
+  }
+  return import("./App");
+});
 
 root.render(
   <React.StrictMode>
-    {`${import.meta.env.VITE_LEGACY_DESIGN}`.toLowerCase() === "true" ? (
-      <AppLegacy />
-    ) : (
-      <App />
-    )}
+    <App />
   </React.StrictMode>
 );
