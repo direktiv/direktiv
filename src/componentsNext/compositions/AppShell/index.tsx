c12aecbb0f268925982acadc8a88f4f5224b0d86
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-09 11:16:39 +0100

    explcitly type children props

--- src/componentsNext/compositions/AppShell/index.tsx
+++ src/componentsNext/compositions/AppShell/index.tsx
@@ -1,13 +1,14 @@
-import { FC } from "react";
+import { FC, PropsWithChildren } from "react";
+
 import Logo from "./Logo";
 import LogoNoText from "./LogoNoText";
 import clsx from "clsx";
 
-export const Root: FC = ({ children }) => (
+export const Root: FC<PropsWithChildren> = ({ children }) => (
   <div className="min-h-full">{children}</div>
 );
 
-export const DrawerRoot: FC = ({ children }) => (
+export const DrawerRoot: FC<PropsWithChildren> = ({ children }) => (
   <div className="drawer">
     <input id="my-drawer" type="checkbox" className="drawer-toggle" />
     {children}
@@ -29,7 +30,7 @@ export const DrawerContent: {
   return <div className="drawer-content">{children({ drawerLabelProps })}</div>;
 };
 
-export const DrawerMenu: FC = ({ children }) => (
+export const DrawerMenu: FC<PropsWithChildren> = ({ children }) => (
   <div className="drawer-side">
     <label htmlFor="my-drawer" className="drawer-overlay"></label>
     <nav className="menu p-4 w-52 bg-base-100 text-base-content">
@@ -41,7 +42,10 @@ export const DrawerMenu: FC = ({ children }) => (
   </div>
 );
 
-export const Sidebar: FC<{ version: string }> = ({ children, version }) => (
+export const Sidebar: FC<PropsWithChildren<{ version: string }>> = ({
+  children,
+  version,
+}) => (
   <div className="lg:fixed lg:inset-y-0 lg:flex lg:w-52 lg:flex-col lg:border-r lg:border-gray-gray5 dark:lg:border-grayDark-gray5">
     {children}
     <div className="hidden lg:block flex-shrink-0 p-5 text-left text-sm text-gray-gray8 dark:text-grayDark-gray8">
@@ -50,12 +54,12 @@ export const Sidebar: FC<{ version: string }> = ({ children, version }) => (
   </div>
 );
 
-export const SidebarMenu: FC = ({ children }) => (
+export const SidebarMenu: FC<PropsWithChildren> = ({ children }) => (
   <div className="hidden lg:block flex-1 overflow-y-auto">
     <nav className="mt-5 px-3 space-y-1">{children}</nav>
   </div>
 );
-export const SidebarLogo: FC = ({ children }) => (
+export const SidebarLogo: FC<PropsWithChildren> = ({ children }) => (
   <div className="grid max-lg:space-x-5 max-lg:grid-cols-3 items-center lg:block border-b border-gray-gray5 dark:border-grayDark-gray5 px-6 py-5">
     {children}
     <LogoNoText className="sm:hidden h-8 w-auto justify-self-center" />
@@ -63,11 +67,9 @@ export const SidebarLogo: FC = ({ children }) => (
   </div>
 );
 
-export const SidebarNavigationItem: FC<{ href: string; active?: boolean }> = ({
-  children,
-  href,
-  active,
-}) => (
+export const SidebarNavigationItem: FC<
+  PropsWithChildren<{ href: string; active?: boolean }>
+> = ({ children, href, active }) => (
   <a
     href={href}
     className={clsx(
@@ -81,19 +83,19 @@ export const SidebarNavigationItem: FC<{ href: string; active?: boolean }> = ({
   </a>
 );
 
-export const Main: FC = ({ children }) => (
+export const Main: FC<PropsWithChildren> = ({ children }) => (
   <div className="flex flex-col lg:pl-52">
     <main className="flex-1">{children}</main>
   </div>
 );
 
-export const MainTopBarRoot: FC = ({ children }) => (
+export const MainTopBarRoot: FC<PropsWithChildren> = ({ children }) => (
   <div className="border-b border-gray-gray5 dark:border-grayDark-gray5 p-4 flex items-center justify-between">
     {children}
   </div>
 );
 
-export const MainTopBarLeft: FC = ({ children }) => (
+export const MainTopBarLeft: FC<PropsWithChildren> = ({ children }) => (
   <div className="min-w-0 flex-1">
     <h1 className="text-lg font-medium leading-6 text-gray-gray12 dark:text-grayDark-gray12 sm:truncate">
       {children}
@@ -101,8 +103,10 @@ export const MainTopBarLeft: FC = ({ children }) => (
   </div>
 );
 
-export const MainTopBarRight: FC = ({ children }) => (
+export const MainTopBarRight: FC<PropsWithChildren> = ({ children }) => (
   <div className="mt-4 space-x-3 flex sm:mt-0 sm:ml-4">{children}</div>
 );
 
-export const MainContent: FC = ({ children }) => <div>{children}</div>;
+export const MainContent: FC<PropsWithChildren> = ({ children }) => (
+  <div>{children}</div>
+);
