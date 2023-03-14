fc33a6bd3fd7dfabbcb04c24c1a1cbe8a9b63804
Alexander Ertli <alexander.ertli@direktiv.io>
2023-03-14 08:00:46 +0000

    Add Logtag-filtering
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/flow/grpc-logs.go
+++ pkg/flow/grpc-logs.go
@@ -2,15 +2,18 @@ package flow
 
 import (
 	"context"
+	"strings"
 
 	"github.com/direktiv/direktiv/pkg/flow/bytedata"
 	"github.com/direktiv/direktiv/pkg/flow/database"
 	"github.com/direktiv/direktiv/pkg/flow/ent"
+	entinst "github.com/direktiv/direktiv/pkg/flow/ent/instance"
 	entlog "github.com/direktiv/direktiv/pkg/flow/ent/logmsg"
 	entns "github.com/direktiv/direktiv/pkg/flow/ent/namespace"
 	entwf "github.com/direktiv/direktiv/pkg/flow/ent/workflow"
 	"github.com/direktiv/direktiv/pkg/flow/grpc"
 	"github.com/direktiv/direktiv/pkg/flow/internallogger"
+	"github.com/google/uuid"
 )
 
 var logsOrderings = []*orderingInfo{
@@ -21,7 +24,40 @@ var logsOrderings = []*orderingInfo{
 	},
 }
 
-var logsFilters = map[*filteringInfo]func(query *ent.LogMsgQuery, v string) (*ent.LogMsgQuery, error){}
+var logsFilters = map[*filteringInfo]func(query *ent.LogMsgQuery, v string) (*ent.LogMsgQuery, error){
+	{
+		field: "ID",
+		ftype: "MATCH",
+	}: func(query *ent.LogMsgQuery, v string) (*ent.LogMsgQuery, error) {
+		id, err := uuid.Parse(v)
+		if err != nil {
+			return nil, err
+		}
+		return query.Where(entlog.HasInstanceWith(entinst.IDEQ(id))), nil
+	},
+	{
+		field: "LEVEL",
+		ftype: "MATCH",
+	}: func(query *ent.LogMsgQuery, v string) (*ent.LogMsgQuery, error) {
+		return query.Where(entlog.LevelEQ(v)), nil
+	},
+	{
+		field: "LEVEL",
+		ftype: "STARTING",
+	}: func(query *ent.LogMsgQuery, v string) (*ent.LogMsgQuery, error) {
+		levels := []string{"debug", "info", "error", "panic"}
+		switch v {
+		case "debug":
+		case "info":
+			levels = levels[1:]
+		case "error":
+			levels = levels[2:]
+		case "panic":
+			levels = levels[3:]
+		}
+		return query.Where(entlog.LevelIn(levels...)), nil
+	},
+}
 
 func (flow *flow) ServerLogs(ctx context.Context, req *grpc.ServerLogsRequest) (*grpc.ServerLogsResponse, error) {
 	flow.sugar.Debugf("Handling gRPC request: %s", this())
@@ -324,6 +360,12 @@ func (flow *flow) InstanceLogs(ctx context.Context, req *grpc.InstanceLogsReques
 		return nil, err
 	}
 
+	filters := req.Pagination.Filter
+	for _, v := range filters {
+		results = queryJSON(v, results)
+		pi.Total = int32(len(results))
+	}
+
 	resp := new(grpc.InstanceLogsResponse)
 	resp.Namespace = cached.Namespace.Name
 	resp.Instance = cached.Instance.ID.String()
@@ -370,6 +412,12 @@ resend:
 		return err
 	}
 
+	filters := req.Pagination.Filter
+	for _, v := range filters {
+		results = queryJSON(v, results)
+		pi.Total = int32(len(results))
+	}
+
 	resp := new(grpc.InstanceLogsResponse)
 	resp.Namespace = cached.Namespace.Name
 	resp.Instance = cached.Instance.ID.String()
@@ -399,4 +447,35 @@ resend:
 	}
 
 	goto resend
+}
+
+func queryJSON(filter *grpc.PageFilter, results []*ent.LogMsg) []*ent.LogMsg {
+	res := results
+	if filter.Field == "QUERY" && filter.Type == "MATCH" {
+		res = queryMatchState(filter.Val, results)
+	}
+	return res
+}
+
+func queryMatchState(q string, in []*ent.LogMsg) []*ent.LogMsg {
+	values := strings.Split(q, "::")
+	state := ""
+	workflow := ""
+	iterator := ""
+	if len(values) >= 2 {
+		workflow = values[0]
+		state = values[1]
+	}
+	if len(values) > 2 {
+		iterator = values[2]
+	}
+	res := make([]*ent.LogMsg, 0)
+	for _, v := range in {
+		if v.Tags["state-id"] == state &&
+			v.Tags["workflow"] == workflow &&
+			v.Tags["loop-index"] == iterator {
+			res = append(res, v)
+		}
+	}
+	return res
 }
