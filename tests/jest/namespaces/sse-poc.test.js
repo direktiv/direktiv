c20730fb63de14f82c9c2c07911d07994d0d84ad
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-14 14:59:31 +0100

    add a server sent event prove of concept test

--- tests/jest/namespaces/sse-poc.test.js
+++ tests/jest/namespaces/sse-poc.test.js
@@ -1,55 +1,56 @@
-import EventSource from "eventsource";
 import common from "../common";
 
-const createNamespaceResponse = {
-  namespace: common.structs.namespaceObject,
-};
-
 const API_HOST = "http://ec2-3-231-218-167.compute-1.amazonaws.com";
-const AUTH_TOKEN = "password"; // can also be null if no password should be used
-const IS_ENTERPRISE = false; // enterprise needs different auth header
+const IS_ENTERPRISE = false;
+const AUTH_TOKEN = "password";
 const NAMESPACE = "my-namespace";
 
-const authHeader = (authToken, isEnterprice = false) => {
-  if (!authToken) {
-    return {};
-  }
-  return isEnterprice
-    ? { Authorization: `Bearer ${authToken}` }
-    : { "direktiv-token": authToken };
-};
-
 describe("Test Server Sent Events", () => {
-  // write a handler
   it(`can handle Server Sent Events`, async () => {
-    const sseListener = new EventSource(
-      `${API_HOST}/api/namespaces/${NAMESPACE}/logs`,
-      {
-        headers: {
-          ...authHeader(AUTH_TOKEN, IS_ENTERPRISE),
-        },
-      }
+    // configure the sse request and store the dispatch function and the message and error mock functions
+    const { dispatch, onErrorMock, onMessageMock } = common.utils.direktivSSE({
+      path: `${API_HOST}/api/namespaces/${NAMESPACE}/logs`,
+      auth: {
+        token: AUTH_TOKEN,
+        isEnterprice: IS_ENTERPRISE,
+      },
+    });
+
+    // call the dispatch function and wait for the first message or error
+    await dispatch();
+
+    // at this point the connection is already closed
+    // we can now check the onErrorMock, onMessageMock
+    // and test if and how they have been called
+    expect(onErrorMock).not.toHaveBeenCalled();
+    expect(onMessageMock).toHaveBeenCalledTimes(1);
+    expect(onMessageMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        namespace: NAMESPACE,
+      })
     );
 
-    // we need to wait for a promise here, otherwise the test will finish before the event is received
-    await new Promise((resolve, reject) => {
-      sseListener.onmessage = (e) => {
-        const json = JSON.parse(e.data);
-        expect(json).toMatchObject({
-          pageInfo: {
-            total: json.results.length,
-          },
-          namespace: NAMESPACE,
-        });
+    // we can also retreive the exact data with which the mock function was called
+    // we get the fist call and the fist argument with [0][0]
+    const logResultsArr = onMessageMock.mock.calls[0][0]?.results;
+    expect(onMessageMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        namespace: NAMESPACE,
+        pageInfo: expect.objectContaining({
+          total: logResultsArr.length, // now we can check if the pageInfo.total is correct
+        }),
+      })
+    );
+  });
 
-        sseListener.close();
-        resolve();
-      };
-
-      // rejecting a promise in jest will fail the test
-      sseListener.onerror = (e) => {
-        reject(e);
-      };
+  it(`returns an unauthorized error when called with no credentials`, async () => {
+    const { dispatch, onErrorMock, onMessageMock } = common.utils.direktivSSE({
+      path: `${API_HOST}/api/namespaces/${NAMESPACE}/logs`,
     });
+    await dispatch();
+    expect(onErrorMock).toHaveBeenCalledWith(
+      expect.objectContaining(common.structs.unauthorizedResponse)
+    );
+    expect(onMessageMock).not.toHaveBeenCalled();
   });
 });
