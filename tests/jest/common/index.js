d37e3b7764dafaec6e6735bda96021b5f574ef9e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-14 14:45:19 +0100

    add utils with direktivSSE

--- tests/jest/common/index.js
+++ tests/jest/common/index.js
@@ -1,10 +1,12 @@
+import config from './config'
+import filesystem from './filesystem.js'
 import regex from './regex.js'
 import structs from './structs.js'
-import filesystem from './filesystem.js'
-import config from './config'
+import utils from './utils'
 
 export default {
     config,
+    utils,
     regex,
     structs,
     filesystem,
