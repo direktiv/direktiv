6de0240cc6f1324ded3060827a4bdbbe4bec8053
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-15 16:13:03 +0100

    make navigation work

--- src/componentsNext/Navigation/index.tsx
+++ src/componentsNext/Navigation/index.tsx
@@ -1,20 +1,18 @@
 import { FC } from "react";
 import { NavLink } from "react-router-dom";
-import { NavigationLink } from "../NavigationLink";
+import { createClassNames } from "../NavigationLink";
 import { pages } from "../../util/router/pages";
 
 const Navigation: FC = () => (
   <>
     {Object.values(pages).map((item) => (
-      <>
-        <NavLink key={item.name} to={item.route.path ?? "#"}>
-          {item.name} <br />
-        </NavLink>
-        <NavigationLink key={item.name} href={item.href} active={item.current}>
-          <item.icon aria-hidden="true" />
-          {item.name}
-        </NavigationLink>
-      </>
+      <NavLink
+        key={item.name}
+        to={item.route.path ?? "#"}
+        className={({ isActive }) => createClassNames(isActive)}
+      >
+        <item.icon aria-hidden="true" /> {item.name}
+      </NavLink>
     ))}
   </>
 );
