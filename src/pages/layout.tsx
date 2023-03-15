b251317b35f694318891e37be339c714dbccad8d
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-15 11:00:52 +0100

    move main content

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -36,65 +36,6 @@ const Layout = () => {
 
   return (
     <Root>
-      <div className="flex flex-col space-y-5 p-10">
-        <div>
-          <h1>
-            theme <span className="font-bold">{theme}</span>
-          </h1>
-          <div className="flex space-x-5">
-            <button
-              className="btn-primary btn"
-              onClick={() => setTheme("dark")}
-            >
-              darkmode
-            </button>
-            <button
-              className="btn-primary btn"
-              onClick={() => setTheme("light")}
-            >
-              lightmode
-            </button>
-            <button className="btn-error btn" onClick={() => setTheme(null)}>
-              reset theme
-            </button>
-          </div>
-        </div>
-        <div>
-          <h1>
-            api key is <span className="font-bold">{apiKey}</span>
-          </h1>
-          <div className="flex space-x-5">
-            <button
-              className="btn-primary btn"
-              onClick={() => setApiKey("password")}
-            >
-              set Api key to password
-            </button>
-            <button className="btn-error btn" onClick={() => setApiKey(null)}>
-              reset api key
-            </button>
-          </div>
-        </div>
-        <div>
-          <h1 className="font-bold">Version</h1>
-          {isVersionLoading ? "Loading version...." : version?.api}
-        </div>
-        <div>
-          <h1 className="font-bold">namespaces</h1>
-          {isLoadingNamespaces
-            ? "Loading namespaces"
-            : namespaces?.results.map((namespace) => (
-                <div key={namespace.name}>{namespace.name}</div>
-              ))}
-        </div>
-        <div>
-          <Link to="/">Start</Link> <Link to="/about">About</Link>
-        </div>
-        <div>
-          <Outlet />
-        </div>
-      </div>
-
       <Sidebar version={version?.api ?? ""}>
         <SidebarTop /> {/* add drawer here */}
         <SidebarMain />
@@ -104,7 +45,72 @@ const Layout = () => {
           <MainTopLeft>1</MainTopLeft>
           <MainTopRight>2</MainTopRight>
         </MainTop>
-        <MainContent>121</MainContent>
+        <MainContent>
+          <div className="flex flex-col space-y-5 p-10">
+            <div>
+              <h1>
+                theme <span className="font-bold">{theme}</span>
+              </h1>
+              <div className="flex space-x-5">
+                <button
+                  className="btn-primary btn"
+                  onClick={() => setTheme("dark")}
+                >
+                  darkmode
+                </button>
+                <button
+                  className="btn-primary btn"
+                  onClick={() => setTheme("light")}
+                >
+                  lightmode
+                </button>
+                <button
+                  className="btn-error btn"
+                  onClick={() => setTheme(null)}
+                >
+                  reset theme
+                </button>
+              </div>
+            </div>
+            <div>
+              <h1>
+                api key is <span className="font-bold">{apiKey}</span>
+              </h1>
+              <div className="flex space-x-5">
+                <button
+                  className="btn-primary btn"
+                  onClick={() => setApiKey("password")}
+                >
+                  set Api key to password
+                </button>
+                <button
+                  className="btn-error btn"
+                  onClick={() => setApiKey(null)}
+                >
+                  reset api key
+                </button>
+              </div>
+            </div>
+            <div>
+              <h1 className="font-bold">Version</h1>
+              {isVersionLoading ? "Loading version...." : version?.api}
+            </div>
+            <div>
+              <h1 className="font-bold">namespaces</h1>
+              {isLoadingNamespaces
+                ? "Loading namespaces"
+                : namespaces?.results.map((namespace) => (
+                    <div key={namespace.name}>{namespace.name}</div>
+                  ))}
+            </div>
+            <div>
+              <Link to="/">Start</Link> <Link to="/about">About</Link>
+            </div>
+            <div>
+              <Outlet />
+            </div>
+          </div>
+        </MainContent>
       </Main>
     </Root>
   );
