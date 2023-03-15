138b1938236e337a3d3e05a5149453a6f6431358
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-15 15:48:54 +0100

    export createClassNames

--- src/componentsNext/NavigationLink/index.tsx
+++ src/componentsNext/NavigationLink/index.tsx
@@ -2,18 +2,18 @@ import { FC, PropsWithChildren } from "react";
 
 import clsx from "clsx";
 
+export const createClassNames = (active: boolean) =>
+  clsx(
+    active
+      ? "bg-primary50 text-gray-gray12 dark:bg-primary700 dark:text-grayDark-gray12"
+      : "text-gray-gray11 hover:bg-gray-gray2 dark:text-grayDark-gray11 dark:hover:bg-grayDark-gray2",
+    "[&>svg]:group group flex items-center rounded-md p-2 text-sm font-medium [&>svg]:mr-3"
+  );
+
 export const NavigationLink: FC<
   PropsWithChildren<{ href: string; active?: boolean }>
 > = ({ children, href, active }) => (
-  <a
-    href={href}
-    className={clsx(
-      active
-        ? "bg-primary50 text-gray-gray12 dark:bg-primary700 dark:text-grayDark-gray12"
-        : "text-gray-gray11 hover:bg-gray-gray2 dark:text-grayDark-gray11 dark:hover:bg-grayDark-gray2",
-      "[&>svg]:group group flex items-center rounded-md p-2 text-sm font-medium [&>svg]:mr-3"
-    )}
-  >
+  <a href={href} className={createClassNames(active ?? false)}>
     {children}
   </a>
 );
