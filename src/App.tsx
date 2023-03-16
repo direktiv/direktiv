5958d1acfc08c7c5cc892f4e712ef2e581d8dca4
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-16 12:50:38 +0100

    add toaster to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useTheme, useThemeActions } from "./util/store/theme";
 
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "./componentsNext/Toast";
 import { router } from "./util/router";
 import { useEffect } from "react";
 
@@ -30,6 +31,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   );
 };
