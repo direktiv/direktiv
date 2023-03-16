b4f578e6cac46eae09e9f6e7cf94c8a284e299f5
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-16 12:06:59 +0100

    make namespace selectable

--- src/pages/Explorer/index.tsx
+++ src/pages/Explorer/index.tsx
@@ -1,18 +1,34 @@
+import { useNamespace, useNamespaceActions } from "../../util/store/namespace";
+
+import Button from "../../componentsNext/Button";
 import { FC } from "react";
 import { useNamespaces } from "../../api/namespaces";
 
 const ExplorerPage: FC = () => {
   const { data: namespaces } = useNamespaces();
+  const selectedNamespace = useNamespace();
+  const { setNamespace } = useNamespaceActions();
 
   return (
     <div>
       <h1>Explorer</h1>
-      <div className="font-bold">Namespaces</div>
-      <div>
+      <div className="py-5 font-bold">Namespaces</div>
+      <div className="flex flex-col space-y-5 ">
         {namespaces?.results.map((namespace) => (
-          <div key={namespace.name}>{namespace.name}</div>
+          <Button
+            key={namespace.name}
+            onClick={() => {
+              setNamespace(namespace.name);
+            }}
+            color="primary"
+            size="sm"
+          >
+            {selectedNamespace === namespace.name && "✅"} {namespace.name}
+          </Button>
         ))}
       </div>
+      <div className="py-5 font-bold">Files</div>
+      <div className="flex flex-col space-y-5 "></div>
     </div>
   );
 };
