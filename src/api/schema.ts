b2bbbed932d29cb71dead1b85cafcedc262fc1a9
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-16 11:46:10 +0100

    define pageinfo schema a little bit more

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -1,8 +1,19 @@
 import { z } from "zod";
 
 export const PageinfoSchema = z.object({
-  order: z.array(z.string()),
-  filter: z.array(z.string()),
+  order: z.array(
+    z.object({
+      field: z.string(), // f.e. "NAME"
+      direction: z.string(),
+    })
+  ),
+  filter: z.array(
+    z.object({
+      field: z.string(), // f.e. "NAME"
+      type: z.string(), // f.e. CONTAINS
+      val: z.string(), // f.e. "something"
+    })
+  ),
   limit: z.number(),
   offset: z.number(),
   total: z.number(),
