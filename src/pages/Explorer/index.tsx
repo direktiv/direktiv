67e700b179f2bb9b83f10cb01801330ad8a67a56
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-16 12:39:45 +0100

    display tree content

--- src/pages/Explorer/index.tsx
+++ src/pages/Explorer/index.tsx
@@ -3,11 +3,14 @@ import { useNamespace, useNamespaceActions } from "../../util/store/namespace";
 import Button from "../../componentsNext/Button";
 import { FC } from "react";
 import { useNamespaces } from "../../api/namespaces";
+import { useTree } from "../../api/tree";
 
 const ExplorerPage: FC = () => {
   const { data: namespaces } = useNamespaces();
   const selectedNamespace = useNamespace();
   const { setNamespace } = useNamespaceActions();
+
+  const { data } = useTree();
 
   return (
     <div>
@@ -28,7 +31,13 @@ const ExplorerPage: FC = () => {
         ))}
       </div>
       <div className="py-5 font-bold">Files</div>
-      <div className="flex flex-col space-y-5 "></div>
+      <div className="flex flex-col space-y-5 ">
+        {data?.children.results.map((file) => (
+          <div key={file.name} color="primary">
+            {file.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
