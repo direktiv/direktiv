ebd17b3a091f8c0ced03bbd33cd13911a0b195f1
Alan Murtagh <alan.murtagh@direktiv.io>
2023-03-20 16:54:12 +1000

    Adds jest tests.
    Signed-off-by: Alan Murtagh <alan.murtagh@direktiv.io>

---
+++ tests/jest/common/structs.js
@@ -0,0 +1,42 @@
+import regex from "./regex.js"
+
+const errorResponse = {
+    code: expect.anything(),
+    message: expect.anything(),
+}
+
+const unauthorizedResponse = {
+    message: "Unauthorized",
+    status: 401,
+    type: "error",
+};
+
+const pageInfoObject = {
+    order: expect.anything(),
+    filter: expect.anything(),
+    limit: expect.anything(),
+    offset: expect.anything(),
+    total: expect.anything(),
+}
+
+const namespaceObject = {
+    name: expect.stringMatching(regex.namespaceRegex),
+    oid: "",
+    createdAt: expect.stringMatching(regex.timestampRegex),
+    updatedAt: expect.stringMatching(regex.timestampRegex),
+}
+
+const nodeObject = {
+    name: expect.stringMatching(regex.nodeNameRegex),
+    path: expect.stringMatching(regex.pathRegex),
+    parent: expect.stringMatching(regex.pathRegex),
+    type: expect.stringMatching(regex.nodeTypeRegex),
+    expandedType: expect.stringMatching(regex.nodeExtendedTypeRegex),
+    readOnly: expect.anything(),
+    attributes: expect.anything(),
+    oid: "",
+    createdAt: expect.stringMatching(regex.timestampRegex),
+    updatedAt: expect.stringMatching(regex.timestampRegex),
+}
+
+export default { errorResponse, pageInfoObject, namespaceObject, nodeObject, unauthorizedResponse }
