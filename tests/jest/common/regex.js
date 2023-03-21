f7efd64436fb4913e4dda1ac72ea13078789fd7c
Alan Murtagh <alan.murtagh@vorteil.io>
2023-03-21 13:53:06 +1000

    Adds the first round of workflow-node jest tests. (#786)
    Signed-off-by: Alan Murtagh <alan.murtagh@direktiv.io>

--- tests/jest/common/regex.js
+++ tests/jest/common/regex.js
@@ -5,6 +5,9 @@ const pathRegex = String.raw`(^[\/]((([a-z][a-z0-9_\-\.]*[a-z0-9])|([a-z]))[\/]?
 const nodeTypeRegex = String.raw`^directory$`
 const nodeExtendedTypeRegex = String.raw`^directory$`
 
+const hashRegex = String.raw`^[0-9a-f]{64}$`
+const base64Regex = String.raw`^[-A-Za-z0-9+/=]*$`
+const uuidRegex = String.raw`^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$`
 
 export default {
     timestampRegex,
@@ -13,4 +16,8 @@ export default {
     pathRegex,
     nodeTypeRegex,
     nodeExtendedTypeRegex,
+
+    hashRegex,
+    base64Regex,
+    uuidRegex,
 }
