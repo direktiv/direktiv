cb99f856bd85f47c77ca5ea31d7aaa70dde5061d
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-22 15:49:39 +0100

    finish new button colors
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/ButtonNew/index.tsx
+++ src/componentsNext/ButtonNew/index.tsx
@@ -31,19 +31,35 @@ const Button = React.forwardRef<
   ) => (
     <button
       className={clsx(
-        "inline-flex items-center justify-center text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 active:scale-95 disabled:pointer-events-none disabled:opacity-50 data-[state=open]:bg-slate-100 dark:hover:bg-slate-800 dark:hover:text-slate-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800",
-        !variant &&
-          "bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900",
-        variant === "destructive" &&
-          "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
-        variant === "outline" &&
-          "border border-slate-200 bg-transparent hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
-        variant === "primary" &&
-          "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
-        variant === "ghost" &&
-          "bg-transparent hover:bg-slate-100 data-[state=open]:bg-transparent dark:text-slate-100 dark:hover:bg-slate-800 dark:hover:text-slate-100 dark:data-[state=open]:bg-transparent",
-        variant === "link" &&
-          "bg-transparent text-slate-900 underline-offset-4 hover:bg-transparent hover:underline dark:bg-transparent dark:text-slate-100 dark:hover:bg-transparent",
+        "inline-flex items-center justify-center text-sm font-medium transition-colors",
+        "focus:outline-none focus:ring-2 focus:ring-gray-4 focus:ring-offset-2",
+        "active:scale-95",
+        "disabled:pointer-events-none disabled:opacity-50",
+        "dark:focus:ring-gray-dark-4",
+        !variant && [
+          "bg-gray-12 text-gray-1",
+          "dark:bg-gray-dark-12 dark:text-gray-dark-1",
+        ],
+        variant === "destructive" && [
+          "bg-danger-10 text-gray-1 hover:bg-danger-11",
+          "dark:bg-danger-dark-10 dark:text-gray-dark-1 dark:hover:bg-danger-dark-11",
+        ],
+        variant === "outline" && [
+          "border border-gray-4 bg-transparent hover:bg-gray-2",
+          "dark:border-gray-dark-4 dark:hover:bg-gray-dark-2",
+        ],
+        variant === "primary" && [
+          "bg-primary-500  text-gray-1 hover:bg-primary-600",
+          "dark:text-gray-dark-1",
+        ],
+        variant === "ghost" && [
+          "bg-transparent hover:bg-gray-3 data-[state=open]:bg-transparent",
+          "hover:bg-gray-dark-3",
+        ],
+        variant === "link" && [
+          "bg-transparent text-gray-12 underline-offset-4 hover:bg-transparent hover:underline",
+          "dark:text-gray-dark-12",
+        ],
         size === "sm" && "h-7 gap-1 px-3 [&>svg]:h-4",
         !size && "h-9 gap-2 py-2 px-4 [&>svg]:h-5",
         size === "lg" && "h-11 gap-3 px-6 [&>svg]:h-6",
