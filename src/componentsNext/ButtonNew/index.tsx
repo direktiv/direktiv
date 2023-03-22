c82cf587a457ce961326b5fd70bc78ae636dad82
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-22 13:54:16 +0100

    implement variant and size
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/ButtonNew/index.tsx
+++ src/componentsNext/ButtonNew/index.tsx
@@ -42,10 +42,26 @@ type ButtonProps = {
 const Button = React.forwardRef<
   HTMLButtonElement,
   React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps
->(({ className, ...props }, ref) => (
+>(({ className, variant, size, ...props }, ref) => (
   <button
     className={clsx(
       "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 active:scale-95 disabled:pointer-events-none     disabled:opacity-50 data-[state=open]:bg-slate-100 dark:hover:bg-slate-800 dark:hover:text-slate-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800",
+      !variant &&
+        "bg-slate-900 text-white hover:bg-slate-700 dark:bg-slate-50 dark:text-slate-900",
+      variant === "destructive" &&
+        "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
+      variant === "outline" &&
+        "border border-slate-200 bg-transparent hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
+      variant === "primary" &&
+        "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
+      variant === "ghost" &&
+        "bg-transparent hover:bg-slate-100 data-[state=open]:bg-transparent dark:text-slate-100 dark:hover:bg-slate-800 dark:hover:text-slate-100 dark:data-[state=open]:bg-transparent",
+      variant === "link" &&
+        "bg-transparent text-slate-900 underline-offset-4 hover:bg-transparent hover:underline dark:bg-transparent dark:text-slate-100 dark:hover:bg-transparent",
+      size === "xs" && "h-8 rounded-md px-2",
+      size === "sm" && "h-9 rounded-md px-2",
+      !size && "h-10 py-2 px-4",
+      size === "lg" && "h-11 rounded-md px-8",
       className
     )}
     ref={ref}
