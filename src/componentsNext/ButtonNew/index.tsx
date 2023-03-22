630aa310a1b5511b303e96891307ce23dd5dc64e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-22 16:31:00 +0100

    change small button height
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/ButtonNew/index.tsx
+++ src/componentsNext/ButtonNew/index.tsx
@@ -60,7 +60,7 @@ const Button = React.forwardRef<
           "bg-transparent text-gray-12 underline-offset-4 hover:bg-transparent hover:underline",
           "dark:text-gray-dark-12",
         ],
-        size === "sm" && "h-7 gap-1 px-3 [&>svg]:h-4",
+        size === "sm" && "h-6 gap-1 px-3 [&>svg]:h-4",
         !size && "h-9 gap-2 py-2 px-4 [&>svg]:h-5",
         size === "lg" && "h-11 gap-3 px-6 [&>svg]:h-6",
         circle && "rounded-full",
