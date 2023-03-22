104e860f5eff965f672ec70975d357c5f04636f5
Alan Murtagh <alan.murtagh@direktiv.io>
2023-03-22 16:01:39 +1000

    Adds additional filesystem constants to the end-to-end test code.
    Signed-off-by: Alan Murtagh <alan.murtagh@direktiv.io>

--- tests/jest/common/filesystem.js
+++ tests/jest/common/filesystem.js
@@ -5,10 +5,12 @@ const nodeTypeWorkflow = 'workflow'
 
 const extendedNodeTypeDirectory = 'directory'
 const extendedNodeTypeWorkflow = 'workflow'
+const extendedNodeTypeMirror = 'git'
 
 export default { 
     nodeTypeDirectory, 
     nodeTypeWorkflow, 
     extendedNodeTypeDirectory, 
     extendedNodeTypeWorkflow,
+    extendedNodeTypeMirror,
 }
