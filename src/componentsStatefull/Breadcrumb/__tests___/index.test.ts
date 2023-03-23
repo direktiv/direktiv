b0f4d3f9de31fa46f01b6c0bfc70a708e3128a09
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-23 10:18:41 +0100

    DIR-243 add some tests
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/componentsStatefull/Breadcrumb/__tests___/index.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, test } from "vitest";
+
+import { analyzePath } from "../index";
+
+describe("analyzePath", () => {
+  test("undefined", () => {
+    expect(analyzePath()).toEqual({
+      path: null,
+      segments: [],
+    });
+  });
+
+  test("empyt string", () => {
+    expect(analyzePath("")).toEqual({
+      path: null,
+      segments: [],
+    });
+  });
+
+  test("/", () => {
+    expect(analyzePath("/")).toEqual({
+      path: null,
+      segments: [],
+    });
+  });
+
+  test("some/path", () => {
+    expect(analyzePath("some/path")).toEqual({
+      path: "some/path",
+      segments: [
+        {
+          relative: "some",
+          absolute: "some",
+        },
+        {
+          relative: "path",
+          absolute: "some/path",
+        },
+      ],
+    });
+  });
+
+  test("some/nested/path", () => {
+    expect(analyzePath("some/nested/path")).toEqual({
+      path: "some/nested/path",
+      segments: [
+        {
+          relative: "some",
+          absolute: "some",
+        },
+        {
+          relative: "nested",
+          absolute: "some/nested",
+        },
+        {
+          relative: "path",
+          absolute: "some/nested/path",
+        },
+      ],
+    });
+  });
+});
