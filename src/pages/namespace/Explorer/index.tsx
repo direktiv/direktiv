e93806a921762901a9d1e252647eb633cee1fbde
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-23 17:04:43 +0100

    display path and button
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/index.tsx
+++ src/pages/namespace/Explorer/index.tsx
@@ -1,7 +1,9 @@
-import { FolderOpen, FolderUp, Github, Play } from "lucide-react";
+import { FolderOpen, FolderTree, FolderUp, Github, Play } from "lucide-react";
 
+import Button from "../../../componentsNext/Button";
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { RxChevronDown } from "react-icons/rx";
 import { analyzePath } from "../../../util/router/utils";
 import moment from "moment";
 import { pages } from "../../../util/router/pages";
@@ -16,8 +18,18 @@ const ExplorerPage: FC = () => {
   const { parent, isRoot } = analyzePath(path);
 
   if (!namespace) return null;
+
   return (
-    <div className="flex flex-col space-y-5 p-10 text-sm">
+    <div className="flex flex-col space-y-5 p-5 text-sm">
+      <div className="flex flex-col text-base max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
+        <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
+          <FolderTree className="h-5" />
+          {data?.node?.path}
+        </h3>
+        <Button variant="primary">
+          Actions <RxChevronDown />
+        </Button>
+      </div>
       <div className="flex flex-col space-y-5 ">
         {!isRoot && (
           <Link
@@ -27,7 +39,7 @@ const ExplorerPage: FC = () => {
             })}
             className="flex items-center space-x-3"
           >
-            <FolderUp />
+            <FolderUp className="h-5" />
             <span>..</span>
           </Link>
         )}
@@ -54,7 +66,7 @@ const ExplorerPage: FC = () => {
           return (
             <div key={file.name}>
               <Link to={linkTarget} className="flex items-center space-x-3">
-                <Icon />
+                <Icon className="h-5" />
                 <span className="flex-1">{file.name}</span>
                 <span className="text-gray-8 dark:text-gray-dark-8">
                   {moment(file.updatedAt).fromNow()}
