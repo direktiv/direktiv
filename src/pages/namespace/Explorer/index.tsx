df2f3c5f4ebf8813f3db500a0492547d2ae86fe7
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-24 08:08:35 +0100

    update explorer header
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/index.tsx
+++ src/pages/namespace/Explorer/index.tsx
@@ -35,78 +35,81 @@ const ExplorerPage: FC = () => {
   if (!namespace) return null;
 
   return (
-    <div className="flex flex-col space-y-5 p-5 text-sm">
-      <div className="flex flex-col text-base max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
-        <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
-          <FolderTree className="h-5" />
-          {data?.node?.path}
-        </h3>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="primary">
-              <PlusCircle /> New <RxChevronDown />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-40">
-            <DropdownMenuLabel>Create</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Play className="mr-2 h-4 w-4" />
-              <span>New Workflow</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Folder className="mr-2 h-4 w-4" />
-              <span>New Directory</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <div>
+      <div className="space-y-5 border-b border-gray-5 bg-base-200 p-5 dark:border-gray-dark-5">
+        <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between ">
+          <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
+            <FolderTree className="h-5" />
+            {data?.node?.path}
+          </h3>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="primary">
+                <PlusCircle /> New <RxChevronDown />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40">
+              <DropdownMenuLabel>Create</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Play className="mr-2 h-4 w-4" />
+                <span>New Workflow</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Folder className="mr-2 h-4 w-4" />
+                <span>New Directory</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
-      <div className="flex flex-col space-y-5 ">
-        {!isRoot && (
-          <Link
-            to={pages.explorer.createHref({
-              namespace,
-              path: parent?.absolute,
-            })}
-            className="flex items-center space-x-3"
-          >
-            <FolderUp className="h-5" />
-            <span>..</span>
-          </Link>
-        )}
-        {data?.children?.results.map((file) => {
-          let Icon = Folder;
-          if (file.expandedType === "workflow") {
-            Icon = Play;
-          }
-          if (file.expandedType === "git") {
-            Icon = Github;
-          }
+      <div className="flex flex-col space-y-5 p-5 text-sm">
+        <div className="flex flex-col space-y-5 ">
+          {!isRoot && (
+            <Link
+              to={pages.explorer.createHref({
+                namespace,
+                path: parent?.absolute,
+              })}
+              className="flex items-center space-x-3"
+            >
+              <FolderUp className="h-5" />
+              <span>..</span>
+            </Link>
+          )}
+          {data?.children?.results.map((file) => {
+            let Icon = Folder;
+            if (file.expandedType === "workflow") {
+              Icon = Play;
+            }
+            if (file.expandedType === "git") {
+              Icon = Github;
+            }
 
-          const linkTarget =
-            file.expandedType === "workflow"
-              ? pages.workflow.createHref({
-                  namespace,
-                  path: file.path,
-                })
-              : pages.explorer.createHref({
-                  namespace,
-                  path: file.path,
-                });
+            const linkTarget =
+              file.expandedType === "workflow"
+                ? pages.workflow.createHref({
+                    namespace,
+                    path: file.path,
+                  })
+                : pages.explorer.createHref({
+                    namespace,
+                    path: file.path,
+                  });
 
-          return (
-            <div key={file.name}>
-              <Link to={linkTarget} className="flex items-center space-x-3">
-                <Icon className="h-5" />
-                <span className="flex-1">{file.name}</span>
-                <span className="text-gray-8 dark:text-gray-dark-8">
-                  {moment(file.updatedAt).fromNow()}
-                </span>
-              </Link>
-            </div>
-          );
-        })}
+            return (
+              <div key={file.name}>
+                <Link to={linkTarget} className="flex items-center space-x-3">
+                  <Icon className="h-5" />
+                  <span className="flex-1">{file.name}</span>
+                  <span className="text-gray-8 dark:text-gray-dark-8">
+                    {moment(file.updatedAt).fromNow()}
+                  </span>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
