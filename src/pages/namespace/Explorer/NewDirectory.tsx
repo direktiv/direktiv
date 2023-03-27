57a6f24c8337f03b82abf7fe4e9fc7933e2aa1cb
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-27 11:46:16 +0200

    use default value
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/NewDirectory.tsx
+++ src/pages/namespace/Explorer/NewDirectory.tsx
@@ -60,7 +60,7 @@ const NewDirectory = ({
           <input
             className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
             id="name"
-            defaultValue="Folder Name"
+            placeholder="folder-name"
             {...register("name")}
           />
         </fieldset>
