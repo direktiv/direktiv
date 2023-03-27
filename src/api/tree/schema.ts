f451e8d2d6375312f1062f3d7dc797b19d09a540
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-27 11:45:13 +0200

    add message to fileNameSchema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/tree/schema.ts
+++ src/api/tree/schema.ts
@@ -32,7 +32,10 @@ export const TreeFolderCreatedSchema = z.object({
 
 export const fileNameSchema = z
   .string()
-  .regex(/^(([a-z][a-z0-9_\-.]*[a-z0-9])|([a-z]))$/);
+  .regex(/^(([a-z][a-z0-9_\-.]*[a-z0-9])|([a-z]))$/, {
+    message:
+      "Please use a folder name that starts with a lowercase letter, use - or _ instead of whitespaces.",
+  });
 
 export type TreeListSchemaType = z.infer<typeof TreeListSchema>;
 export type NodeSchemaType = z.infer<typeof NodeSchema>;
