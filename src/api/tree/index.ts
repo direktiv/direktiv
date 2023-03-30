e1194aeeaa7aa2ced56c1c5a7bbd5c7680423481
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-30 17:42:37 +0200

    force leading slash
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/tree/index.ts
+++ src/api/tree/index.ts
@@ -3,6 +3,11 @@ import { forceLeadingSlash } from "./utils";
 export const treeKeys = {
   all: (apiKey: string, namespace: string, path: string) =>
     [
-      { scope: "tree", apiKey, namespace, path: forceLeadingSlash(path) },
+      {
+        scope: "tree",
+        apiKey,
+        namespace,
+        path: path ? forceLeadingSlash(path) : "/",
+      },
     ] as const,
 };
