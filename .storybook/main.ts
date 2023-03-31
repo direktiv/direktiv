1e3d32905976ee4e18ab498c3aa7f2b43d386e57
Stefan Kracht <stefan.kracht@direktiv.io>
2023-03-31 15:29:17 +0200

    remove storybook styling
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -18,14 +18,6 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-react-router-v6",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: true,
-      },
-    },
   ],
   framework: {
     name: "@storybook/react-vite",
