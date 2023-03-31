c70fd6e9eb5b2f8581e1d9a45c225dec6dbdb245
porleaaron <porleaaron28@gmail.com>
2023-03-31 22:10:57 +0800

    update on pagination

--- src/design/Pagination/index.stories.tsx
+++ src/design/Pagination/index.stories.tsx
@@ -1,5 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Pagination from "./index";
+import { Pagination, PaginationLink } from "./index";
+import React, { useState } from "react";
+import {
+  RxChevronLeft,
+  RxChevronRight,
+  RxDoubleArrowLeft,
+  RxDoubleArrowRight,
+} from "react-icons/rx";
 
 const meta = {
   title: "Components/Pagination",
@@ -9,15 +16,77 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const StoryComponent = ({ ...args }) => {
+  const [page, setPage] = useState<number>(1);
+  const handleClick = (index: number) => {
+    setPage(index);
+  };
+  return (
+    <Pagination {...args}>
+      <PaginationLink
+        icon
+        onClick={() => {
+          if (page > 1) setPage(page - 1);
+        }}
+      >
+        <RxChevronLeft className="h-5 w-5" aria-hidden="true" />
+      </PaginationLink>
+      {new Array(3).fill(0).map((prop, key) => (
+        <PaginationLink
+          key={`pg-lint-${key}`}
+          onClick={() => handleClick(key + 1)}
+          active={page === key + 1}
+        >
+          {key + 1}
+        </PaginationLink>
+      ))}
+      <PaginationLink
+        icon
+        onClick={() => {
+          if (page < 3) setPage(page + 1);
+        }}
+      >
+        <RxChevronRight className="h-5 w-5" aria-hidden="true" />
+      </PaginationLink>
+    </Pagination>
+  );
+};
 export const Default: Story = {
-  render: ({ ...args }) => <Pagination {...args} />,
+  render: ({ ...args }) => <StoryComponent {...args} />,
+  args: {
+    children: "Pagination Content",
+  },
   argTypes: {
-    total: {
-      description: "total pages",
-      control: {
-        type: "text",
-      },
-      type: { name: "number", required: false },
+    align: {
+      description: "select variant",
+      control: "select",
+      options: ["left", "center", "right"],
+      type: { name: "string", required: false },
     },
   },
+};
+
+export const NoPageButtons = () => {
+  const [page, setPage] = useState(100);
+  return (
+    <Pagination align="center">
+      <PaginationLink
+        icon
+        onClick={() => {
+          if (page > 1) setPage(page - 1);
+        }}
+      >
+        <RxDoubleArrowLeft className="h-5 w-5" aria-hidden="true" />
+      </PaginationLink>
+      <PaginationLink active>{`PAGE ${page}`}</PaginationLink>
+      <PaginationLink
+        icon
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        <RxDoubleArrowRight className="h-5 w-5" aria-hidden="true" />
+      </PaginationLink>
+    </Pagination>
+  );
 };
--- src/design/Pagination/index.tsx
+++ src/design/Pagination/index.tsx
@@ -1,198 +1,66 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
-
-import { useState } from "react";
+import React from "react";
+import clsx from "clsx";
 
 interface PaginationProps {
-  total?: number;
-  onChange: (index: number) => void;
+  children: React.ReactNode;
+  align?: "center" | "left" | "right";
 }
-export default function Pagination(props: PaginationProps) {
-  const { total = 5, onChange } = props;
-  const [current, setCurrent] = useState<number>(1);
-  const handleChange = (index: number) => {
-    setCurrent(index);
-    onChange(index);
-  };
-  const handlePrev = () => {
-    if (current > 1) {
-      setCurrent(current - 1);
-    }
-  };
-  const handleNext = () => {
-    if (current < total) {
-      setCurrent(current + 1);
-    }
-  };
+
+export const Pagination: React.FC<PaginationProps> = ({
+  children,
+  align = "right",
+}) => (
+  <div
+    className={clsx(
+      "flex border-t border-gray-2 bg-white px-4 py-3 sm:px-6 ",
+      align === "center" && "justify-center",
+      align === "right" && "justify-end",
+      align === "left" && "justify-start"
+    )}
+  >
+    <nav
+      className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+      aria-label="Pagination"
+    >
+      {children}
+    </nav>
+  </div>
+);
+Pagination.displayName = "Pagination";
+export interface PaginationLinkProps {
+  key?: string;
+  onClick?: () => void;
+  active?: boolean;
+  icon?: boolean;
+}
+export const PaginationLink = React.forwardRef<
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement> & PaginationLinkProps
+>(({ children, key, onClick, active, icon = false }, ref) => {
   const inactiveClass =
-    "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-3 hover:bg-gray-1 focus:z-20 focus:outline-offset-0 cursor-pointer";
+    "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-3 hover:bg-gray-1 focus:z-20 focus:outline-offset-0 cursor-pointer dark:bg-gray-12 dark:text-gray-1";
   const activeClass =
-    "relative z-10 inline-flex items-center bg-primary-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 cursor-pointer";
+    "relative z-10 inline-flex items-center bg-gray-12 px-4 py-2 text-sm font-semibold text-gray-1 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 cursor-pointer dark:bg-gray-dark-12 dark:text-gray-dark-1";
 
-  const abButton = () => (
-    <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-11 ring-1 ring-inset ring-gray-3 focus:outline-offset-0">
-      ...
-    </span>
+  return icon ? (
+    <button
+      ref={ref}
+      key={key}
+      onClick={onClick}
+      className="relative inline-flex cursor-pointer items-center p-2 text-gray-4 ring-1 ring-inset ring-gray-3 hover:bg-gray-1 focus:z-20 focus:outline-offset-0 dark:bg-gray-12 dark:text-gray-1"
+    >
+      {children}
+    </button>
+  ) : (
+    <button
+      ref={ref}
+      key={key}
+      onClick={onClick}
+      aria-current="page"
+      className={active ? activeClass : inactiveClass}
+    >
+      {children}
+    </button>
   );
-  const main = () => (
-    <>
-      <a
-        onClick={() => handleChange(1)}
-        aria-current="page"
-        className={current === 1 ? activeClass : inactiveClass}
-      >
-        1
-      </a>
-      {current < 5 ? (
-        <a
-          onClick={() => handleChange(2)}
-          className={current === 2 ? activeClass : inactiveClass}
-        >
-          2
-        </a>
-      ) : (
-        abButton()
-      )}
-      {current > 4 ? (
-        current < total - 3 ? (
-          <a
-            className={inactiveClass}
-            onClick={() => handleChange(current - 1)}
-          >
-            {current - 1}
-          </a>
-        ) : (
-          <a className={inactiveClass} onClick={() => handleChange(total - 4)}>
-            {total - 4}
-          </a>
-        )
-      ) : (
-        <a
-          onClick={() => handleChange(3)}
-          className={current === 3 ? activeClass : inactiveClass}
-        >
-          {3}
-        </a>
-      )}
-      {current > 4 ? (
-        current < total - 2 ? (
-          <a className={activeClass} onClick={() => handleChange(total - 2)}>
-            {current}
-          </a>
-        ) : (
-          <a className={inactiveClass} onClick={() => handleChange(total - 3)}>
-            {total - 3}
-          </a>
-        )
-      ) : (
-        <a
-          onClick={() => handleChange(4)}
-          className={current === 4 ? activeClass : inactiveClass}
-        >
-          {4}
-        </a>
-      )}
-      {current > total - 3 ? (
-        <a
-          className={current === total - 2 ? activeClass : inactiveClass}
-          onClick={() => handleChange(total - 2)}
-        >
-          {total - 2}
-        </a>
-      ) : current > 3 ? (
-        <a className={inactiveClass} onClick={() => handleChange(current + 1)}>
-          {current + 1}
-        </a>
-      ) : (
-        abButton()
-      )}
-
-      {current > total - 4 ? (
-        <a
-          className={current === total - 1 ? activeClass : inactiveClass}
-          onClick={() => handleChange(total - 1)}
-        >
-          {total - 1}
-        </a>
-      ) : current < 4 ? (
-        <a
-          className={current === total - 1 ? activeClass : inactiveClass}
-          onClick={() => handleChange(total - 1)}
-        >
-          {total - 1}
-        </a>
-      ) : (
-        abButton()
-      )}
-
-      <a
-        onClick={() => handleChange(total)}
-        className={current === total ? activeClass : inactiveClass}
-      >
-        {total}
-      </a>
-    </>
-  );
-
-  const simple = () => (
-    <>
-      {new Array(total).fill(0).map((prop, key) => (
-        <a
-          key={key}
-          onClick={() => handleChange(key + 1)}
-          aria-current="page"
-          className={current === key + 1 ? activeClass : inactiveClass}
-        >
-          {key + 1}
-        </a>
-      ))}
-    </>
-  );
-  return (
-    <div className="flex items-center justify-between border-t border-gray-2 bg-white px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          onClick={handlePrev}
-          className="relative inline-flex cursor-pointer items-center rounded-md border border-gray-3 bg-white px-4 py-2 text-sm font-medium text-gray-11 hover:bg-gray-1"
-        >
-          Previous
-        </a>
-        <a
-          onClick={handleNext}
-          className="relative ml-3 inline-flex cursor-pointer items-center rounded-md border border-gray-3 bg-white px-4 py-2 text-sm font-medium text-gray-11 hover:bg-gray-1"
-        >
-          Next
-        </a>
-      </div>
-      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-        <div></div>
-        <div>
-          <nav
-            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-            aria-label="Pagination"
-          >
-            <a
-              onClick={handlePrev}
-              className="relative inline-flex cursor-pointer items-center rounded-l-md p-2 text-gray-4 ring-1 ring-inset ring-gray-3 hover:bg-gray-1 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
-            {
-              //show all buttons when total is smaller than 7
-              //show 7 buttons when total is bigger
-              total > 6 ? main() : simple()
-            }
-
-            <a
-              onClick={handleNext}
-              className="relative inline-flex cursor-pointer items-center rounded-r-md p-2 text-gray-4 ring-1 ring-inset ring-gray-3 hover:bg-gray-1 focus:z-20 focus:outline-offset-0"
-            >
-              <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
-          </nav>
-        </div>
-      </div>
-    </div>
-  );
-}
+});
+PaginationLink.displayName = "PaginationLink";
