966172db10a831ded31aca138396c78b08e562b8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-02 19:28:04 +0200

    PaginationProvider first draft (untested)
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/PaginationProvider/index.tsx
+++ src/componentsNext/PaginationProvider/index.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import { useState } from "react";
 
-const PaginationProvider = () => <h1></h1>;
+const PaginationProvider = <TArrayItem,>({
+  children,
+  items,
+  pageSize: pageSizeProp,
+}: {
+  children: (props: {
+    currentItems: TArrayItem[];
+    gotoFirstPage: () => void;
+    gotoLastPage: () => void;
+    gotoNextPage: () => void;
+    gotoPreviousPage: () => void;
+    isFirstPage: boolean;
+    isLastPage: boolean;
+    page: number;
+  }) => JSX.Element;
+  pageSize?: number;
+  items: TArrayItem[];
+}) => {
+  const firstPage = 1;
+  const [page, setPage] = useState(firstPage);
+  const pageSize = pageSizeProp || 10;
+  const lastPage = Math.ceil(items.length / pageSize);
+  const isLastPage = page === lastPage;
+  const isFirstPage = page === firstPage;
+
+  const sliceStart = (page - 1) * pageSize;
+  const sliceEnd = sliceStart + pageSize;
+  const currentItems = items.slice(sliceStart, sliceEnd);
+
+  const gotoFirstPage = () => setPage(1);
+  const gotoLastPage = () => setPage(lastPage);
+  const gotoNextPage = () =>
+    setPage((page) => (page <= lastPage ? page + 1 : page));
+  const gotoPreviousPage = () =>
+    setPage((page) => (page >= firstPage ? page - 1 : page));
+
+  return children({
+    currentItems,
+    gotoFirstPage,
+    gotoLastPage,
+    gotoNextPage,
+    gotoPreviousPage,
+    isFirstPage,
+    isLastPage,
+    page,
+  });
+};
 
 export default PaginationProvider;
