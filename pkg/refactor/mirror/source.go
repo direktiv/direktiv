d7aeca97e8ce0ca58d3fc1ae44e4c51430c33f1e
sir-hassan <yassir.hassan@direktiv.io>
2023-04-10 22:19:46 +0200

    remove old sync function.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/refactor/mirror/source.go
+++ pkg/refactor/mirror/source.go
@@ -11,6 +11,17 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+// Source is an interface that represent a mirror source (git repo is a valid mirror source example).
+// In Direktiv, a Mirror is a directory of files that sits somewhere (local or remote) and a user wants to mirror (copy)
+// his direktiv namespace files from it.
+// Source knows how to access the mirror files (connecting to a remote server in case of git) and copy the files in
+// the user's direktiv namespace. Parameter 'settings' is used to configure the sourcing (pulling) mirror process.
+// Parameter 'dir' specifies the directory where Source should copy the mirror to.
+type Source interface {
+	// PullInPath pulls (copies) mirror into local directory specified by 'dir' parameter.
+	PullInPath(config *Config, dir string) error
+}
+
 // MockedSource mocks Source interface.
 type MockedSource struct {
 	Paths map[string]string
