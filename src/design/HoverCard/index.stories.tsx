185d1c137dd7574593ce59c137bd6525228aa917
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-11 14:20:20 +0200

    update hover card content
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/HoverCard/index.stories.tsx
+++ src/design/HoverCard/index.stories.tsx
@@ -1,5 +1,4 @@
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./index";
-
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
@@ -13,15 +12,8 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: ({ ...args }) => (
     <HoverCard {...args}>
-      <HoverCardTrigger>Hover</HoverCardTrigger>
-      <HoverCardContent>
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardTrigger>Hover Me</HoverCardTrigger>
+      <HoverCardContent>Content goes here</HoverCardContent>
     </HoverCard>
   ),
 };
@@ -30,36 +22,15 @@ export const ContentAlignment = () => (
   <div className=" flex w-full flex-row justify-around">
     <HoverCard>
       <HoverCardTrigger>Hover, Align Start</HoverCardTrigger>
-      <HoverCardContent align="start">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent align="start">Content goes here</HoverCardContent>
     </HoverCard>
     <HoverCard>
       <HoverCardTrigger>Hover, Align Center</HoverCardTrigger>
-      <HoverCardContent align="center">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent align="center">Content goes here</HoverCardContent>
     </HoverCard>
     <HoverCard>
       <HoverCardTrigger>Hover, Align End</HoverCardTrigger>
-      <HoverCardContent align="end">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent align="end">Content goes here</HoverCardContent>
     </HoverCard>
   </div>
 );
@@ -68,47 +39,19 @@ export const ContentSide = () => (
   <div className=" flex h-64 w-full flex-row items-center justify-around">
     <HoverCard>
       <HoverCardTrigger>Hover Me, to see right</HoverCardTrigger>
-      <HoverCardContent side="right">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent side="right">Content goes here</HoverCardContent>
     </HoverCard>
     <HoverCard>
       <HoverCardTrigger>Hover Me, to see left</HoverCardTrigger>
-      <HoverCardContent side="left">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent side="left">Content goes here</HoverCardContent>
     </HoverCard>
     <HoverCard>
       <HoverCardTrigger>Hover Me, to see top</HoverCardTrigger>
-      <HoverCardContent side="top">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent side="top">Content goes here</HoverCardContent>
     </HoverCard>
     <HoverCard>
       <HoverCardTrigger>Hover Me, to see bottom</HoverCardTrigger>
-      <HoverCardContent side="bottom">
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent side="bottom">Content goes here</HoverCardContent>
     </HoverCard>
   </div>
 );
@@ -119,27 +62,13 @@ export const ContentSideOffset = () => (
       <HoverCardTrigger>
         Hover Me, to see 5px Margin of the Conntent
       </HoverCardTrigger>
-      <HoverCardContent sideOffset={5}>
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent sideOffset={5}>Content goes here</HoverCardContent>
     </HoverCard>
     <HoverCard>
       <HoverCardTrigger>
         Hover Me, to see 30px Margin of the Conntent
       </HoverCardTrigger>
-      <HoverCardContent sideOffset={30}>
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent sideOffset={30}>Content goes here</HoverCardContent>
     </HoverCard>
   </div>
 );
@@ -148,26 +77,12 @@ export const OpenCloseDelay = () => (
   <div className=" flex h-64 w-full flex-row items-center justify-around">
     <HoverCard openDelay={3000} closeDelay={3000}>
       <HoverCardTrigger>Hover Me, to see in 3 secs</HoverCardTrigger>
-      <HoverCardContent>
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent>Content goes here</HoverCardContent>
     </HoverCard>
 
     <HoverCard openDelay={1000} closeDelay={1000}>
       <HoverCardTrigger>Hover Me, to see in 1 sec</HoverCardTrigger>
-      <HoverCardContent>
-        <div className="space-y-2">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-        </div>
-      </HoverCardContent>
+      <HoverCardContent>Content goes here</HoverCardContent>
     </HoverCard>
   </div>
 );
