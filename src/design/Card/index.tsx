6c56335354303dff4ef6724b5e545ff65600a33e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-11 12:18:02 +0200

    remove some wrapper from card component
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Card/index.tsx
+++ src/design/Card/index.tsx
@@ -9,23 +9,21 @@ export const Card: React.FC<CardProps> = React.forwardRef<
   HTMLDivElement,
   CardProps
 >(({ children, className, withBackground, noShadow, ...props }, ref) => (
-  <div className={clsx("mt-8 flow-root")} ref={ref} {...props}>
-    <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-        <div
-          className={clsx(
-            "overflow-hidden ring-1 sm:rounded-lg",
-            " bg-white ring-black/5 ",
-            " dark:bg-black dark:ring-white/5",
-            noShadow ? "shadow-none" : "shadow",
-            withBackground && "bg-gray-2 dark:bg-gray-dark-2",
-            className
-          )}
-        >
-          {children}
-        </div>
-      </div>
-    </div>
+  <div
+    ref={ref}
+    {...props}
+    className={clsx(
+      "rounded-md ring-1",
+      "ring-black/5",
+      "dark:ring-white/5",
+      !noShadow && "shadow",
+      withBackground
+        ? "bg-gray-1 dark:bg-gray-dark-1"
+        : "bg-white dark:bg-black",
+      className
+    )}
+  >
+    {children}
   </div>
 ));
 
