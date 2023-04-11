cb11566a5b426b875858eb549655a382a02e3865
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-11 14:54:06 +0200

    update story
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Separator/index.stories.tsx
+++ src/design/Separator/index.stories.tsx
@@ -10,50 +10,39 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: () => (
-    <div>
-      <div className="space-y-1">
-        <h4 className="text-sm font-medium leading-none">
-          The Separator Component UI
-        </h4>
-        <p className="text-sm text-gray-9 dark:text-gray-dark-9">
-          An open-source UI component library.
-        </p>
-      </div>
-      <Separator className="my-4" />
-      <div className="flex h-5 items-center space-x-4 text-sm">
-        <div>Blog</div>
-        <Separator orientation="vertical" />
-        <div>Docs</div>
-        <Separator orientation="vertical" />
-        <div>Source</div>
-      </div>
+  render: ({ ...args }) => (
+    <div className={args.vertical ? "flex h-5 items-center space-x-2" : ""}>
+      <div>Some</div>
+      <Separator {...args} className="my-2" />
+      <div>Content</div>
     </div>
   ),
   tags: ["autodocs"],
+  argTypes: {
+    vertical: {
+      description: "user vertical separator",
+      type: {
+        name: "boolean",
+        required: false,
+      },
+    },
+  },
 };
+
 export const HorizontalSeparator = () => (
   <div>
-    <div className="space-y-1">
-      <h4 className="text-sm font-medium leading-none">
-        The Separator Component UI
-      </h4>
-    </div>
+    <div>The Separator Component UI</div>
     <Separator className="my-4" />
-    <div className="space-y-1">
-      <h4 className="text-sm font-medium leading-none">
-        The Separator Component UI
-      </h4>
-    </div>
+    <div>The Separator Component UI</div>
   </div>
 );
 
 export const VerticalSeparator = () => (
-  <div className="flex h-5 items-center space-x-4 text-sm">
+  <div className="flex h-5 items-center space-x-4">
     <div>Blog</div>
-    <Separator orientation="vertical" />
+    <Separator vertical />
     <div>Docs</div>
-    <Separator orientation="vertical" />
+    <Separator vertical />
     <div>Source</div>
   </div>
 );
