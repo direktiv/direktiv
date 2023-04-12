2f6eae800340743e46e86d700525c0988e0d0db8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-12 18:58:50 +0200

    small color adjustments
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Toggle/index.tsx
+++ src/design/Toggle/index.tsx
@@ -16,9 +16,9 @@ const Toggle = React.forwardRef<
   <TogglePrimitive.Root
     ref={ref}
     className={clsx(
-      "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors   focus:outline-none  focus:ring-2  focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-      "text-gray-11 hover:bg-gray-2 hover:text-gray-12 focus:ring-gray-7 focus:ring-offset-gray-1 data-[state=on]:bg-gray-4 data-[state=on]:text-gray-11",
-      "dark:hover:bg-gray-dark-2 dark:focus:ring-gray-dark-7  dark:data-[state=on]:bg-gray-dark-4",
+      "inline-flex items-center justify-center rounded-md bg-transparent text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      "text-gray-11 hover:bg-gray-2 hover:text-gray-12 focus:ring-gray-4 focus:ring-offset-gray-1 data-[state=on]:bg-gray-4 data-[state=on]:text-gray-11",
+      "dark:hover:bg-gray-dark-2 dark:focus:ring-gray-dark-4 dark:data-[state=on]:bg-gray-dark-4",
       "dark:text-gray-dark-11 dark:hover:text-gray-dark-12 dark:focus:ring-offset-gray-dark-1 dark:data-[state=on]:text-gray-dark-11",
       outline &&
         "border border-gray-4 hover:bg-gray-2 dark:border-gray-dark-4 dark:bg-gray-dark-2",
