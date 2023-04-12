d5adb137632c7e4ed60341df55938c6c49512d91
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-12 19:24:48 +0200

    adjust size to match button
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Toggle/index.tsx
+++ src/design/Toggle/index.tsx
@@ -22,9 +22,9 @@ const Toggle = React.forwardRef<
       "dark:text-gray-dark-11 dark:hover:text-gray-dark-12 dark:focus:ring-offset-gray-dark-1 dark:data-[state=on]:text-gray-dark-11",
       outline &&
         "border border-gray-4 hover:bg-gray-2 dark:border-gray-dark-4 dark:bg-gray-dark-2",
-      size === "sm" && "h-9 px-2.5",
+      size === "sm" && "h-6 px-2.5",
       size === "lg" && "h-11 px-5",
-      !size && "h-10 px-3",
+      !size && "h-9 px-3",
       className
     )}
     {...props}
