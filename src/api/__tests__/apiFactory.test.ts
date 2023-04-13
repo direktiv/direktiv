0525c1ec011d8ec4a357aa06e5ec1be69dd5f3c7
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-13 18:27:06 +0200

    test payload
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/__tests__/apiFactory.test.ts
+++ src/api/__tests__/apiFactory.test.ts
@@ -10,16 +10,17 @@ import {
   vi,
 } from "vitest";
 import { renderHook, waitFor } from "@testing-library/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import { UseQueryWrapper } from "../../../test/utils";
 import { apiFactory } from "../utils";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import { useQuery } from "@tanstack/react-query";
 import { z } from "zod";
 
 const API_KEY = "THIS-IS-MY-API-KEY";
 const apiEndpoint = "http://localhost/my-api";
+const apiEndpointPost = "http://localhost/my-api-post";
 const apiEndpoint404 = "http://localhost/404";
 const apiEndpointWithDynamicSegment = "http://localhost/this-is-dynamic/my-api";
 const apiEndpointEmptyResponse = "http://localhost/empty-response";
@@ -48,7 +49,15 @@ const testApi = setupServer(
   rest.get(apiEndpointEmptyResponse, (_req, res, ctx) => res(ctx.status(204))),
   rest.get(apiEndpointTextResponse, (_req, res, ctx) =>
     res(ctx.text("this is a text response"))
-  )
+  ),
+  rest.post(apiEndpointPost, async (req, res, ctx) => {
+    const body = await req.text();
+    return res(
+      ctx.json({
+        body: body,
+      })
+    );
+  })
 );
 
 beforeAll(() => {
@@ -95,6 +104,14 @@ const api404 = apiFactory({
   method: "GET",
   schema: z.object({
     response: z.string(),
+  }),
+});
+
+const apiPost = apiFactory({
+  pathFn: () => apiEndpointPost,
+  method: "POST",
+  schema: z.object({
+    body: z.string(),
   }),
 });
 
@@ -264,6 +281,46 @@ describe("processApiResponse", () => {
     await waitFor(() => {
       expect(result.current.isSuccess).toBe(true);
       expect(result.current.data?.response).toBe("dynamic segment this works");
+    });
+  });
+
+  test.only("payload will be stringified", async () => {
+    const useCallWithPost = (params: unknown) =>
+      useMutation({
+        mutationFn: () =>
+          apiPost({
+            apiKey: API_KEY,
+            params,
+            pathParams: undefined,
+          }),
+      });
+
+    const { result: resultJSON } = renderHook(
+      () => useCallWithPost({ my: "payload" }),
+      { wrapper: UseQueryWrapper }
+    );
+
+    const { result: resultString } = renderHook(
+      () => useCallWithPost("this is a string"),
+      { wrapper: UseQueryWrapper }
+    );
+
+    const { result: resultBoolean } = renderHook(() => useCallWithPost(true), {
+      wrapper: UseQueryWrapper,
+    });
+
+    resultJSON.current.mutate();
+    resultString.current.mutate();
+    resultBoolean.current.mutate();
+    await waitFor(() => {
+      expect(resultJSON.current.isSuccess).toBe(true);
+      expect(resultJSON.current.data?.body).toBe(`{"my":"payload"}`);
+
+      expect(resultString.current.isSuccess).toBe(true);
+      expect(resultString.current.data?.body).toBe("this is a string");
+
+      expect(resultBoolean.current.isSuccess).toBe(true);
+      expect(resultBoolean.current.data?.body).toBe("true");
     });
   });
 });
