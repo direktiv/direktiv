e6e15d5b1541b1fe112052f5c1fe63c7d9e5ea36
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-24 15:12:16 +0200

    add info about typesafe env variables
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
+// at the moment, we only get a type check on build- and server-start-time
+// and we break the app early if any of the env variables are missing or set
+// to an invalid value.
+// If you use import.meta.env.VITE_APP_VERSION in a component, it will
+// not be typed yet.
 const envVariablesSchema = z.object({
   VITE_DEV_API_DOMAIN: z.string().optional(),
   VITE_APP_VERSION: z
