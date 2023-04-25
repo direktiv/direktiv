d5ca19f02bd700377c264b4f3db8bafcb520f35f
Alexander Ertli <alexander.ertli@direktiv.io>
2023-04-25 20:33:24 +0000

    Fix init-project config
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- cmd/exec/cmd/config/project-conf-init.go
+++ cmd/exec/cmd/config/project-conf-init.go
@@ -7,6 +7,7 @@ import (
 	root "github.com/direktiv/direktiv/cmd/exec/cmd"
 	"github.com/direktiv/direktiv/pkg/project"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
 )
 
@@ -14,11 +15,14 @@ var confPInitCmd = &cobra.Command{
 	Use:   "project-init",
 	Short: "Creates direktiv-project from current directory.",
 	Long:  "Creates " + project.ConfigFileName + " in current directory to mark it as direktiv-project.",
-	Run: func(cmd *cobra.Command, args []string) {
-		pf, err := root.ProjectFolder()
+	PreRun: func(cmd *cobra.Command, args []string) {
+		err := root.InitWD()
 		if err != nil {
-			root.Fail(cmd, "Could not get project directory: %s", err.Error())
+			root.Fail(cmd, "Error: %s", err)
 		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		pf := viper.GetString("directory")
 		conf := make(map[string][]string)
 		conf["Ignore"] = []string{""}
 		data, err := yaml.Marshal(&conf)
