cad4dc2e081c3dfd5311b281c0176051597a6556
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-26 10:43:13 +0200

    remove some space
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Input/index.tsx
+++ src/design/Input/index.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<
     ref={ref}
     className={clsx(
       className,
-      "flex h-9 w-full rounded-md border   py-2 px-3 text-sm  focus:outline-none focus:ring-2  focus:ring-offset-2  disabled:cursor-not-allowed disabled:opacity-50",
+      "flex h-9 w-full rounded-md border py-2 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
       "border-gray-4 bg-white text-black placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
       "dark:border-gray-dark-4 dark:bg-black dark:text-white dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1"
     )}
