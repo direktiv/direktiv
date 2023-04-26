c671c6f8cbad7292fba9eb4fe2c2dfd053f2347b
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-26 15:00:07 +0200

    revert changes on the input field
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Input/index.tsx
+++ src/design/Input/index.tsx
@@ -10,9 +10,9 @@ const Input = React.forwardRef<
     ref={ref}
     className={clsx(
       className,
-      "flex h-9 w-full rounded-md border py-2 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-      "border-gray-4 bg-white text-black placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
-      "dark:border-gray-dark-4 dark:bg-black dark:text-white dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1"
+      "flex h-9 w-full rounded-md border bg-transparent py-2 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
+      "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1"
     )}
     {...props}
   />
