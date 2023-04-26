52ab76f4070f19cca92ec8869a0f63698c491092
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-26 09:42:34 +0200

    run prettier
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require('@tailwindcss/nesting')(require('postcss-nesting')),
+    require("@tailwindcss/nesting")(require("postcss-nesting")),
     require("tailwindcss"),
-    require("autoprefixer")
+    require("autoprefixer"),
   ],
 };
