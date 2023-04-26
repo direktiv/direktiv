928c5ad35974920c7e19c8b33b837a51063e6149
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-26 11:21:45 +0200

    change order
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Select/index.stories.tsx
+++ src/design/Select/index.stories.tsx
@@ -153,18 +153,8 @@ export const TriggerVariants = () => (
       </SelectContent>
     </Select>
     <Select>
-      <SelectTrigger variant="ghost">
-        <SelectValue placeholder="ghost select" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="1">Item 1</SelectItem>
-        <SelectItem value="2">Item 2</SelectItem>
-        <SelectItem value="3">Item 3</SelectItem>
-      </SelectContent>
-    </Select>
-    <Select>
-      <SelectTrigger variant="destructive">
-        <SelectValue placeholder="destructive select" />
+      <SelectTrigger variant="primary">
+        <SelectValue placeholder="primary select" />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value="1">Item 1</SelectItem>
@@ -183,8 +173,8 @@ export const TriggerVariants = () => (
       </SelectContent>
     </Select>
     <Select>
-      <SelectTrigger variant="primary">
-        <SelectValue placeholder="primary select" />
+      <SelectTrigger variant="ghost">
+        <SelectValue placeholder="ghost select" />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value="1">Item 1</SelectItem>
@@ -195,6 +185,16 @@ export const TriggerVariants = () => (
     <Select>
       <SelectTrigger variant="link">
         <SelectValue placeholder="link select" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="1">Item 1</SelectItem>
+        <SelectItem value="2">Item 2</SelectItem>
+        <SelectItem value="3">Item 3</SelectItem>
+      </SelectContent>
+    </Select>
+    <Select>
+      <SelectTrigger variant="destructive">
+        <SelectValue placeholder="destructive select" />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value="1">Item 1</SelectItem>
