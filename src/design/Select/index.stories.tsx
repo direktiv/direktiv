1ea8d378a19ad219f429acf6c6130d5a2d47d6f8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-26 11:20:15 +0200

    add default select to variants
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Select/index.stories.tsx
+++ src/design/Select/index.stories.tsx
@@ -143,6 +143,16 @@ export const LoadingState = () => (
 export const TriggerVariants = () => (
   <div className="flex space-x-3">
     <Select>
+      <SelectTrigger>
+        <SelectValue placeholder="default select" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="1">Item 1</SelectItem>
+        <SelectItem value="2">Item 2</SelectItem>
+        <SelectItem value="3">Item 3</SelectItem>
+      </SelectContent>
+    </Select>
+    <Select>
       <SelectTrigger variant="ghost">
         <SelectValue placeholder="ghost select" />
       </SelectTrigger>
