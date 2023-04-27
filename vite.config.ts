cd75cc31413edc231a474a813df52a86db3230f4
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-27 17:19:59 +0200

    allow urls with dots
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 
 import fs from "fs";
 import path from "path";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
@@ -68,7 +69,13 @@ export default ({ mode }) => {
         },
       },
     },
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin(), reactVirtualized()],
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      reactVirtualized(),
+      pluginRewriteAll(),
+    ],
     test: {
       globals: true,
       environment: "jsdom",
