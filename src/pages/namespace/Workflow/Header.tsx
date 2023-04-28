532c7588072ced2e1ed1e27c556b743af9497c2a
Stefan Kracht <stefan.kracht@direktiv.io>
2023-04-28 08:55:39 +0200

    add menu array and move all language stuff into translation file
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Workflow/Header.tsx
+++ src/pages/namespace/Workflow/Header.tsx
@@ -1,68 +1,66 @@
 import { GitCommit, GitMerge, PieChart, Play, Settings } from "lucide-react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../design/Tabs";
+import { Tabs, TabsList, TabsTrigger } from "../../../design/Tabs";
 
 import Button from "../../../design/Button";
 import { FC } from "react";
 import { RxChevronDown } from "react-icons/rx";
+import { useTranslation } from "react-i18next";
 
-const Header: FC = () => (
-  <div className="space-y-5 border-b border-gray-5 bg-base-200 p-5 pb-0 dark:border-gray-dark-5">
-    <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
-      <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
-        <Play className="h-5" />
-        workflow.yml
-      </h3>
-      <Button variant="primary">
-        Actions <RxChevronDown />
-      </Button>
+const Header: FC = () => {
+  const { t } = useTranslation();
+
+  const tabs = [
+    {
+      value: "activeRevision",
+      icon: <GitCommit aria-hidden="true" />,
+      title: t("pages.explorer.workflow.menu.activeRevision"),
+    },
+    {
+      value: "revisions",
+      icon: <GitMerge aria-hidden="true" />,
+      title: t("pages.explorer.workflow.menu.revisions"),
+    },
+    {
+      value: "overview",
+      icon: <PieChart aria-hidden="true" />,
+      title: t("pages.explorer.workflow.menu.overview"),
+    },
+    {
+      value: "settings",
+      icon: <Settings aria-hidden="true" />,
+      title: t("pages.explorer.workflow.menu.settings"),
+    },
+  ];
+
+  return (
+    <div className="space-y-5 border-b border-gray-5 bg-base-200 p-5 pb-0 dark:border-gray-dark-5">
+      <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
+        <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
+          <Play className="h-5" />
+          workflow.yml
+        </h3>
+        <Button variant="primary">
+          {t("pages.explorer.workflow.actionsBtn")} <RxChevronDown />
+        </Button>
+      </div>
+      <div>
+        <nav className="-mb-px flex space-x-8">
+          <Tabs defaultValue="overview">
+            <TabsList>
+              {tabs.map((tab) => (
+                <TabsTrigger asChild value={tab.value} key={tab.value}>
+                  <a>
+                    {tab.icon}
+                    {tab.title}
+                  </a>
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          </Tabs>
+        </nav>
+      </div>
     </div>
-    <div>
-      <nav className="-mb-px flex space-x-8">
-        <Tabs defaultValue="overview">
-          <TabsList>
-            <TabsTrigger value="overview">
-              <PieChart aria-hidden="true" />
-              Overview
-            </TabsTrigger>
-            <TabsTrigger value="active-rev">
-              <GitCommit aria-hidden="true" />
-              Active Revisions
-            </TabsTrigger>
-            <TabsTrigger value="revisions">
-              <GitMerge aria-hidden="true" />
-              Revisions
-            </TabsTrigger>
-            <TabsTrigger value="settings">
-              <Settings aria-hidden="true" />
-              Settings
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">
-            <p className="text-sm text-gray-8 dark:text-gray-dark-8">
-              Make changes to your account here. Click save when you&apos;re
-              done.
-            </p>
-          </TabsContent>
-          <TabsContent value="password">
-            <p className="text-sm text-gray-8 dark:text-gray-dark-8">
-              Change your password here. After saving, you&apos;ll be logged
-              out.
-            </p>
-          </TabsContent>
-          <TabsContent value="third">
-            <p className="text-sm text-gray-8 dark:text-gray-dark-8">
-              Your third content here
-            </p>
-          </TabsContent>
-          <TabsContent value="fourth">
-            <p className="text-sm text-gray-8 dark:text-gray-dark-8">
-              The fourth content comes here
-            </p>
-          </TabsContent>
-        </Tabs>
-      </nav>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
