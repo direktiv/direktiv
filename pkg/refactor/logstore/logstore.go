c84e72ba84615f429e95c6733d70c63bede43cc2
Alexander Ertli <alexander.ertli@direktiv.io>
2023-05-02 20:26:00 +0000

    Improve logstorer interfaces
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/refactor/logstore/logstore.go
+++ pkg/refactor/logstore/logstore.go
@@ -9,13 +9,14 @@ import (
 
 // LogStore manages storing and querying LogEntries.
 type LogStore interface {
-	// appends a log-entry to the logs of the corresponding RecipientID, RecipientType pair.
-	Append(recipientID uuid.UUID, recipientType RecipientType, l LogEntry) error
-	// returns a list of log-entries using the provided LogQuery.
-	// Example usage:
-	// ql := GetLogsQuery(recipienttId, recipientType, loglevel)
-	// logs, err := logstore.Get(ctx, ql)
-	Get(ctx context.Context, ql LogQuery) ([]*LogEntry, error)
+	// returns a query object to do further queries on the Server Logs
+	ForServer() LogQuery
+	// returns a query object to do further queries on the Instance Logs
+	ForInstance(recipientID uuid.UUID) LogQuery
+	// returns a query object to do further queries on the Workspace Logs
+	ForWorkspace(recipientID uuid.UUID) LogQuery
+	// returns a query object to do further queries on the Namespace Logs
+	ForNamespace(recipientID uuid.UUID) LogQuery
 }
 
 // Represents an individual log entry for activity.
@@ -30,24 +31,32 @@ type LogEntry struct {
 
 // LogQuery generates a query statement to receive Log-Entries.
 type LogQuery interface {
-	// BuildStatement() is intended to be called inside the get method of the logstorer.
-	// the resulting string of BuildStatement() has to be interpreted by logstorer and used to return the right logentries from the log-storage.
-	BuildStatement() (string, error)
+	// appends a log-entry to the logs of the corresponding RecipientID, RecipientType pair.
+	Append(ctx context.Context, l LogEntry) error
+	// returns a list of log-entries using the provided LogQuery.
+	Get(ctx context.Context, ls LogSelector) ([]*LogEntry, error)
+	// use to build a logSelector for receiving log-entries
+	NewFilterBuilder() LogSelectorBuilder
 }
 
-// represents valid recipient type for log-messages.
-type RecipientType string
+// LogSelectorBuilder constructs a LogSelector.
+type LogSelectorBuilder interface {
+	// select logs with matching level.
+	WithLogLevel(level Level)
+	// select first x entries off Log-entries matching all other attributes.
+	WithLimit(x int)
+	// select matching results starting given offset.
+	WithOffset(offset int)
+	// select logs with matching tag value pair.
+	WithTag(tag, value string)
+	// get the final LogSelector.
+	Get() (LogSelector, error)
+}
 
-const (
-	Server    RecipientType = "server"
-	Namespace RecipientType = "namespace"
-	Workflow  RecipientType = "workflow"
-	Instance  RecipientType = "instance"
-	Mirror    RecipientType = "mirror"
-)
-
-func (rt RecipientType) String() string {
-	return string(rt)
+// LogSelector generates a query statement to receive Log-Entries.
+// A LogSelector is created using a LogSelectorBuilder.
+type LogSelector interface {
+	buildStatement() (string, error)
 }
 
 // represents valid log levels.
