882954f10075c458342ccf56daff35d043c81865
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-02 11:19:33 +0200

    fix tailwind class
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Dropdown/index.tsx
+++ src/design/Dropdown/index.tsx
@@ -68,7 +68,7 @@ const DropdownMenuContent = React.forwardRef<
       ref={ref}
       sideOffset={sideOffset}
       className={clsx(
-        "data-[side=top] :slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden animate-in data-[side=right]:slide-in-from-left-2 data-[side=left]:slide-in-from-right-2 data-[side=bottom]:slide-in-from-top-2",
+        "z-50 min-w-[8rem] overflow-hidden animate-in data-[side=top]:slide-in-from-bottom-2 data-[side=right]:slide-in-from-left-2 data-[side=left]:slide-in-from-right-2 data-[side=bottom]:slide-in-from-top-2",
         "rounded-md shadow-md ring-1",
         "bg-gray-1 ring-gray-3",
         "dark:bg-gray-dark-1 dark:ring-gray-dark-3",
