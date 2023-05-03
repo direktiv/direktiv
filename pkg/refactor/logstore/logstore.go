03ec005753d9f4fc3b7f6726c142c8ee03a55d5a
Alexander Ertli <alexander.ertli@direktiv.io>
2023-05-03 09:58:04 +0000

    Fix linting
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/refactor/logstore/logstore.go
+++ pkg/refactor/logstore/logstore.go
@@ -7,11 +7,11 @@ import (
 
 // LogStore manages storing and querying LogEntries.
 type LogStore interface {
-    // appends a log entry to the logs. Passed keyAnValues will be associated with the log entry.
-    // Example: store.Append(ctx, time.Time, "message", "key", "value", "other-key", "value")
+	// appends a log entry to the logs. Passed keyAnValues will be associated with the log entry.
+	// Example: store.Append(ctx, time.Time, "message", "key", "value", "other-key", "value")
 	Append(ctx context.Context, timestamp time.Time, msg string, keysAndValues ...interface{}) error
-    // returns a list of log-entries that have matching associated fields with the provided keysAndValues pairs.
-    // Example: store.Get(ctx, "key1", value1, "key2", value2)
+	// returns a list of log-entries that have matching associated fields with the provided keysAndValues pairs.
+	// Example: store.Get(ctx, "key1", value1, "key2", value2)
 	Get(ctx context.Context, keysAndValues ...interface{}) ([]*LogEntry, error)
 }
 
