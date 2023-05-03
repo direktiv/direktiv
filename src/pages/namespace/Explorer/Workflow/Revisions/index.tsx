97d19223f0ec7b4bfba82e0387ea612becd36a68
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-03 11:34:50 +0200

    add dummy tags
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Revisions/index.tsx
+++ src/pages/namespace/Explorer/Workflow/Revisions/index.tsx
@@ -10,11 +10,21 @@ const WorkflowRevisionsPage: FC = () => {
     path,
   });
 
+  const test = ["test", "test2", "test3"];
+
   return (
     <div className="flex flex-col space-y-4 p-4">
       <h1>WorkflowRevisionsPage</h1>
       <Card className="p-4">
         <Badge>{data?.revision?.hash.slice(0, 8)}</Badge>
+      </Card>
+
+      <Card className="space-x-2 p-4">
+        {test.map((rev) => (
+          <Badge variant="outline" key={rev}>
+            {rev}
+          </Badge>
+        ))}
       </Card>
     </div>
   );
