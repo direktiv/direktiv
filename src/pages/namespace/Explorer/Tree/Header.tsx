01a1df3ce7b9c9a242ee0803cf9c60cf4a09c066
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-03 14:00:14 +0200

    change header bg
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Tree/Header.tsx
+++ src/pages/namespace/Explorer/Tree/Header.tsx
@@ -54,7 +54,7 @@ const ExplorerHeader: FC = () => {
 
   if (!namespace) return null;
   return (
-    <div className="space-y-5 border-b border-gray-5 bg-gray-2 p-5 dark:border-gray-dark-5 dark:bg-gray-dark-2">
+    <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 dark:border-gray-dark-5 dark:bg-gray-dark-1">
       <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
         <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
           <Link
