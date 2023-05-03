96aa3a81d19e00add49a2c25546e6d4b4114dfa6
Alexander Ertli <alexander.ertli@direktiv.io>
2023-05-03 09:54:02 +0000

    Improve documentation
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/refactor/logstore/logstore.go
+++ pkg/refactor/logstore/logstore.go
@@ -7,13 +7,15 @@ import (
 
 // LogStore manages storing and querying LogEntries.
 type LogStore interface {
-	// appends a log-entry to the logs.
+    // appends a log entry to the logs. Passed keyAnValues will be associated with the log entry.
+    // Example: store.Append(ctx, time.Time, "message", "key", "value", "other-key", "value")
 	Append(ctx context.Context, timestamp time.Time, msg string, keysAndValues ...interface{}) error
-	// returns a list of log-entries which have matching fields with the provides keysAndValues pairs.
+    // returns a list of log-entries that have matching associated fields with the provided keysAndValues pairs.
+    // Example: store.Get(ctx, "key1", value1, "key2", value2)
 	Get(ctx context.Context, keysAndValues ...interface{}) ([]*LogEntry, error)
 }
 
-// Represents an individual log entry for activity.
+// Represents an individual log entry.
 type LogEntry struct {
 	// the timestamp of the log-entry.
 	T   time.Time
