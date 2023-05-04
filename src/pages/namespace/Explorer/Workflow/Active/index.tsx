04b775a6ce2f796b5b6045203136e3991ef59c2a
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-04 11:39:54 +0200

    add tollbar
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Active/index.tsx
+++ src/pages/namespace/Explorer/Workflow/Active/index.tsx
@@ -1,7 +1,17 @@
-import Badge from "../../../../../design/Badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../../../../../design/Dropdown";
+import { GitBranchPlus, GitMerge, Play, Save, Undo } from "lucide-react";
+
+import Button from "../../../../../design/Button";
 import { Card } from "../../../../../design/Card";
 import Editor from "@monaco-editor/react";
 import { FC } from "react";
+import { RxChevronDown } from "react-icons/rx";
 import { pages } from "../../../../../util/router/pages";
 import { useNodeContent } from "../../../../../api/tree/query/get";
 import { useTheme } from "../../../../../util/store/theme";
@@ -15,18 +25,45 @@ const WorkflowOverviewPage: FC = () => {
 
   return (
     <div className="flex grow flex-col space-y-4 p-4">
-      <Card className="p-4">
-        <Badge>{data?.revision?.hash.slice(0, 8)}</Badge>
+      <Card className="grow p-4">
+        <Editor
+          options={{
+            scrollBeyondLastLine: false,
+            cursorBlinking: "smooth",
+          }}
+          loading=""
+          language="yaml"
+          theme={theme === "dark" ? "vs-dark" : "vs-light"}
+          value={data?.revision?.source && atob(data?.revision?.source)}
+        />
       </Card>
-      <Editor
-        options={{
-          scrollBeyondLastLine: false,
-          cursorBlinking: "smooth",
-        }}
-        language="yaml"
-        theme={theme === "dark" ? "vs-dark" : "vs-light"}
-        value={data?.revision?.source && atob(data?.revision?.source)}
-      />
+      <div className="flex justify-end gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              <GitMerge />
+              Revisions <RxChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>Choose Feature</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <GitBranchPlus className="mr-2 h-4 w-4" /> Make Revision
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Undo className="mr-2 h-4 w-4" /> Revert
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button variant="outline">
+          <Play />
+          Run
+        </Button>
+        <Button variant="outline">
+          <Save />
+          Save
+        </Button>
+      </div>
     </div>
   );
 };
