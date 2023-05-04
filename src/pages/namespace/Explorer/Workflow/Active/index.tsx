33062caa142aa66ac8d211410af02accf4fa4838
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-04 11:12:26 +0200

    minimal editor
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Active/index.tsx
+++ src/pages/namespace/Explorer/Workflow/Active/index.tsx
@@ -1,26 +1,32 @@
 import Badge from "../../../../../design/Badge";
 import { Card } from "../../../../../design/Card";
+import Editor from "@monaco-editor/react";
 import { FC } from "react";
 import { pages } from "../../../../../util/router/pages";
 import { useNodeContent } from "../../../../../api/tree/query/get";
+import { useTheme } from "../../../../../util/store/theme";
 
 const WorkflowOverviewPage: FC = () => {
   const { path } = pages.explorer.useParams();
+  const theme = useTheme();
   const { data } = useNodeContent({
     path,
   });
 
   return (
-    <div className="flex flex-col space-y-4 p-4">
-      <h1>WorkflowOverviewPage</h1>
-      <Card className="flex flex-col space-y-4 p-4">
-        <div>
-          <Badge>{data?.revision?.hash.slice(0, 8)}</Badge>
-        </div>
-        <Card className="p-4">
-          {data?.revision?.source && atob(data?.revision?.source)}
-        </Card>
+    <div className="flex grow flex-col space-y-4 p-4">
+      <Card className="p-4">
+        <Badge>{data?.revision?.hash.slice(0, 8)}</Badge>
       </Card>
+      <Editor
+        options={{
+          scrollBeyondLastLine: false,
+          cursorBlinking: "smooth",
+        }}
+        language="yaml"
+        theme={theme === "dark" ? "vs-dark" : "vs-light"}
+        value={data?.revision?.source && atob(data?.revision?.source)}
+      />
     </div>
   );
 };
