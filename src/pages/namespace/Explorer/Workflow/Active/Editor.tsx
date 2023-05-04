dfe531c59b9d3f0927ee370eae61952a3a7e4803
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-04 18:07:54 +0200

    add driektiv-dark theme (WIP)
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Active/Editor.tsx
+++ src/pages/namespace/Explorer/Workflow/Active/Editor.tsx
@@ -3,21 +3,34 @@ import { FC } from "react";
 import MonacoEditor from "@monaco-editor/react";
 import { useTheme } from "../../../../../util/store/theme";
 
+function setEditorTheme(monaco: any) {
+  monaco.editor.defineTheme("direktiv-dark", {
+    base: "vs-dark",
+    inherit: true,
+    rules: [],
+    colors: {
+      "editor.background": "#000000",
+    },
+  });
+}
+
 const Editor: FC<EditorProps> = ({ ...props }) => {
   const theme = useTheme();
   return (
     <MonacoEditor
+      beforeMount={setEditorTheme}
       options={{
         scrollBeyondLastLine: false,
         cursorBlinking: "smooth",
         wordWrap: true,
+        fontSize: "13px",
         minimap: {
           enabled: false,
         },
       }}
       loading=""
       language="yaml"
-      theme={theme === "dark" ? "vs-dark" : "vs-light"}
+      theme={theme === "dark" ? "direktiv-dark" : "vs-light"}
       {...props}
     />
   );
