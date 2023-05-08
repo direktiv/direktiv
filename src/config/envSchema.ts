1d9972a96d50f55cb671279bd863151350808f69
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2023-05-08 14:22:29 +0200

    Fix VITE_DEV_API_DOMAIN for playwright tests
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- src/config/envSchema.ts
+++ src/config/envSchema.ts
@@ -21,7 +21,6 @@ const envVariablesSchema = z.object({
     .transform((value) => `${value}`.toLocaleLowerCase() === "true"),
   VITE_E2E_UI_HOST: z.string().optional(),
   VITE_E2E_UI_PORT: z.string().optional(),
-  VITE_E2E_API_HOST: z.string().optional(),
 });
 
 export default envVariablesSchema;
