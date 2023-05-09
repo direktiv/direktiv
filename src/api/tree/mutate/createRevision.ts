7e837d94c9cf39b0597915ca990a8d41482903ed
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-09 09:53:49 +0200

    add create revision api
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/api/tree/mutate/createRevision.ts
@@ -0,0 +1,58 @@
+import { WorkflowCreatedSchema } from "../schema";
+import { apiFactory } from "../../utils";
+import { forceLeadingSlash } from "../utils";
+import { useApiKey } from "../../../util/store/apiKey";
+import { useMutation } from "@tanstack/react-query";
+import { useNamespace } from "../../../util/store/namespace";
+import { useToast } from "../../../design/Toast";
+
+const createRevision = apiFactory({
+  pathFn: ({ namespace, path }: { namespace: string; path?: string }) =>
+    `/api/namespaces/${namespace}/tree${forceLeadingSlash(
+      path
+    )}?op=save-workflow&ref=latest`,
+  method: "POST",
+  schema: WorkflowCreatedSchema,
+});
+
+export const useCreateRevision = () => {
+  const apiKey = useApiKey();
+  const namespace = useNamespace();
+  const { toast } = useToast();
+
+  if (!namespace) {
+    throw new Error("namespace is undefined");
+  }
+
+  return useMutation({
+    mutationFn: ({
+      path,
+      fileContent,
+    }: {
+      path: string;
+      fileContent: string;
+    }) =>
+      createRevision({
+        apiKey: apiKey ?? undefined,
+        params: fileContent,
+        pathParams: {
+          namespace: namespace,
+          path,
+        },
+      }),
+    onSuccess(data) {
+      toast({
+        title: "Revision created",
+        description: `Revision ${data.node.name} was created`,
+        variant: "success",
+      });
+    },
+    onError: () => {
+      toast({
+        title: "An error occurred",
+        description: "could not create workflow 😢",
+        variant: "error",
+      });
+    },
+  });
+};
