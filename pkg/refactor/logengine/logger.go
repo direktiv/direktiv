9becdeb85aa281129b08e0dffdc7f700db8a7aae
Alexander Ertli <alexander.ertli@direktiv.io>
2023-05-09 11:09:15 +0000

    Impl loggerw
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/refactor/logengine/logger.go
+++ pkg/refactor/logengine/logger.go
@@ -1,8 +1,62 @@
 package logengine
 
-type logger interface {
-	// logs the msg. Msg can contain string format specifiers.
-	// Values passed via a will be applied to the format specifiers if present.
-	// Tags will be associated with the log-entry in the logs.
-	Log(tags map[string]interface{}, level string, msg string, a ...interface{})
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type LogNotify interface {
+	NotifyLogs(recipientID string, recipientType string)
+}
+
+type loggerw struct {
+	sugar *zap.SugaredLogger
+	store LogStore
+	pub   LogNotify
+}
+
+type Log func(tags map[string]interface{}, level string, msg string, a ...interface{}) error
+
+func Logger(ls LogStore, sug zap.SugaredLogger, pub LogNotify) Log {
+	return loggerw{sugar: &sug, store: ls, pub: pub}.log
+}
+
+func (logger loggerw) log(tags map[string]interface{}, level string, msg string, a ...interface{}) error {
+	msg = fmt.Sprintf(msg, a...)
+
+	if len(tags) == 0 {
+		logger.sugar.Infow(msg)
+	} else {
+		ar := make([]interface{}, len(tags)+len(tags))
+		i := 0
+		for k, v := range tags {
+			ar[i] = k
+			ar[i+1] = v
+			i += 2
+		}
+		switch level {
+		case "info":
+			logger.sugar.Infow(msg, ar...)
+		case "debug":
+			logger.sugar.Debugw(msg, ar...)
+		case "error":
+			logger.sugar.Errorw(msg, ar...)
+		case "panic":
+			logger.sugar.Panicw(msg, ar...)
+		default:
+			logger.sugar.Debugw(msg, ar...) // this should never happen
+		}
+	}
+
+	tags["level"] = level
+	err := logger.store.Append(context.Background(), time.Now(), msg, tags)
+	if err != nil {
+		return err
+	}
+	logger.pub.NotifyLogs(fmt.Sprintf("%s", tags["sender"]), fmt.Sprintf("%s", tags["senderType"]))
+
+	return nil
 }
