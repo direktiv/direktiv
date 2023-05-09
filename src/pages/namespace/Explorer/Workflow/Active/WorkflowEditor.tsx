f65ab279c55540937ac43a24d82c37fcf56f5287
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-09 10:44:02 +0200

    make workflow editor its own component
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/pages/namespace/Explorer/Workflow/Active/WorkflowEditor.tsx
@@ -0,0 +1,137 @@
+import { Bug, GitBranchPlus, GitMerge, Play, Save, Undo } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../../../../design/Dropdown";
+import { FC, useEffect, useState } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../../../../../design/Popover";
+
+import Button from "../../../../../design/Button";
+import { Card } from "../../../../../design/Card";
+import Editor from "../../../../../design/Editor";
+import { RxChevronDown } from "react-icons/rx";
+import moment from "moment";
+import { useCreateRevision } from "../../../../../api/tree/mutate/createRevision";
+import { useNodeContent } from "../../../../../api/tree/query/get";
+import { useTheme } from "../../../../../util/store/theme";
+import { useUpdateWorkflow } from "../../../../../api/tree/mutate/updateWorkflow";
+
+// get type of useNodeContent return value
+type NodeContentType = ReturnType<typeof useNodeContent>["data"];
+
+const WorkflowEditor: FC<{
+  data: NonNullable<NodeContentType>;
+  path: string;
+}> = ({ data, path }) => {
+  const [error, setError] = useState<string | undefined>();
+  const [hasUnsavedChanged, setHasUnsavedChanged] = useState(false);
+  const workflowData = data.revision?.source && atob(data?.revision?.source);
+
+  const { mutate: updateWorkflow, isLoading } = useUpdateWorkflow({
+    onError: (error) => {
+      error && setError(error);
+    },
+  });
+
+  const [value, setValue] = useState<string | undefined>(workflowData);
+  const theme = useTheme();
+
+  const { mutate: createRevision } = useCreateRevision();
+
+  useEffect(() => {
+    setHasUnsavedChanged(workflowData !== value);
+  }, [value, workflowData]);
+
+  const onSave = (toSave: string | undefined) => {
+    if (toSave) {
+      setError(undefined);
+      updateWorkflow({
+        path,
+        fileContent: toSave,
+      });
+    }
+  };
+
+  return (
+    <div className="relative flex grow flex-col space-y-4 p-4">
+      {hasUnsavedChanged ? "unsaved" : "saved"}
+      <Card className="grow p-4">
+        <Editor
+          value={workflowData}
+          onChange={(newData) => {
+            setValue(newData);
+          }}
+          theme={theme ?? undefined}
+          onSave={onSave}
+        />
+      </Card>
+      <div className="flex flex-col items-center justify-end gap-4 sm:flex-row">
+        <div className="grow text-sm text-gray-8 dark:text-gray-dark-8">
+          {/* must use fromNow(true) because otherwise after saving, it sometimes shows Updated in a few seconds */}
+          {data?.revision?.createdAt && (
+            <>Updated {moment(data?.revision?.createdAt).fromNow(true)} ago</>
+          )}
+        </div>
+        {error && (
+          <Popover defaultOpen>
+            <PopoverTrigger asChild>
+              <Button variant="destructive">
+                <Bug />
+                There is one issue
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent asChild>
+              <div className="flex">
+                <div className="grow">{error}</div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        )}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              <GitMerge />
+              Revisions <RxChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuItem
+              onClick={() => {
+                createRevision({
+                  path,
+                });
+              }}
+            >
+              <GitBranchPlus className="mr-2 h-4 w-4" /> Make Revision
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Undo className="mr-2 h-4 w-4" /> Revert
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button variant="outline">
+          <Play />
+          Run
+        </Button>
+        <Button
+          variant="outline"
+          disabled={isLoading}
+          onClick={() => {
+            onSave(value);
+          }}
+        >
+          <Save />
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default WorkflowEditor;
--- src/pages/namespace/Explorer/Workflow/Active/index.tsx
+++ src/pages/namespace/Explorer/Workflow/Active/index.tsx
@@ -1,128 +1,17 @@
-import { Bug, GitBranchPlus, GitMerge, Play, Save, Undo } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../../../../../design/Dropdown";
-import { FC, useState } from "react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "../../../../../design/Popover";
-
-import Button from "../../../../../design/Button";
-import { Card } from "../../../../../design/Card";
-import Editor from "../../../../../design/Editor";
-import { RxChevronDown } from "react-icons/rx";
-import moment from "moment";
+import { FC } from "react";
+import WorkflowEditor from "./WorkflowEditor";
 import { pages } from "../../../../../util/router/pages";
-import { useCreateRevision } from "../../../../../api/tree/mutate/createRevision";
 import { useNodeContent } from "../../../../../api/tree/query/get";
-import { useTheme } from "../../../../../util/store/theme";
-import { useUpdateWorkflow } from "../../../../../api/tree/mutate/updateWorkflow";
 
 const WorkflowOverviewPage: FC = () => {
   const { path } = pages.explorer.useParams();
   const { data } = useNodeContent({ path });
-  const [error, setError] = useState<string | undefined>();
+  if (!data || !path) return null;
 
-  const { mutate: updateWorkflow, isLoading } = useUpdateWorkflow({
-    onError: (error) => {
-      error && setError(error);
-    },
-  });
-
-  const workflowData = data?.revision?.source && atob(data?.revision?.source);
-  const [value, setValue] = useState<string | undefined>(workflowData);
-  const theme = useTheme();
-
-  const { mutate: createRevision } = useCreateRevision();
-
-  if (!path) return null;
-
-  const onSave = (toSave: string | undefined) => {
-    if (toSave) {
-      setError(undefined);
-      updateWorkflow({
-        path,
-        fileContent: toSave,
-      });
-    }
-  };
-
-  return (
-    <div className="relative flex grow flex-col space-y-4 p-4">
-      <Card className="grow p-4">
-        <Editor
-          value={workflowData}
-          onChange={setValue}
-          theme={theme ?? undefined}
-          onSave={onSave}
-        />
-      </Card>
-      <div className="flex flex-col items-center justify-end gap-4 sm:flex-row">
-        <div className="grow text-sm text-gray-8 dark:text-gray-dark-8">
-          {/* must use fromNow(true) because otherwise after saving, it sometimes shows Updated in a few seconds */}
-          {data?.revision?.createdAt && (
-            <>Updated {moment(data?.revision?.createdAt).fromNow(true)} ago</>
-          )}
-        </div>
-        {error && (
-          <Popover defaultOpen>
-            <PopoverTrigger asChild>
-              <Button variant="destructive">
-                <Bug />
-                There is one issue
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent asChild>
-              <div className="flex">
-                <div className="grow">{error}</div>
-              </div>
-            </PopoverContent>
-          </Popover>
-        )}
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline">
-              <GitMerge />
-              Revisions <RxChevronDown />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuItem
-              onClick={() => {
-                createRevision({
-                  path,
-                });
-              }}
-            >
-              <GitBranchPlus className="mr-2 h-4 w-4" /> Make Revision
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Undo className="mr-2 h-4 w-4" /> Revert
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <Button variant="outline">
-          <Play />
-          Run
-        </Button>
-        <Button
-          variant="outline"
-          disabled={isLoading}
-          onClick={() => {
-            onSave(value);
-          }}
-        >
-          <Save />
-          Save
-        </Button>
-      </div>
-    </div>
-  );
+  // Editor is moved into a separate component to give us a state where
+  // data and path is alwawys defined. This makes handling side effects
+  // much easier.
+  return <WorkflowEditor data={data} path={path} />;
 };
 
 export default WorkflowOverviewPage;
