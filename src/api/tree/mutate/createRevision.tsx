8729a212a6797517587740a37ed8958e747e9df8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-09 12:13:47 +0200

    add notification with link to revision
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/tree/mutate/createRevision.tsx
+++ src/api/tree/mutate/createRevision.tsx
@@ -1,10 +1,13 @@
+import { ToastAction, useToast } from "../../../design/Toast";
+
 import { WorkflowCreatedSchema } from "../schema";
 import { apiFactory } from "../../utils";
 import { forceLeadingSlash } from "../utils";
+import { pages } from "../../../util/router/pages";
 import { useApiKey } from "../../../util/store/apiKey";
 import { useMutation } from "@tanstack/react-query";
 import { useNamespace } from "../../../util/store/namespace";
-import { useToast } from "../../../design/Toast";
+import { useNavigate } from "react-router-dom";
 
 const createRevision = apiFactory({
   pathFn: ({ namespace, path }: { namespace: string; path: string }) =>
@@ -19,6 +22,7 @@ export const useCreateRevision = () => {
   const apiKey = useApiKey();
   const namespace = useNamespace();
   const { toast } = useToast();
+  const navigate = useNavigate();
 
   if (!namespace) {
     throw new Error("namespace is undefined");
@@ -34,11 +38,28 @@ export const useCreateRevision = () => {
           path,
         },
       }),
-    onSuccess(data) {
+    onSuccess: (data, variables) => {
       toast({
         title: "Revision created",
-        description: `Revision ${data.revision.name} was created`,
+        description: `Revision ${data.revision.name.slice(0, 8)} was created`,
         variant: "success",
+        action: (
+          <ToastAction
+            altText="Open Revision"
+            onClick={() => {
+              navigate(
+                pages.explorer.createHref({
+                  namespace,
+                  path: variables.path,
+                  subpage: "workflow-revisions",
+                  revision: data.revision.name,
+                })
+              );
+            }}
+          >
+            Open Revision
+          </ToastAction>
+        ),
       });
     },
     onError: () => {
