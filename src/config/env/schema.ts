3f72eb8e0f1a0e635c427a2266ed82d5cd472b56
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-10 15:41:47 +0200

    move env to correct schema file and delete the dublicate
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/config/env/schema.ts
+++ src/config/env/schema.ts
@@ -19,4 +19,6 @@ export const envVariablesSchema = z.object({
     .string()
     .optional()
     .transform((value) => `${value}`.toLocaleLowerCase() === "true"),
+  VITE_E2E_UI_HOST: z.string().optional(),
+  VITE_E2E_UI_PORT: z.string().optional(),
 });
