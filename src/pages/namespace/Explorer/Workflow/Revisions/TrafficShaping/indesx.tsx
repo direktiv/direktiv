223c5b7ad49b86793b77f9c4cc0416f27f620a43
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-16 17:39:12 +0200

    use add all props to RevisionSelector
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Revisions/TrafficShaping/indesx.tsx
+++ src/pages/namespace/Explorer/Workflow/Revisions/TrafficShaping/indesx.tsx
@@ -3,21 +3,48 @@ import { FC } from "react";
 import { Network } from "lucide-react";
 import RevisionSelector from "./RevisionSelector";
 import { Slider } from "../../../../../../design/Slider";
+import { pages } from "../../../../../../util/router/pages";
+import { useNodeRevisions } from "../../../../../../api/tree/query/revisions";
+import { useNodeTags } from "../../../../../../api/tree/query/tags";
+import { useTranslation } from "react-i18next";
 
-const TrafficShaping: FC = () => (
-  <>
-    <h3 className="flex items-center gap-x-2 font-bold text-gray-10 dark:text-gray-dark-10">
-      <Network />
-      Traffic Shaping
-    </h3>
-    <Card className="flex gap-x-3 p-4">
-      <RevisionSelector className="grow" />
-      <RevisionSelector className="grow" />
-      <div className="grow p-4">
-        <Slider />
-      </div>
-    </Card>
-  </>
-);
+const TrafficShaping: FC = () => {
+  const { t } = useTranslation();
+  const { path } = pages.explorer.useParams();
+  const { data: revisions, isLoading: revisionsLoading } = useNodeRevisions({
+    path,
+  });
+  const { data: tags, isLoading: tagsLoading } = useNodeTags({ path });
+
+  const isLoading = tagsLoading || revisionsLoading;
+
+  return (
+    <>
+      <h3 className="flex items-center gap-x-2 font-bold text-gray-10 dark:text-gray-dark-10">
+        <Network />
+        {t("pages.explorer.tree.workflow.revisions.trafficShaping.title")}
+      </h3>
+      <Card className="flex gap-x-3 p-4">
+        <RevisionSelector
+          className="grow"
+          tags={tags?.results ?? []}
+          revisions={revisions?.results ?? []}
+          isLoading={isLoading}
+          onSelect={(rev) => console.log(rev)}
+        />
+        <RevisionSelector
+          className="grow"
+          tags={tags?.results ?? []}
+          revisions={revisions?.results ?? []}
+          isLoading={isLoading}
+          onSelect={(rev) => console.log(rev)}
+        />
+        <div className="grow p-4">
+          <Slider />
+        </div>
+      </Card>
+    </>
+  );
+};
 
 export default TrafficShaping;
