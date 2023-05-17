51c9c649f4b1202307ffb7efdc74a474fe3eda85
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-17 13:49:44 +0200

    minor refactoring
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Revisions/TrafficShaping/index.tsx
+++ src/pages/namespace/Explorer/Workflow/Revisions/TrafficShaping/index.tsx
@@ -13,37 +13,51 @@ import { useRouter } from "~/api/tree/query/router";
 import { useSetRouter } from "~/api/tree/mutate/setRouter";
 import { useTranslation } from "react-i18next";
 
+const useRevisionsAndTags = ({ path }: { path?: string }) => {
+  const {
+    data: tags,
+    isLoading: isLoadingTags,
+    isFetched: isFetchedTags,
+  } = useNodeTags({ path });
+  const {
+    data: revisions,
+    isLoading: isLoadingRevisions,
+    isFetched: isFetchedRevisions,
+  } = useNodeRevisions({
+    path,
+  });
+  const isLoading = isLoadingTags || isLoadingRevisions;
+  const isFetched = isFetchedTags && isFetchedRevisions;
+  return { tags, revisions, isLoading, isFetched };
+};
+
 const TrafficShaping: FC = () => {
   const { t } = useTranslation();
   const { path } = pages.explorer.useParams();
-  const { data: revisions, isLoading: revisionsLoading } = useNodeRevisions({
+  const { data: router } = useRouter({ path });
+  const { tags, revisions, isLoading, isFetched } = useRevisionsAndTags({
     path,
   });
-  const { data: router } = useRouter({ path });
-  const { data: tags, isLoading: tagsLoading } = useNodeTags({ path });
+
+  // component state
+  const [routeA, setRouteA] = useState("");
+  const [routeB, setRouteB] = useState("");
+  const [weight, setWeight] = useState(0);
+
+  // server state
   const { mutate: setRouter, isLoading: isLoadingMutation } = useSetRouter();
-  const isLoadingData = tagsLoading || revisionsLoading;
-
-  const [a, setA] = useState("");
-  const [b, setB] = useState("");
-  const [weight, setWeight] = useState(50);
-
-  const aServer = router?.routes?.[0]?.ref;
-  const bServer = router?.routes?.[0]?.ref;
+  const routeAServer = router?.routes?.[0]?.ref;
+  const routeBServer = router?.routes?.[0]?.ref;
   const weightServer = router?.routes?.[0]?.weight;
 
-  useEffect(() => {
-    if (aServer) setA(aServer);
-  }, [aServer]);
-
-  useEffect(() => {
-    if (bServer) setB(bServer);
-  }, [bServer]);
-
+  // synch component state with server state
   useEffect(() => {
     if (weightServer) setWeight(weightServer);
-  }, [weightServer]);
+    if (routeAServer) setRouteA(routeAServer);
+    if (routeBServer) setRouteB(routeBServer);
+  }, [weightServer, routeAServer, routeBServer]);
 
+  if (!isFetched) return null; // wait for data to to avoid layout shift
   if (!path) return null;
 
   return (
@@ -53,20 +67,26 @@ const TrafficShaping: FC = () => {
         {t("pages.explorer.tree.workflow.revisions.trafficShaping.title")}
       </h3>
       <Card className="p-4">
+        <div className="text-gray-12 dark:text-gray-dark-12">
+          <h1>No traffic distribution for this workflow is configured. </h1>
+          You can choose two revisions and set a weight between 0 and 100 to
+          distribute the traffic between them.
+        </div>
+        <Separator className="my-4" />
         <div className="flex flex-col gap-3 sm:flex-row">
           <RevisionSelector
             className="flex w-full"
             tags={tags?.results ?? []}
             revisions={revisions?.results ?? []}
-            isLoading={isLoadingData}
-            onSelect={setA}
+            isLoading={isLoading}
+            onSelect={setRouteA}
           />
           <RevisionSelector
             className="flex w-full"
             tags={tags?.results ?? []}
             revisions={revisions?.results ?? []}
-            isLoading={isLoadingData}
-            onSelect={setB}
+            isLoading={isLoading}
+            onSelect={setRouteB}
           />
           <div className="flex w-full">
             <Slider
@@ -82,16 +102,18 @@ const TrafficShaping: FC = () => {
           <div>
             <Button
               variant="primary"
-              disabled={isLoadingMutation || a.length === 0 || b.length === 0}
+              disabled={
+                isLoadingMutation || routeA.length === 0 || routeB.length === 0
+              }
               onClick={() => {
                 setRouter({
                   path,
                   routeA: {
-                    ref: a,
+                    ref: routeA,
                     weight,
                   },
                   routeB: {
-                    ref: b,
+                    ref: routeB,
                     weight: 100 - weight,
                   },
                 });
@@ -100,13 +122,6 @@ const TrafficShaping: FC = () => {
               Save
             </Button>
           </div>
-        </div>
-        <Separator className="my-4" />
-        <div>
-          {aServer} - {bServer} - {weightServer ?? "none"}
-        </div>
-        <div>
-          {a} - {b} - {weight ?? "none"}
         </div>
       </Card>
     </>
