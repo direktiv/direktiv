3baea4c5542df18ddb55ffc82ad1552b507c7ed1
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-19 17:27:37 +0200

    reload the page
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- e2e/explorer/workflow/revisions/trafficShaping.spec.ts
+++ e2e/explorer/workflow/revisions/trafficShaping.spec.ts
@@ -108,11 +108,14 @@ test("it is possible to configure traffic shaping", async ({ page }) => {
     revisionsReponse: [firstRevision, secondRevision],
   } = await createWorkflowWithThreeRevisions(namespace, name);
 
+  const firstRevisionName = firstRevision.revision.name;
+  const secondRevisionName = secondRevision.revision.name;
+
   await page.goto(`/${namespace}/explorer/workflow/revisions/${name}`);
   await page.getByTestId(`route-a-selector`).click();
-  await page.getByTestId(firstRevision.revision.name).click();
+  await page.getByTestId(firstRevisionName).click();
   await page.getByTestId(`route-b-selector`).click();
-  await page.getByTestId(secondRevision.revision.name).click();
+  await page.getByTestId(secondRevisionName).click();
 
   // move slider
   const slider = await page
@@ -123,19 +126,38 @@ test("it is possible to configure traffic shaping", async ({ page }) => {
     targetPosition: { x: 50, y: 0 },
   });
   const sliderValue = await slider.getAttribute("aria-valuenow");
+  const secondShaping = 100 - parseInt(sliderValue ?? "");
   expect(sliderValue).not.toBe("0");
 
   await expect(
     page.getByTestId("traffic-shaping-note"),
     "there is a hint that describes the traffic shaping"
   ).toHaveText(
-    `The traffic will be split between ${firstRevision.revision.name} and ${
-      secondRevision.revision.name
-    } with a ratio of ${sliderValue} to ${100 - parseInt(sliderValue ?? "")} %`
+    `The traffic will be split between ${firstRevisionName} and ${secondRevisionName} with a ratio of ${sliderValue} to ${secondShaping} %`
   );
 
   // save
   await page.getByTestId("traffic-shaping-save-btn").click();
 
-  // TODO: check for toast message
+  // reload page
+  await page.reload();
+
+  // TODO: waiting for DIR-576 to get fixed
+  // since returned routes are random, we serialize the data in aphabetical order
+  // this might lead to swapped routes in the UI after a reload, but this will be
+  // constistent from now on (otherwise it would always be random)
+  const isInAlphabeticalOrder =
+    firstRevisionName.localeCompare(secondRevisionName) === -1;
+
+  await expect(
+    page.getByTestId("traffic-shaping-note"),
+    "there is a hint that describes the traffic shaping"
+  ).toHaveText(
+    isInAlphabeticalOrder
+      ? `The traffic will be split between ${firstRevisionName} and ${secondRevisionName} with a ratio of ${sliderValue} to ${secondShaping} %`
+      : `The traffic will be split between ${secondRevisionName} and ${firstRevisionName} with a ratio of ${secondShaping} to ${sliderValue} %`
+  );
+
+  // reload page
+  // check if the shaping is displayey in the list
 });
