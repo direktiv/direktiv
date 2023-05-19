7c84536eee2c9646c95f597b8bfbaa999d73345e
Alexander Ertli <alexander.ertli@direktiv.io>
2023-05-19 07:27:56 +0000

    Remove mock from test-case
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/refactor/logengine/logger_test.go
+++ pkg/refactor/logengine/logger_test.go
@@ -2,57 +2,27 @@ package logengine_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
-	"time"
 
+	"github.com/direktiv/direktiv/pkg/refactor/datastore/sql"
 	"github.com/direktiv/direktiv/pkg/refactor/logengine"
+	"github.com/direktiv/direktiv/pkg/refactor/utils"
 )
 
-var _ logengine.LogStore = &mockstore{}
-
-type mockstore struct {
-	logs map[string]*logengine.LogEntry
-}
-
-func (m *mockstore) Append(ctx context.Context, timestamp time.Time, level string, msg string, keysAndValues map[string]interface{}) error {
-	_ = ctx
-	_ = level
-	_ = timestamp
-	if _, ok := keysAndValues["some_DB_Col"]; !ok {
-		return fmt.Errorf("provide some_DB_Col")
-	}
-	if len(m.logs) == 0 {
-		m.logs = make(map[string]*logengine.LogEntry)
-	}
-	m.logs[fmt.Sprintf("%s", keysAndValues["some_DB_Col"])] = &logengine.LogEntry{T: time.Now(), Msg: msg, Fields: keysAndValues}
-
-	return nil
-}
-
-func (m *mockstore) Get(ctx context.Context, keysAndValues map[string]interface{}, limit int, offset int) ([]*logengine.LogEntry, error) {
-	_ = ctx
-	_ = limit
-	_ = offset
-	if len(m.logs) == 0 {
-		m.logs = make(map[string]*logengine.LogEntry)
-	}
-	if _, ok := keysAndValues["some_DB_Col"]; !ok {
-		return nil, fmt.Errorf("provide some_DB_Col")
-	}
-	res := make([]*logengine.LogEntry, 0)
-	res = append(res, m.logs[fmt.Sprintf("%s", keysAndValues["some_DB_Col"])])
-
-	return res, nil
-}
-
 func Test_Log(t *testing.T) {
-	ds := logengine.DataStoreBetterLogger{Store: &mockstore{}, LogError: func(template string, args ...interface{}) { t.Errorf(template, args...) }}
+	db, err := utils.NewMockGorm()
+	if err != nil {
+		t.Fatalf("unepxected NewMockGorm() error = %v", err)
+	}
+	sq := sql.NewSQLStore(db, "some_secret_key_")
+	logstore := sq.Logs()
+
+	ds := logengine.DataStoreBetterLogger{Store: logstore, LogError: func(template string, args ...interface{}) { t.Errorf(template, args...) }}
 	tags := make(map[string]interface{})
-	tags["some_DB_Col"] = "value1"
-	ds.Log(tags, "error", "test")
+	tags["workflow_id"] = "some-id"
+	ds.Log(tags, "error", "test %s", "msg")
 	keysNValues := make(map[string]interface{})
-	keysNValues["some_DB_Col"] = "value1"
+	keysNValues["workflow_id"] = "some-id"
 	got, err := ds.Store.Get(context.Background(), keysNValues, -1, -1)
 	if err != nil {
 		t.Error(err)
@@ -61,7 +31,7 @@ func Test_Log(t *testing.T) {
 		t.Error("got no results")
 		t.Fail()
 	}
-	if got[0].Msg != "test" {
+	if got[0].Msg != "test msg" {
 		t.Fail()
 	}
 }
