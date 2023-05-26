237883e9df0eadb1d95762e9684e647e7bfa5282
porleaaron <porleaaron28@gmail.com>
2023-05-27 02:17:51 +0800

    prettier fix

--- src/design/Input/index.tsx
+++ src/design/Input/index.tsx
@@ -14,7 +14,7 @@ const Input = React.forwardRef<
         "flex h-9 w-full rounded-md border bg-transparent py-2 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
         "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1",
-        className,
+        className
       )
     )}
     {...props}
--- src/design/JSONschemaForm/index.tsx
+++ src/design/JSONschemaForm/index.tsx
@@ -1,99 +1,99 @@
-import Form, { FieldProps, Widget } from "@rjsf/core";
-import { RJSFSchema, RegistryFieldsType, RegistryWidgetsType, UiSchema, WidgetProps } from '@rjsf/utils';
+import {
+  RJSFSchema,
+  WidgetProps,
+} from "@rjsf/utils";
 
 import Button from "../Button";
+import Form from "@rjsf/core";
 import Input from "../Input";
 import React from "react";
 
 const FormInput: React.FunctionComponent<WidgetProps> = (props) => {
-    console.log({ props })
-    const [val, setVal] = React.useState<string>("")
-    return <Input
-        className="w-full mb-2 mt-1 form-control"
-        type={props.options.inputType}
-        required={props.required}
-        id={props.id}
-        value={val}
-        onChange={(e) => {
-            props.onChange(e.target.value)
-            console.log({ e })
-            setVal(e.target.value)
-        }}
-
-    />;
-}
+  const [val, setVal] = React.useState<string>("");
+  return (
+    <Input
+      className="form-control mb-2 mt-1 w-full"
+      type={props.options.inputType}
+      required={props.required}
+      id={props.id}
+      value={val}
+      onChange={(e) => {
+        props.onChange(e.target.value);
+        setVal(e.target.value);
+      }}
+    />
+  );
+};
 
 export const JSONschemaForm = () => {
-    const uiSchema = {
-        password: {
-            'ui:widget': FormInput,
-            'ui:options': {
-                inputType: 'password',
-            },
-        },
-        firstName: {
-            'ui:widget': FormInput,
-            'ui:options': {
-                inputType: 'text'
-            },
-        },
-        lastName: {
-            'ui:widget': FormInput,
-            'ui:options': {
-                inputType: 'text',
-            },
-        },
-        bio: {
-            'ui:widget': FormInput,
-            'ui:options': {
-                inputType: 'text',
-            },
-        },
-        age: {
-            'ui:widget': FormInput,
-            'ui:options': {
-                inputType: 'number',
-            },
-        },
-        "ui:submitButtonOptions": {
-            norender: true
-        }
-    };
-    const schema: RJSFSchema = {
-        title: "A registration form",
-        type: "object",
-        required: ["firstName", "lastName"],
-        properties: {
-            password: {
-                type: "string",
-                title: "Password",
-            },
-            lastName: {
-                type: "string",
-                title: "Last name",
-            },
-            bio: {
-                type: "string",
-                title: "Bio",
-            },
-            firstName: {
-                type: "string",
-                title: "First name",
-            },
-            age: {
-                type: "integer",
-                title: "Age",
-            },
-        },
-    }
-    return (
-        // <div className='[&>form>div>fieldset>legend]:text-primary-500'>
-        <Form
-            uiSchema={uiSchema}
-            schema={schema}
-        >
-            <Button type='submit'>Submit</Button>
-        </Form>
-        // </div>
-    );
-}
+  const uiSchema = {
+    password: {
+      "ui:widget": FormInput,
+      "ui:options": {
+        inputType: "password",
+      },
+    },
+    firstName: {
+      "ui:widget": FormInput,
+      "ui:options": {
+        inputType: "text",
+      },
+    },
+    lastName: {
+      "ui:widget": FormInput,
+      "ui:options": {
+        inputType: "text",
+      },
+    },
+    bio: {
+      "ui:widget": FormInput,
+      "ui:options": {
+        inputType: "text",
+      },
+    },
+    age: {
+      "ui:widget": FormInput,
+      "ui:options": {
+        inputType: "number",
+      },
+    },
+    "ui:submitButtonOptions": {
+      norender: true,
+    },
+  };
+  const schema: RJSFSchema = {
+    title: "A registration form",
+    type: "object",
+    required: ["firstName", "lastName"],
+    properties: {
+      password: {
+        type: "string",
+        title: "Password",
+      },
+      lastName: {
+        type: "string",
+        title: "Last name",
+      },
+      bio: {
+        type: "string",
+        title: "Bio",
+      },
+      firstName: {
+        type: "string",
+        title: "First name",
+      },
+      age: {
+        type: "integer",
+        title: "Age",
+      },
+    },
+  };
+
+  return (
+    // <div className='[&>form>div>fieldset>legend]:text-primary-500'>
+    <Form uiSchema={uiSchema} schema={schema}>
+      <Button type="submit">Submit</Button>
+    </Form>
+    // </div>
+  );
+};
