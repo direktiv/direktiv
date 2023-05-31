49bf46e181b9ed3bf7b1c5eb2c3664ea87cc9a34
Stefan Kracht <stefan.kracht@direktiv.io>
2023-05-31 15:44:56 +0200

    fix typo

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -45,7 +45,7 @@ export const apiFactory =
     // the zod schema that the response should be parsed against. This will give
     // us not only the typesafety of the response, it also validates the response
     // at runtime. Runtime validation is important to catch unexpected responses
-    // fromt the api very early in the application lifecycle and give us confidence
+    // from the api very early in the application lifecycle and give us confidence
     // about the Typescript types. It comes with the downside that the app is more
     // likely to show errors to the user instead of trying to handle them (which
     // does not scale very well when the complexity of an app grows and leads to
