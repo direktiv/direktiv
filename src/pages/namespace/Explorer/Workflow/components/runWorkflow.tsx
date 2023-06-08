326d40960c1d8c2bfe29eafeb66b289387da9b06
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-08 14:53:37 +0200

    add editor to run workflow
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/components/runWorkflow.tsx
+++ src/pages/namespace/Explorer/Workflow/components/runWorkflow.tsx
@@ -7,9 +7,12 @@ import {
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import Button from "~/design/Button";
-import Input from "~/design/Input";
-import { Tag } from "lucide-react";
+import { Card } from "~/design/Card";
+import Editor from "~/design/Editor";
+import { Play } from "lucide-react";
 import { useCreateTag } from "~/api/tree/mutate/createTag";
+import { useState } from "react";
+import { useTheme } from "~/util/store/theme";
 import { useTranslation } from "react-i18next";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -20,13 +23,15 @@ type FormInput = {
 
 const RunWorkflow = ({ path, close }: { path: string; close: () => void }) => {
   const { t } = useTranslation();
+  const theme = useTheme();
   const {
-    register,
     handleSubmit,
     formState: { isDirty, isValid, isSubmitted },
   } = useForm<FormInput>({
     resolver: zodResolver(z.object({})),
   });
+
+  const [workflowData, setWorkflowData] = useState("{\n\n}");
 
   // TODO: replace useCreateTag with useRunWorkflow
   const { mutate: runWorkflow, isLoading } = useCreateTag({
@@ -48,21 +53,29 @@ const RunWorkflow = ({ path, close }: { path: string; close: () => void }) => {
     <>
       <DialogHeader>
         <DialogTitle>
-          <Tag /> {t("pages.explorer.tree.workflow.revisions.tag.titleTag")}
+          <Play /> {t("pages.explorer.tree.workflow.runWorkflow.title")}
         </DialogTitle>
       </DialogHeader>
       <div className="my-3 flex flex-col gap-y-5">
         <form id={formId} onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            {...register("payload")}
-            data-testid="dialog-create-tag-input-name"
-          />
+          <Card className="h-96 w-full p-4" noShadow>
+            <Editor
+              value={workflowData}
+              onChange={(newData) => {
+                if (newData) {
+                  setWorkflowData(newData);
+                }
+              }}
+              language="json"
+              theme={theme ?? undefined}
+            />
+          </Card>
         </form>
       </div>
       <DialogFooter>
         <DialogClose asChild>
           <Button variant="ghost">
-            {t("pages.explorer.tree.workflow.revisions.tag.cancelBtn")}
+            {t("pages.explorer.tree.workflow.runWorkflow.cancelBtn")}
           </Button>
         </DialogClose>
         <Button
@@ -72,8 +85,8 @@ const RunWorkflow = ({ path, close }: { path: string; close: () => void }) => {
           form={formId}
           data-testid="dialog-create-tag-btn-submit"
         >
-          {!isLoading && <Tag />}
-          {t("pages.explorer.tree.workflow.revisions.tag.createBtn")}
+          {!isLoading && <Play />}
+          {t("pages.explorer.tree.workflow.runWorkflow.runBtn")}
         </Button>
       </DialogFooter>
     </>
