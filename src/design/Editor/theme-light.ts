e0dc7cf29f72b9d1d2a412861642f2189085e818
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-13 15:34:30 +0200

    move themes into its own files
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/design/Editor/theme-light.ts
@@ -0,0 +1,102 @@
+// for reference:
+// https://github.com/Microsoft/vscode/blob/913e891c34f8b4fe2c0767ec9f8bfd3b9dbe30d9/src/vs/editor/standalone/common/themes.ts#L13
+export default {
+  base: "vs",
+  inherit: true,
+  rules: [
+    {
+      foreground: "C7C7C7", // gray.gray8
+      fontStyle: "italic",
+      token: "comment",
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "number",
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "string.yaml",
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "type",
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "string.key.json", // JSON Key
+    },
+    {
+      foreground: "5364FF",
+      token: "string.value.json", // JSON Value
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "tag", // HTML Tag name
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "delimiter.html", // HTML Tag <>
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "metatag.html", // HTML Meta tag
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "metatag.content.html", // HTML Meta tag content
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "delimiter", // HTML Meta tag content
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "attribute.name", // HTML Attribute Name
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "attribute.value.html", // HTML Attribute Name
+    },
+    {
+      foreground: "30A46C", // success-9
+      token: "comment",
+    },
+    {
+      foreground: "30A46C", // success-9
+      token: "attribute.value",
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "attribute.value.number", // html attribute value number, e.g. [5]px
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "attribute.value.unit", // html attribute value unit, e.g. 5[px]
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "string", // css string value: e.g. font-family: "Segoe UI","HelveticaNeue-Light",
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "metatag", // metatag in Shell script e.g. #!/bin/bash
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "keyword", // keyword in Shell script
+    },
+    {
+      foreground: "858585", // gray.gray10
+      token: "variable.predefined", // variable defined in Shell script
+    },
+    {
+      foreground: "5364FF", // primary.500
+      token: "variable", // Shell script variable
+    },
+  ],
+  colors: {
+    "editor.foreground": "#858585", // added for HTML Tag Content
+    "editor.background": "#ffffff",
+    "editor.selectionBackground": "#00000012", // blackA.blackA4
+  },
+} as const;
