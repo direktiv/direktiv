a35e38e315708decede1950fa5637685970bcb2e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-13 11:13:03 +0200

    change progress bar bg color
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Progress/index.tsx
+++ src/design/Progress/index.tsx
@@ -16,7 +16,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary-500 transition-all dark:bg-primary-400"
+      className="h-full w-full flex-1 bg-gray-12 transition-all dark:bg-gray-dark-12"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
