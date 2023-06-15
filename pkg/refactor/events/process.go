a9c2404e4ea9d8aaca473d71fa8e90d101e4e00e
Alexander Ertli <alexander.ertli@direktiv.io>
2023-06-15 11:34:24 +0000

    Add event processing interface
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/refactor/events/process.go
+++ pkg/refactor/events/process.go
@@ -9,10 +9,16 @@ import (
 	"github.com/google/uuid"
 )
 
-type (
-	EventHandler func(ctx context.Context, events ...*Event)
-	//	ChainedEventHandlers []EventHandler
+type EventProcessing interface {
+	ProcessEvents(
+		ctx context.Context,
+		namespace uuid.UUID,
+		cloudevents []cloudevents.Event,
+	)
+}
 
+type (
+	EventHandler     func(ctx context.Context, events ...*Event)
 	WorkflowStart    func(workflowID uuid.UUID, events ...*cloudevents.Event)
 	WakeEventsWaiter func(instanceID uuid.UUID, step int, events []*cloudevents.Event)
 )
@@ -20,47 +26,61 @@ type (
 type EventEngine struct {
 	WorkflowStart       WorkflowStart
 	WakeInstance        WakeEventsWaiter
-	UpdateListeners     func(ctx context.Context, listener []*EventListener) []error
 	GetListenersByTopic func(context.Context, string) ([]*EventListener, error)
+	UpdateListeners     func(ctx context.Context, listener []*EventListener) []error
 }
 
-func process(ctx context.Context,
-	namespace uuid.UUID,
-	cloudevents []cloudevents.Event, h []EventHandler,
-) {
-	events := make([]*Event, 0, len(cloudevents))
-	for _, e := range cloudevents {
-		events = append(events, &Event{
-			Namespace:  namespace,
-			ReceivedAt: time.Now(),
-			Event:      &e,
-		})
-	}
-	for _, eh := range h {
-		eh(ctx, events...)
-	}
-}
-
-func (ee EventEngine) GetEventHandlers(ctx context.Context,
+func (ee EventEngine) ProcessEvents(
+	ctx context.Context,
 	namespace uuid.UUID,
 	cloudevents []cloudevents.Event,
-) ([]EventHandler, error) {
-	handlers := make([]EventHandler, 0)
-	for _, cloudevent := range cloudevents {
+) {
+	topics := ee.GetTopics(ctx, namespace, cloudevents)
+	listeners, _ := ee.GetListeners(ctx, topics...)
+	// TODO log err
+	h, _ := ee.GetEventHandlers(ctx, listeners)
+	// TODO log errors
+	ee.HandleEvents(ctx, namespace, cloudevents, h)
+	ee.UsePostProcessingEvents(ctx, listeners)
+}
 
-		topic := namespace.String() + "-" + cloudevent.Type()
+func (ee EventEngine) GetListeners(ctx context.Context, topics ...string) ([]*EventListener, error) {
+	res := make([]*EventListener, 0)
+
+	for _, topic := range topics {
 		listeners, err := ee.GetListenersByTopic(ctx, topic)
 		if err != nil {
 			return nil, err
 		}
-		for _, l := range listeners {
-			handlers = append(handlers, ee.createEventHandler(l))
-		}
+		res = append(res, listeners...)
+	}
+	return res, nil
+}
+
+func (EventEngine) GetTopics(ctx context.Context, namespace uuid.UUID, cloudevents []cloudevents.Event) []string {
+	topics := make(map[string]string)
+	for _, cloudevent := range cloudevents {
+		topic := namespace.String() + "-" + cloudevent.Type()
+		topics[topic] = ""
+	}
+	topicls := make([]string, 0, len(topics))
+	for topic := range topics {
+		topicls = append(topicls, topic)
+	}
+	return topicls
+}
+
+func (ee EventEngine) GetEventHandlers(ctx context.Context,
+	listeners []*EventListener,
+) ([]EventHandler, error) {
+	handlers := make([]EventHandler, 0, len(listeners))
+	for _, l := range listeners {
+		handlers = append(handlers, ee.CreateEventHandler(l))
 	}
 	return handlers, nil
 }
 
-func (ee EventEngine) createEventHandler(l *EventListener) EventHandler {
+func (ee EventEngine) CreateEventHandler(l *EventListener) EventHandler {
 	if l.Deleted {
 		return func(ctx context.Context, events ...*Event) {}
 	}
@@ -312,10 +332,35 @@ func (ee EventEngine) createEventHandler(l *EventListener) EventHandler {
 	}
 }
 
-func (ee EventEngine) UseMePostProcessingAEvent(ctx context.Context, listeners []*EventListener) error {
+func (ee EventEngine) UsePostProcessingEvents(ctx context.Context,
+	listeners []*EventListener,
+) error {
+	errs := ee.UpdateListeners(ctx, listeners)
+	for _, err := range errs {
+		if err != nil {
+			return err
+		}
+	}
 	return fmt.Errorf("unimplemented")
 }
 
 func EventPassedGatekeeper(globPatterns []string, event cloudevents.Event) bool {
 	return true // todo
+}
+
+func (EventEngine) HandleEvents(ctx context.Context,
+	namespace uuid.UUID,
+	cloudevents []cloudevents.Event, h []EventHandler,
+) {
+	events := make([]*Event, 0, len(cloudevents))
+	for _, e := range cloudevents {
+		events = append(events, &Event{
+			Namespace:  namespace,
+			ReceivedAt: time.Now(),
+			Event:      &e,
+		})
+	}
+	for _, eh := range h {
+		eh(ctx, events...)
+	}
 }
