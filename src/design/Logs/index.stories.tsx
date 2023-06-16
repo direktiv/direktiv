81dd8f296993313c0ec2338b239f5593c0fbed6c
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-16 14:53:34 +0200

    add button to toggle line wrap
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Logs/index.stories.tsx
+++ src/design/Logs/index.stories.tsx
@@ -1,8 +1,10 @@
 import { LogEntry, Logs } from "./index";
 import type { Meta, StoryObj } from "@storybook/react";
+import Button from "../Button";
 import { Card } from "../Card";
 import Editor from "../Editor";
 import { YamlSample } from "../Editor/languageSamples";
+import { useState } from "react";
 
 const meta = {
   title: "Components/Logs",
@@ -74,35 +76,43 @@ export const NoWrapLog = () => (
   </Card>
 );
 
-export const EditorVSLogsFontCompare = () => (
-  <div className="flex flex-row gap-5">
-    <Card className="h-[500px] flex-1">
-      <Logs linewrap>
-        <LogEntry time="12:34:23">
-          Preparing workflow triggered by api.
-        </LogEntry>
-        <LogEntry time="12:34:23">Starting workflow demo.yml.</LogEntry>
-        <LogEntry time="12:34:23">
-          Running state logic (step:1) -- helloworld
-        </LogEntry>
-        <LogEntry time="12:34:23">Transforming state data.</LogEntry>
-        <LogEntry time="12:34:23" variant="warning">
-          Warning: this is a very long line with a warning. this is a very long
-          line with a warning. this is a very long line with a warning. this is
-          a very long line with a warning. this is a very long line with a
-          warning. this is a very long line with a warning. this is a very long
-          line with a warning. this is a very long line with a warning. this is
-          a very long line with a warning. this is a very long line with a
-          warning. this is a very long line with a warning. this is a very long
-          line with a warning.
-        </LogEntry>
-        <LogEntry time="12:34:23" variant="success">
-          Workflow demo.yml completed.
-        </LogEntry>
-      </Logs>
-    </Card>
-    <Card className="h-[500px] flex-1 p-4">
-      <Editor className="grow" value={YamlSample} />
-    </Card>
-  </div>
-);
+export const EditorVSLogsFontCompare = () => {
+  const [lineWrap, setLineWrap] = useState(true);
+  return (
+    <div className="flex flex-col gap-y-5">
+      <Button className="self-start" onClick={() => setLineWrap((old) => !old)}>
+        {!lineWrap && "don't"} wrap long lines
+      </Button>
+      <div className="flex flex-row gap-5">
+        <Card className="h-[500px] flex-1">
+          <Logs linewrap={lineWrap}>
+            <LogEntry time="12:34:23">
+              Preparing workflow triggered by api.
+            </LogEntry>
+            <LogEntry time="12:34:23">Starting workflow demo.yml.</LogEntry>
+            <LogEntry time="12:34:23">
+              Running state logic (step:1) -- helloworld
+            </LogEntry>
+            <LogEntry time="12:34:23">Transforming state data.</LogEntry>
+            <LogEntry time="12:34:23" variant="warning">
+              Warning: this is a very long line with a warning. this is a very
+              long line with a warning. this is a very long line with a warning.
+              this is a very long line with a warning. this is a very long line
+              with a warning. this is a very long line with a warning. this is a
+              very long line with a warning. this is a very long line with a
+              warning. this is a very long line with a warning. this is a very
+              long line with a warning. this is a very long line with a warning.
+              this is a very long line with a warning.
+            </LogEntry>
+            <LogEntry time="12:34:23" variant="success">
+              Workflow demo.yml completed.
+            </LogEntry>
+          </Logs>
+        </Card>
+        <Card className="h-[500px] flex-1 p-4">
+          <Editor className="grow" value={YamlSample} />
+        </Card>
+      </div>
+    </div>
+  );
+};
