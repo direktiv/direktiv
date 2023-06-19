c6927ba1a02ef2e742e5c9b72d5614af0dd211af
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-19 11:02:29 +0200

    remove unused test
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- e2e/explorer/workflow/utils.ts
+++ e2e/explorer/workflow/utils.ts
@@ -1,47 +1,5 @@
 import { Page, expect } from "@playwright/test";
 
-import { faker } from "@faker-js/faker";
-
-const defaultDescription = "A simple 'no-op' state that returns 'Hello world!'";
-
-export const actionEditAndSaveWorkflow = async (
-  page: Page,
-  updatedText?: string
-): Promise<[string, string]> => {
-  const description = page.getByText(
-    updatedText ? updatedText : defaultDescription
-  );
-  await description.click();
-  // type random text in that textarea which is for description
-  const testText = faker.random.alphaNumeric(9);
-  await page.type("textarea", testText);
-
-  const textArea = page.getByRole("textbox");
-
-  //now click on Save
-  const saveButton = page.getByTestId("workflow-editor-btn-save");
-  await saveButton.click();
-
-  // save button should be disabled/enabled while/after the api call
-  await expect(
-    saveButton,
-    "save button should be disabled during the api call"
-  ).toBeDisabled();
-  await expect(
-    saveButton,
-    "save button should be enabled after the api call"
-  ).toBeEnabled();
-
-  // after saving is completed screen should have those new changed text before/after the page reload
-  await expect(
-    page.getByText(testText),
-    "after saving, screen should have the updated text"
-  ).toBeVisible();
-
-  const updatedWorkflow = await textArea.inputValue();
-  return [updatedWorkflow, testText];
-};
-
 export const actionWaitForSuccessToast = async (page: Page) => {
   const successToast = page.getByTestId("toast-success");
   await expect(
