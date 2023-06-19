4cda354ecb35dba4c1a353824211f5857e69a5a3
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-19 10:20:18 +0200

    test workflowInputSchema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/components/__tests__/utils.test.ts
+++ src/pages/namespace/Explorer/Workflow/components/__tests__/utils.test.ts
@@ -5,8 +5,54 @@ import {
   validationAsSecondState,
 } from "./workflowTemplates";
 import { describe, expect, test } from "vitest";
+import { getValidationSchemaFromYaml, workflowInputSchema } from "../utils";
 
-import { getValidationSchemaFromYaml } from "../utils";
+describe("workflowInputSchema", () => {
+  describe("invalid workflow input", () => {
+    test("an empty string is not a valid workflow input", () => {
+      expect(workflowInputSchema.safeParse("").success).toEqual(false);
+    });
+
+    test("undefined and null is not a valid workflow input", () => {
+      expect(workflowInputSchema.safeParse(undefined).success).toEqual(false);
+      expect(workflowInputSchema.safeParse(null).success).toEqual(false);
+    });
+
+    test("an invalid JSON is not a valid workflow input", () => {
+      expect(workflowInputSchema.safeParse("{").success).toEqual(false);
+      expect(workflowInputSchema.safeParse("eklmflm").success).toEqual(false);
+      expect(workflowInputSchema.safeParse(1).success).toEqual(false);
+    });
+
+    test("a JSON without quotes around the key is not a valid workflow input", () => {
+      expect(workflowInputSchema.safeParse(`{key:1}`).success).toEqual(false);
+      expect(workflowInputSchema.safeParse(`{"key":1}`).success).toEqual(true);
+    });
+
+    test("a JSON with trailing comma is not a valid workflow input", () => {
+      expect(
+        workflowInputSchema.safeParse(`{"key":1,"key":2},`).success
+      ).toEqual(false);
+      expect(
+        workflowInputSchema.safeParse(`{"key":1,"key":2}`).success
+      ).toEqual(true);
+    });
+  });
+
+  describe("invalid workflow input", () => {
+    test("an empty JSON is a valid workflow input", () => {
+      expect(workflowInputSchema.safeParse("{}").success).toEqual(true);
+    });
+
+    test("any valid JSON is a valid workflow input", () => {
+      expect(
+        workflowInputSchema.safeParse(
+          `{"string": "1", "integer": 1, "boolean": true, "array": [1,2,3], "object": {"key": "value"}}`
+        ).success
+      ).toEqual(true);
+    });
+  });
+});
 
 describe("getValidationSchemaFromYaml", () => {
   describe("valid yaml input", () => {
