d87dabeab621f61677ba7311b60986f9735e183e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-20 10:19:39 +0200

    update schema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/instances/schema.ts
+++ src/api/instances/schema.ts
@@ -5,10 +5,10 @@ const instanceSchema = z.object({
   updatedAt: z.string(),
   id: z.string(),
   as: z.string(), // f.e. "some.yaml:latest",
-  status: z.string(), // TODO: "complete", what else?
+  status: z.enum(["pending", "failed", "crashed", "complete"]),
   errorCode: z.string(),
   errorMessage: z.string(),
-  invoker: z.string(), // TODO: can be "api", what else?
+  invoker: z.string(), // "api", "cron", "cloudevent", "complete" (if it's created as a subflow from another instance it's something like instance:%v, where %v is the instance ID of its parent
 });
 
 export const InstancesInputSchema = z.object({
