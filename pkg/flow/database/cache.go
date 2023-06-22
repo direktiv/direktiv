9c4cadc3888c81ab797050696e0d38b0f3275b67
sir-hassan <yassir.hassan@direktiv.io>
2023-06-22 17:20:16 +0200

    Remove dead code.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/flow/database/cache.go
+++ pkg/flow/database/cache.go
@@ -1,29 +1,5 @@
 package database
 
-import (
-	"encoding/json"
-
-	"github.com/google/uuid"
-)
-
 const (
 	PubsubNotifyFunction = "cache"
 )
-
-type Notifier interface {
-	PublishToCluster(string)
-}
-
-type notification struct {
-	Operation string
-	ID        uuid.UUID
-	Recursive bool
-}
-
-func (n *notification) Marshal() string {
-	data, err := json.Marshal(n)
-	if err != nil {
-		panic(err)
-	}
-	return string(data)
-}
