c4e102b2e6476148e86ec3aef1b6149a91e9d084
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2023-06-23 15:39:51 +0200

    Table matrix layout for broadcasts with checkboxes
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- src/pages/namespace/Settings/Broadcasts/index.tsx
+++ src/pages/namespace/Settings/Broadcasts/index.tsx
@@ -1,10 +1,23 @@
+import {
+  Table,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from "~/design/Table";
+
 import { Card } from "~/design/Card";
+import { Checkbox } from "~/design/Checkbox";
 import { FC } from "react";
-import ItemRow from "../components/ItemRow";
 import { Radio } from "lucide-react";
-import { Table } from "~/design/Table";
 import { useBroadcasts } from "~/api/broadcasts/query/useBroadcasts";
 import { useTranslation } from "react-i18next";
+
+// To do: instead of this, maybe make components for each cell type
+const leftColClasses = "col-span-2";
+const labelCellClasses = "place-self-center px-2";
+const switchCellClasses = "place-self-center px-2";
+const rowClasses = "grid grid-cols-5 gap-0 lg:pr-8 xl:pr-12";
 
 const Broadcasts: FC = () => {
   const { t } = useTranslation();
@@ -13,10 +26,7 @@ const Broadcasts: FC = () => {
 
   if (!data?.broadcast) return null;
 
-  const broadcasts = Object.entries(data.broadcast).map(([key, value]) => ({
-    name: key,
-    value,
-  }));
+  const broadcasts = data.broadcast;
 
   return (
     <>
@@ -29,10 +39,104 @@ const Broadcasts: FC = () => {
 
       <Card>
         <Table>
-          {/* TODO: Table layout with toggles, this is temporary */}
-          {broadcasts.map((item, i) => (
-            <ItemRow key={i} item={item} onDelete={() => "not implemented"} />
-          ))}
+          <TableHead>
+            <TableRow className={rowClasses}>
+              <TableHeaderCell className={leftColClasses}></TableHeaderCell>
+              <TableHeaderCell className={labelCellClasses}>
+                Success
+              </TableHeaderCell>
+              <TableHeaderCell className={labelCellClasses}>
+                Started
+              </TableHeaderCell>
+              <TableHeaderCell className={labelCellClasses}>
+                Failed
+              </TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableRow className={rowClasses}>
+            <TableCell className={leftColClasses}>Instance</TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["instance.success"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["instance.started"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["instance.failed"]} />
+            </TableCell>
+          </TableRow>
+          <TableHead>
+            <TableRow className={rowClasses}>
+              <TableHeaderCell className={leftColClasses}></TableHeaderCell>
+              <TableHeaderCell className={labelCellClasses}>
+                Create
+              </TableHeaderCell>
+              <TableHeaderCell className={labelCellClasses}>
+                Update
+              </TableHeaderCell>
+              <TableHeaderCell className={labelCellClasses}>
+                Delete
+              </TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableRow className={rowClasses}>
+            <TableCell className={leftColClasses}>Directory</TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["directory.create"]} />
+            </TableCell>
+            <TableCell></TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["directory.delete"]} />
+            </TableCell>
+          </TableRow>
+          <TableRow className={rowClasses}>
+            <TableCell className={leftColClasses}>Workflow</TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["workflow.create"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["workflow.update"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["workflow.delete"]} />
+            </TableCell>
+          </TableRow>
+          <TableRow className={rowClasses}>
+            <TableCell className={leftColClasses}>Instance variable</TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["instance.variable.create"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["instance.variable.update"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["instance.variable.delete"]} />
+            </TableCell>
+          </TableRow>
+          <TableRow className={rowClasses}>
+            <TableCell className={leftColClasses}>Namespace variable</TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["namespace.variable.create"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["namespace.variable.update"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["namespace.variable.delete"]} />
+            </TableCell>
+          </TableRow>
+          <TableRow className={rowClasses}>
+            <TableCell className={leftColClasses}>Workflow variable</TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["workflow.variable.create"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["workflow.variable.update"]} />
+            </TableCell>
+            <TableCell className={switchCellClasses}>
+              <Checkbox checked={broadcasts["workflow.variable.delete"]} />
+            </TableCell>
+          </TableRow>
         </Table>
       </Card>
     </>
