c97a1abc3baf973340e460a14d4af2c729dfcfa9
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-23 08:02:24 +0200

    add description to twMergeClsx
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -2,5 +2,17 @@ import clsx, { ClassValue } from "clsx";
 
 import { twMerge } from "tailwind-merge";
 
+/**
+ * this method combines the usage of two utility libraries:
+ * - clsx https://github.com/lukeed/clsx#readme for more info
+ *    clsx is a tiny utility for constructing className strings
+ *    conditionally.
+ * - tailwind-merge https://github.com/dcastil/tailwind-merge
+ *    twMerge is a function to efficiently merge Tailwind CSS
+ *    classes in JS without style conflicts.
+ *
+ *    input: twMergeClsx("bg-red", "bg-[#B91C1C]")
+ *    output: bg-[#B91C1C]
+ */
 export const twMergeClsx = (...inputs: ClassValue[]) =>
   twMerge(clsx(...inputs));
