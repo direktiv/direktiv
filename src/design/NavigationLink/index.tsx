dd2a0050923cc4593c0a38800a43d33118cc9f5e
porleaaron <porleaaron28@gmail.com>
2023-06-23 16:23:59 +0800

    hotfix -3

--- src/design/NavigationLink/index.tsx
+++ src/design/NavigationLink/index.tsx
@@ -2,12 +2,13 @@ import { FC, PropsWithChildren } from "react";
 
 import { twMergeClsx } from "~/util/helpers";
 
-export const createClassNames = (active: boolean) =>
+export const createClassNames = (active: boolean, className?: string) =>
   twMergeClsx(
     active
       ? "bg-primary-50 text-gray-12 dark:bg-primary-700 dark:text-gray-dark-12"
       : "text-gray-11 hover:bg-gray-2 dark:text-gray-dark-11 dark:hover:bg-gray-dark-2",
-    "[&>svg]:group group flex items-center rounded-md p-2 text-sm font-medium [&>svg]:mr-3"
+    "[&>svg]:group group flex items-center rounded-md p-2 text-sm font-medium [&>svg]:mr-3",
+    className
   );
 
 type ATagProps = JSX.IntrinsicElements["a"];
