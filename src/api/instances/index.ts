e4cd2455c7061d3ca5f2ec540e93a92babdf03b6
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-24 21:19:36 +0200

    add instances keys
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/api/instances/index.ts
@@ -0,0 +1,19 @@
+export const instanceKeys = {
+  instancesList: (
+    namespace: string,
+    {
+      apiKey,
+      limit,
+      offset,
+    }: { apiKey?: string; limit: number; offset: number }
+  ) =>
+    [
+      {
+        scope: "instance-list",
+        apiKey,
+        namespace,
+        limit,
+        offset,
+      },
+    ] as const,
+};
