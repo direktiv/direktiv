a50857da0b16cccebe0c204899fcbea574e48c04
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-24 21:23:50 +0200

    show very simple instances list
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/List/index.tsx
+++ src/pages/namespace/Instances/List/index.tsx
@@ -1,3 +1,18 @@
-const InstancesListPage = () => <h1>Instances list view</h1>;
+import { useInstances } from "~/api/instances/query/get";
+
+const InstancesListPage = () => {
+  const { data } = useInstances({ limit: 10, offset: 0 });
+
+  return (
+    <div>
+      <h1>List</h1>
+      {data?.instances.results.map((instance) => (
+        <div key={instance.id}>
+          {instance.as} <i>{instance.id}</i>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default InstancesListPage;
