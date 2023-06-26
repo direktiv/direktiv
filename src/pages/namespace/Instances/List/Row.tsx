252940204e9b2f8dafb56bb6de6252672c4d95e6
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-26 15:00:11 +0200

    link to workflow and color status badge
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/List/Row.tsx
+++ src/pages/namespace/Instances/List/Row.tsx
@@ -1,3 +1,6 @@
+import { ComponentProps, FC } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Stats, stat } from "fs";
 import { TableCell, TableRow } from "~/design/Table";
 import {
   Tooltip,
@@ -7,11 +10,26 @@ import {
 } from "~/design/Tooltip";
 
 import Badge from "~/design/Badge";
-import { FC } from "react";
 import { InstanceSchemaType } from "~/api/instances/schema";
-import { Link } from "react-router-dom";
 import { pages } from "~/util/router/pages";
 import useUpdatedAt from "~/hooksNext/useUpdatedAt";
+
+type BadgeVariant = ComponentProps<typeof Badge>["variant"];
+type InstanceStatus = InstanceSchemaType["status"];
+
+const statusToBadgeVariant = (status: InstanceStatus): BadgeVariant => {
+  switch (status) {
+    case "complete":
+      return "success";
+    case "crashed":
+    case "failed":
+      return "destructive";
+    case "pending":
+      return undefined;
+    default:
+      break;
+  }
+};
 
 const InstanceTableRow: FC<{
   instance: InstanceSchemaType;
@@ -20,25 +38,60 @@ const InstanceTableRow: FC<{
   const [name, revision] = instance.as.split(":");
   const updatedAt = useUpdatedAt(instance.updatedAt);
   const createdAt = useUpdatedAt(instance.createdAt);
+  const navigate = useNavigate();
+
+  const isLatestRevision = revision === "latest";
 
   return (
     <TooltipProvider>
-      <TableRow key={instance.id}>
-        <TableCell>
-          <Link
-            to={pages.instances.createHref({
+      <TableRow
+        key={instance.id}
+        onClick={() => {
+          navigate(
+            pages.instances.createHref({
               namespace,
               instance: instance.id,
-            })}
-          >
-            {name}
-          </Link>
+            })
+          );
+        }}
+        className="cursor-pointer"
+      >
+        <TableCell>
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                onClick={(e) => {
+                  e.stopPropagation(); // prevent the onClick on the row from firing
+                }}
+                to={pages.explorer.createHref({
+                  namespace,
+                  path: name,
+                  subpage: isLatestRevision ? "workflow" : "workflow-revisions",
+                  revision: isLatestRevision ? undefined : revision,
+                })}
+                className="hover:underline"
+              >
+                {name}
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>click to open workflow</TooltipContent>
+          </Tooltip>
+        </TableCell>
+        <TableCell
+          className="w-32"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <Badge variant="outline">{instance.id.slice(0, 8)}</Badge>
         </TableCell>
         <TableCell className="w-28">
           <Badge variant="outline">{revision}</Badge>
         </TableCell>
         <TableCell className="w-28">
-          <Badge variant="success">{instance.status}</Badge>
+          <Badge variant={statusToBadgeVariant(instance.status)}>
+            {instance.status}
+          </Badge>
         </TableCell>
         <TableCell className="w-40">
           <Tooltip>
