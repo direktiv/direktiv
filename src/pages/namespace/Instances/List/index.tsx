e6473af32e0c4f260b58b81bae800ee774d154ef
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-26 16:23:30 +0200

    translate list
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/List/index.tsx
+++ src/pages/namespace/Instances/List/index.tsx
@@ -8,40 +8,62 @@ import {
 
 import { Boxes } from "lucide-react";
 import { Card } from "~/design/Card";
+import NoResult from "./NoResult";
 import Row from "./Row";
 import { useInstances } from "~/api/instances/query/get";
+import { useTranslation } from "react-i18next";
 
 const InstancesListPage = () => {
-  const { data } = useInstances({ limit: 10, offset: 0 });
+  const { data, isFetched } = useInstances({ limit: 10, offset: 0 });
+  const { t } = useTranslation();
+
+  if (!isFetched) return null;
+  const noResults = data?.instances.results.length === 0 && isFetched;
 
   return (
     <div className="flex grow flex-col gap-y-4 p-5">
       <h3 className="flex items-center gap-x-2 font-bold text-gray-10 dark:text-gray-dark-10">
         <Boxes className="h-5" />
-        Recently executed instances
+        {t("pages.instances.list.title")}
       </h3>
       <Card>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeaderCell>name</TableHeaderCell>
-              <TableHeaderCell>id</TableHeaderCell>
-              <TableHeaderCell>revision id</TableHeaderCell>
-              <TableHeaderCell>state</TableHeaderCell>
-              <TableHeaderCell>started at</TableHeaderCell>
-              <TableHeaderCell>last updated</TableHeaderCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data?.instances.results.map((instance) => (
-              <Row
-                instance={instance}
-                key={instance.id}
-                namespace={data.namespace}
-              />
-            ))}
-          </TableBody>
-        </Table>
+        {noResults ? (
+          <NoResult />
+        ) : (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeaderCell>
+                  {t("pages.instances.list.tableHeader.name")}
+                </TableHeaderCell>
+                <TableHeaderCell>
+                  {t("pages.instances.list.tableHeader.id")}
+                </TableHeaderCell>
+                <TableHeaderCell>
+                  {t("pages.instances.list.tableHeader.revisionId")}
+                </TableHeaderCell>
+                <TableHeaderCell>
+                  {t("pages.instances.list.tableHeader.state")}
+                </TableHeaderCell>
+                <TableHeaderCell>
+                  {t("pages.instances.list.tableHeader.startedAt")}
+                </TableHeaderCell>
+                <TableHeaderCell>
+                  {t("pages.instances.list.tableHeader.updatedAt")}
+                </TableHeaderCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.instances.results.map((instance) => (
+                <Row
+                  instance={instance}
+                  key={instance.id}
+                  namespace={data.namespace}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </Card>
     </div>
   );
