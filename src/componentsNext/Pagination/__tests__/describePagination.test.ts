bb696765425642500a73fc90753f48d1b4b845e4
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-29 14:17:46 +0200

    test neighbours parameter (1 failing test)
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/Pagination/__tests__/describePagination.test.ts
+++ src/componentsNext/Pagination/__tests__/describePagination.test.ts
@@ -44,29 +44,76 @@ describe("describePagination", () => {
     });
   });
 
+  describe("configure neighbours", () => {
+    test("by default it is configured have 1 neighbour", () => {
+      const defaultPagination = describePagination({
+        pages: 10,
+        currentPage: 1,
+      });
+
+      const oneNeighbourPagination = describePagination({
+        pages: 10,
+        currentPage: 1,
+        neighbours: 1,
+      });
+      expect(defaultPagination).toStrictEqual(oneNeighbourPagination);
+    });
+
+    test("increasing neighbours will eventually get rid of the ...", () => {
+      const result1Neighbour = describePagination({
+        pages: 10,
+        currentPage: 1,
+        neighbours: 2,
+      });
+      expect(result1Neighbour).toStrictEqual([1, 2, 3, "...", 8, 9, 10]);
+
+      const result2Neighbours = describePagination({
+        pages: 10,
+        currentPage: 1,
+        neighbours: 3,
+      });
+      expect(result2Neighbours).toStrictEqual([1, 2, 3, 4, "...", 7, 8, 9, 10]);
+
+      const result3Neighbours = describePagination({
+        pages: 10,
+        currentPage: 1,
+        neighbours: 3,
+      });
+      expect(result3Neighbours).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+  });
+
+  describe("implausible input", () => {
+    test("it will return an empty array when current page is less than 1", () => {
+      const pagination0 = describePagination({ pages: 1, currentPage: 0 });
+      expect(pagination0).toStrictEqual([]);
+
+      const paginationMinus1 = describePagination({
+        pages: 1,
+        currentPage: -1,
+      });
+      expect(paginationMinus1).toStrictEqual([]);
+    });
+
+    test("it will return an empty array when page is less than 1", () => {
+      const pagination0 = describePagination({ pages: 0, currentPage: 1 });
+      expect(pagination0).toStrictEqual([]);
+
+      const paginationMinus1 = describePagination({
+        pages: -1,
+        currentPage: 1,
+      });
+      expect(paginationMinus1).toStrictEqual([]);
+    });
+
+    test("it will return an empty array when currentPage is bigger than pages", () => {
+      const pagination = describePagination({ pages: 1, currentPage: 2 });
+      expect(pagination).toStrictEqual([]);
+    });
+  });
+
   test("it handles 1 page", () => {
     const pagination = describePagination({ pages: 1, currentPage: 1 });
     expect(pagination).toStrictEqual([1]);
-  });
-
-  test("it will return an empty array when current page is less than 1", () => {
-    const pagination0 = describePagination({ pages: 1, currentPage: 0 });
-    expect(pagination0).toStrictEqual([]);
-
-    const paginationMinus1 = describePagination({ pages: 1, currentPage: -1 });
-    expect(paginationMinus1).toStrictEqual([]);
-  });
-
-  test("it will return an empty array when page is less than 1", () => {
-    const pagination0 = describePagination({ pages: 0, currentPage: 1 });
-    expect(pagination0).toStrictEqual([]);
-
-    const paginationMinus1 = describePagination({ pages: -1, currentPage: 1 });
-    expect(paginationMinus1).toStrictEqual([]);
-  });
-
-  test("it will return an empty array when currentPage is bigger than pages", () => {
-    const pagination = describePagination({ pages: 1, currentPage: 2 });
-    expect(pagination).toStrictEqual([]);
   });
 });
