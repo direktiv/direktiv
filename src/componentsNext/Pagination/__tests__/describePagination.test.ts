f8b7bb6236d01acc69d5ce36f08dcbef5ad96cae
Stefan Kracht <stefan.kracht@direktiv.io>
2023-06-29 15:31:19 +0200

    remove .only()
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/Pagination/__tests__/describePagination.test.ts
+++ src/componentsNext/Pagination/__tests__/describePagination.test.ts
@@ -59,7 +59,7 @@ describe("describePagination", () => {
       expect(defaultPagination).toStrictEqual(oneNeighbourPagination);
     });
 
-    test.only("increasing neighbours will eventually get rid of the ...", () => {
+    test("increasing neighbours will eventually get rid of the ...", () => {
       const result1Neighbour = describePagination({
         pages: 10,
         currentPage: 3,
@@ -82,7 +82,7 @@ describe("describePagination", () => {
       expect(result3Neighbours).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
 
-    test.only("when neighbours is very high, it will just not having any ...", () => {
+    test("when neighbours is very high, it will just not having any ...", () => {
       const result = describePagination({
         pages: 10,
         currentPage: 3,
@@ -120,7 +120,7 @@ describe("describePagination", () => {
       expect(pagination).toStrictEqual([]);
     });
 
-    test.only("it will return an empty array when neighbours is negative", () => {
+    test("it will return an empty array when neighbours is negative", () => {
       const result = describePagination({
         pages: 10,
         currentPage: 1,
