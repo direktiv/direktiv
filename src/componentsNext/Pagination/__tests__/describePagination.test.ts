f92da1668fec9d2fa66775aa83aecaeb9edd6d87
Stefan <121789579+stefan-kracht@users.noreply.github.com>
2023-07-03 15:21:29 +0200

    Update src/componentsNext/Pagination/__tests__/describePagination.test.ts
    Co-authored-by: Sebastian Armbrust <129740294+sebxian@users.noreply.github.com>

--- src/componentsNext/Pagination/__tests__/describePagination.test.ts
+++ src/componentsNext/Pagination/__tests__/describePagination.test.ts
@@ -86,7 +86,7 @@ describe("describePagination", () => {
       expect(result3Neighbours).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
 
-    test("when neighbours is very high, it will just not having any ellipsis", () => {
+    test("when neighbours is very high, it will not have any ellipsis", () => {
       const result = describePagination({
         pages: 10,
         currentPage: 3,
