ade2ecec105921caca91eff87ce90b6c52d22425
porleaaron <porleaaron28@gmail.com>
2023-07-05 17:40:21 +0800

    update appshell story to see the sticky header

--- src/design/Appshell/index.stories.tsx
+++ src/design/Appshell/index.stories.tsx
@@ -364,7 +364,7 @@ export const MoreDetailedShell = () => {
             </MainTopRight>
           </MainTop>
           <MainContent>
-            <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 pb-0 dark:border-gray-dark-5 dark:bg-gray-dark-1">
+            <div className="h-screen space-y-5 border-b border-gray-5 bg-gray-1 p-5 pb-0 dark:border-gray-dark-5 dark:bg-gray-dark-1">
               <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between ">
                 <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
                   <Play className="h-5" />
