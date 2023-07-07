a16af00cd2451ef702ebb51325e972fe25726c1a
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-07 14:45:02 +0200

    adjust description to new text color
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/JSONschemaForm/index.tsx
+++ src/design/JSONschemaForm/index.tsx
@@ -177,7 +177,9 @@ const TitleFieldTemplate = (props: TitleFieldProps) => {
 const DescriptionFieldTemplate = (props: DescriptionFieldProps) => {
   const { description } = props;
   return (
-    <div className="mb-2 text-gray-8 dark:text-gray-dark-8">{description}</div>
+    <div className="mb-2 text-gray-10 dark:text-gray-dark-10">
+      {description}
+    </div>
   );
 };
 
