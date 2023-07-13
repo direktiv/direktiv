74136653a9f4ae7a4886952a08a8adebd957a69f
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-13 10:03:19 +0200

    update toggle color according to recent contrast changes
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Toggle/index.tsx
+++ src/design/Toggle/index.tsx
@@ -15,9 +15,10 @@ const Toggle = React.forwardRef<
     ref={ref}
     className={twMergeClsx(
       "inline-flex items-center justify-center rounded-md bg-transparent text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-      "text-gray-11 hover:bg-gray-2 hover:text-gray-12 focus:ring-gray-4 focus:ring-offset-gray-1 data-[state=on]:bg-gray-4 data-[state=on]:text-gray-11",
-      "dark:hover:bg-gray-dark-2 dark:focus:ring-gray-dark-4 dark:data-[state=on]:bg-gray-dark-4",
-      "dark:text-gray-dark-11 dark:hover:text-gray-dark-12 dark:focus:ring-offset-gray-dark-1 dark:data-[state=on]:text-gray-dark-11",
+      "text-gray-12 hover:bg-gray-2 focus:ring-gray-4 focus:ring-offset-gray-1",
+      "data-[state=on]:bg-gray-3",
+      "dark:text-gray-dark-12 dark:hover:bg-gray-dark-2 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1",
+      "dark:data-[state=on]:bg-gray-dark-3",
       outline &&
         "border border-gray-4 hover:bg-gray-2 dark:border-gray-dark-4 dark:bg-gray-dark-2",
       size === "sm" && "h-6 px-2.5 [&>svg]:h-4",
