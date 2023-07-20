36d1e2289218d37c86e39e7e72dca84780f880b1
Alexander Ertli <alexander.ertli@direktiv.io>
2023-07-20 12:22:17 +0000

    Replace -, _
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- scripts/reinstall-dev-stack.sh
+++ scripts/reinstall-dev-stack.sh
@@ -62,8 +62,8 @@ flow:
   image: "direktiv"
   dbimage: "direktiv"
   tag: "latest"
-  experimental-features: true
-  developer-mode: false
+  experimental_features: true
+  developer_mode: false
 
 ui:
   image: "ui"
