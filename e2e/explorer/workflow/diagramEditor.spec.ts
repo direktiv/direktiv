c3d69ca88f09495a6078619385c361d67fb0d896
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-21 11:01:31 +0200

    fix indention
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- e2e/explorer/workflow/diagramEditor.spec.ts
+++ e2e/explorer/workflow/diagramEditor.spec.ts
@@ -154,34 +154,34 @@ test("it will update the diagram when the workflow is saved", async ({
     async () =>
       await navigator.clipboard
         .writeText(`description: A simple 'validate' state workflow that checks an email
-    states:
-    - id: data
-      type: noop
-      transform:
-        email: "trent.hilliam@direktiv.io"
-      transition: validate-email
-    - id: validate-email
-      type: validate
-      subject: jq(.)
-      schema:
-        type: object
-        properties:
-          email:
-            type: string
-            format: email
-      catch:
-      - error: direktiv.schema.*
-        transition: email-not-valid 
-      transition: email-valid
-    - id: email-not-valid
-      type: noop
-      transform:
-        result: "Email is not valid."
-    - id: email-valid
-      type: noop
-      transform:
-        result: "Email is valid."
-    `)
+states:
+- id: data
+  type: noop
+  transform:
+    email: "trent.hilliam@direktiv.io"
+  transition: validate-email
+- id: validate-email
+  type: validate
+  subject: jq(.)
+  schema:
+    type: object
+    properties:
+      email:
+        type: string
+        format: email
+  catch:
+  - error: direktiv.schema.*
+    transition: email-not-valid 
+  transition: email-valid
+- id: email-not-valid
+  type: noop
+  transform:
+    result: "Email is not valid."
+- id: email-valid
+  type: noop
+  transform:
+    result: "Email is valid."
+  `)
   );
 
   // TODO: fix meta key
