022e6a4888a23e1239994a4fcb895d720dc38119
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 14:51:31 +0200

    add one separate component
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/pages/namespace/Instances/Detail/InstanceDetail.tsx
@@ -0,0 +1,36 @@
+import { Card } from "~/design/Card";
+import Diagram from "./Diagram";
+import { FC } from "react";
+import Header from "./Header";
+import Input from "./Input";
+import Logs from "./Logs";
+import Output from "./Output";
+import { useInstanceDetails } from "~/api/instances/query/details";
+
+const InstancesDetail: FC<{ instanceId: string }> = ({ instanceId }) => {
+  const { data } = useInstanceDetails({ instanceId });
+
+  if (!data) return null;
+
+  return (
+    <div className="flex grow flex-col">
+      <Header instanceId={instanceId} />
+      <div className="grid grow grid-cols-2 gap-5 p-5">
+        <Card className="p-5">
+          <Logs />
+        </Card>
+        <Card>
+          <Diagram workflowPath={data.workflow.path} flow={data.flow} />
+        </Card>
+        <Card className="p-5">
+          <Input instanceId={instanceId} />
+        </Card>
+        <Card className="p-5">
+          <Output instanceId={instanceId} />
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default InstancesDetail;
--- src/pages/namespace/Instances/Detail/index.tsx
+++ src/pages/namespace/Instances/Detail/index.tsx
@@ -1,45 +1,14 @@
-import { Card } from "~/design/Card";
-import Diagram from "./Diagram";
-import { FC } from "react";
-import Header from "./Header";
-import Input from "./Input";
-import Logs from "./Logs";
-import Output from "./Output";
+import InstancesDetail from "./InstanceDetail";
 import { pages } from "~/util/router/pages";
-import { useInstanceDetails } from "~/api/instances/query/details";
 
-const InstancesDetailPage = () => {
+const Instance = () => {
   const { instance } = pages.instances.useParams();
   if (!instance) return null;
-  return <InstancesDetailPageWithId instanceId={instance} />;
+
+  // Details page is moved into a separate component to give us a state
+  // where the id alwawys defined. This is required for the data fetching
+  // hook that require the id (and hooks can not be conditionally called)
+  return <InstancesDetail instanceId={instance} />;
 };
 
-const InstancesDetailPageWithId: FC<{ instanceId: string }> = ({
-  instanceId,
-}) => {
-  const { data } = useInstanceDetails({ instanceId });
-
-  if (!data) return null;
-
-  return (
-    <div className="flex grow flex-col">
-      <Header instanceId={instanceId} />
-      <div className="grid grow grid-cols-2 gap-5 p-5">
-        <Card className="p-5">
-          <Logs />
-        </Card>
-        <Card>
-          <Diagram workflowPath={data.workflow.path} flow={data.flow} />
-        </Card>
-        <Card className="p-5">
-          <Input instanceId={instanceId} />
-        </Card>
-        <Card className="p-5">
-          <Output instanceId={instanceId} />
-        </Card>
-      </div>
-    </div>
-  );
-};
-
-export default InstancesDetailPage;
+export default Instance;
