d3d6e1857fb00219b48d8e69d7d47e093d0261a1
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 09:38:00 +0200

    show input
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Input.tsx
+++ src/pages/namespace/Instances/Detail/Input.tsx
@@ -1,5 +1,18 @@
 import { Card } from "~/design/Card";
+import { FC } from "react";
+import { useInput } from "~/api/instances/query/input";
 
-const Input = () => <Card className="p-5">Input</Card>;
+const Input: FC<{ instanceId: string }> = ({ instanceId }) => {
+  const { data } = useInput({ instanceId });
+  if (!data) return null;
+
+  const workflowInput = atob(data.data);
+
+  return (
+    <Card className="p-5">
+      <pre>{workflowInput}</pre>
+    </Card>
+  );
+};
 
 export default Input;
--- src/pages/namespace/Instances/Detail/index.tsx
+++ src/pages/namespace/Instances/Detail/index.tsx
@@ -2,14 +2,21 @@ import Diagram from "./Diagram";
 import Input from "./Input";
 import Logs from "./Logs";
 import Output from "./Output";
+import { pages } from "~/util/router/pages";
 
-const InstancesDetailPage = () => (
-  <div className="grid grow grid-cols-2 gap-5 p-5">
-    <Input />
-    <Output />
-    <Logs />
-    <Diagram />
-  </div>
-);
+const InstancesDetailPage = () => {
+  const { instance } = pages.instances.useParams();
+
+  if (!instance) return null;
+
+  return (
+    <div className="grid grow grid-cols-2 gap-5 p-5">
+      <Input instanceId={instance} />
+      <Output />
+      <Logs />
+      <Diagram />
+    </div>
+  );
+};
 
 export default InstancesDetailPage;
