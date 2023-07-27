f8781edbfaca6d080479be16b228968c1e71a9d8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 17:04:31 +0200

    handle error in output
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Output.tsx
+++ src/pages/namespace/Instances/Detail/Output.tsx
@@ -4,11 +4,20 @@ import { useOutput } from "~/api/instances/query/output";
 import { useTheme } from "~/util/store/theme";
 
 const Output: FC<{ instanceId: string }> = ({ instanceId }) => {
-  const { data } = useOutput({ instanceId });
+  const { data, isFetched, isError } = useOutput({ instanceId });
   const theme = useTheme();
-  if (!data) return null;
 
-  const workflowOutput = atob(data.data);
+  if (!isFetched) return null;
+
+  let workflowOutput = "// no data";
+
+  if (isError) {
+    workflowOutput = "// no output data was resolved";
+  }
+
+  if (data) {
+    workflowOutput = atob(data.data);
+  }
 
   return (
     <Editor
