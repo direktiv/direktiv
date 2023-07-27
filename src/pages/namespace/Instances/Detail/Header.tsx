dff06c0b369a418488be9566f28bed3fa79e4340
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 11:10:50 +0200

    load instance details
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Header.tsx
+++ src/pages/namespace/Instances/Detail/Header.tsx
@@ -1,5 +1,7 @@
+import Badge from "~/design/Badge";
 import { Box } from "lucide-react";
 import { FC } from "react";
+import { statusToBadgeVariant } from "../utils";
 import { useInstanceDetails } from "~/api/instances/query/details";
 
 const Header: FC<{ instanceId: string }> = ({ instanceId }) => {
@@ -7,11 +9,16 @@ const Header: FC<{ instanceId: string }> = ({ instanceId }) => {
 
   if (!data) return null;
 
-  console.log(data);
-
   return (
     <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 dark:border-gray-dark-5 dark:bg-gray-dark-1">
-      <Box />
+      <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
+        <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
+          <Box className="h-5" /> {data.instance.id.slice(0, 8)}
+        </h3>
+        <Badge variant={statusToBadgeVariant(data.instance.status)}>
+          {data.instance.status}
+        </Badge>
+      </div>
     </div>
   );
 };
