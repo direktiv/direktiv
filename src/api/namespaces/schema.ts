9318b75655fa0d6ef6f358ceb4e096157273e26a
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2023-07-27 17:57:25 +0200

    Add mirror tab to createNamespace dialog
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- src/api/namespaces/schema.ts
+++ src/api/namespaces/schema.ts
@@ -17,4 +17,15 @@ export const NamespaceCreatedSchema = z.object({
   namespace: NamespaceSchema,
 });
 
+// note: in the current API implementation, a mirror is created
+// by creating a namespace with this in the payload.
+export const MirrorSchema = z.object({
+  passphrase: z.string().optional(),
+  privateKey: z.string().optional(),
+  publicKey: z.string().optional(),
+  ref: z.string().nonempty(),
+  url: z.string().url().nonempty(),
+});
+
 export type NamespaceListSchemaType = z.infer<typeof NamespaceListSchema>;
+export type MirrorSchemaType = z.infer<typeof MirrorSchema>;
--- src/componentsNext/NamespaceCreate/index.tsx
+++ src/componentsNext/NamespaceCreate/index.tsx
@@ -5,7 +5,16 @@ import {
   DialogTitle,
 } from "~/design/Dialog";
 import { Home, PlusCircle } from "lucide-react";
+import { MirrorSchema, MirrorSchemaType } from "~/api/namespaces/schema";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/design/Select";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { Tabs, TabsList, TabsTrigger } from "~/design/Tabs";
 
 import Button from "~/design/Button";
 import FormErrors from "~/componentsNext/FormErrors";
@@ -16,37 +25,45 @@ import { useCreateNamespace } from "~/api/namespaces/mutate/createNamespace";
 import { useListNamespaces } from "~/api/namespaces/query/get";
 import { useNamespaceActions } from "~/util/store/namespace";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 type FormInput = {
   name: string;
-};
+} & MirrorSchemaType;
+
+const mirrorAuthTypes = ["none", "ssh", "token"] as const;
 
 const NamespaceCreate = ({ close }: { close: () => void }) => {
   const { t } = useTranslation();
+  const [isMirror, setIsMirror] = useState<boolean>(false);
+  const [authType, setAuthType] = useState<string>("none");
   const { data } = useListNamespaces();
   const { setNamespace } = useNamespaceActions();
+  const navigate = useNavigate();
+
   const existingNamespaces = data?.results.map((n) => n.name) || [];
 
-  const navigate = useNavigate();
+  const nameSchema = fileNameSchema.and(
+    z.string().refine((name) => !existingNamespaces.some((n) => n === name), {
+      message: t("components.namespaceCreate.nameAlreadyExists"),
+    })
+  );
+
+  const simpleNamespaceResolver = zodResolver(z.object({ name: nameSchema }));
+
+  const mirrorResolver = zodResolver(
+    MirrorSchema.and(z.object({ name: nameSchema }))
+  );
+
   const {
     register,
     handleSubmit,
     formState: { isDirty, errors, isValid, isSubmitted },
   } = useForm<FormInput>({
-    resolver: zodResolver(
-      z.object({
-        name: fileNameSchema.and(
-          z
-            .string()
-            .refine((name) => !existingNamespaces.some((n) => n === name), {
-              message: t("components.namespaceCreate.nameAlreadyExists"),
-            })
-        ),
-      })
-    ),
+    resolver: isMirror ? mirrorResolver : simpleNamespaceResolver,
   });
 
   const { mutate: createNamespace, isLoading } = useCreateNamespace({
@@ -61,8 +78,18 @@ const NamespaceCreate = ({ close }: { close: () => void }) => {
     },
   });
 
-  const onSubmit: SubmitHandler<FormInput> = ({ name }) => {
-    createNamespace({ name });
+  const onSubmit: SubmitHandler<FormInput> = ({
+    name,
+    ref,
+    url,
+    passphrase,
+    publicKey,
+    privateKey,
+  }) => {
+    createNamespace({
+      name,
+      mirror: { ref, url, passphrase, publicKey, privateKey },
+    });
   };
 
   // you can not submit if the form has not changed or if there are any errors and
@@ -78,12 +105,27 @@ const NamespaceCreate = ({ close }: { close: () => void }) => {
         </DialogTitle>
       </DialogHeader>
 
+      <Tabs className="w-[400px]" defaultValue="namespace">
+        <TabsList>
+          <TabsTrigger value="namespace" onClick={() => setIsMirror(false)}>
+            Namespace
+          </TabsTrigger>
+          <TabsTrigger value="mirror" onClick={() => setIsMirror(true)}>
+            Mirror
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
+
       <div className="my-3">
         <FormErrors errors={errors} className="mb-5" />
-        <form id={formId} onSubmit={handleSubmit(onSubmit)}>
+        <form
+          id={formId}
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-y-5"
+        >
           <fieldset className="flex items-center gap-5">
             <label className="w-[90px] text-right text-[14px]" htmlFor="name">
-              {t("components.namespaceCreate.namespaceLabel")}
+              {t("components.namespaceCreate.nameLabel")}
             </label>
             <Input
               id="name"
@@ -92,8 +134,111 @@ const NamespaceCreate = ({ close }: { close: () => void }) => {
               {...register("name")}
             />
           </fieldset>
+
+          {isMirror && (
+            <>
+              <fieldset className="flex items-center gap-5">
+                <label
+                  className="w-[90px] text-right text-[14px]"
+                  htmlFor="name"
+                >
+                  {t("components.namespaceCreate.urlLabel")}
+                </label>
+                <Input
+                  id="name"
+                  data-testid="new-namespace-name"
+                  placeholder={t("components.namespaceCreate.placeholder")}
+                  {...register("url")}
+                />
+              </fieldset>
+
+              <fieldset className="flex items-center gap-5">
+                <label
+                  className="w-[90px] text-right text-[14px]"
+                  htmlFor="name"
+                >
+                  {t("components.namespaceCreate.refLabel")}
+                </label>
+                <Input
+                  id="name"
+                  data-testid="new-namespace-name"
+                  placeholder={t("components.namespaceCreate.placeholder")}
+                  {...register("ref")}
+                />
+              </fieldset>
+
+              <Select value={authType} onValueChange={setAuthType}>
+                <SelectTrigger variant="outline" className="ml-[90px]">
+                  <SelectValue placeholder="Select Auth Method" />
+                </SelectTrigger>
+                <SelectContent>
+                  {mirrorAuthTypes.map((option) => (
+                    <SelectItem
+                      key={option}
+                      value={option}
+                      onClick={() => setAuthType(option)}
+                    >
+                      {t(`components.namespaceCreate.authType.${option}`)}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+
+              {(authType === "token" || authType == "ssh") && (
+                <fieldset className="flex items-center gap-5">
+                  <label
+                    className="w-[90px] text-right text-[14px]"
+                    htmlFor="name"
+                  >
+                    {t("components.namespaceCreate.passphrase")}
+                  </label>
+                  <Input
+                    id="name"
+                    data-testid="new-namespace-name"
+                    placeholder={t("components.namespaceCreate.placeholder")}
+                    {...register("passphrase")}
+                  />
+                </fieldset>
+              )}
+
+              {authType === "ssh" && (
+                <>
+                  <fieldset className="flex items-center gap-5">
+                    <label
+                      className="w-[90px] text-right text-[14px]"
+                      htmlFor="name"
+                    >
+                      {t("components.namespaceCreate.publicKey")}
+                    </label>
+                    <Input
+                      id="name"
+                      data-testid="new-namespace-name"
+                      placeholder={t("components.namespaceCreate.placeholder")}
+                      {...register("publicKey")}
+                    />
+                  </fieldset>
+
+                  <fieldset className="flex items-center gap-5">
+                    <label
+                      className="w-[90px] text-right text-[14px]"
+                      htmlFor="name"
+                    >
+                      {t("components.namespaceCreate.privateKey")}
+                    </label>
+                    <Input
+                      id="name"
+                      data-testid="new-namespace-name"
+                      placeholder={t("components.namespaceCreate.placeholder")}
+                      {...register("privateKey")}
+                    />
+                  </fieldset>
+                </>
+              )}
+            </>
+          )}
         </form>
       </div>
+
       <DialogFooter>
         <DialogClose asChild>
           <Button variant="ghost">
