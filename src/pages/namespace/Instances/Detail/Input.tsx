368856f4f0ac222dd7df3ef4ca03cfecffd21aa4
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 10:07:55 +0200

    use Editor for input
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Input.tsx
+++ src/pages/namespace/Instances/Detail/Input.tsx
@@ -1,16 +1,24 @@
 import { Card } from "~/design/Card";
+import Editor from "~/design/Editor";
 import { FC } from "react";
 import { useInput } from "~/api/instances/query/input";
+import { useTheme } from "~/util/store/theme";
 
 const Input: FC<{ instanceId: string }> = ({ instanceId }) => {
   const { data } = useInput({ instanceId });
+  const theme = useTheme();
   if (!data) return null;
 
   const workflowInput = atob(data.data);
 
   return (
     <Card className="p-5">
-      <pre>{workflowInput}</pre>
+      <Editor
+        value={workflowInput}
+        language="json"
+        theme={theme ?? undefined}
+        options={{ readOnly: true }}
+      />
     </Card>
   );
 };
