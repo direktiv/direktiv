57c896f58c4d14c91c4016662ffc0aa1dbf6a117
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 10:09:23 +0200

    use Editor for output
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Output.tsx
+++ src/pages/namespace/Instances/Detail/Output.tsx
@@ -1,16 +1,24 @@
 import { Card } from "~/design/Card";
+import Editor from "~/design/Editor";
 import { FC } from "react";
 import { useOutput } from "~/api/instances/query/output";
+import { useTheme } from "~/util/store/theme";
 
 const Output: FC<{ instanceId: string }> = ({ instanceId }) => {
   const { data } = useOutput({ instanceId });
+  const theme = useTheme();
   if (!data) return null;
 
   const workflowOutput = atob(data.data);
 
   return (
     <Card className="p-5">
-      <pre>{workflowOutput}</pre>
+      <Editor
+        value={workflowOutput}
+        language="json"
+        theme={theme ?? undefined}
+        options={{ readOnly: true }}
+      />
     </Card>
   );
 };
