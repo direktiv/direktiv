40c4f47e41b825295411a68341d032913b4373b4
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 14:51:36 +0200

    add demo logs
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Logs.tsx
+++ src/pages/namespace/Instances/Detail/Logs.tsx
@@ -1,3 +1,26 @@
-const Logs = () => <div>Logs</div>;
+import { LogEntry, Logs } from "~/design/Logs";
 
-export default Logs;
+const LogsPanel = () => (
+  <Logs linewrap={true} className="grow">
+    <LogEntry time="12:34:23">Preparing workflow triggered by api.</LogEntry>
+    <LogEntry time="12:34:23">Starting workflow demo.yml.</LogEntry>
+    <LogEntry time="12:34:23">
+      Running state logic (step:1) -- helloworld
+    </LogEntry>
+    <LogEntry time="12:34:23">Transforming state data.</LogEntry>
+    <LogEntry time="12:34:23" variant="warning">
+      Warning: this is a very long line with a warning. this is a very long line
+      with a warning. this is a very long line with a warning. this is a very
+      long line with a warning. this is a very long line with a warning. this is
+      a very long line with a warning. this is a very long line with a warning.
+      this is a very long line with a warning. this is a very long line with a
+      warning. this is a very long line with a warning. this is a very long line
+      with a warning. this is a very long line with a warning.
+    </LogEntry>
+    <LogEntry time="12:34:23" variant="success">
+      Workflow demo.yml completed.
+    </LogEntry>
+  </Logs>
+);
+
+export default LogsPanel;
