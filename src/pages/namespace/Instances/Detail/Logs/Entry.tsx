1195211d32f762c3fe26f54d8ac225d3074768d1
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-27 17:48:13 +0200

    style log level
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Logs/Entry.tsx
+++ src/pages/namespace/Instances/Detail/Logs/Entry.tsx
@@ -1,17 +1,33 @@
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
+
 import { LogEntry } from "~/design/Logs";
 import { LogEntryType } from "~/api/logs/schema";
 import moment from "moment";
+
+type LogEntryVariant = ComponentProps<typeof LogEntry>["variant"];
+type logLevel = LogEntryType["level"];
+
+const logLevelToLogEntryVariant = (level: logLevel): LogEntryVariant => {
+  switch (level) {
+    case "error":
+    case "panic":
+      return "error";
+    case "info":
+      return "info";
+    case "debug":
+      return undefined;
+    default:
+      break;
+  }
+};
 
 const Entry: FC<{ logEntry: LogEntryType }> = ({ logEntry }) => {
   const { msg, t, level } = logEntry;
 
   const time = moment(t).format("HH:mm:ss");
 
-  const isErr = level === "error";
-
   return (
-    <LogEntry variant={isErr ? "error" : undefined} time={time}>
+    <LogEntry variant={logLevelToLogEntryVariant(level)} time={time}>
       {msg}
     </LogEntry>
   );
