561a7519457d6ec018dc1885edac277193a968b8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-28 16:09:16 +0200

    dedup
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/logs/query/get.ts
+++ src/api/logs/query/get.ts
@@ -4,6 +4,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import type { QueryFunctionContext } from "@tanstack/react-query";
 import { apiFactory } from "~/api/apiFactory";
 import { logKeys } from "../";
+import moment from "moment";
 import { useApiKey } from "~/util/store/apiKey";
 import { useNamespace } from "~/util/store/namespace";
 import { useStreaming } from "~/api/streaming";
@@ -48,7 +49,37 @@ export const useLogs = (
           apiKey: apiKey ?? undefined,
           instanceId,
         }),
-        () => msg
+        (old) => {
+          if (!old) {
+            return msg;
+          }
+          /**
+           * Dedup logs. The onMessage callback gets called in two different cases:
+           *
+           * case 1:
+           * when the SSE connection is established, the whole set of logs is received
+           *
+           * case 2:
+           * after the connection is established and only some new log entries are received
+           *
+           * it's also important to note that multiple components can subscribe to the same
+           * cache, so we can have case 1 and 2 at the same time, or case 1 after case 2
+           */
+          const lastCachedLog = old.results[old.results.length - 1];
+          const newestIncomingLog = msg.results[0];
+          let newResults: typeof old.results = [];
+          if (lastCachedLog && newestIncomingLog) {
+            const newestLogTimeFromCache = moment(lastCachedLog.t);
+            // new results are all logs that are newer than the last cached log
+            newResults = msg.results.filter((entry) =>
+              newestLogTimeFromCache.isBefore(entry.t)
+            );
+          }
+          return {
+            ...old,
+            results: [...old.results, ...newResults],
+          };
+        }
       );
     },
   });
