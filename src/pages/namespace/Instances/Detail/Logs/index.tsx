4712f5a8e318d5f73567a28a65e1c9d1db568af3
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-31 12:36:30 +0200

    add icons
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Logs/index.tsx
+++ src/pages/namespace/Instances/Detail/Logs/index.tsx
@@ -1,8 +1,35 @@
+import { Bug, Copy, Filter, Maximize2, WrapText } from "lucide-react";
+
+import Button from "~/design/Button";
+import { ButtonBar } from "~/design/ButtonBar";
 import { FC } from "react";
 import ScrollContainer from "./ScrollContainer";
 
 const LogsPanel: FC<{ instanceId: string }> = ({ instanceId }) => (
-  <ScrollContainer instanceId={instanceId} />
+  <>
+    <div className="mb-5 flex gap-x-5">
+      <h3 className="grow font-medium">Logs</h3>
+      <ButtonBar>
+        <Button icon variant="outline" size="sm">
+          <Bug />
+        </Button>
+        <Button icon variant="outline" size="sm">
+          <WrapText />
+        </Button>
+        <Button icon variant="outline" size="sm">
+          <Filter />
+        </Button>
+        <Button icon variant="outline" size="sm">
+          <Maximize2 />
+        </Button>
+        <Button icon variant="outline" size="sm">
+          <Copy />
+        </Button>
+      </ButtonBar>
+    </div>
+
+    <ScrollContainer instanceId={instanceId} />
+  </>
 );
 
 export default LogsPanel;
