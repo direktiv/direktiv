7a4167ec183fd67ff99a764ad41266e345ee5b2c
Alexander Ertli <alexander.ertli@direktiv.io>
2023-07-31 09:46:57 +0000

    Remove eventualy
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/cluster/bus_test.go
+++ pkg/cluster/bus_test.go
@@ -209,7 +209,10 @@ func TestBusCluster(t *testing.T) {
 	// clientConfig.LookupdPollInterval = time.Millisecond * 100
 
 	createConsumer := func(topic, channel, connect string, mh *messageHandler) {
-		consumer, _ := nsq.NewConsumer(topic, channel, clientConfig)
+		consumer, err := nsq.NewConsumer(topic, channel, clientConfig)
+		if err != nil {
+			t.Errorf("creating client failed %s", err)
+		}
 		consumer.AddHandler(mh)
 		consumer.ConnectToNSQLookupd(connect)
 	}
@@ -243,22 +246,82 @@ func TestBusCluster(t *testing.T) {
 	err = producer.Publish("topic2", []byte("msg3"))
 	assert.NoError(t, err)
 
-	require.Eventually(t, func() bool {
-		status := true
+	status := make(chan bool)
 
-		// both message handler should have gotten the two messages
-		if mh1.counter != 2 || mh2.counter != 2 {
-			status = false
+	go func() {
+		// Both message handlers should have gotten the two messages
+		timeout := time.After(3 * time.Minute)
+
+		for {
+			if !(mh1.counter != 2 || mh2.counter != 2) {
+				// On the same channel. Only one should get it
+				if !((mh3.counter == 1 && mh4.counter == 1) ||
+					(mh3.counter == 0 && mh4.counter == 0)) {
+					status <- true
+				}
+			}
+
+			select {
+			case <-timeout:
+				// Handle the timeout case
+				// Add any necessary code here to handle the timeout condition
+				status <- false
+				return // Return from the goroutine to stop it
+			case <-time.After(time.Second):
+				// Wait for 1 second before checking again
+			}
+		}
+	}()
+
+	// Read the value from the status channel
+	if <-status == false {
+		t.Errorf("time out waiting for messages from bus")
+	}
+}
+
+func waitForMessageHandlers(t *testing.T, mh1, mh2, mh3, mh4 *messageHandler, expectedMsgCount int) {
+	// Create a channel to signal when the conditions are met or timeout occurs
+	status := make(chan bool)
+
+	// Start a goroutine to check the conditions
+	go func() {
+		timeout := 60 * time.Second
+		interval := 100 * time.Millisecond
+		maxAttempts := int(timeout / interval)
+
+		for attempt := 0; attempt < maxAttempts; attempt++ {
+			// Check the conditions
+			if mh1.counter == expectedMsgCount &&
+				mh2.counter == expectedMsgCount &&
+				((mh3.counter == 1 && mh4.counter == 1) || (mh3.counter == 0 && mh4.counter == 0)) {
+				status <- true // Signal that conditions are met
+				return
+			}
+
+			// Sleep for the specified interval before the next attempt
+			time.Sleep(interval)
 		}
 
-		// on the same channel. only one should get it
-		if (mh3.counter == 1 && mh4.counter == 1) ||
-			(mh3.counter == 0 && mh4.counter == 0) {
-			status = false
-		}
+		// If the loop completes without meeting the expected conditions, signal failure
+		status <- false
+	}()
 
-		return status
-	}, 60*time.Second, 100*time.Millisecond, "one channel does not work")
+	// Wait for the goroutine to signal completion or timeout
+	select {
+	case success := <-status:
+		if success {
+			// Test successful, conditions are met
+			return
+		}
+		// If status is false, it means the test failed due to the timeout
+		t.Fatalf("One channel does not work: mh1.counter=%d, mh2.counter=%d, mh3.counter=%d, mh4.counter=%d",
+			mh1.counter, mh2.counter, mh3.counter, mh4.counter)
+
+	case <-time.After(time.Minute * 2):
+		// Timeout occurred, fail the test
+		t.Fatalf("Timeout: One channel does not work: mh1.counter=%d, mh2.counter=%d, mh3.counter=%d, mh4.counter=%d",
+			mh1.counter, mh2.counter, mh3.counter, mh4.counter)
+	}
 }
 
 type messageHandler struct {
