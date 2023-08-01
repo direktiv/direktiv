2990fd10073494c07738c61d4151415c20111b64
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-01 17:05:06 +0200

    add maximized panel
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/util/store/logs.ts
+++ src/util/store/logs.ts
@@ -2,9 +2,11 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface LogsPreferencesState {
+  maximizedPanel: "none" | "logs" | "diagram" | "input" | "output";
   wordWrap: boolean;
   verboseLogs: boolean;
   actions: {
+    setMaximizedPanel: (layout: LogsPreferencesState["maximizedPanel"]) => void;
     setWordWrap: (layout: LogsPreferencesState["wordWrap"]) => void;
     setVerboseLogs: (layout: LogsPreferencesState["verboseLogs"]) => void;
   };
@@ -13,9 +15,11 @@ interface LogsPreferencesState {
 const useLogsPreferencesState = create<LogsPreferencesState>()(
   persist(
     (set) => ({
+      maximizedPanel: "none",
       wordWrap: false,
       verboseLogs: false,
       actions: {
+        setMaximizedPanel: (newVal) => set(() => ({ maximizedPanel: newVal })),
         setWordWrap: (newVal) => set(() => ({ wordWrap: newVal })),
         setVerboseLogs: (newVal) => set(() => ({ verboseLogs: newVal })),
       },
