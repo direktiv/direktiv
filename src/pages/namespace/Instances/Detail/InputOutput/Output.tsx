d9a9df645ae4ea4d1f8d3742b3dfa7633a9417fb
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-02 18:32:52 +0200

    add tooltips and make buttons work
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/InputOutput/Output.tsx
+++ src/pages/namespace/Instances/Detail/InputOutput/Output.tsx
@@ -1,10 +1,20 @@
 import { FC, PropsWithChildren } from "react";
+import { Maximize2, Minimize2 } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/design/Tooltip";
+import {
+  useLogsPreferencesActions,
+  useLogsPreferencesMaximizedPanel,
+} from "~/util/store/logs";
 
 import Button from "~/design/Button";
 import { ButtonBar } from "~/design/ButtonBar";
 import CopyButton from "~/design/CopyButton";
 import Editor from "~/design/Editor";
-import { Maximize2 } from "lucide-react";
 import { useInstanceId } from "../state/instanceContext";
 import { useOutput } from "~/api/instances/query/output";
 import { useTheme } from "~/util/store/theme";
@@ -26,6 +36,9 @@ const Output: FC<{ instanceIsFinished: boolean }> = ({
     enabled: instanceIsFinished,
   });
   const theme = useTheme();
+  const { setMaximizedPanel } = useLogsPreferencesActions();
+  const maximizedPanel = useLogsPreferencesMaximizedPanel();
+  const isMaximized = maximizedPanel === "input-output";
 
   if (!instanceIsFinished) {
     return (
@@ -46,16 +59,45 @@ const Output: FC<{ instanceIsFinished: boolean }> = ({
   return (
     <div className="flex grow flex-col gap-5 pb-12">
       <ButtonBar className="justify-end">
-        <CopyButton
-          value={workflowOutput}
-          buttonProps={{
-            variant: "outline",
-            size: "sm",
-          }}
-        />
-        <Button icon size="sm" variant="outline">
-          <Maximize2 />
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <CopyButton
+                  value={workflowOutput}
+                  buttonProps={{
+                    variant: "outline",
+                    size: "sm",
+                  }}
+                />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              {t("pages.instances.detail.inputOutput.copyOutput")}
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <Button
+                  icon
+                  size="sm"
+                  variant="outline"
+                  onClick={() => {
+                    setMaximizedPanel(isMaximized ? "none" : "input-output");
+                  }}
+                >
+                  {isMaximized ? <Minimize2 /> : <Maximize2 />}
+                </Button>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              {isMaximized
+                ? t("pages.instances.detail.inputOutput.minimizeOutput")
+                : t("pages.instances.detail.inputOutput.maximizeOutput")}
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </ButtonBar>
       <Editor
         value={workflowOutput}
