05cd91f6363b5476752e6b1831b528d0cb5454e3
porleaaron <porleaaron28@gmail.com>
2023-08-03 14:53:27 +0800

    remove asLabel

--- src/design/Button/index.stories.tsx
+++ src/design/Button/index.stories.tsx
@@ -274,11 +274,3 @@ export const AsChild = () => (
     </Button>
   </div>
 );
-export const AsLabel = () => (
-  <div className="flex flex-wrap gap-5">
-    <Button asLabel>Button As Label</Button>
-    <Button asLabel variant="primary">
-      Primary Button as Label
-    </Button>
-  </div>
-);
