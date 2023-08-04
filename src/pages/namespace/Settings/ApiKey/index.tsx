2570ef4c7c0f851b4b7e91d40e0e6e8a23eb5557
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-04 07:52:07 +0200

    show current api key input field, submit via enter
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Settings/ApiKey/index.tsx
+++ src/pages/namespace/Settings/ApiKey/index.tsx
@@ -1,57 +1,61 @@
 import { Eye, EyeOff } from "lucide-react";
+import { useApiActions, useApiKey } from "~/util/store/apiKey";
 
 import Button from "~/design/Button";
 import { Card } from "~/design/Card";
 import Input from "~/design/Input";
 import { InputWithButton } from "~/design/InputWithButton";
-import { useApiActions } from "~/util/store/apiKey";
 import { useState } from "react";
 
 const ApiKeyPanel = () => {
-  const [apiKey, setApiKey] = useState("");
   const [showKey, setShowKey] = useState(false);
   const { setApiKey: storeApiKey } = useApiActions();
+  const apiKeySore = useApiKey();
+  const [apiKey, setApiKey] = useState(apiKeySore ?? "");
 
   return (
-    <Card className="flex flex-col gap-5 p-5">
-      <InputWithButton className="w-full">
-        <Input
-          value={apiKey}
-          onChange={(e) => {
-            setApiKey(e.target.value);
-          }}
-          type={showKey ? "text" : "password"}
-          placeholder="enter API key"
-        />
-
-        <Button
-          icon
-          variant="ghost"
-          onClick={() => {
-            setShowKey((prev) => !prev);
-          }}
-        >
-          {showKey ? <EyeOff /> : <Eye />}
-        </Button>
-      </InputWithButton>
-
-      <Button
-        block
-        disabled={!apiKey}
-        onClick={() => {
+    <Card>
+      <form
+        className="flex flex-col gap-5 p-5"
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
           storeApiKey(apiKey);
         }}
       >
-        set API key
-      </Button>
-      <Button
-        variant="destructive"
-        onClick={() => {
-          storeApiKey(null);
-        }}
-      >
-        unset API key
-      </Button>
+        <InputWithButton className="w-full">
+          <Input
+            value={apiKey}
+            onChange={(e) => {
+              setApiKey(e.target.value);
+            }}
+            type={showKey ? "text" : "password"}
+            placeholder="enter API key"
+          />
+
+          <Button
+            icon
+            variant="ghost"
+            onClick={() => {
+              setShowKey((prev) => !prev);
+            }}
+          >
+            {showKey ? <EyeOff /> : <Eye />}
+          </Button>
+        </InputWithButton>
+
+        <Button block type="submit" disabled={!apiKey}>
+          set API key
+        </Button>
+        <Button
+          variant="destructive"
+          onClick={() => {
+            storeApiKey(null);
+          }}
+        >
+          unset API key
+        </Button>
+      </form>
     </Card>
   );
 };
