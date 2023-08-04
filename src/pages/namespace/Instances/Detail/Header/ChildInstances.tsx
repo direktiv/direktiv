1121a7b7e2940adbeecf0eba284036f2592bb74f
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-04 18:45:17 +0200

    display child instances
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/Header/ChildInstances.tsx
+++ src/pages/namespace/Instances/Detail/Header/ChildInstances.tsx
@@ -1,12 +1,32 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "~/design/Command";
+import { ConditionalWrapper, twMergeClsx } from "~/util/helpers";
+import { Popover, PopoverContent, PopoverTrigger } from "~/design/Popover";
+
+import Badge from "~/design/Badge";
+import Button from "~/design/Button";
+import { RefreshCcw } from "lucide-react";
+import { pages } from "~/util/router/pages";
+import { statusToBadgeVariant } from "../../utils";
 import { t } from "i18next";
 import { useInstanceId } from "../store/instanceContext";
 import { useInstances } from "~/api/instances/query/get";
+import { useNamespace } from "~/util/store/namespace";
+import { useState } from "react";
 
-const maxChildInstancesToShow = 20;
+const maxChildInstancesToShow = 50;
 
 const ChildInstances = () => {
   const instanceId = useInstanceId();
-  const { data } = useInstances({
+  const namespace = useNamespace();
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const { data, refetch, isFetching } = useInstances({
     limit: maxChildInstancesToShow + 1,
     offset: 0,
     filters: {
@@ -17,7 +37,16 @@ const ChildInstances = () => {
     },
   });
 
+  if (!namespace) return null;
+
+  const onInstanceSelect = (instance: string) => {
+    // useNavigate(); is not working for some reason
+    location.href = pages.instances.createHref({ namespace, instance });
+  };
+
   const childCount = data?.instances.results.length ?? 0;
+
+  const needsPopover = childCount > 0;
   const moreInstances = childCount > maxChildInstancesToShow;
 
   // need to return an element with a height to avoid layout shifts because of css grid
@@ -28,18 +57,96 @@ const ChildInstances = () => {
       <div className="text-gray-10 dark:text-gray-dark-10">
         {t("pages.instances.list.tableHeader.childInstances.label")}
       </div>
-      <span>
-        {moreInstances
-          ? t(
-              "pages.instances.list.tableHeader.childInstances.instanceCountMax",
-              {
-                count: childCount,
-              }
-            )
-          : t("pages.instances.list.tableHeader.childInstances.instanceCount", {
-              count: childCount,
-            })}
-      </span>
+      <div className="flex gap-x-1">
+        <ConditionalWrapper
+          condition={needsPopover}
+          wrapper={(children) => (
+            <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+              <PopoverTrigger asChild>{children}</PopoverTrigger>
+              <PopoverContent className="w-96 p-0">
+                <Command>
+                  <CommandInput
+                    placeholder={
+                      moreInstances
+                        ? t(
+                            "pages.instances.list.tableHeader.childInstances.searchPlaceholderMax",
+                            {
+                              count: maxChildInstancesToShow,
+                            }
+                          )
+                        : t(
+                            "pages.instances.list.tableHeader.childInstances.searchPlaceholder"
+                          )
+                    }
+                  />
+                  <CommandList className="max-h-[278px]">
+                    <CommandEmpty>
+                      {t(
+                        "pages.instances.list.tableHeader.childInstances.notFound"
+                      )}
+                    </CommandEmpty>
+                    <CommandGroup>
+                      {data.instances.results.map((instance) => (
+                        <CommandItem
+                          key={instance.id}
+                          value={instance.id}
+                          onSelect={(currentValue: string) => {
+                            onInstanceSelect(currentValue);
+                            setPopoverOpen(false);
+                          }}
+                        >
+                          <div className="flex gap-x-4">
+                            <Badge
+                              variant={statusToBadgeVariant(instance.status)}
+                              className="font-normal"
+                              icon={instance.status}
+                            />
+                            <Badge variant="outline">
+                              {instance.id.slice(0, 8)}
+                            </Badge>
+
+                            {instance.as}
+                          </div>
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+          )}
+        >
+          <span
+            className={twMergeClsx(needsPopover && "cursor-pointer underline")}
+          >
+            {moreInstances
+              ? t(
+                  "pages.instances.list.tableHeader.childInstances.instanceCountMax",
+                  {
+                    count: maxChildInstancesToShow,
+                  }
+                )
+              : t(
+                  "pages.instances.list.tableHeader.childInstances.instanceCount",
+                  {
+                    count: childCount,
+                  }
+                )}
+          </span>
+        </ConditionalWrapper>
+        <Button
+          icon
+          size="sm"
+          variant="ghost"
+          className="relative -top-0.5"
+          disabled={isFetching}
+          onClick={() => {
+            refetch();
+          }}
+        >
+          <RefreshCcw />
+        </Button>
+      </div>
     </div>
   );
 };
