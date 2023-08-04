4954d83b54aec64884f24a4a831d6d443c8f04f5
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-04 07:44:34 +0200

    add instanceWasSuccessfull
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/InputOutput/index.tsx
+++ src/pages/namespace/Instances/Detail/InputOutput/index.tsx
@@ -13,9 +13,11 @@ const InputOutput = () => {
   const { data } = useInstanceDetails({ instanceId });
   const tabs = ["input", "output"] as const;
 
-  const instanceIsFinished = data?.instance.status === "complete";
+  const instanceWasSuccessfull = data?.instance.status === "complete";
+  const instanceIsFinished = data?.instance.status !== "pending";
+
   const [activeTab, setActiveTab] = useState<(typeof tabs)[number]>(
-    instanceIsFinished ? "output" : "input"
+    instanceWasSuccessfull ? "output" : "input"
   );
 
   useEffect(() => {
@@ -23,8 +25,8 @@ const InputOutput = () => {
      * switch to the output tab when the instance is switching
      * to a finished state while this component is mounted
      */
-    setActiveTab(instanceIsFinished ? "output" : "input");
-  }, [instanceIsFinished]);
+    setActiveTab(instanceWasSuccessfull ? "output" : "input");
+  }, [instanceWasSuccessfull]);
 
   if (!data) return null;
 
