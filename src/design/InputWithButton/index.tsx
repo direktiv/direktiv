4b1726c8c1b2b9ceb4fec4240f320afba9c53bdb
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-07 15:07:03 +0200

    [ was missing
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/InputWithButton/index.tsx
+++ src/design/InputWithButton/index.tsx
@@ -14,7 +14,7 @@ export const InputWithButton: React.FC<HTMLAttributes<HTMLDivElement>> = ({
     <div
       {...props}
       className={twMergeClsx(
-        "&_input]:pr-10 relative flex w-full items-end",
+        "relative flex w-full items-end [&_input]:pr-10",
         className
       )}
     >
