f2d15dab1b0ec93e6d4fc275876b2b91d8cb37cb
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-07 15:07:59 +0200

    delete unused export
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/InputWithButton/index.tsx
+++ src/design/InputWithButton/index.tsx
@@ -30,33 +30,3 @@ export const InputWithButton: React.FC<HTMLAttributes<HTMLDivElement>> = ({
   );
 };
 InputWithButton.displayName = "InputWithButton";
-
-export const InputWithButtonWithState: React.FC<
-  HTMLAttributes<HTMLDivElement> & {
-    icon?: boolean;
-  }
-> = ({ children, className, ...props }) => {
-  const [firstChild, secondChild] = Array.isArray(children)
-    ? children
-    : [children];
-  return (
-    <div
-      {...props}
-      className={twMergeClsx(
-        "relative flex items-end p-1",
-        "[&_input]:border-0 [&_input]:outline-none [&_input]:focus:border-0 [&_input]:focus:outline-none",
-        className
-      )}
-    >
-      {firstChild}
-      <div
-        className={twMergeClsx(
-          "absolute right-1 flex h-9 items-center justify-center [&_button]:h-8"
-        )}
-      >
-        {secondChild}
-      </div>
-    </div>
-  );
-};
-InputWithButtonWithState.displayName = "InputWithButtonWithState";
