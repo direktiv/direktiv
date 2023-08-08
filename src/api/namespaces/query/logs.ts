cce3d1db0c5f9c3d5a71ed50daf2104d1ccdee69
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-08 14:20:07 +0200

    make streaming work
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/namespaces/query/logs.ts
+++ src/api/namespaces/query/logs.ts
@@ -6,10 +6,51 @@ import {
 } from "@tanstack/react-query";
 
 import { apiFactory } from "~/api/apiFactory";
+import moment from "moment";
 import { namespaceKeys } from "..";
 import { useApiKey } from "~/util/store/apiKey";
 import { useNamespace } from "~/util/store/namespace";
 import { useStreaming } from "~/api/streaming";
+
+const updateCache = (
+  oldData: NamespaceLogListSchemaType | undefined,
+  msg: NamespaceLogListSchemaType
+) => {
+  if (!oldData) {
+    return msg;
+  }
+  /**
+   * Dedup logs. The onMessage callback gets called in two different cases:
+   *
+   * case 1:
+   * when the SSE connection is established, the whole set of logs is received
+   *
+   * case 2:
+   * after the connection is established and only some new log entries are received
+   *
+   * it's also important to note that multiple components can subscribe to the same
+   * cache, so we can have case 1 and 2 at the same time, or case 1 after case 2
+   */
+  const lastCachedLog = oldData.results[oldData.results.length - 1];
+  let newResults: typeof oldData.results = [];
+
+  // there was a previous cache, but with no entries yet
+  if (!lastCachedLog) {
+    newResults = msg.results;
+    // there was a previous cache with entries
+  } else {
+    const newestLogTimeFromCache = moment(lastCachedLog.t);
+    // new results are all logs that are newer than the last cached log
+    newResults = msg.results.filter((entry) =>
+      newestLogTimeFromCache.isBefore(entry.t)
+    );
+  }
+
+  return {
+    ...oldData,
+    results: [...oldData.results, ...newResults],
+  };
+};
 
 export const getInstanceDetails = apiFactory({
   url: ({ namespace, baseUrl }: { baseUrl?: string; namespace: string }) =>
@@ -45,7 +86,7 @@ export const useNamespaceLogsStream = ({
     onMessage: (msg) => {
       queryClient.setQueryData<NamespaceLogListSchemaType>(
         namespaceKeys.logs(namespace, { apiKey: apiKey ?? undefined }),
-        () => msg
+        (oldData) => updateCache(oldData, msg)
       );
     },
   });
