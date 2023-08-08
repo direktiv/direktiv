87e57ba700759549108c10bca659bc2c78e592e4
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2023-08-08 14:31:41 +0200

    Add relative time and tooltip
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- src/pages/namespace/Events/List/Row.tsx
+++ src/pages/namespace/Events/List/Row.tsx
@@ -1,8 +1,15 @@
 import { TableCell, TableRow } from "~/design/Table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/design/Tooltip";
 
 import { EventSchemaType } from "~/api/events/schema";
 import TooltipCopyBadge from "../../../../design/TooltipCopyBadge";
-import { TooltipProvider } from "~/design/Tooltip";
+import { useTranslation } from "react-i18next";
+import useUpdatedAt from "~/hooksNext/useUpdatedAt";
 
 const Row = ({
   event,
@@ -10,19 +17,36 @@ const Row = ({
 }: {
   event: EventSchemaType;
   namespace: string;
-}) => (
-  <TooltipProvider>
-    <TableRow data-testid={`event-row-${event.id}`}>
-      <TableCell>{event.type}</TableCell>
-      <TableCell>
-        <TooltipCopyBadge value={event.id} variant="outline">
-          {event.id.slice(0, 8)}
-        </TooltipCopyBadge>
-      </TableCell>
-      <TableCell>{event.source}</TableCell>
-      <TableCell>{event.receivedAt}</TableCell>
-    </TableRow>
-  </TooltipProvider>
-);
+}) => {
+  const { t } = useTranslation();
+
+  const receivedAt = useUpdatedAt(event.receivedAt);
+
+  return (
+    <TooltipProvider>
+      <TableRow data-testid={`event-row-${event.id}`}>
+        <TableCell>{event.type}</TableCell>
+        <TableCell>
+          <TooltipCopyBadge value={event.id} variant="outline">
+            {event.id.slice(0, 8)}
+          </TooltipCopyBadge>
+        </TableCell>
+        <TableCell>{event.source}</TableCell>
+        <TableCell>
+          <Tooltip>
+            <TooltipTrigger data-testid="receivedAt-tooltip-trigger">
+              {t("pages.events.list.tableRow.realtiveTime", {
+                relativeTime: receivedAt,
+              })}
+            </TooltipTrigger>
+            <TooltipContent data-testid="receivedAt-tooltip-content">
+              {event.receivedAt}
+            </TooltipContent>
+          </Tooltip>
+        </TableCell>
+      </TableRow>
+    </TooltipProvider>
+  );
+};
 
 export default Row;
