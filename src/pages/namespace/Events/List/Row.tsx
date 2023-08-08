0cc39e879c49b69e2b727ceb0d6a8ed8da4f1e66
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2023-08-08 13:33:32 +0200

    Use TooltipCopyBadge in events list
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- src/pages/namespace/Events/List/Row.tsx
+++ src/pages/namespace/Events/List/Row.tsx
@@ -1,6 +1,7 @@
 import { TableCell, TableRow } from "~/design/Table";
 
 import { EventSchemaType } from "~/api/events/schema";
+import TooltipCopyBadge from "../../../../design/TooltipCopyBadge";
 import { TooltipProvider } from "~/design/Tooltip";
 
 const Row = ({
@@ -13,7 +14,12 @@ const Row = ({
   <TooltipProvider>
     <TableRow>
       <TableCell>{event.type}</TableCell>
-      <TableCell>{event.id}</TableCell>
+      <TableCell>
+        <TooltipCopyBadge
+          value={event.id}
+          displayValue={event.id.slice(0, 8)}
+        />
+      </TableCell>
       <TableCell>{event.source}</TableCell>
       <TableCell>{event.receivedAt}</TableCell>
     </TableRow>
