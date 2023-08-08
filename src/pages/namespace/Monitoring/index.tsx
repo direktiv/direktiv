72ced235e86542bd102aa1e65e199f008cfce689
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-08 10:08:01 +0200

    stream logs
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Monitoring/index.tsx
+++ src/pages/namespace/Monitoring/index.tsx
@@ -1,9 +1,12 @@
 import { ActivitySquare } from "lucide-react";
 import { Card } from "~/design/Card";
+import { useNamespacelogs } from "~/api/namespaces/query/logs";
 import { useTranslation } from "react-i18next";
 
 const InstancesPage = () => {
   const { t } = useTranslation();
+
+  useNamespacelogs();
   return (
     <div className="flex grow flex-col gap-y-4 p-5">
       <h3 className="flex items-center gap-x-2 font-bold">
