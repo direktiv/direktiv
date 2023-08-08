bcb85a72ebeb6bd3259178ae35030dd660938f02
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-08 11:11:51 +0200

    main layout
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Monitoring/index.tsx
+++ src/pages/namespace/Monitoring/index.tsx
@@ -1,6 +1,7 @@
 import { ActivitySquare } from "lucide-react";
 import { Card } from "~/design/Card";
 import Logs from "./Logs";
+import { twMergeClsx } from "~/util/helpers";
 import { useNamespacelogs } from "~/api/namespaces/query/logs";
 import { useTranslation } from "react-i18next";
 
@@ -14,9 +15,20 @@ const InstancesPage = () => {
         <ActivitySquare className="h-5" />
         {t("pages.monitoring.title")}
       </h3>
-      <Card className="p-5">
-        <Logs />
-      </Card>
+      <div
+        className={twMergeClsx(
+          "grid grow gap-5",
+          "grid-rows-[100vh_50vh_50vh]",
+          "md:grid-rows-[minmax(300px,45vh)_1fr]",
+          "md:grid-cols-[minmax(430px,1fr)_1fr]"
+        )}
+      >
+        <Card className="relative grid grid-rows-[auto,1fr,auto] p-5 md:col-span-2">
+          <Logs />
+        </Card>
+        <Card className="p-5"></Card>
+        <Card className="p-5"></Card>
+      </div>
     </div>
   );
 };
