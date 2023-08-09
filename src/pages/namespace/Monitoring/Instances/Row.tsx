69b25ce46377ce370ce1cb2cf2a6e289ecd69ca3
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-09 12:23:45 +0200

    link to workflow
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Monitoring/Instances/Row.tsx
+++ src/pages/namespace/Monitoring/Instances/Row.tsx
@@ -15,18 +15,46 @@ import Alert from "~/design/Alert";
 import Badge from "~/design/Badge";
 import { ConditionalWrapper } from "~/util/helpers";
 import { InstanceSchemaType } from "~/api/instances/schema";
+import { Link } from "react-router-dom";
 import TooltipCopyBadge from "~/design/TooltipCopyBadge";
+import { pages } from "~/util/router/pages";
 import { statusToBadgeVariant } from "../../Instances/utils";
+import { useNamespace } from "~/util/store/namespace";
 import { useTranslation } from "react-i18next";
 import useUpdatedAt from "~/hooksNext/useUpdatedAt";
 
 export const InstanceRow = ({ instance }: { instance: InstanceSchemaType }) => {
   const { t } = useTranslation();
   const updatedAt = useUpdatedAt(instance.updatedAt);
+  const namespace = useNamespace();
+
+  if (!namespace) return null;
+
   return (
     <TooltipProvider>
       <TableRow>
-        <TableCell>{instance.as}</TableCell>
+        <TableCell>
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                onClick={(e) => {
+                  e.stopPropagation(); // prevent the onClick on the row from firing when clicking the workflow link
+                }}
+                to={pages.explorer.createHref({
+                  namespace,
+                  path: instance.as,
+                  subpage: "workflow",
+                })}
+                className="hover:underline"
+              >
+                {instance.as}
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              {t("pages.monitoring.instances.openWorkflowTooltip")}
+            </TooltipContent>
+          </Tooltip>
+        </TableCell>
         <TableCell>
           <TooltipCopyBadge value={instance.id} variant="outline">
             {instance.id.slice(0, 8)}
@@ -38,11 +66,7 @@ export const InstanceRow = ({ instance }: { instance: InstanceSchemaType }) => {
             wrapper={(children) => (
               <HoverCard>
                 <HoverCardTrigger>{children}</HoverCardTrigger>
-                <HoverCardContent
-                  asChild
-                  noBackground
-                  data-testid={`instance-row-state-error-tooltip-${instance.id}`}
-                >
+                <HoverCardContent asChild noBackground>
                   <Alert variant="error">
                     <span className="font-bold">{instance.errorCode}</span>
                     <br />
@@ -53,7 +77,6 @@ export const InstanceRow = ({ instance }: { instance: InstanceSchemaType }) => {
             )}
           >
             <Badge
-              data-testid={`instance-row-state-${instance.id}`}
               variant={statusToBadgeVariant(instance.status)}
               icon={instance.status}
             >
@@ -63,18 +86,12 @@ export const InstanceRow = ({ instance }: { instance: InstanceSchemaType }) => {
         </TableCell>
         <TableCell>
           <Tooltip>
-            <TooltipTrigger
-              data-testid={`instance-row-relative-updated-time-${instance.id}`}
-            >
+            <TooltipTrigger>
               {t("pages.instances.list.tableRow.realtiveTime", {
                 relativeTime: updatedAt,
               })}
             </TooltipTrigger>
-            <TooltipContent
-              data-testid={`instance-row-absolute-updated-time-${instance.id}`}
-            >
-              {instance.updatedAt}
-            </TooltipContent>
+            <TooltipContent>{instance.updatedAt}</TooltipContent>
           </Tooltip>
         </TableCell>
       </TableRow>
