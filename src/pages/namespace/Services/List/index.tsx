6f45372a8cba01c3649e36b4542e1a2411d9aa3b
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-14 10:05:26 +0200

    list services
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Services/List/index.tsx
+++ src/pages/namespace/Services/List/index.tsx
@@ -1,14 +1,27 @@
 import { Boxes } from "lucide-react";
 import { Card } from "~/design/Card";
+import { useServices } from "~/api/services/query/get";
 
-const ServicesListPage = () => (
-  <div className="flex grow flex-col gap-y-4 p-5">
-    <h3 className="flex items-center gap-x-2 font-bold">
-      <Boxes className="h-5" />
-      Services List
-    </h3>
-    <Card></Card>
-  </div>
-);
+const ServicesListPage = () => {
+  const { data } = useServices();
+
+  console.log("🚀", data);
+
+  return (
+    <div className="flex grow flex-col gap-y-4 p-5">
+      <h3 className="flex items-center gap-x-2 font-bold">
+        <Boxes className="h-5" />
+        Services List
+      </h3>
+      <Card>
+        {data?.functions.map((service) => (
+          <h1 className="p-2" key={service.info.name}>
+            {service.info.name}
+          </h1>
+        ))}
+      </Card>
+    </div>
+  );
+};
 
 export default ServicesListPage;
