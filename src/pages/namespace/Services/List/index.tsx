7b125caf75d96760344cd6c57fb41c03864188da
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-14 10:34:03 +0200

    add delete service button
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Services/List/index.tsx
+++ src/pages/namespace/Services/List/index.tsx
@@ -1,11 +1,12 @@
 import { Boxes } from "lucide-react";
+import Button from "~/design/Button";
 import { Card } from "~/design/Card";
+import { useDeleteService } from "~/api/services/mutate/delete";
 import { useServices } from "~/api/services/query/get";
 
 const ServicesListPage = () => {
   const { data } = useServices();
-
-  console.log("🚀", data);
+  const { mutate: deleteService } = useDeleteService();
 
   return (
     <div className="flex grow flex-col gap-y-4 p-5">
@@ -16,7 +17,18 @@ const ServicesListPage = () => {
       <Card>
         {data?.functions.map((service) => (
           <h1 className="p-2" key={service.info.name}>
-            {service.info.name}
+            {service.info.name}{" "}
+            <Button
+              variant="destructive"
+              size="sm"
+              onClick={() => {
+                deleteService({
+                  service: service.info.name,
+                });
+              }}
+            >
+              Delete
+            </Button>
           </h1>
         ))}
       </Card>
