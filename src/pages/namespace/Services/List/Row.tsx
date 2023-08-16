b15d25c5a8a1c3c3866df6730278553fc96f6b31
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-16 11:25:59 +0200

    remove condition names
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Services/List/Row.tsx
+++ src/pages/namespace/Services/List/Row.tsx
@@ -65,7 +65,7 @@ const ServicesTableRow: FC<{
                   message={res.message}
                   className="w-fit"
                 >
-                  {t(`pages.services.list.conditionNames.${res.name}`)}
+                  {res.name}
                 </StatusBadge>
               );
             })}
