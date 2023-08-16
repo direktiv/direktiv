ac0e5d8562963dda36bc81344d80de19ffa0fac3
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-16 15:23:27 +0200

    style table header
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Services/Detail/index.tsx
+++ src/pages/namespace/Services/Detail/index.tsx
@@ -1,8 +1,22 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from "~/design/Table";
+
+import { Card } from "~/design/Card";
+import { Diamond } from "lucide-react";
 import { pages } from "~/util/router/pages";
 import { useServiceDetails } from "~/api/services/query/details";
+import { useTranslation } from "react-i18next";
 
 const ServiceDetailPage = () => {
   const { service } = pages.services.useParams();
+
+  const { t } = useTranslation();
 
   const { data } = useServiceDetails({
     service: service ?? "",
@@ -10,8 +24,39 @@ const ServiceDetailPage = () => {
   if (!service) return null;
 
   return (
-    <div>
-      <h1>service detail</h1>
+    <div className="flex grow flex-col gap-y-4 p-5">
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <h3 className="flex grow items-center gap-x-2 pb-1 font-bold">
+          <Diamond className="h-5" />
+          {service}
+        </h3>
+      </div>
+
+      <Card>
+        <Table>
+          <TableHead>
+            <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
+              <TableHeaderCell>
+                {t("pages.services.list.tableHeader.name")}
+              </TableHeaderCell>
+              <TableHeaderCell className="w-48">
+                {t("pages.services.list.tableHeader.image")}
+              </TableHeaderCell>
+              <TableHeaderCell className="w-16">
+                {t("pages.services.list.tableHeader.scale")}
+              </TableHeaderCell>
+              <TableHeaderCell className="w-20">
+                {t("pages.services.list.tableHeader.size")}
+              </TableHeaderCell>
+              <TableHeaderCell className="w-48">
+                {t("pages.services.list.tableHeader.cmd")}
+              </TableHeaderCell>
+              <TableHeaderCell className="w-16" />
+            </TableRow>
+          </TableHead>
+          <TableBody></TableBody>
+        </Table>
+      </Card>
       {data?.revisions?.map((revision) => (
         <div key={revision.name}>{revision.name}</div>
       ))}
