664ad571a19905ab47050a9b0b84cee46180a435
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-16 14:45:05 +0200

    fetch details
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Services/Detail/index.tsx
+++ src/pages/namespace/Services/Detail/index.tsx
@@ -1,7 +1,12 @@
 import { pages } from "~/util/router/pages";
+import { useServiceDetails } from "~/api/services/query/details";
 
 const ServiceDetailPage = () => {
   const { service } = pages.services.useParams();
+
+  const { data } = useServiceDetails({
+    service,
+  });
   if (!service) return null;
 
   return <h1>service detail page: {service}</h1>;
