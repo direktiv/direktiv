e91dc74ac810397673800e11304a4ffa84e58613
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-16 11:54:13 +0200

    export ServiceStreamingSchemaType
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/services/schema.ts
+++ src/api/services/schema.ts
@@ -130,3 +130,4 @@ export type ServiceSchemaType = z.infer<typeof ServiceSchema>;
 export type StatusSchemaType = z.infer<typeof StatusSchema>;
 export type ServicesListSchemaType = z.infer<typeof ServicesListSchema>;
 export type ServiceFormSchemaType = z.infer<typeof ServiceFormSchema>;
+export type ServiceStreamingSchemaType = z.infer<typeof ServiceStreamingSchema>;
