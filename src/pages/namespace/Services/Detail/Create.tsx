eeab2a04d6a5211e7544390aea32b0174dead233
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-17 09:10:25 +0200

    add create revision component
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/pages/namespace/Services/Detail/Create.tsx
@@ -0,0 +1,194 @@
+import {
+  DialogClose,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/design/Dialog";
+import { Diamond, PlusCircle } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/design/Select";
+import {
+  ServiceRevisionFormSchema,
+  ServiceRevisionFormSchemaType,
+} from "~/api/services/schema";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import Button from "~/design/Button";
+import FormErrors from "~/componentsNext/FormErrors";
+import Input from "~/design/Input";
+import { Slider } from "~/design/Slider";
+import { useCreateServiceRevision } from "~/api/services/mutate/createRevision";
+import { useServiceDetails } from "~/api/services/query/details";
+import { useTranslation } from "react-i18next";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const availableSizes = [0, 1, 2] as const;
+
+const CreateRevision = ({
+  service,
+  close,
+}: {
+  service: string;
+  close: () => void;
+}) => {
+  const { t } = useTranslation();
+
+  const { data } = useServiceDetails({ service });
+  const { mutate: createServiceRevision, isLoading } = useCreateServiceRevision(
+    {
+      onSuccess: () => {
+        close();
+      },
+    }
+  );
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    setValue,
+    formState: { isDirty, errors, isValid, isSubmitted },
+  } = useForm<ServiceRevisionFormSchemaType>({
+    defaultValues: {
+      minscale: 0,
+      size: 1,
+    },
+    resolver: zodResolver(ServiceRevisionFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<ServiceRevisionFormSchemaType> = ({
+    cmd,
+    image,
+    minscale,
+    size,
+  }) => {
+    createServiceRevision({
+      service,
+      payload: {
+        cmd,
+        image,
+        minscale,
+        size,
+      },
+    });
+  };
+
+  // you can not submit if the form has not changed or if there are any errors and
+  // you have already submitted the form (errors will first show up after submit)
+  const disableSubmit = !isDirty || (isSubmitted && !isValid);
+
+  const formId = `new-service-revision`;
+
+  const maxScale = data?.config.maxscale;
+  if (maxScale === undefined) return null;
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>
+          <Diamond /> {t("pages.services.revision.create.title")}
+        </DialogTitle>
+      </DialogHeader>
+
+      <div className="my-3">
+        <FormErrors errors={errors} className="mb-5" />
+        <form
+          id={formId}
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-5"
+        >
+          <fieldset className="flex items-center gap-5">
+            <label className="w-[90px] text-right text-[14px]" htmlFor="image">
+              {t("pages.services.revision.create.imageLabel")}
+            </label>
+            <Input
+              id="image"
+              placeholder={t("pages.services.revision.create.imagePlaceholder")}
+              {...register("image")}
+            />
+          </fieldset>
+          <fieldset className="flex items-center gap-5">
+            <label className="w-[90px] text-right text-[14px]" htmlFor="scale">
+              {t("pages.services.revision.create.scaleLabel")}
+            </label>
+            <div className="flex w-full gap-5">
+              <Input className="w-12" readOnly value={watch("size")} disabled />
+              <Slider
+                id="scale"
+                step={1}
+                min={0}
+                max={maxScale}
+                value={[watch("size") ?? 0]}
+                onValueChange={(e) => {
+                  const newValue = e[0];
+                  newValue !== undefined && setValue("size", newValue);
+                }}
+              />
+            </div>
+          </fieldset>
+          <fieldset className="flex items-center gap-5">
+            <label
+              className="w-[90px] text-right text-[14px]"
+              htmlFor="minscale"
+            >
+              {t("pages.services.revision.create.sizeLabel")}
+            </label>
+            <Select
+              value={`${getValues("minscale")}`}
+              onValueChange={(value) => setValue("minscale", parseInt(value))}
+            >
+              <SelectTrigger variant="outline" className="w-full" id="minscale">
+                <SelectValue
+                  placeholder={t(
+                    "pages.services.revision.create.sizePlaceholder"
+                  )}
+                />
+              </SelectTrigger>
+              <SelectContent>
+                {availableSizes.map((size) => (
+                  <SelectItem key={size} value={`${size}`}>
+                    {t(`pages.services.revision.create.sizeValues.${size}`)}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </fieldset>
+          <fieldset className="flex items-center gap-5">
+            <label className="w-[90px] text-right text-[14px]" htmlFor="cmd">
+              {t("pages.services.revision.create.cmdLabel")}
+            </label>
+            <Input
+              id="cmd"
+              placeholder={t("pages.services.revision.create.cmdPlaceholder")}
+              {...register("cmd")}
+            />
+          </fieldset>
+        </form>
+      </div>
+      <DialogFooter>
+        <DialogClose asChild>
+          <Button variant="ghost">
+            {t("pages.services.revision.create.createBtn")}
+          </Button>
+        </DialogClose>
+        <Button
+          type="submit"
+          disabled={disableSubmit}
+          loading={isLoading}
+          form={formId}
+        >
+          {!isLoading && <PlusCircle />}
+          {t("pages.services.revision.create.createBtn")}
+        </Button>
+      </DialogFooter>
+    </>
+  );
+};
+
+export default CreateRevision;
--- src/pages/namespace/Services/Detail/index.tsx
+++ src/pages/namespace/Services/Detail/index.tsx
@@ -1,3 +1,5 @@
+import { Dialog, DialogContent, DialogTrigger } from "~/design/Dialog";
+import { Diamond, PlusCircle } from "lucide-react";
 import {
   Table,
   TableBody,
@@ -5,59 +7,96 @@ import {
   TableHeaderCell,
   TableRow,
 } from "~/design/Table";
+import { useEffect, useState } from "react";
 
+import Button from "~/design/Button";
 import { Card } from "~/design/Card";
-import { Diamond } from "lucide-react";
+import CreateServiceRevision from "./Create";
 import { pages } from "~/util/router/pages";
 import { useServiceDetails } from "~/api/services/query/details";
 import { useTranslation } from "react-i18next";
 
 const ServiceDetailPage = () => {
-  const { service } = pages.services.useParams();
-
   const { t } = useTranslation();
-
+  const { service } = pages.services.useParams();
   const { data } = useServiceDetails({
     service: service ?? "",
   });
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [deleteService, setDeleteService] = useState<string>();
+  const [createService, setCreateService] = useState(false);
+
+  useEffect(() => {
+    if (dialogOpen === false) {
+      setDeleteService(undefined);
+      setCreateService(false);
+    }
+  }, [dialogOpen]);
+
   if (!service) return null;
 
   return (
     <div className="flex grow flex-col gap-y-4 p-5">
-      <div className="flex flex-col gap-4 sm:flex-row">
-        <h3 className="flex grow items-center gap-x-2 pb-1 font-bold">
-          <Diamond className="h-5" />
-          {service}
-        </h3>
-      </div>
-      <Card>
-        <Table>
-          <TableHead>
-            <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
-              <TableHeaderCell>
-                {t("pages.services.list.tableHeader.name")}
-              </TableHeaderCell>
-              <TableHeaderCell className="w-48">
-                {t("pages.services.list.tableHeader.image")}
-              </TableHeaderCell>
-              <TableHeaderCell className="w-16">
-                {t("pages.services.list.tableHeader.scale")}
-              </TableHeaderCell>
-              <TableHeaderCell className="w-20">
-                {t("pages.services.list.tableHeader.size")}
-              </TableHeaderCell>
-              <TableHeaderCell className="w-48">
-                {t("pages.services.list.tableHeader.cmd")}
-              </TableHeaderCell>
-              <TableHeaderCell className="w-16" />
-            </TableRow>
-          </TableHead>
-          <TableBody></TableBody>
-        </Table>
-      </Card>
-      {data?.revisions?.map((revision) => (
-        <div key={revision.name}>{revision.name}</div>
-      ))}
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <div className="flex flex-col gap-4 sm:flex-row">
+          <h3 className="flex grow items-center gap-x-2 pb-1 font-bold">
+            <Diamond className="h-5" />
+            {service}
+          </h3>
+          <DialogTrigger asChild>
+            <Button onClick={() => setCreateService(true)} variant="outline">
+              <PlusCircle />
+              {t("pages.services.revision.list.create")}
+            </Button>
+          </DialogTrigger>
+        </div>
+        <Card>
+          <Table>
+            <TableHead>
+              <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
+                <TableHeaderCell>
+                  {t("pages.services.list.tableHeader.name")}
+                </TableHeaderCell>
+                <TableHeaderCell className="w-48">
+                  {t("pages.services.list.tableHeader.image")}
+                </TableHeaderCell>
+                <TableHeaderCell className="w-16">
+                  {t("pages.services.list.tableHeader.scale")}
+                </TableHeaderCell>
+                <TableHeaderCell className="w-20">
+                  {t("pages.services.list.tableHeader.size")}
+                </TableHeaderCell>
+                <TableHeaderCell className="w-48">
+                  {t("pages.services.list.tableHeader.cmd")}
+                </TableHeaderCell>
+                <TableHeaderCell className="w-16" />
+              </TableRow>
+            </TableHead>
+            <TableBody></TableBody>
+          </Table>
+          {data?.revisions?.map((revision) => (
+            <div key={revision.name}>{revision.name}</div>
+          ))}
+        </Card>
+
+        <DialogContent>
+          {/* {deleteService && (
+            <Delete
+              service={deleteService}
+              close={() => {
+                setDialogOpen(false);
+              }}
+            />
+          )} */}
+          {createService && (
+            <CreateServiceRevision
+              service={service}
+              close={() => setDialogOpen(false)}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
