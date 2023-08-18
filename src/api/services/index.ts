b6ecda3ea53ccc05649671f9138fcd39ba0a68df
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-18 12:06:48 +0200

    add serviceRevisionDetail key
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -19,4 +19,21 @@ export const serviceKeys = {
         service,
       },
     ] as const,
+  serviceRevisionDetail: (
+    namespace: string,
+    {
+      apiKey,
+      service,
+      revision,
+    }: { apiKey?: string; service: string; revision: string }
+  ) =>
+    [
+      {
+        scope: "service-detail-revision",
+        apiKey,
+        namespace,
+        service,
+        revision,
+      },
+    ] as const,
 };
