b6982165678617be6dea366f27cb460c7494d115
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-18 13:02:36 +0200

    add hook for revision detail
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/api/services/query/revision.ts
@@ -0,0 +1,35 @@
+import { ServicesRevisionListSchemaType } from "../schema";
+import { serviceKeys } from "..";
+import { useApiKey } from "~/util/store/apiKey";
+import { useNamespace } from "~/util/store/namespace";
+import { useQuery } from "@tanstack/react-query";
+
+/**
+ * The queryFn of this hook will never return any data because we only have
+ * a streaming endoint for this data. This hook is only used to subscribe to
+ * the correct cache key. Data for this key will be added by a streaming provider
+ */
+export const useServiceRevision = ({
+  service,
+  revision,
+}: {
+  service: string;
+  revision: string;
+}) => {
+  const apiKey = useApiKey();
+  const namespace = useNamespace();
+
+  if (!namespace) {
+    throw new Error("namespace is undefined");
+  }
+
+  return useQuery({
+    queryKey: serviceKeys.serviceRevisionDetail(namespace, {
+      apiKey: apiKey ?? undefined,
+      service,
+      revision,
+    }),
+    queryFn: (): ServicesRevisionListSchemaType | undefined => undefined,
+    enabled: !!namespace,
+  });
+};
--- src/pages/namespace/Services/Detail/Revision/Header.tsx
+++ src/pages/namespace/Services/Detail/Revision/Header.tsx
@@ -1,13 +1,20 @@
 import { pages } from "~/util/router/pages";
+import { useServiceRevision } from "~/api/services/query/revision";
 
 const Header = () => {
   const { service, revision } = pages.services.useParams();
+
+  const { data } = useServiceRevision({
+    service: service ?? "",
+    revision: revision ?? "",
+  });
+
   if (!service) return null;
 
   return (
     <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 dark:border-gray-dark-5 dark:bg-gray-dark-1">
       <div className="flex flex-col gap-x-7 max-md:space-y-4 md:flex-row md:items-center md:justify-start">
-        revisions page: service: {service} - revision: {revision}
+        revisions page: service: {service} - revision: {revision} - {data?.name}
       </div>
     </div>
   );
