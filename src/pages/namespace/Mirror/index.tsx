e4d5dff6811a318535cc380d1c412e54d7bf6058
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2023-08-21 17:54:55 +0200

    Add placeholder for non-mirror namespaces
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- src/pages/namespace/Mirror/index.tsx
+++ src/pages/namespace/Mirror/index.tsx
@@ -1,13 +1,13 @@
+import NoMirror from "./NoMirror";
 import { Outlet } from "react-router-dom";
+import { useNodeContent } from "~/api/tree/query/node";
 
 const MirrorPage = () => {
-  const text = "Mirror page";
-  return (
-    <div>
-      <h2>{text}</h2>
-      <Outlet />
-    </div>
-  );
+  const { data } = useNodeContent({ path: "/" });
+
+  const isMirror = data?.node?.expandedType === "git";
+
+  return <div>{isMirror ? <Outlet /> : <NoMirror />}</div>;
 };
 
 export default MirrorPage;
