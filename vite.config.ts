990dbe99c2122f7ce0fa889b0d84583845cc2530
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-23 15:47:29 +0200

    support embedded deployment builds
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- vite.config.ts
+++ vite.config.ts
@@ -51,6 +51,7 @@ export default ({ mode }) => {
           }
         : {},
     },
+    base: "./", // support embedded deployment
     build: {
       commonjsOptions: {
         // https://github.com/vitejs/vite/issues/2139#issuecomment-1405624744
