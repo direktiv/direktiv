43ecd07a46b650dc659e28bd9e0690fc31483312
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-25 08:53:04 +0200

    remove unused VITE_APP_VERSION
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/config/env/schema.ts
+++ src/config/env/schema.ts
@@ -2,15 +2,6 @@ import { z } from "zod";
 
 export const envVariablesSchema = z.object({
   VITE_DEV_API_DOMAIN: z.string().optional(),
-  VITE_APP_VERSION: z
-    .string()
-    .optional()
-    .transform((value) => {
-      if (value && `${value}`.length === 0) {
-        return undefined;
-      }
-      return value;
-    }),
   VITE_IS_ENTERPRISE: z
     .string()
     .optional()
