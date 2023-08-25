cee4483990019e43e036123902623e8a75c60b33
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-25 15:22:16 +0200

    store api key in localstorage when correct
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/Authdialog/index.tsx
+++ src/componentsNext/Authdialog/index.tsx
@@ -5,21 +5,57 @@ import {
   DialogHeader,
   DialogTitle,
 } from "~/design/Dialog";
-import { KeyRound, LogIn } from "lucide-react";
+import { Eye, EyeOff, KeyRound, LogIn } from "lucide-react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 import Button from "~/design/Button";
+import FormErrors from "../FormErrors";
 import Input from "~/design/Input";
+import { InputWithButton } from "~/design/InputWithButton";
 import Logo from "~/design/Logo";
+import { useApiActions } from "~/util/store/apiKey";
+import { useAuthenticate } from "~/api/authenticate/mutate/authenticate";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormInput = {
+  apiKey: string;
+};
 
 export const Authdialog = () => {
   const { t } = useTranslation();
+  const [showKey, setShowKey] = useState(false);
+  const { setApiKey: storeApiKey } = useApiActions();
+  const { mutate: authenticate, isLoading } = useAuthenticate({
+    onSuccess: (isKeyCorrect, apiKey) => {
+      if (isKeyCorrect) {
+        storeApiKey(apiKey);
+      }
+    },
+  });
 
-  const isLoading = false; // TODO:
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, errors, isValid, isSubmitted },
+  } = useForm<FormInput>({
+    resolver: zodResolver(
+      z.object({
+        apiKey: z.string(),
+      })
+    ),
+  });
+
+  const onSubmit: SubmitHandler<FormInput> = ({ apiKey }) => {
+    authenticate(apiKey);
+  };
 
   // TODO: check if there is an old, and show the user if its wrong
   // TODO: delete old one if not needed anymore
 
+  const disableSubmit = !isDirty || (isSubmitted && !isValid);
   const formId = `authdialog-form`;
 
   return (
@@ -33,27 +69,44 @@ export const Authdialog = () => {
             <KeyRound /> {t("pages.authenticate.title")}
           </DialogTitle>
         </DialogHeader>
-        <div className="my-3">
-          {/* <FormErrors errors={errors} className="mb-5" /> */}
-          {/* <form id={formId} onSubmit={handleSubmit(onSubmit)}> */}
-          <fieldset className="flex items-center gap-5">
-            <label className="w-[90px] text-right text-[14px]" htmlFor="name">
-              {t("pages.authenticate.apiKey")}
-            </label>
-            <Input
-              id="name"
-              placeholder={t("pages.authenticate.apiKeyPlaceholder")}
-              // {...register("name")}
-            />
-          </fieldset>
-          {/* </form> */}
+        <div className="my-3">{t("pages.authenticate.description")}</div>
+        <div className="mb-3">
+          <FormErrors errors={errors} className="mb-3" />
+          <form id={formId} onSubmit={handleSubmit(onSubmit)}>
+            <fieldset className="flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[14px]"
+                htmlFor="apiKey"
+              >
+                {t("pages.authenticate.apiKey")}
+              </label>
+              <InputWithButton className="w-full">
+                <Input
+                  id="apiKey"
+                  placeholder={t("pages.authenticate.apiKeyPlaceholder")}
+                  {...register("apiKey")}
+                  type={showKey ? "text" : "password"}
+                />
+                <Button
+                  icon
+                  type="button"
+                  variant="ghost"
+                  onClick={() => {
+                    setShowKey((prev) => !prev);
+                  }}
+                >
+                  {showKey ? <EyeOff /> : <Eye />}
+                </Button>
+              </InputWithButton>
+            </fieldset>
+          </form>
         </div>
         <DialogFooter>
           <Button
-          // type="submit"
-          // disabled={disableSubmit}
-          // loading={isLoading}
-          // form={formId}
+            type="submit"
+            disabled={disableSubmit}
+            loading={isLoading}
+            form={formId}
           >
             {!isLoading && <LogIn />}
             {t("pages.authenticate.loginBtn")}
