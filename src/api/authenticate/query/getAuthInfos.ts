aa2533e408e608c25d94632f318e48372d964f81
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-25 13:45:26 +0200

    also react to 403
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/authenticate/query/getAuthInfos.ts
+++ src/api/authenticate/query/getAuthInfos.ts
@@ -11,11 +11,11 @@ const authTest = async ({
   getVersion({
     apiKey,
     urlParams: undefined,
-  }) // when test call succeeds, and matches the schema out auth test passes
+  }) // when test call succeeds and matches the schema our auth test passes
     .then(() => true)
-    // when the test call fails with a 401,
+    // when the test call fails with a 401 or 403 our auth test fails
     .catch((err) => {
-      if (err !== 401) {
+      if (err !== 401 || err !== 403) {
         // any other error should bubble up, like a 500 or schema validation error
         throw err;
       }
