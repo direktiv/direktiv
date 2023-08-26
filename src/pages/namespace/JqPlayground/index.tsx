06597622a9c6950ec391e256da51b18ced1c39f2
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-26 17:57:40 +0200

    layout form and add translation
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/JqPlayground/index.tsx
+++ src/pages/namespace/JqPlayground/index.tsx
@@ -2,9 +2,12 @@ import { FC, useState } from "react";
 
 import Button from "~/design/Button";
 import Input from "~/design/Input";
+import { Play } from "lucide-react";
 import { useExecuteJQuery } from "~/api/jq/mutate/executeQuery";
+import { useTranslation } from "react-i18next";
 
 const JqPlaygroundPage: FC = () => {
+  const { t } = useTranslation();
   const { mutate: executeQuery } = useExecuteJQuery();
   const [query, setQuery] = useState(".foo[1]");
 
@@ -17,19 +20,23 @@ const JqPlaygroundPage: FC = () => {
 
   return (
     <div className="flex flex-col space-y-10 p-5">
-      <Input
-        value={query}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
-      />
-      <Button
-        onClick={() => {
-          executeQuery({ query, inputJSON: JSON.stringify(data) });
-        }}
-      >
-        Submit
-      </Button>
+      <div className="flex flex-col gap-5 sm:flex-row">
+        <Input
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+        <Button
+          className="grow sm:w-64"
+          onClick={() => {
+            executeQuery({ query, inputJSON: JSON.stringify(data) });
+          }}
+        >
+          <Play />
+          {t("pages.jqPlayground.submitBtn")}
+        </Button>
+      </div>
     </div>
   );
 };
