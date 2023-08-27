a802fa2515631f0c3310cd580996a1e8beb2ffce
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-27 17:31:30 +0200

    add header and placeholder
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/JqPlayground/index.tsx
+++ src/pages/namespace/JqPlayground/index.tsx
@@ -1,10 +1,10 @@
 import { FC, useState } from "react";
+import { Play, PlaySquare } from "lucide-react";
 
 import Button from "~/design/Button";
 import { Card } from "~/design/Card";
 import Editor from "~/design/Editor";
 import Input from "~/design/Input";
-import { Play } from "lucide-react";
 import { useExecuteJQuery } from "~/api/jq/mutate/executeQuery";
 import { useTheme } from "~/util/store/theme";
 import { useTranslation } from "react-i18next";
@@ -36,51 +36,62 @@ const JqPlaygroundPage: FC = () => {
   };
 
   return (
-    <Card className="m-5 p-5">
-      <form id={formId} onSubmit={handleSubmit} className="flex flex-col gap-5">
-        <div className="flex flex-col gap-5 sm:flex-row">
-          <Input
-            value={query}
-            onChange={(e) => {
-              setQuery(e.target.value);
-            }}
-          />
-          <Button
-            className="grow sm:w-64"
-            type="submit"
-            disabled={isLoading}
-            loading={isLoading}
-          >
-            {!isLoading && <Play />}
-            {t("pages.jqPlayground.submitBtn")}
-          </Button>
-        </div>
-        <div className="flex gap-5">
-          <Card className="h-96 w-full p-4" noShadow background="weight-1">
-            <Editor
-              value={input}
-              language="json"
-              onChange={(newData) => {
-                if (newData) {
-                  setInput(newData);
-                }
+    <div className="flex grow flex-col gap-y-4 p-5">
+      <h3 className="flex items-center gap-x-2 font-bold">
+        <PlaySquare className="h-5" />
+        {t("pages.jqPlayground.title")}
+      </h3>
+      <Card className="p-5">
+        <form
+          id={formId}
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5"
+        >
+          <div className="flex flex-col gap-5 sm:flex-row">
+            <Input
+              placeholder={t("pages.jqPlayground.queryPlaceholder")}
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
               }}
-              theme={theme ?? undefined}
             />
-          </Card>
-          <Card className="h-96 w-full p-4" noShadow background="weight-1">
-            <Editor
-              language="json"
-              value={result}
-              options={{
-                readOnly: true,
-              }}
-              theme={theme ?? undefined}
-            />
-          </Card>
-        </div>
-      </form>
-    </Card>
+            <Button
+              className="grow sm:w-64"
+              type="submit"
+              disabled={isLoading}
+              loading={isLoading}
+            >
+              {!isLoading && <Play />}
+              {t("pages.jqPlayground.submitBtn")}
+            </Button>
+          </div>
+          <div className="flex gap-5">
+            <Card className="h-96 w-full p-4" noShadow background="weight-1">
+              <Editor
+                value={input}
+                language="json"
+                onChange={(newData) => {
+                  if (newData) {
+                    setInput(newData);
+                  }
+                }}
+                theme={theme ?? undefined}
+              />
+            </Card>
+            <Card className="h-96 w-full p-4" noShadow background="weight-1">
+              <Editor
+                language="json"
+                value={result}
+                options={{
+                  readOnly: true,
+                }}
+                theme={theme ?? undefined}
+              />
+            </Card>
+          </div>
+        </form>
+      </Card>
+    </div>
   );
 };
 
