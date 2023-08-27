c3ecf0ec20f3e9a015c9b064022cb36f7b69c3e1
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-27 18:58:12 +0200

    add cheatsheet
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/pages/namespace/JqPlayground/cheatsheet.ts
@@ -0,0 +1,75 @@
+export default [
+  {
+    example: ".",
+    tip: "unchanged input",
+    filter: ".",
+    json: '{ "foo": { "bar": { "baz": 123 } } }',
+  },
+  {
+    example: ".foo, .foo.bar, .foo?",
+    tip: "value at key",
+    filter: ".foo",
+    json: '{"foo": 42, "bar": "less interesting data"}',
+  },
+  {
+    example: ".[], .[]?, .[2], .[10:15]",
+    tip: "array operation",
+    filter: ".foo[1]",
+    json: '{"foo": [{"name":"JSON", "good":true}, {"name":"XML", "good":false}]}',
+  },
+  {
+    example: "[], {}",
+    tip: "array/object construction",
+    filter: "{user, title: .titles[]}",
+    json: '{"user":"stedolan","titles":["JQ Primer", "More JQ"]}',
+  },
+  {
+    example: "length",
+    tip: "length of a value",
+    filter: ".foo[] | length",
+    json: '{"foo": [[1,2], "string", {"a":2}, null]}',
+  },
+  {
+    example: "keys",
+    tip: "keys in an array",
+    filter: "keys",
+    json: '{"abc": 1, "abcd": 2, "Foo": 3}',
+  },
+  {
+    example: ",",
+    tip: "feed input into multiple filters",
+    filter: ".foo, .bar",
+    json: '{ "foo": 42, "bar": "something else", "baz": true}',
+  },
+  {
+    example: "|",
+    tip: "pipe output of one filter to the next filter",
+    filter: ".foo[] | .name",
+    json: '{"foo": [{"name":"JSON", "good":true}, {"name":"XML", "good":false}]}',
+  },
+  {
+    example: "select(foo)",
+    tip: "input unchanged if foo returns true",
+    filter: "map(select(. >= 2))",
+    json: '{"a": 1, "b": 2, "c": 4, "d": 7}',
+  },
+  {
+    example: "map(foo)",
+    tip: "invoke filter foo for each input",
+    filter: "map(.+1)",
+    json: '{"a": 1, "b": 2, "c": 3}',
+  },
+  {
+    example: "if-then-else-end",
+    tip: "conditionals",
+    filter:
+      'if .foo == 0 then "zero" elif .foo == 1 then "one" else "many" end',
+    json: '{"foo": 2}',
+  },
+  {
+    example: "(foo)",
+    tip: "string interpolation",
+    filter: '"The input was \\(.input), which is one less than \\(.input+1)"',
+    json: '{"input": 42}',
+  },
+] as const;
