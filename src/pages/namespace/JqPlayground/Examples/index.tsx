8aa8ed922f0063c1191821c0a625862551937ddc
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-28 09:00:01 +0200

    add headline to examples
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/JqPlayground/Examples/index.tsx
+++ src/pages/namespace/JqPlayground/Examples/index.tsx
@@ -1,6 +1,7 @@
+import { Bookmark, Play } from "lucide-react";
+
 import Button from "~/design/Button";
 import { Card } from "~/design/Card";
-import { Play } from "lucide-react";
 import exampleSnippets from "./exampleSnippets";
 import { useTranslation } from "react-i18next";
 
@@ -11,28 +12,34 @@ const Examples = ({
 }) => {
   const { t } = useTranslation();
   return (
-    <Card className="grid grid-cols-2 gap-5 p-5 text-sm">
-      {exampleSnippets.map(({ query, input, tip, example }, index) => (
-        <Card key={index} className="flex gap-2 p-5">
-          <div className="grid grow grid-cols-2">
-            <div className="font-mono text-primary-500">{example}</div>
-            <div>{tip}</div>
-          </div>
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={() =>
-              onExampleClick({
-                query,
-                input,
-              })
-            }
-          >
-            <Play />
-            {t("pages.jqPlayground.examples.buttionLabel")}
-          </Button>
-        </Card>
-      ))}
+    <Card className="flex flex-col gap-5 p-5">
+      <h3 className="flex grow items-center gap-x-2 font-medium">
+        <Bookmark className="h-5" />
+        {t("pages.jqPlayground.examples.title")}
+      </h3>
+      <div className="grid grid-cols-2 gap-5 text-sm">
+        {exampleSnippets.map(({ query, input, tip, example }, index) => (
+          <Card key={index} className="flex gap-2 p-5">
+            <div className="grid grow grid-cols-2">
+              <div className="font-mono text-primary-500">{example}</div>
+              <div>{tip}</div>
+            </div>
+            <Button
+              size="sm"
+              variant="outline"
+              onClick={() =>
+                onExampleClick({
+                  query,
+                  input,
+                })
+              }
+            >
+              <Play />
+              {t("pages.jqPlayground.examples.buttionLabel")}
+            </Button>
+          </Card>
+        ))}
+      </div>
     </Card>
   );
 };
