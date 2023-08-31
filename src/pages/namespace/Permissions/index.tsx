01682837119d9a70c165d851c7f770d1ffca6869
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-31 14:33:04 +0200

    add tabs
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Permissions/index.tsx
+++ src/pages/namespace/Permissions/index.tsx
@@ -1,45 +1,81 @@
+import { FileCheck, KeyRound, Users } from "lucide-react";
 import { Link, Outlet } from "react-router-dom";
+import { Tabs, TabsList, TabsTrigger } from "~/design/Tabs";
 
 import { pages } from "~/util/router/pages";
 import { useNamespace } from "~/util/store/namespace";
+import { useTranslation } from "react-i18next";
 
 const PermissionsPage = () => {
-  const test = pages.permissions?.createHref;
+  const permissions = pages.permissions;
 
   const namespace = useNamespace();
+  const { t } = useTranslation();
 
-  if (!test) return null;
+  if (!permissions) return null;
   if (!namespace) return null;
 
+  const {
+    isPermissionsGroupPage,
+    isPermissionsPolicyPage,
+    isPermissionsTokenPage,
+  } = permissions.useParams();
+
+  const tabs = [
+    {
+      value: "policy",
+      active: isPermissionsPolicyPage,
+      icon: <FileCheck aria-hidden="true" />,
+      title: t("pages.permissions.tabs.policy"),
+      link: permissions.createHref({
+        namespace,
+      }),
+    },
+    {
+      value: "groups",
+      active: isPermissionsGroupPage,
+      icon: <Users aria-hidden="true" />,
+      title: t("pages.permissions.tabs.groups"),
+      link: permissions.createHref({
+        namespace,
+        subpage: "groups",
+      }),
+    },
+    {
+      value: "tokens",
+      active: isPermissionsTokenPage,
+      icon: <KeyRound aria-hidden="true" />,
+      title: t("pages.permissions.tabs.tokens"),
+      link: permissions.createHref({
+        namespace,
+        subpage: "tokens",
+      }),
+    },
+  ] as const;
+
   return (
-    <div>
-      <div className="flex gap-5">
-        <Link
-          to={test({
-            namespace,
-          })}
-        >
-          policy
-        </Link>
-        <Link
-          to={test({
-            namespace,
-            subpage: "groups",
-          })}
-        >
-          Groups
-        </Link>
-        <Link
-          to={test({
-            namespace,
-            subpage: "tokens",
-          })}
-        >
-          Tokens
-        </Link>
+    <>
+      <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 pb-0 dark:border-gray-dark-5 dark:bg-gray-dark-1">
+        <Tabs defaultValue={tabs.find((tab) => tab.active)?.value}>
+          <TabsList>
+            {tabs.map((tab) => (
+              <TabsTrigger
+                asChild
+                value={tab.value}
+                key={tab.value}
+                data-testid={`event-tabs-trg-${tab.value}`}
+              >
+                <Link to={tab.link}>
+                  {tab.icon}
+                  {tab.title}
+                </Link>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
       </div>
       <Outlet />
-    </div>
+    </>
   );
 };
 
