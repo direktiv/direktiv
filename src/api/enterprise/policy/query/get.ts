524e51a63bae160e99c3dc6194b0b42e11a9ec1f
Stefan Kracht <stefan.kracht@direktiv.io>
2023-08-31 15:42:33 +0200

    mock data
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/enterprise/policy/query/get.ts
+++ src/api/enterprise/policy/query/get.ts
@@ -5,7 +5,10 @@ import { apiFactory } from "~/api/apiFactory";
 import { policyKeys } from "..";
 import { useApiKey } from "~/util/store/apiKey";
 import { useNamespace } from "~/util/store/namespace";
+import { z } from "zod";
 
+// TODO: remove this
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getPolicy = apiFactory({
   url: ({ namespace, baseUrl }: { baseUrl?: string; namespace: string }) =>
     `${baseUrl ?? ""}/api/v2/namespace/${namespace}/policy`,
@@ -13,10 +16,31 @@ const getPolicy = apiFactory({
   schema: PolicySchema,
 });
 
+const getPolicyMock = (_params: {
+  apiKey?: string;
+  urlParams: { namespace: string };
+}): Promise<z.infer<typeof PolicySchema>> =>
+  Promise.resolve(`package authorization
+
+default allow = false
+
+allow {
+    input.method == "GET"
+    input.path = ["customers", customerID]
+    input.user_roles[_] = "admin"
+}
+
+allow {
+    input.method == "GET"
+    input.path = ["customers", customerID]
+    input.user_roles[_] = "support"
+}
+`);
+
 const fetchPolicy = async ({
   queryKey: [{ apiKey, namespace }],
 }: QueryFunctionContext<ReturnType<(typeof policyKeys)["get"]>>) =>
-  getPolicy({
+  getPolicyMock({
     apiKey,
     urlParams: {
       namespace,
