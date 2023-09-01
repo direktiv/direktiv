c1a51d5035866dd80128e92e8d63400504ae0d5d
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-01 15:28:17 +0200

    style PermissionInfo
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Permissions/components/PermissionsInfo.tsx
+++ src/pages/namespace/Permissions/components/PermissionsInfo.tsx
@@ -1,23 +1,27 @@
+import Badge from "~/design/Badge";
+import { ComponentProps } from "react";
 import { usePermissionKeys } from "~/api/enterprise/permissions/query/get";
+import usePermissionLevel from "./usePermissionLevel";
 import { useTranslation } from "react-i18next";
 
 const PermissionsInfo = ({ permissions }: { permissions: string[] }) => {
   const { t } = useTranslation();
-  const { data: permissionKeys, isSuccess } = usePermissionKeys();
+  const { data: availablePermissions, isSuccess } = usePermissionKeys();
+  const { hasAllPermissions, badgeVariant } = usePermissionLevel(
+    permissions,
+    availablePermissions ?? []
+  );
+
   if (!isSuccess) return null;
 
-  const hasAllPermissions =
-    permissionKeys?.length > 0 &&
-    permissionKeys?.every((perm) => permissions.includes(perm));
-
   return (
-    <div>
+    <Badge className="cursor-pointer" variant={badgeVariant}>
       {hasAllPermissions
         ? t("pages.permissions.permissionsInfo.all")
         : t("pages.permissions.permissionsInfo.partial", {
             count: permissions.length,
           })}
-    </div>
+    </Badge>
   );
 };
 
