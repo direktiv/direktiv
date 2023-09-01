d7944e9235063fe9cfc8f10182ba48080102ec16
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-01 16:32:07 +0200

    finish PermissionInfo
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Permissions/components/PermissionsInfo.tsx
+++ src/pages/namespace/Permissions/components/PermissionsInfo.tsx
@@ -1,5 +1,10 @@
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "~/design/HoverCard";
+
 import Badge from "~/design/Badge";
-import { ComponentProps } from "react";
 import { usePermissionKeys } from "~/api/enterprise/permissions/query/get";
 import usePermissionLevel from "./usePermissionLevel";
 import { useTranslation } from "react-i18next";
@@ -15,13 +20,28 @@ const PermissionsInfo = ({ permissions }: { permissions: string[] }) => {
   if (!isSuccess) return null;
 
   return (
-    <Badge className="cursor-pointer" variant={badgeVariant}>
-      {hasAllPermissions
-        ? t("pages.permissions.permissionsInfo.all")
-        : t("pages.permissions.permissionsInfo.partial", {
-            count: permissions.length,
-          })}
-    </Badge>
+    <HoverCard>
+      <HoverCardTrigger>
+        <Badge className="cursor-pointer" variant={badgeVariant}>
+          {hasAllPermissions
+            ? t("pages.permissions.permissionsInfo.all")
+            : t("pages.permissions.permissionsInfo.partial", {
+                count: permissions.length,
+              })}
+        </Badge>
+      </HoverCardTrigger>
+      <HoverCardContent className="flex max-w-xl flex-wrap gap-3">
+        {availablePermissions.map((permission) => (
+          <Badge
+            key={permission}
+            className="cursor-pointer"
+            variant={permissions.includes(permission) ? "success" : "outline"}
+          >
+            {permission}
+          </Badge>
+        ))}
+      </HoverCardContent>
+    </HoverCard>
   );
 };
 
