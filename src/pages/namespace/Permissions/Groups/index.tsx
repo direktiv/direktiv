301f324a35c791baa63493dad8031e9056c906ce
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-01 08:28:57 +0200

    request groups
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Permissions/Groups/index.tsx
+++ src/pages/namespace/Permissions/Groups/index.tsx
@@ -1,3 +1,16 @@
-const GroupsPage = () => <div>Groups Page</div>;
+import { useGroups } from "~/api/enterprise/groups/query/get";
+
+const GroupsPage = () => {
+  const { data } = useGroups();
+
+  return (
+    <div>
+      <h1>groups</h1>
+      {data?.groups.map((group) => (
+        <div key={group.id}>{group.group}</div>
+      ))}
+    </div>
+  );
+};
 
 export default GroupsPage;
