f6c322b2560537fb6750e797d378dd916c8d312e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-01 08:53:24 +0200

    display tokens
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Permissions/Tokens/index.tsx
+++ src/pages/namespace/Permissions/Tokens/index.tsx
@@ -1,3 +1,16 @@
-const TokensPage = () => <div>Tokens Page</div>;
+import { useTokens } from "~/api/enterprise/tokens/query/get";
+
+const TokensPage = () => {
+  const { data } = useTokens();
+
+  return (
+    <div>
+      <h1>tokens</h1>
+      {data?.tokens.map((token) => (
+        <div key={token.id}>{token.description}</div>
+      ))}
+    </div>
+  );
+};
 
 export default TokensPage;
