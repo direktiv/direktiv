3d4fa24e4669b0a40a1807bdc43b257f45a7f5ff
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-02 10:04:44 +0200

    add file upload to secrets
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Settings/Secrets/Create.tsx
+++ src/pages/namespace/Settings/Secrets/Create.tsx
@@ -36,10 +36,27 @@ const Create = ({ onSuccess }: CreateProps) => {
   const {
     register,
     handleSubmit,
+    setValue,
+    setError,
     formState: { errors },
   } = useForm<SecretFormSchemaType>({
     resolver: zodResolver(SecretFormSchema),
   });
+
+  const onFilepickerChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+    try {
+      const fileContent = await file.text();
+      setValue("value", fileContent);
+    } catch (e) {
+      setError("value", {
+        message: t("pages.settings.secrets.create.fileError"),
+      });
+    }
+  };
 
   return (
     <DialogContent>
@@ -71,9 +88,8 @@ const Create = ({ onSuccess }: CreateProps) => {
           <label className="w-[150px] text-right" htmlFor="file-upload">
             {t("pages.settings.secrets.create.file")}
           </label>
-          <Input id="file-upload" type="file" />
+          <Input id="file-upload" type="file" onChange={onFilepickerChange} />
         </fieldset>
-
         <fieldset className="flex items-start gap-5">
           <Textarea
             className="h-96"
