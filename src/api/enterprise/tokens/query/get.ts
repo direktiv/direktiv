ce17e7f1570543691eea105a6a403425e491423c
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-05 15:36:53 +0200

    remove token query mock
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/enterprise/tokens/query/get.ts
+++ src/api/enterprise/tokens/query/get.ts
@@ -2,15 +2,10 @@ import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
 
 import { TokenListSchema } from "../schema";
 import { apiFactory } from "~/api/apiFactory";
-import { faker } from "@faker-js/faker";
-import moment from "moment";
 import { tokenKeys } from "..";
 import { useApiKey } from "~/util/store/apiKey";
 import { useNamespace } from "~/util/store/namespace";
-import { z } from "zod";
 
-// TODO: remove the line below and delete the mock function
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getTokens = apiFactory({
   url: ({ namespace, baseUrl }: { baseUrl?: string; namespace: string }) =>
     `${baseUrl ?? ""}/api/v2/namespaces/${namespace}/tokens`,
@@ -18,74 +13,74 @@ const getTokens = apiFactory({
   schema: TokenListSchema,
 });
 
-const getTokensMock = (_params: {
-  apiKey?: string;
-  urlParams: { namespace: string };
-}): Promise<z.infer<typeof TokenListSchema>> =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        tokens: [
-          {
-            id: faker.datatype.uuid(),
-            description: "some super token",
-            permissions: [
-              "opaManage",
-              "variablesView",
-              "registriesManage",
-              "explorerManage",
-              "registriesView",
-              "nsconfigView",
-              "eventsSend",
-              "instancesView",
-              "secretsView",
-              "secretsManage",
-              "servicesView",
-              "servicesManage",
-              "instancesManage",
-              "explorerView",
-              "workflowView",
-              "workflowManage",
-              "variablesManage",
-              "nsconfigManage",
-              "deleteNamespace",
-              "eventsView",
-              "workflowExecute",
-              "workflowStore",
-              "permissionsView",
-              "permissionsManage",
-              "opaView",
-              "eventsManage",
-            ],
-            created: new Date().toISOString(),
-            expires: moment().add(1, "day").toISOString(),
-            expired: false,
-          },
-          {
-            id: faker.datatype.uuid(),
-            description: "some useless token",
-            permissions: [],
-            created: new Date().toISOString(),
-            expires: moment().add(1, "month").toISOString(),
-            expired: false,
-          },
-          {
-            id: faker.datatype.uuid(),
-            description: "some expired token",
-            permissions: ["workflowView", "permissionsView"],
-            created: new Date().toISOString(),
-            expires: moment().subtract(1, "day").toISOString(),
-            expired: true,
-          },
-        ],
-      });
-    }, 500);
-  });
+// const getTokensMock = (_params: {
+//   apiKey?: string;
+//   urlParams: { namespace: string };
+// }): Promise<z.infer<typeof TokenListSchema>> =>
+//   new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve({
+//         tokens: [
+//           {
+//             id: faker.datatype.uuid(),
+//             description: "some super token",
+//             permissions: [
+//               "opaManage",
+//               "variablesView",
+//               "registriesManage",
+//               "explorerManage",
+//               "registriesView",
+//               "nsconfigView",
+//               "eventsSend",
+//               "instancesView",
+//               "secretsView",
+//               "secretsManage",
+//               "servicesView",
+//               "servicesManage",
+//               "instancesManage",
+//               "explorerView",
+//               "workflowView",
+//               "workflowManage",
+//               "variablesManage",
+//               "nsconfigManage",
+//               "deleteNamespace",
+//               "eventsView",
+//               "workflowExecute",
+//               "workflowStore",
+//               "permissionsView",
+//               "permissionsManage",
+//               "opaView",
+//               "eventsManage",
+//             ],
+//             created: new Date().toISOString(),
+//             expires: moment().add(1, "day").toISOString(),
+//             expired: false,
+//           },
+//           {
+//             id: faker.datatype.uuid(),
+//             description: "some useless token",
+//             permissions: [],
+//             created: new Date().toISOString(),
+//             expires: moment().add(1, "month").toISOString(),
+//             expired: false,
+//           },
+//           {
+//             id: faker.datatype.uuid(),
+//             description: "some expired token",
+//             permissions: ["workflowView", "permissionsView"],
+//             created: new Date().toISOString(),
+//             expires: moment().subtract(1, "day").toISOString(),
+//             expired: true,
+//           },
+//         ],
+//       });
+//     }, 500);
+//   });
 
 const fetchTokens = async ({
   queryKey: [{ apiKey, namespace }],
 }: QueryFunctionContext<ReturnType<(typeof tokenKeys)["tokenList"]>>) =>
-  getTokensMock({
+  getTokens({
     apiKey,
     urlParams: { namespace },
   });
