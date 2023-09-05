99b0d64291e9bece0e2361ff74f5eece3f414722
Sebastian Armbrust <129740294+sebxian@users.noreply.github.com>
2023-09-05 10:26:39 +0200

    Spelling fixes

--- e2e/usermenu.spec.ts
+++ e2e/usermenu.spec.ts
@@ -20,7 +20,7 @@ test("it is possible to switch between light and dark mode", async ({
   expect(
     await themeSwitchBtn.textContent(),
     "the theme button should display 'switch to Dark mode' initially"
-  ).toMatch(/Switch to Dark mode/);
+  ).toMatch(/Switch to dark mode/);
 
   // switch to dark mode
   await themeSwitchBtn.click();
@@ -35,7 +35,7 @@ test("it is possible to switch between light and dark mode", async ({
   expect(
     await themeSwitchBtn.textContent(),
     "the theme button should now display 'switch to Light mode'"
-  ).toMatch(/Switch to Light mode/);
+  ).toMatch(/Switch to light mode/);
 
   // back to light mode again
   await themeSwitchBtn.click();
@@ -45,7 +45,7 @@ test("it is possible to switch between light and dark mode", async ({
   expect(
     await themeSwitchBtn.textContent(),
     "the theme button should display 'switch to Dark mode' initially"
-  ).toMatch(/Switch to Dark mode/);
+  ).toMatch(/Switch to dark mode/);
 
   expect(
     await getStyle(bodyTag, "background-color"),
