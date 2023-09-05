721d2dfcb8fe8aa41999e1aa1342d738f3978043
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-05 11:24:39 +0200

    add some space
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Settings/Secrets/Create.tsx
+++ src/pages/namespace/Settings/Secrets/Create.tsx
@@ -84,12 +84,14 @@ const Create = ({ onSuccess }: CreateProps) => {
             {...register("name")}
           />
         </fieldset>
+
         <fieldset className="flex items-center gap-5">
           <label className="w-[150px] text-right" htmlFor="file-upload">
             {t("pages.settings.secrets.create.file")}
           </label>
           <Input id="file-upload" type="file" onChange={onFilepickerChange} />
         </fieldset>
+
         <fieldset className="flex items-start gap-5">
           <Textarea
             className="h-96"
