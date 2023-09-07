9d08b7a664e231ca520819deb9ea47291beb0193
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-07 10:32:53 +0200

    extend useIsAuthRequired with isEnterprise check
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/hooksNext/useApiKeyHandling.ts
+++ src/hooksNext/useApiKeyHandling.ts
@@ -1,29 +1,43 @@
+import env from "~/config/env";
 import { useApiKey } from "~/util/store/apiKey";
 import { useAuthTest } from "~/api/authenticate/query/getAuthInfos";
+
+/**
+ * Send test request to check if api needs auth. In enterprise
+ * mode, this test will be skipped and will always return true
+ */
+const useIsAuthRequired = () => {
+  const { VITE_IS_ENTERPRISE: isEnterprise } = env;
+  const { data: testSucceeded, isFetched: isFnished } = useAuthTest({
+    enabled: !isEnterprise,
+  });
+  return isEnterprise
+    ? { authRequired: true, isFinished: true }
+    : { authRequired: !testSucceeded, isFinished: isFnished };
+};
 
 /**
  * this hook will test, whether the the api needs an auth key by sending a
  * request. It also tests, whether the stored key is valid (if there is one)
  */
 const useApiKeyHandling = () => {
-  const apiKeyFromLocalstorage = useApiKey();
-  const apiKeyisPresent = !!apiKeyFromLocalstorage;
+  const storedKey = useApiKey();
+  const keyIsPresent = !!storedKey;
 
-  const { data: testWithStoredKey, isFetched: testWithStoredKeyFinished } =
+  const { data: storedKeyTestResult, isFetched: testWithStoredKeyFinished } =
     useAuthTest({
-      apikey: apiKeyFromLocalstorage ?? undefined,
-      enabled: apiKeyisPresent,
+      apikey: storedKey ?? undefined,
+      enabled: keyIsPresent,
     });
 
-  const { data: testWithoutKey, isFetched: testWithoutKeyFinished } =
-    useAuthTest();
+  const { authRequired, isFinished: authCheckFinished } = useIsAuthRequired();
 
   return {
-    isKeyRequired: !testWithoutKey,
-    isCurrentKeyValid: apiKeyisPresent ? testWithStoredKey : testWithoutKey,
-    isFetched: apiKeyisPresent
-      ? testWithoutKeyFinished && testWithStoredKeyFinished
-      : testWithoutKeyFinished,
+    isKeyRequired: authRequired,
+    isCurrentKeyValid: keyIsPresent ? storedKeyTestResult : !authRequired,
+    isFetched: keyIsPresent
+      ? authCheckFinished && testWithStoredKeyFinished
+      : authCheckFinished,
   };
 };
 
