52d7c63d125428a315f8d64803b3bfef9316dbdd
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-07 11:15:58 +0200

    wait for request to be finished before showing anything
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Mirror/index.tsx
+++ src/pages/namespace/Mirror/index.tsx
@@ -3,9 +3,11 @@ import { Outlet } from "react-router-dom";
 import { useNodeContent } from "~/api/tree/query/node";
 
 const MirrorPage = () => {
-  const { data } = useNodeContent({ path: "/" });
+  const { data, isSuccess } = useNodeContent({ path: "/" });
 
-  const isMirror = data?.node?.expandedType === "git";
+  if (!isSuccess) return null;
+
+  const isMirror = data.node.expandedType === "git";
 
   return <div>{isMirror ? <Outlet /> : <NoMirror />}</div>;
 };
