3b1798736b14aab772dbceaad50edaf6b4aa8ede
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-07 08:15:53 +0200

    move query client into its own file
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,17 @@ import "./App.css";
 import "reactflow/dist/base.css";
 import "./design/WorkflowDiagram/style.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useTheme, useThemeActions } from "~/util/store/theme";
 
 import { AuthenticationProvider } from "./componentsNext/AuthenticationProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "~/design/Toast";
 import env from "./config/env/";
+import queryClient from "./util/queryClient";
 import { router } from "~/util/router";
 import { useEffect } from "react";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      networkMode: "always", // the default networkMode sometimes assumes that the client is offline
-    },
-    mutations: {
-      retry: false,
-      networkMode: "always", // the default networkMode sometimes assumes that the client is offline
-    },
-  },
-});
 
 const App = () => {
   const theme = useTheme();
