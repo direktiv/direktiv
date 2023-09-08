cd231f1bc54c84182be366c0cf850a02769aad95
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-08 11:26:04 +0200

    extend useQueryWithPermissionHandling with isAllowed and noPermissionMessage
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/errorHandling.ts
+++ src/api/errorHandling.ts
@@ -9,6 +9,7 @@ import {
 
 import { t } from "i18next";
 import { useToast } from "~/design/Toast";
+import { useTranslation } from "react-i18next";
 import { z } from "zod";
 
 /**
@@ -122,6 +123,34 @@ type UseQueryParam<
   TQueryKey extends QueryKey
 > = UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>;
 
+/**
+ * this type defines the additional properties that are added to the useQuery return value
+ */
+type ExtendedUseQueryReturn =
+  | {
+      isAllowed: false;
+      noPermissionMessage: string;
+    }
+  | {
+      isAllowed: true;
+      noPermissionMessage: undefined;
+    };
+
+/**
+ * useQueryWithPermissionHandling is a wrapper around useQuery that will add permission
+ * handling to the useQuery result. It checkes for the error and determines if the error
+ * is a permission error.
+ *
+ * It behaves the same way as useQuery but has two additional properties:
+ *
+ * isAllowed: A boolean value that indicates whether the user has the necessary permissions
+ * to proceed.
+ *
+ * noPermissionMessage: A string containing a message to display to the user when they lack
+ * the required permissions. This message comes either from the API response or from a generic
+ * message in the translation file if the API does not return any specific message. This propery
+ * is undefined when isAllowed is true and always a string when isAllowed is false.
+ */
 export const useQueryWithPermissionHandling = <
   TQueryFnData = unknown,
   TError = unknown,
@@ -129,7 +158,27 @@ export const useQueryWithPermissionHandling = <
   TQueryKey extends QueryKey = QueryKey
 >(
   useQueryParams: UseQueryParam<TQueryFnData, TError, TData, TQueryKey>
-): UseQueryResult<TData, TError> =>
-  useQuery({
+): UseQueryResult<TData, TError> & ExtendedUseQueryReturn => {
+  const { t } = useTranslation();
+  const useQueryReturnValue = useQuery({
     ...useQueryParams,
   });
+
+  const { error } = useQueryReturnValue;
+
+  if (error) {
+    const permissionStatus = getPermissionStatus(error);
+    const isAllowed = permissionStatus.isAllowed;
+    if (isAllowed === false) {
+      const noPermissionMessage =
+        permissionStatus.message ?? t("api.generic.noPermissionDescription");
+      return { ...useQueryReturnValue, isAllowed, noPermissionMessage };
+    }
+  }
+
+  return {
+    ...useQueryReturnValue,
+    isAllowed: true,
+    noPermissionMessage: undefined,
+  };
+};
