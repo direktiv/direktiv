29bd10d590897fdc7cfb53c4ca71da542249f930
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-08 16:52:29 +0200

    show a note when no permissions to view instance detail page
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Instances/Detail/InstanceDetail.tsx
+++ src/pages/namespace/Instances/Detail/InstanceDetail.tsx
@@ -1,7 +1,9 @@
+import { Card } from "~/design/Card";
 import Diagram from "./Main/Diagram";
 import Header from "./Header";
 import InputOutput from "./Main/InputOutput";
 import Logs from "./Main/Logs";
+import { NoPermissions } from "~/design/Table";
 import WorkspaceLayout from "./Main";
 import { useInstanceDetails } from "~/api/instances/query/details";
 import { useInstanceId } from "./store/instanceContext";
@@ -9,10 +11,20 @@ import { useLogsPreferencesMaximizedPanel } from "~/util/store/logs";
 
 const InstancesDetail = () => {
   const instanceId = useInstanceId();
-  const { data } = useInstanceDetails({ instanceId });
+  const { data, isFetched, isAllowed, noPermissionMessage } =
+    useInstanceDetails({ instanceId });
   const preferedLayout = useLogsPreferencesMaximizedPanel();
 
+  if (!isFetched) return null;
+  if (!isAllowed)
+    return (
+      <Card className="m-5 flex grow flex-col p-4">
+        <NoPermissions>{noPermissionMessage}</NoPermissions>
+      </Card>
+    );
+
   if (!data) return null;
+
   return (
     <div className="grid grow grid-rows-[auto_1fr]">
       <Header />
