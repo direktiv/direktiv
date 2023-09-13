9783a92cfe05fcfe8e5f0866f31c9453a6dcd327
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-13 15:45:37 +0200

    update useRefreshSession to be void and logout on auth error
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/enterprise/session/query/ping.ts
+++ src/api/enterprise/session/query/ping.ts
@@ -2,6 +2,8 @@ import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
 
 import { SessionSchema } from "../schema";
 import { apiFactory } from "~/api/apiFactory";
+import enterpriseConfig from "~/config/enterprise";
+import { getPermissionStatus } from "~/api/errorHandling";
 import { sessionKeys } from "..";
 import { useApiKey } from "~/util/store/apiKey";
 
@@ -22,10 +24,15 @@ const fetchSession = async ({
 
 export const useRefreshSession = ({ enabled }: { enabled: boolean }) => {
   const apiKey = useApiKey();
-  return useQuery({
+
+  const { isError, error } = useQuery({
     queryKey: sessionKeys.get(apiKey ?? undefined),
     queryFn: fetchSession,
     enabled,
     refetchInterval: 1000 * 60,
   });
+
+  if (isError && getPermissionStatus(error).isAllowed === false) {
+    window.location.href = enterpriseConfig.logoutPath;
+  }
 };
