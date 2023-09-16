2c900d6a1e695d882f21dcf9861903ba1045e02f
Alan Murtagh <alan.murtagh@direktiv.io>
2023-09-16 12:29:54 +1000

    Reimplements missing internal grpcs.
    Signed-off-by: Alan Murtagh <alan.murtagh@direktiv.io>

--- pkg/flow/grpc-instance-variables.go
+++ pkg/flow/grpc-instance-variables.go
@@ -6,16 +6,174 @@ import (
 
 	"github.com/direktiv/direktiv/pkg/flow/bytedata"
 	"github.com/direktiv/direktiv/pkg/flow/grpc"
+	libengine "github.com/direktiv/direktiv/pkg/refactor/engine"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+func (internal *internal) InstanceVariableParcels(req *grpc.VariableInternalRequest, srv grpc.Internal_InstanceVariableParcelsServer) error {
+	internal.sugar.Debugf("Handling gRPC request: %s", this())
+
+	ctx := srv.Context()
+
+	inst, err := internal.getInstance(ctx, req.GetInstance())
+	if err != nil {
+		return err
+	}
+
+	resp, err := internal.flow.InstanceVariable(ctx, &grpc.InstanceVariableRequest{
+		Namespace: inst.Instance.NamespaceID.String(),
+		Instance:  inst.Instance.ID.String(),
+		Key:       req.GetKey(),
+	})
+	if err != nil {
+		return err
+	}
+
+	iresp := &grpc.VariableInternalResponse{
+		Instance:  inst.Instance.ID.String(),
+		Key:       resp.GetKey(),
+		CreatedAt: resp.GetCreatedAt(),
+		UpdatedAt: resp.GetUpdatedAt(),
+		Checksum:  resp.GetChecksum(),
+		TotalSize: resp.GetTotalSize(),
+		Data:      resp.GetData(),
+		MimeType:  resp.GetMimeType(),
+	}
+
+	err = srv.Send(iresp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type setInstanceVariableParcelsTranslator struct {
+	internal *internal
+	inst     *libengine.Instance
+	grpc.Internal_SetInstanceVariableParcelsServer
+}
+
+func (srv *setInstanceVariableParcelsTranslator) SendAndClose(resp *grpc.SetInstanceVariableResponse) error {
+	var inst string
+	if srv.inst != nil {
+		inst = srv.inst.Instance.ID.String()
+	}
+
+	return srv.Internal_SetInstanceVariableParcelsServer.SendAndClose(&grpc.SetVariableInternalResponse{
+		Instance:  inst,
+		Key:       resp.GetKey(),
+		CreatedAt: resp.GetCreatedAt(),
+		UpdatedAt: resp.GetUpdatedAt(),
+		Checksum:  resp.GetChecksum(),
+		TotalSize: resp.GetTotalSize(),
+		MimeType:  resp.GetMimeType(),
+	})
+}
+
+func (srv *setInstanceVariableParcelsTranslator) Recv() (*grpc.SetInstanceVariableRequest, error) {
+	req, err := srv.Internal_SetInstanceVariableParcelsServer.Recv()
+	if err != nil {
+		return nil, err
+	}
+
+	if srv.inst == nil {
+		ctx := srv.Context()
+
+		srv.inst, err = srv.internal.getInstance(ctx, req.GetInstance())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &grpc.SetInstanceVariableRequest{
+		Namespace: srv.inst.TelemetryInfo.NamespaceName,
+		Instance:  srv.inst.Instance.ID.String(),
+		Key:       req.GetKey(),
+		TotalSize: req.GetTotalSize(),
+		Data:      req.GetData(),
+		MimeType:  req.GetMimeType(),
+	}, nil
+}
+
+func (internal *internal) SetInstanceVariableParcels(srv grpc.Internal_SetInstanceVariableParcelsServer) error {
+	internal.sugar.Debugf("Handling gRPC request: %s", this())
+
+	fsrv := &setInstanceVariableParcelsTranslator{
+		internal: internal,
+		Internal_SetInstanceVariableParcelsServer: srv,
+	}
+
+	err := internal.flow.SetInstanceVariableParcels(fsrv)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (flow *flow) InstanceVariable(ctx context.Context, req *grpc.InstanceVariableRequest) (*grpc.InstanceVariableResponse, error) {
-	// TODO: Alan, need fix here.
-	// nolint:nilnil
-	return nil, nil
+	flow.sugar.Debugf("Handling gRPC request: %s", this())
+
+	inst, err := flow.getInstance(ctx, req.GetNamespace(), req.GetInstance())
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := flow.beginSqlTx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	item, err := tx.DataStore().RuntimeVariables().GetByInstanceAndName(ctx, inst.Instance.ID, req.GetKey())
+	if err != nil {
+		return nil, err
+	}
+
+	var resp grpc.InstanceVariableResponse
+
+	resp.Namespace = req.GetNamespace()
+	resp.Instance = inst.Instance.ID.String()
+	resp.Key = item.Name
+	resp.CreatedAt = timestamppb.New(item.CreatedAt)
+	resp.UpdatedAt = timestamppb.New(item.UpdatedAt)
+	resp.TotalSize = int64(item.Size)
+	resp.MimeType = item.MimeType
+
+	if resp.TotalSize > parcelSize {
+		return nil, status.Error(codes.ResourceExhausted, "variable too large to return without using the parcelling API")
+	}
+	data, err := tx.DataStore().RuntimeVariables().LoadData(ctx, item.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp.Data = data
+
+	return &resp, nil
 }
 
 func (flow *flow) InstanceVariableParcels(req *grpc.InstanceVariableRequest, srv grpc.Flow_InstanceVariableParcelsServer) error {
-	// TODO: Alan, need fix here.
+	flow.sugar.Debugf("Handling gRPC request: %s", this())
+
+	ctx := srv.Context()
+
+	resp, err := flow.InstanceVariable(ctx, &grpc.InstanceVariableRequest{
+		Namespace: req.GetNamespace(),
+		Instance:  req.GetInstance(),
+		Key:       req.GetKey(),
+	})
+	if err != nil {
+		return err
+	}
+	err = srv.Send(resp)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- pkg/flow/grpc-namespace-variables.go
+++ pkg/flow/grpc-namespace-variables.go
@@ -10,11 +10,112 @@ import (
 	"github.com/direktiv/direktiv/pkg/flow/bytedata"
 	"github.com/direktiv/direktiv/pkg/flow/grpc"
 	"github.com/direktiv/direktiv/pkg/refactor/core"
+	libengine "github.com/direktiv/direktiv/pkg/refactor/engine"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+func (internal *internal) NamespaceVariableParcels(req *grpc.VariableInternalRequest, srv grpc.Internal_NamespaceVariableParcelsServer) error {
+	internal.sugar.Debugf("Handling gRPC request: %s", this())
+
+	ctx := srv.Context()
+
+	inst, err := internal.getInstance(ctx, req.GetInstance())
+	if err != nil {
+		return err
+	}
+
+	resp, err := internal.flow.NamespaceVariable(ctx, &grpc.NamespaceVariableRequest{
+		Namespace: inst.Instance.NamespaceID.String(),
+		Key:       req.GetKey(),
+	})
+	if err != nil {
+		return err
+	}
+
+	iresp := &grpc.VariableInternalResponse{
+		Instance:  inst.Instance.ID.String(),
+		Key:       resp.GetKey(),
+		CreatedAt: resp.GetCreatedAt(),
+		UpdatedAt: resp.GetUpdatedAt(),
+		Checksum:  resp.GetChecksum(),
+		TotalSize: resp.GetTotalSize(),
+		Data:      resp.GetData(),
+		MimeType:  resp.GetMimeType(),
+	}
+
+	err = srv.Send(iresp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type setNamespaceVariableParcelsTranslator struct {
+	internal *internal
+	inst     *libengine.Instance
+	grpc.Internal_SetNamespaceVariableParcelsServer
+}
+
+func (srv *setNamespaceVariableParcelsTranslator) SendAndClose(resp *grpc.SetNamespaceVariableResponse) error {
+	var inst string
+	if srv.inst != nil {
+		inst = srv.inst.Instance.ID.String()
+	}
+
+	return srv.Internal_SetNamespaceVariableParcelsServer.SendAndClose(&grpc.SetVariableInternalResponse{
+		Instance:  inst,
+		Key:       resp.GetKey(),
+		CreatedAt: resp.GetCreatedAt(),
+		UpdatedAt: resp.GetUpdatedAt(),
+		Checksum:  resp.GetChecksum(),
+		TotalSize: resp.GetTotalSize(),
+		MimeType:  resp.GetMimeType(),
+	})
+}
+
+func (srv *setNamespaceVariableParcelsTranslator) Recv() (*grpc.SetNamespaceVariableRequest, error) {
+	req, err := srv.Internal_SetNamespaceVariableParcelsServer.Recv()
+	if err != nil {
+		return nil, err
+	}
+
+	if srv.inst == nil {
+		ctx := srv.Context()
+
+		srv.inst, err = srv.internal.getInstance(ctx, req.GetInstance())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &grpc.SetNamespaceVariableRequest{
+		Namespace: srv.inst.TelemetryInfo.NamespaceName,
+		Key:       req.GetKey(),
+		TotalSize: req.GetTotalSize(),
+		Data:      req.GetData(),
+		MimeType:  req.GetMimeType(),
+	}, nil
+}
+
+func (internal *internal) SetNamespaceVariableParcels(srv grpc.Internal_SetNamespaceVariableParcelsServer) error {
+	internal.sugar.Debugf("Handling gRPC request: %s", this())
+
+	fsrv := &setNamespaceVariableParcelsTranslator{
+		internal: internal,
+		Internal_SetNamespaceVariableParcelsServer: srv,
+	}
+
+	err := internal.flow.SetNamespaceVariableParcels(fsrv)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (flow *flow) NamespaceVariable(ctx context.Context, req *grpc.NamespaceVariableRequest) (*grpc.NamespaceVariableResponse, error) {
 	flow.sugar.Debugf("Handling gRPC request: %s", this())
