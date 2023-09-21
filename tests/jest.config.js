bb6dc16f7813a95764024655f57bbe1b37107ec1
Alexander Ertli <alexander.ertli@direktiv.io>
2023-09-21 13:13:00 +0000

    Debug info in tests
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,5 +1,8 @@
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-
+if (isGithubActions) {
+  console.log("Running in GitHub Actions environment.");
+  console.log("DIREKTIV_HOST:", process.env.DIREKTIV_HOST);
+}
 module.exports = {
   testTimeout: isGithubActions ? 40000 : 10000,
   watchPlugins: [
