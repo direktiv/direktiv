9f4ed192092a0041ecdde218567e6b6895e1483b
sir-hassan <yassir.hassan@direktiv.io>
2023-09-27 09:21:52 +0200

    Bugfix: instance variables appear in namespace vars page
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/refactor/core/runtime_variables.go
+++ pkg/refactor/core/runtime_variables.go
@@ -53,8 +53,8 @@ type RuntimeVariablesStore interface {
 	// ListByWorkflowPath gets all runtime variable entries from store that are linked to specific namespace & workflow path
 	ListByWorkflowPath(ctx context.Context, namespaceID uuid.UUID, workflowPath string) ([]*RuntimeVariable, error)
 
-	// ListByNamespaceID gets all runtime variable entries from store that are linked to specific namespace id
-	ListByNamespaceID(ctx context.Context, namespaceID uuid.UUID) ([]*RuntimeVariable, error)
+	// ListNamespaceLevel gets all runtime variable entries from store that are at namespace level.
+	ListNamespaceLevel(ctx context.Context, namespaceID uuid.UUID) ([]*RuntimeVariable, error)
 
 	// Set tries to update runtime variable data and mimetype fields or insert a new one if no matching variable to
 	// update. Param variable should have one reference field set and name field set.
