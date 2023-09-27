9ba5d9c35f8d87da52ff489c5f1d66d1e7e70cf3
Stefan Kracht <stefan.kracht@direktiv.io>
2023-09-27 11:21:03 +0200

    remove regex and avoid hard coded frontend url

--- e2e/settings/deleteNamespace.spec.ts
+++ e2e/settings/deleteNamespace.spec.ts
@@ -42,28 +42,33 @@ test("it is possible to delete namespaces", async ({ page }) => {
   await confirmButton.click();
 
   await page.waitForURL("/");
-  expect(page.url(), "url should navigate to the very initial state").toBe(
-    "http://localhost:3333/"
-  );
-  const regex = /^http:\/\/localhost:3333\/[a-zA-Z0-9]+\/explorer\/tree$/;
 
-  await expect
-    .poll(
-      async () => page.url(),
-      "redirected url should match with namespace path regexp"
-    )
-    .toMatch(regex);
-
-  const namespaces = await getNamespaces({
+  const namespacesAfterDelete = await getNamespaces({
     urlParams: {
       baseUrl: process.env.VITE_DEV_API_DOMAIN,
     },
     headers,
   });
 
-  const deletedNamespaceIsInResults = namespaces.results.some(
+  const deletedNamespaceIsInResults = namespacesAfterDelete.results.some(
     (item) => item.name === namespace,
     "the api does not include the current namespace in the namespace list after deletion"
   );
   expect(deletedNamespaceIsInResults).toBe(false);
+
+  const frontpageUrl = new URL(page.url());
+  expect(frontpageUrl.pathname, "url should navigate to the landingpage").toBe(
+    "/"
+  );
+
+  await expect
+    .poll(async () => {
+      const currentPath = new URL(page.url()).pathname;
+      const namespace = currentPath.split("/")[1];
+      return (
+        currentPath.endsWith("/explorer/tree") &&
+        namespacesAfterDelete.results.some((ns) => ns.name === namespace)
+      );
+    }, "redirected url should match with namespace path regexp")
+    .toBe(true);
 });
