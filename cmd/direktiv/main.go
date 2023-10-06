392dda4404ee97041d2dd68052b09afa31a26568
sir-hassan <yassir.hassan@direktiv.io>
2023-10-06 20:06:43 +0200

    Remove unused code.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- cmd/direktiv/main.go
+++ cmd/direktiv/main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/direktiv/direktiv/cmd/api"
 	"github.com/direktiv/direktiv/cmd/flow"
-	flow_dbinit "github.com/direktiv/direktiv/cmd/flow-dbinit"
 	"github.com/direktiv/direktiv/cmd/sidecar"
 )
 
@@ -21,8 +20,6 @@ func main() {
 		sidecar.RunApplication()
 	case "flow":
 		flow.RunApplication()
-	case "flow_dbinit":
-		flow_dbinit.RunApplication()
 	case "":
 		log.Fatalf("error: empty DIREKTIV_APP environment variable.\n")
 	default:
