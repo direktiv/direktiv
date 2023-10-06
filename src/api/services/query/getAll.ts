3e3a69e607fcbebaaa55565186fe68fe50f54929
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-06 09:59:03 +0200

    disable streaming
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/services/query/getAll.ts
+++ src/api/services/query/getAll.ts
@@ -1,11 +1,5 @@
-import { QueryFunctionContext, useQueryClient } from "@tanstack/react-query";
-import {
-  ServiceStreamingSchema,
-  ServiceStreamingSchemaType,
-  ServicesListSchema,
-  ServicesListSchemaType,
-} from "../schema/services";
-
+import { QueryFunctionContext } from "@tanstack/react-query";
+import { ServicesListSchema } from "../schema/services";
 import { apiFactory } from "~/api/apiFactory";
 import { forceLeadingSlash } from "~/api/tree/utils";
 import { memo } from "react";
@@ -13,7 +7,6 @@ import { serviceKeys } from "..";
 import { useApiKey } from "~/util/store/apiKey";
 import { useNamespace } from "~/util/store/namespace";
 import useQueryWithPermissions from "~/api/useQueryWithPermissions";
-import { useStreaming } from "~/api/streaming";
 
 export const getServices = apiFactory({
   url: ({
@@ -44,86 +37,87 @@ const fetchServices = async ({
     urlParams: { namespace, workflow },
   });
 
-const updateCache = (
-  oldData: ServicesListSchemaType | undefined,
-  streamingPayload: ServiceStreamingSchemaType
-) => {
-  if (!oldData) {
-    return undefined;
-  }
+// const updateCache = (
+//   oldData: ServicesListSchemaType | undefined,
+//   streamingPayload: ServiceStreamingSchemaType
+// ) => {
+//   if (!oldData) {
+//     return undefined;
+//   }
 
-  return {
-    ...oldData,
-    /**
-     * Please note that we don't add new services to the react-query cache via streaming
-     * because the streamingPayload.event === "ADDED" cannot be trusted. Instead, we
-     * update the cache manually. The drawback is this will not detect services added outside
-     * of the current browser window.
-     */
-    functions: oldData.functions
-      // swap the element that came in (if it already is in the cache)
-      .map((func) => {
-        if (func.serviceName === streamingPayload.function.serviceName) {
-          return streamingPayload.function;
-        }
-        return func;
-      })
-      // remove element if it was deleted
-      .filter((func) => {
-        if (streamingPayload.event !== "DELETED") {
-          return true;
-        }
-        return func.serviceName !== streamingPayload.function.serviceName;
-      }),
-  };
-};
+//   return {
+//     ...oldData,
+//     /**
+//      * Please note that we don't add new services to the react-query cache via streaming
+//      * because the streamingPayload.event === "ADDED" cannot be trusted. Instead, we
+//      * update the cache manually. The drawback is this will not detect services added outside
+//      * of the current browser window.
+//      */
+//     functions: oldData.functions
+//       // swap the element that came in (if it already is in the cache)
+//       .map((func) => {
+//         if (func.serviceName === streamingPayload.function.serviceName) {
+//           return streamingPayload.function;
+//         }
+//         return func;
+//       })
+//       // remove element if it was deleted
+//       .filter((func) => {
+//         if (streamingPayload.event !== "DELETED") {
+//           return true;
+//         }
+//         return func.serviceName !== streamingPayload.function.serviceName;
+//       }),
+//   };
+// };
 
-export const useServicesStream = ({
-  enabled = true,
-  workflow,
-}: { enabled?: boolean; workflow?: string } = {}) => {
-  const apiKey = useApiKey();
-  const namespace = useNamespace();
-  const queryClient = useQueryClient();
+// export const useServicesStream = ({
+//   enabled = true,
+//   workflow,
+// }: { enabled?: boolean; workflow?: string } = {}) => {
+//   const apiKey = useApiKey();
+//   const namespace = useNamespace();
+//   const queryClient = useQueryClient();
 
-  if (!namespace) {
-    throw new Error("namespace is undefined");
-  }
+//   if (!namespace) {
+//     throw new Error("namespace is undefined");
+//   }
 
-  return useStreaming({
-    url: workflow
-      ? `/api/functions/namespaces/${namespace}/tree${forceLeadingSlash(
-          workflow
-        )}?op=services`
-      : `/api/functions/namespaces/${namespace}`,
-    apiKey: apiKey ?? undefined,
-    enabled,
-    schema: ServiceStreamingSchema,
-    onMessage: (msg) => {
-      queryClient.setQueryData<ServicesListSchemaType>(
-        serviceKeys.servicesList(namespace, {
-          apiKey: apiKey ?? undefined,
-          workflow: workflow ?? undefined,
-        }),
-        (oldData) => updateCache(oldData, msg)
-      );
-    },
-  });
-};
+//   return useStreaming({
+//     url: workflow
+//       ? `/api/functions/namespaces/${namespace}/tree${forceLeadingSlash(
+//           workflow
+//         )}?op=services`
+//       : `/api/functions/namespaces/${namespace}`,
+//     apiKey: apiKey ?? undefined,
+//     enabled,
+//     schema: ServiceStreamingSchema,
+//     onMessage: (msg) => {
+//       queryClient.setQueryData<ServicesListSchemaType>(
+//         serviceKeys.servicesList(namespace, {
+//           apiKey: apiKey ?? undefined,
+//           workflow: workflow ?? undefined,
+//         }),
+//         (oldData) => updateCache(oldData, msg)
+//       );
+//     },
+//   });
+// };
 
-type ServicesStreamingSubscriberType = {
-  enabled?: boolean;
-  workflow?: string;
-};
+// type ServicesStreamingSubscriberType = {
+//   enabled?: boolean;
+//   workflow?: string;
+// };
 
 export const ServicesStreamingSubscriber = memo(
-  ({ enabled, workflow }: ServicesStreamingSubscriberType) => {
-    useServicesStream({
-      enabled: enabled ?? true,
-      workflow: workflow ?? undefined,
-    });
-    return null;
-  }
+  () => null
+  // ({ enabled, workflow }: ServicesStreamingSubscriberType) => {
+  //   useServicesStream({
+  //     enabled: enabled ?? true,
+  //     workflow: workflow ?? undefined,
+  //   });
+  //   return null;
+  // }
 );
 
 ServicesStreamingSubscriber.displayName = "ServicesStreamingSubscriber";
