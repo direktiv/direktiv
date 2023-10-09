767aa9a9d08315fc7bc9333a09e1cb21e4960711
julia-zielke <julia.zielke@direktiv.io>
2023-10-09 16:12:11 +0200

    First Draft of Notification Bell active/inactive
    Signed-off-by: julia-zielke <julia.zielke@direktiv.io>

--- src/design/Notification/index.tsx
+++ src/design/Notification/index.tsx
@@ -1,19 +1,13 @@
 import { Bell } from "lucide-react";
 
-const Notification = ({
-  className,
-  hasMessage,
-}: {
-  className?: string;
-  hasMessage?: boolean;
-}): JSX.Element =>
+const Notification = ({ hasMessage }: { hasMessage?: boolean }): JSX.Element =>
   hasMessage ? (
     <div className="relative h-6 w-6">
       <Bell className="relative" />
       <div className="absolute top-0 right-0 rounded-full bg-danger-10 p-1"></div>
     </div>
   ) : (
-    <Bell />
+    <Bell className="relative" />
   );
 
 export default Notification;
