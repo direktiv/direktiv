8a8c394f9d743762ec25eafd9b59eadb97477210
sir-hassan <yassir.hassan@direktiv.io>
2023-10-10 15:31:28 +0200

    Run gofumpt
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/util/env.go
+++ pkg/util/env.go
@@ -1,9 +1,9 @@
 package util
 
 const (
-	//DirektivServiceNamespace = "DIREKTIV_SERVICE_NAMESPACE"
-	//DirektivNamespace        = "DIREKTIV_NAMESPACE"
-	//DirektivIngressEndpoint   = "DIREKTIV_INTERNAL_ENDPOINT"
+	// DirektivServiceNamespace = "DIREKTIV_SERVICE_NAMESPACE"
+	// DirektivNamespace        = "DIREKTIV_NAMESPACE"
+	// DirektivIngressEndpoint   = "DIREKTIV_INTERNAL_ENDPOINT"
 
 	DirektivLogJSON = "DIREKTIV_LOG_JSON"
 )
