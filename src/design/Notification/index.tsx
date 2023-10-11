6409a1941169b06adfd9f77357aab95e17ff524e
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-11 15:01:07 +0200

    add border to notification indicator
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Notification/index.tsx
+++ src/design/Notification/index.tsx
@@ -4,7 +4,7 @@ const Notification = ({ hasMessage }: { hasMessage?: boolean }): JSX.Element =>
   hasMessage ? (
     <div className="relative h-6 w-6">
       <Bell className="relative" />
-      <div className="absolute top-0 right-0 rounded-full bg-danger-10 p-1"></div>
+      <div className="absolute top-0 right-0 rounded-full border-2 border-white bg-danger-10 p-1 dark:border-black dark:bg-danger-dark-10"></div>
     </div>
   ) : (
     <Bell className="relative" />
