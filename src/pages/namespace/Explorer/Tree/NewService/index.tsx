84779de202ab081dbce30d228adc7e790bd46b14
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-13 09:58:57 +0200

    add split view buttons
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Tree/NewService/index.tsx
+++ src/pages/namespace/Explorer/Tree/NewService/index.tsx
@@ -1,22 +1,23 @@
+import { Code, Columns, Play, PlusCircle } from "lucide-react";
 import {
   DialogClose,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "~/design/Dialog";
-import { Play, PlusCircle } from "lucide-react";
 
 import Button from "~/design/Button";
+import { ButtonBar } from "~/design/ButtonBar";
 import { Card } from "~/design/Card";
 import Editor from "~/design/Editor";
 import { JSONSchemaForm } from "~/design/JSONschemaForm";
 import { RJSFSchema } from "@rjsf/utils";
 import { ScrollArea } from "~/design/ScrollArea";
+import { Toggle } from "~/design/Toggle";
 import { stringify } from "json-to-pretty-yaml";
 import { useState } from "react";
 import { useTheme } from "~/util/store/theme";
 import { useTranslation } from "react-i18next";
-import yamljs from "js-yaml";
 
 const jsonToYaml = (json: Record<string, unknown>) => {
   if (Object.keys(json).length === 0) {
@@ -64,12 +65,24 @@ const NewService = ({
   const { t } = useTranslation();
   const disableSubmit = false;
   const isLoading = false;
-
+  const [splitView, setSplitView] = useState(true);
   const [serviceConfig, setServiceConfig] = useState({
     direktiv_api: "service/v1",
   });
   const formId = `new-service-${path}`;
   const theme = useTheme();
+
+  /**
+   * TODO:
+   * [x] disable editor
+   * [x] style headlines (with icons?)
+   * - JSONSchemaForm onChange,
+   * - add a zod schema that will
+   *   - validate the formdata
+   *   - parse out empty strings
+   *   - handles missing fields (or dies the schema do this?)
+   * - add a toggle to use code only
+   */
 
   return (
     <>
@@ -79,10 +92,8 @@ const NewService = ({
           {t("pages.explorer.tree.newService.title")}
         </DialogTitle>
       </DialogHeader>
-
-      <div className="flex gap-5">
-        <Card className="h-96 w-full p-4" noShadow background="weight-1">
-          Form
+      <div className="flex flex-col gap-5">
+        <Card className="h-auto w-full p-4" noShadow background="weight-1">
           <ScrollArea className="h-full">
             <JSONSchemaForm
               formData={serviceConfig}
@@ -96,7 +107,6 @@ const NewService = ({
           </ScrollArea>
         </Card>
         <Card className="h-96 w-full p-4" noShadow background="weight-1">
-          Preview
           <Editor
             value={jsonToYaml(serviceConfig)}
             theme={theme ?? undefined}
@@ -105,6 +115,24 @@ const NewService = ({
             }}
           />
         </Card>
+        <ButtonBar>
+          <Toggle
+            pressed={!splitView}
+            onClick={() => {
+              setSplitView(false);
+            }}
+          >
+            <Code />
+          </Toggle>
+          <Toggle
+            pressed={splitView}
+            onClick={() => {
+              setSplitView(true);
+            }}
+          >
+            <Columns />
+          </Toggle>
+        </ButtonBar>
       </div>
       <DialogFooter>
         <DialogClose asChild>
@@ -112,7 +140,6 @@ const NewService = ({
             {t("pages.explorer.tree.newService.cancelBtn")}
           </Button>
         </DialogClose>
-
         <Button
           data-testid="new-workflow-submit"
           type="submit"
