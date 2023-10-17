69cff6823fc4b7829e0cadc1779b31d84ec15330
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-17 10:08:45 +0200

    remove VITE_LEGACY_DESIGN
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,12 @@
 import "./i18n";
 
-import React, { lazy } from "react";
+import App from "./App";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import env from "./config/env/";
 
 const app = document.getElementById("root");
 
 const root = createRoot(app);
-
-const App = lazy(() => {
-  // lazy load is important here, if would just conditionally render one
-  // or the other, the css imports of both modules would be loaded in eather case
-  if (env.VITE_LEGACY_DESIGN) {
-    return import("./AppLegacy");
-  }
-  return import("./App");
-});
 
 root.render(
   <React.StrictMode>
