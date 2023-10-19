00ff48cff8652d97c45e60d5dff2c1f44dabcef5
sir-hassan <yassir.hassan@direktiv.io>
2023-10-19 17:11:31 +0200

    Fix e2e tests.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/common/helpers.js
+++ tests/common/helpers.js
@@ -22,21 +22,23 @@ async function deleteAllNamespaces() {
     expect(listResponse.body.results.length).toEqual(0)
 }
 
-async function expectCreateNamespace(expect, testNamespace) {
-    const res = await request(common.config.getDirektivHost()).put(`/api/namespaces/${testNamespace}`)
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toMatchObject({
-        namespace: {
-            name: testNamespace,
-            oid: expect.stringMatching(common.regex.uuidRegex),
-            // regex /^2.*Z$/ matches timestamps like 2023-03-01T14:19:52.383871512Z
-            createdAt: expect.stringMatching(/^2.*Z$/),
-            updatedAt: expect.stringMatching(/^2.*Z$/),
-        }
+async function itShouldCreateNamespace(it, expect, ns) {
+    it(`should create a new namespace ${ns}`, async () => {
+        const res = await request(common.config.getDirektivHost()).put(`/api/namespaces/${ns}`)
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toMatchObject({
+            namespace: {
+                name: ns,
+                oid: expect.stringMatching(common.regex.uuidRegex),
+                // regex /^2.*Z$/ matches timestamps like 2023-03-01T14:19:52.383871512Z
+                createdAt: expect.stringMatching(/^2.*Z$/),
+                updatedAt: expect.stringMatching(/^2.*Z$/),
+            }
+        })
     })
 }
 
 export default {
     deleteAllNamespaces,
-    expectCreateNamespace,
+    itShouldCreateNamespace,
 }
