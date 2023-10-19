4089f89ab4ab2eb093ace886cd1bd83453dfa59c
sir-hassan <yassir.hassan@direktiv.io>
2023-10-19 17:38:53 +0200

    Fix e2e tests.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/common/helpers.js
+++ tests/common/helpers.js
@@ -4,22 +4,18 @@ import config from "./config";
 import common from "./index";
 
 async function deleteAllNamespaces() {
-    var listResponse = await request(config.getDirektivHost()).get(`/api/namespaces`)
-    expect(listResponse.statusCode).toEqual(200)
-    var namespaces = listResponse.body.results
-
-    if (namespaces.length == 0) {
-        return
+    let listResponse = await request(config.getDirektivHost()).get(`/api/namespaces`)
+    if(listResponse.statusCode !== 200) {
+        throw Error(`none ok namespaces list statusCode(${listResponse.statusCode})`)
     }
 
-    for (let i = 0; i < namespaces.length; i++) {
-        var name = namespaces[i].name
-        var req = await request(config.getDirektivHost()).delete(`/api/namespaces/${name}?recursive=true`)
-    }
+    for (const namespace of listResponse.body.results) {
+        let response = await request(config.getDirektivHost()).delete(`/api/namespaces/${namespace.name}?recursive=true`)
 
-    var listResponse = await request(config.getDirektivHost()).get(`/api/namespaces`)
-    expect(listResponse.statusCode).toEqual(200)
-    expect(listResponse.body.results.length).toEqual(0)
+        if(response.statusCode !== 200) {
+            throw Error(`none ok namespace(${namespace.name}) delete statusCode(${response.statusCode})`)
+        }
+    }
 }
 
 async function itShouldCreateNamespace(it, expect, ns) {
