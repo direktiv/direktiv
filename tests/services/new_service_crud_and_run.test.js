df017b149b552291cb8e161cc62f691d7e1136b2
sir-hassan <yassir.hassan@direktiv.io>
2023-10-19 17:02:30 +0200

    Add service create test
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

---
+++ tests/services/new_service_crud_and_run.test.js
@@ -0,0 +1,43 @@
+import request from 'supertest'
+import common from "../common";
+
+const testNamespace = "test-services"
+
+beforeAll(async () => {
+    // delete a 'test-namespace' if it's already exit.
+    await request(common.config.getDirektivHost()).delete(`/api/namespaces/${testNamespace}?recursive=true`)
+});
+
+describe('Test services crud operations', () =>
+{
+    it(`should create a new namespace`, async () => {
+        await common.helpers.expectCreateNamespace(expect, testNamespace);
+    })
+
+    it(`should create a new service file`, async () => {
+        const res = await request(common.config.getDirektivHost())
+            .put(`/api/namespaces/${testNamespace}/tree/my-workflow.yaml?op=create-workflow`)
+            .set({
+                'Content-Type': 'text/plain',
+            })
+
+            .send(`
+direktiv_api: service/v1
+name: s1
+image: redis
+cmd: redis-server
+scale: 2
+`)
+
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toMatchObject({
+            namespace: testNamespace,
+        })
+    })
+
+});
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
