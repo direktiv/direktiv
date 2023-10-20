6f2515631f65d7e943e41d89b49562bdf4824189
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-20 15:40:42 +0200

    remove empty className

--- src/design/WorkflowDiagram/index.stories.tsx
+++ src/design/WorkflowDiagram/index.stories.tsx
@@ -217,7 +217,7 @@ export const UpdateWorkflow = () => {
 
   return (
     <Card className="flex h-96 flex-col gap-y-5 p-5" background="weight-1">
-      <ButtonBar className="">
+      <ButtonBar>
         <Button
           onClick={() => {
             setWorkflow(exampleWorkflow);
