a8e396f3e3616406e32435a44a3c958685a61702
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-25 15:19:55 +0200

    minor markup cleanup

--- src/componentsNext/NotificationMenu/index.tsx
+++ src/componentsNext/NotificationMenu/index.tsx
@@ -10,7 +10,6 @@ import {
 import { Check } from "lucide-react";
 import { Link } from "react-router-dom";
 import { twMergeClsx } from "~/util/helpers";
-import { useNamespace } from "~/util/store/namespace";
 import { useNamespaceLinting } from "~/api/namespaceLinting/query/useNamespaceLinting";
 import { useNotificationConfig } from "./config";
 import { useTranslation } from "react-i18next";
@@ -25,59 +24,55 @@ const NotificationMenu: React.FC<NotificationMenuProps> = ({ className }) => {
   const notificationConfig = useNotificationConfig();
   const showIndicator = !!data?.issues.length;
 
-  const textLoading = t("components.notificationMenu.isLoading.description");
-  const textNoIssues = t("components.notificationMenu.noIssues.description");
-  const title = t("components.notificationMenu.title");
-
-  const namespace = useNamespace();
-  if (!namespace) return null;
-
   const possibleNotifications = Object.entries(notificationConfig ?? {});
 
   return (
     <div className={twMergeClsx("self-end text-right", className)}>
       <Notification showIndicator={showIndicator}>
-        <NotificationTitle>{title}</NotificationTitle>
+        <NotificationTitle>
+          {t("components.notificationMenu.title")}
+        </NotificationTitle>
         <NotificationMenuSeparator />
-        {isLoading && <NotificationLoading>{textLoading}</NotificationLoading>}
-        {showIndicator && (
-          <div>
-            {possibleNotifications.map(
-              ([notificationType, notificationConfig], index, srcArr) => {
-                const isLast = index === srcArr.length - 1;
-                const matchingNotification = data.issues.filter(
-                  (issue) => notificationType === issue.type
-                );
-                if (matchingNotification.length <= 0) {
-                  return null;
-                }
-                return (
-                  <div key={notificationType}>
-                    <NotificationClose
-                      className="w-full hover:bg-gray-3 dark:hover:bg-gray-dark-3"
-                      asChild
-                    >
-                      <Link to={notificationConfig.href}>
-                        <NotificationMessage
-                          text={notificationConfig.description(
-                            matchingNotification.length
-                          )}
-                          icon={notificationConfig.icon}
-                        />
-                      </Link>
-                    </NotificationClose>
-                    {!isLast && <NotificationMenuSeparator />}
-                  </div>
-                );
-              }
-            )}
-          </div>
+        {isLoading && (
+          <NotificationLoading>
+            {t("components.notificationMenu.isLoading.description")}
+          </NotificationLoading>
         )}
-        {!showIndicator && (
+        {showIndicator ? (
+          possibleNotifications.map(
+            ([notificationType, notificationConfig], index, srcArr) => {
+              const isLastListItem = index === srcArr.length - 1;
+              const matchingNotification = data.issues.filter(
+                (issue) => notificationType === issue.type
+              );
+              if (matchingNotification.length <= 0) {
+                return null;
+              }
+              return (
+                <div key={notificationType}>
+                  <NotificationClose
+                    className="w-full hover:bg-gray-3 dark:hover:bg-gray-dark-3"
+                    asChild
+                  >
+                    <Link to={notificationConfig.href}>
+                      <NotificationMessage
+                        text={notificationConfig.description(
+                          matchingNotification.length
+                        )}
+                        icon={notificationConfig.icon}
+                      />
+                    </Link>
+                  </NotificationClose>
+                  {!isLastListItem && <NotificationMenuSeparator />}
+                </div>
+              );
+            }
+          )
+        ) : (
           <NotificationMessage
-            text={textNoIssues}
+            text={t("components.notificationMenu.noIssues.description")}
             icon={Check}
-          ></NotificationMessage>
+          />
         )}
       </Notification>
     </div>
