3d2ae41525af9d80c54a1fb51b90fe4c8b7e549c
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-27 15:58:17 +0200

    add header
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Workflow/Services/ServiceDetails.tsx
+++ src/pages/namespace/Explorer/Workflow/Services/ServiceDetails.tsx
@@ -1,7 +1,11 @@
+import Header from "~/pages/namespace/Services/Detail/Header";
 import { Pods } from "~/pages/namespace/Services/Detail/Pods";
 
 const ServiceDetails = ({ serviceId }: { serviceId: string }) => (
   <div className="flex grow flex-col">
+    <div className="flex-none">
+      <Header serviceId={serviceId} />
+    </div>
     <Pods serviceId={serviceId} />
   </div>
 );
