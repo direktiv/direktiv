4cf600af1dbfd70b8064dc8c030fc89e31332273
Stefan Kracht <stefan.kracht@direktiv.io>
2023-10-27 09:57:35 +0200

    add service subpage
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/pages/namespace/Explorer/Service/index.tsx
@@ -0,0 +1,55 @@
+import { Card } from "~/design/Card";
+import { FC } from "react";
+import { Layers } from "lucide-react";
+import { NoPermissions } from "~/design/Table";
+import { Outlet } from "react-router-dom";
+import { analyzePath } from "~/util/router/utils";
+import { pages } from "~/util/router/pages";
+import { useNamespace } from "~/util/store/namespace";
+import { useNodeContent } from "~/api/tree/query/node";
+
+const Header: FC = () => {
+  const { path } = pages.explorer.useParams();
+  const namespace = useNamespace();
+  const { segments } = analyzePath(path);
+  const filename = segments[segments.length - 1];
+
+  const {
+    isAllowed,
+    noPermissionMessage,
+    isFetched: isPermissionCheckFetched,
+  } = useNodeContent({ path });
+
+  if (!namespace) return null;
+  if (!path) return null;
+
+  if (!isPermissionCheckFetched) return null;
+
+  if (isAllowed === false)
+    return (
+      <Card className="m-5 flex grow">
+        <NoPermissions>{noPermissionMessage}</NoPermissions>
+      </Card>
+    );
+
+  return (
+    <>
+      <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 pb-0 dark:border-gray-dark-5 dark:bg-gray-dark-1">
+        <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
+          <h3
+            className="flex items-center gap-x-2 font-bold text-primary-500"
+            data-testid="workflow-header"
+          >
+            <Layers className="h-5" />
+            {filename?.relative}
+          </h3>
+          <div className="flex gap-x-3"></div>
+        </div>
+      </div>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Header;
