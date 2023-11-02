aa2d284d11ca9a9371734a4b9584f317f5f2113a
Alexander Ertli <alexander.ertli@direktiv.io>
2023-11-02 10:23:11 +0000

    Add inial js plugin
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

---
+++ pkg/refactor/gateway/plugin_js_exec.go
@@ -0,0 +1,60 @@
+package gateway
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/dop251/goja"
+)
+
+type jsExecutionPlugin struct {
+	conf jsExecutionPluginConfig
+}
+
+type jsExecutionPluginConfig struct {
+	Script string `json:"script"`
+}
+
+func (p *jsExecutionPlugin) build(c map[string]interface{}) (serve, error) {
+	if err := unmarshalConfig(c, p.conf); err != nil {
+		return nil, err
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) bool {
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error reading request body: %s", err), http.StatusInternalServerError)
+
+			return false
+		}
+		defer r.Body.Close()
+
+		vm := goja.New()
+		vm.Set("body", string(bodyBytes))
+
+		scriptWrapper := fmt.Sprintf(`function transform() { %s } transform();`, p.conf.Script)
+		_, err = vm.RunString(scriptWrapper)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Script execution error: %s", err), http.StatusInternalServerError)
+
+			return false
+		}
+
+		modifiedBody := vm.Get("body").String()
+		r.Body = io.NopCloser(strings.NewReader(modifiedBody))
+
+		return true
+	}, nil
+}
+
+func (p *jsExecutionPlugin) getSchema() interface{} {
+	return jsExecutionPluginConfig{}
+}
+
+//nolint:gochecknoinits
+func init() {
+	// Register the plugin in the registry
+	registry["js_execution_plugin"] = &jsExecutionPlugin{}
+}
