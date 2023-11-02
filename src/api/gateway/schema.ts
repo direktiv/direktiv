e9aad195e302e988b15eb07c9b1ef4b74bd4eac6
Stefan Kracht <stefan.kracht@direktiv.io>
2023-11-02 10:11:25 +0100

    update plugin schema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/api/gateway/schema.ts
+++ src/api/gateway/schema.ts
@@ -44,8 +44,47 @@ export const EndpointListSchema = z.object({
   data: z.array(EndpointSchema),
 });
 
-export const PluginsListSchema = z.object({
-  data: z.record(z.unknown()),
+/**
+ * example
+  {
+    "$defs": {
+      "examplePluginConfig": {
+        "additionalProperties": false,
+        "properties": {
+          "echo_value": {
+            "type": "string"
+          }
+        },
+        "required": [
+          "echo_value"
+        ],
+        "type": "object"
+      }
+    },
+    "$id": "https://github.com/direktiv/direktiv/pkg/refactor/gateway/example-plugin-config",
+    "$ref": "#/$defs/examplePluginConfig",
+    "$schema": "https://json-schema.org/draft/2020-12/schema"
+  } 
+ */
+const PluginJSONSchema = z.object({
+  $defs: z.record(z.record(z.unknown())),
+  $id: z.string(),
+  $ref: z.string(),
+  $schema: z.string(),
 });
+
+/**
+ * example
+  {
+    "data": {
+      "example_plugin": {...}
+    }
+  }
+ */
+export const PluginsListSchema = z.object({
+  data: z.record(PluginJSONSchema),
+});
+
+export type PluginsListSchemaType = z.infer<typeof PluginsListSchema>;
 
 export type GatewaySchemeType = z.infer<typeof EndpointSchema>;
