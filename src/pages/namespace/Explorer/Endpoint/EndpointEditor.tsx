b8036a4875dd720756746f3e37d2360d31ac0cbc
Stefan Kracht <stefan.kracht@direktiv.io>
2023-11-02 10:12:08 +0100

    first editor draft
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor.tsx
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor.tsx
@@ -1,9 +1,12 @@
+import { FC, useState } from "react";
+
 import Button from "~/design/Button";
 import { Card } from "~/design/Card";
-import { FC } from "react";
 import { Save } from "lucide-react";
 import { useNodeContent } from "~/api/tree/query/node";
+import { usePlugins } from "~/api/gateway/query/getPlugins";
 import { useTranslation } from "react-i18next";
+import { useUpdateWorkflow } from "~/api/tree/mutate/updateWorkflow";
 
 export type NodeContentType = ReturnType<typeof useNodeContent>["data"];
 
@@ -12,11 +15,39 @@ const EndpointEditor: FC<{
   path: string;
 }> = ({ data, path }) => {
   const { t } = useTranslation();
+  const { data: plugins } = usePlugins();
+  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+  const [error, setError] = useState<string | undefined>();
 
-  const isLoading = false;
+  const { mutate: updateService, isLoading } = useUpdateWorkflow({
+    onError: (error) => {
+      error && setError(error);
+    },
+    onSuccess: () => {
+      setHasUnsavedChanges(false);
+    },
+  });
+
+  const workflowData = atob(data.revision?.source ?? "");
+
+  const onSave = (toSave: string | undefined) => {
+    if (toSave) {
+      setError(undefined);
+      updateService({
+        path,
+        fileContent: toSave,
+      });
+    }
+  };
+
   return (
     <div className="relative flex grow flex-col space-y-4 p-5">
-      <Card className="flex grow flex-col p-4">test</Card>
+      <Card className="flex flex-col p-4" noShadow>
+        <div>Error: {error}</div>
+        <div>hasUnsavedChanges: {hasUnsavedChanges ? "yes" : "no"}</div>
+        <pre>{workflowData}</pre>
+      </Card>
+      <Card className="flex grow flex-col p-4"></Card>
       <div className="flex flex-col justify-end gap-4 sm:flex-row sm:items-center">
         <Button variant="outline" disabled={isLoading}>
           <Save />
