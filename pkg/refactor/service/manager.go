b557911e910b6ad83796dca8f10c4e151b90db9d
Alan Murtagh <alan.murtagh@direktiv.io>
2023-11-21 10:13:48 +1000

    Services api now returns a sorted list with initialized empty arrays instead of nulls.
    Signed-off-by: Alan Murtagh <alan.murtagh@direktiv.io>

--- pkg/refactor/service/manager.go
+++ pkg/refactor/service/manager.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"math/big"
 	"slices"
+	"sort"
 	"sync"
 	"time"
 
@@ -200,6 +201,22 @@ func (m *manager) SetServices(list []*core.ServiceConfig) {
 	}
 }
 
+type serviceList []*core.ServiceStatus
+
+func (x serviceList) Len() int {
+	return len(x)
+}
+
+func (x serviceList) Less(i, j int) bool {
+	return x[i].FilePath < x[j].FilePath
+}
+
+func (x serviceList) Swap(i, j int) {
+	tmp := x[i]
+	x[i] = x[j]
+	x[j] = tmp
+}
+
 func (m *manager) getList(filterNamespace string, filterTyp string, filterPath string, filterName string) ([]*core.ServiceStatus, error) {
 	// clone the list
 	sList := make([]*core.ServiceConfig, 0)
@@ -244,10 +261,12 @@ func (m *manager) getList(filterNamespace string, filterTyp string, filterPath s
 			result = append(result, &core.ServiceStatus{
 				ID:            v.GetID(),
 				ServiceConfig: *v,
-				Conditions:    nil,
+				Conditions:    make([]any, 0),
 			})
 		}
 	}
+
+	sort.Sort(serviceList(result))
 
 	return result, nil
 }
