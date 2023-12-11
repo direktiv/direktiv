ad397483dfbf85cf1a60cc16e3f2f43fc8a34899
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-11 14:52:15 +0100

    request consumer
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Gateway/Consumers/index.tsx
+++ src/pages/namespace/Gateway/Consumers/index.tsx
@@ -1,4 +1,13 @@
+import { useConsumers } from "~/api/gateway/query/getConsumers";
+
 const ConsumersPage = () => {
+  const {
+    data: gatewayList,
+    isSuccess,
+    isAllowed,
+    noPermissionMessage,
+  } = useConsumers();
+
   return <div>CONSUMERS</div>;
 };
 
