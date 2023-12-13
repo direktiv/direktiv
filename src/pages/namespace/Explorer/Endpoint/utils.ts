6191771fb4b3af46426a9babd990a66ccbd87af4
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-13 12:44:53 +0100

    extend schema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/utils.ts
+++ src/pages/namespace/Explorer/Endpoint/utils.ts
@@ -4,8 +4,10 @@ import { z } from "zod";
 
 export const EndpointFormSchema = z.object({
   direktiv_api: z.literal("endpoint/v1"),
-  methods: z.array(MethodsSchema).nonempty().optional(),
   allow_anonymous: z.boolean().optional(),
+  path: z.string().nonempty().optional(),
+  timeout: z.number().int().positive().optional(),
+  methods: z.array(MethodsSchema).nonempty().optional(),
 });
 
 type EndpointFormSchemaType = z.infer<typeof EndpointFormSchema>;
