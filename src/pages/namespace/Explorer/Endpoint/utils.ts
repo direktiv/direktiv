569519bce8a50d86f6c11e4f89b8abf79f2f85cd
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-14 10:47:50 +0100

    add remove if empty
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/utils.ts
+++ src/pages/namespace/Explorer/Endpoint/utils.ts
@@ -3,10 +3,13 @@ import { stringify } from "json-to-pretty-yaml";
 import yamljs from "js-yaml";
 import { z } from "zod";
 
+const removeIfEmpty = (val: string | undefined) =>
+  val === "" ? undefined : val;
+
 export const EndpointFormSchema = z.object({
   direktiv_api: z.literal("endpoint/v1"),
   allow_anonymous: z.boolean().optional(),
-  path: z.string().nonempty().optional(),
+  path: z.string().optional().transform(removeIfEmpty),
   timeout: z.number().int().positive().optional(),
   methods: z.array(MethodsSchema).nonempty().optional(),
 });
