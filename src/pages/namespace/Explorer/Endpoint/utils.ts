ed5b2393bf204a6858d4c011ebc967be57aa2ecd
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-14 14:14:45 +0100

    add removeIfEmptyArray
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/utils.ts
+++ src/pages/namespace/Explorer/Endpoint/utils.ts
@@ -3,15 +3,18 @@ import { stringify } from "json-to-pretty-yaml";
 import yamljs from "js-yaml";
 import { z } from "zod";
 
-const removeIfEmpty = (val: string | undefined) =>
+const removeIfEmptyString = (val: string | undefined) =>
   val === "" ? undefined : val;
+
+const removeIfEmptyArray = <T>(arr: T[] | undefined) =>
+  arr?.length === 0 ? undefined : arr;
 
 export const EndpointFormSchema = z.object({
   direktiv_api: z.literal("endpoint/v1"),
   allow_anonymous: z.boolean().optional(),
-  path: z.string().optional().transform(removeIfEmpty),
+  path: z.string().optional().transform(removeIfEmptyString),
   timeout: z.number().int().positive().optional(),
-  methods: z.array(MethodsSchema).nonempty().optional(),
+  methods: z.array(MethodsSchema).optional().transform(removeIfEmptyArray),
 });
 
 export type EndpointFormSchemaType = z.infer<typeof EndpointFormSchema>;
