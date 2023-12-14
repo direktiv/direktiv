63c8aba6fd22cb89694bda838c1227a82a349e4d
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-14 16:27:28 +0100

    configure target plugins
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/utils.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/utils.ts
@@ -3,12 +3,39 @@ import { stringify } from "json-to-pretty-yaml";
 import yamljs from "js-yaml";
 import { z } from "zod";
 
+const InstantResposeFormSchema = z.object({
+  type: z.literal("instant-response"),
+  configuration: z.object({
+    content_type: z.string().nonempty(),
+    status_code: z.number().int().positive(),
+    status_message: z.string().nonempty(),
+  }),
+});
+
+const TargetFlowFormSchema = z.object({
+  type: z.literal("target-flow"),
+  configuration: z.object({
+    flow: z.string().nonempty(),
+    content_type: z.string().nonempty(),
+    namespace: z.string().nonempty().optional(),
+    async: z.boolean().optional(),
+  }),
+});
+
 export const EndpointFormSchema = z.object({
   direktiv_api: z.literal("endpoint/v1"),
   allow_anonymous: z.boolean().optional(),
   path: z.string().nonempty().optional(),
   timeout: z.number().int().positive().optional(),
   methods: z.array(MethodsSchema).nonempty().optional(),
+  plugins: z
+    .object({
+      target: z.discriminatedUnion("type", [
+        InstantResposeFormSchema,
+        TargetFlowFormSchema,
+      ]),
+    })
+    .optional(),
 });
 
 export type EndpointFormSchemaType = z.infer<typeof EndpointFormSchema>;
