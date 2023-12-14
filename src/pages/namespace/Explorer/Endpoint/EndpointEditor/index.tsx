1521ac881e2a43b909b3c22fc1d182848035f01b
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-14 10:41:20 +0100

    fix grow
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/index.tsx
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/index.tsx
@@ -6,6 +6,7 @@ import { Card } from "~/design/Card";
 import EndpointPreview from "../EndpointPreview";
 import { FC } from "react";
 import { Form } from "./Form";
+import FormErrors from "~/componentsNext/FormErrors";
 import { RouteSchemeType } from "~/api/gateway/schema";
 import { Save } from "lucide-react";
 import { stringify } from "json-to-pretty-yaml";
@@ -39,7 +40,7 @@ const EndpointEditor: FC<EndpointEditorProps> = ({ data, path }) => {
     <Form endpointConfig={endpointConfig}>
       {({
         formControls: {
-          formState: { isDirty },
+          formState: { isDirty, touchedFields, errors },
           handleSubmit,
         },
         formMarkup,
@@ -49,19 +50,34 @@ const EndpointEditor: FC<EndpointEditorProps> = ({ data, path }) => {
           className="relative flex grow flex-col space-y-4 p-5"
         >
           <Card className="flex grow flex-col p-4">
-            <div className="grid grow grid-cols-2">
-              {!endpointConfig ? (
-                <Alert variant="error">
-                  {t("pages.explorer.endpoint.editor.form.serialisationError")}
-                </Alert>
-              ) : (
-                <div>{formMarkup}</div>
-              )}
-              <Card className="grid grid-rows-2 p-5">
-                <pre>{endpointFileContent}</pre>
-                <pre>{JSON.stringify(endpointConfig, null, 2)}</pre>
-              </Card>
+            <div className="grow">
+              <div className="grid grow grid-cols-2">
+                {!endpointConfig ? (
+                  <Alert variant="error">
+                    {t(
+                      "pages.explorer.endpoint.editor.form.serialisationError"
+                    )}
+                  </Alert>
+                ) : (
+                  <div>
+                    <FormErrors errors={errors} className="mb-5" />
+                    {formMarkup}
+                  </div>
+                )}
+                <div className="grid grid-rows-3 gap-3">
+                  <Card className="p-5">
+                    <pre>{endpointFileContent}</pre>
+                  </Card>
+                  <Card className="p-5">
+                    <pre>{JSON.stringify(endpointConfig, null, 2)}</pre>
+                  </Card>
+                  <Card className="p-5">
+                    <pre>{JSON.stringify(touchedFields, null, 2)}</pre>
+                  </Card>
+                </div>
+              </div>
             </div>
+
             <div className="flex justify-end gap-2 pt-2 text-sm text-gray-8 dark:text-gray-dark-8">
               {isDirty && (
                 <span className="text-center">
@@ -72,7 +88,7 @@ const EndpointEditor: FC<EndpointEditorProps> = ({ data, path }) => {
           </Card>
           <div className="flex flex-col justify-end gap-4 sm:flex-row sm:items-center">
             <EndpointPreview fileContent={stringify(endpointConfig)} />
-            <Button variant="outline" disabled={isLoading}>
+            <Button variant="outline" disabled={isLoading} type="submit">
               <Save />
               {t("pages.explorer.endpoint.editor.saveBtn")}
             </Button>
