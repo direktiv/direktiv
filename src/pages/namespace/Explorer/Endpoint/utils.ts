c85b0afaec8cecc9f50a3fce22d5815102f57672
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-14 15:25:10 +0100

    make path non optional
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/utils.ts
+++ src/pages/namespace/Explorer/Endpoint/utils.ts
@@ -6,7 +6,7 @@ import { z } from "zod";
 export const EndpointFormSchema = z.object({
   direktiv_api: z.literal("endpoint/v1"),
   allow_anonymous: z.boolean().optional(),
-  path: z.string().optional(),
+  path: z.string().nonempty().optional(),
   timeout: z.number().int().positive().optional(),
   methods: z.array(MethodsSchema).nonempty().optional(),
 });
