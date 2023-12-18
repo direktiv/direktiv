e22918d95e80b5fdd11b10251c450f2a6043ccb9
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-18 15:40:42 +0100

    add RequestConvertFormSchema fields
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/requestConvert.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/requestConvert.ts
@@ -3,7 +3,12 @@ import { z } from "zod";
 
 export const RequestConvertFormSchema = z.object({
   type: z.literal(inboundPluginTypes.requestConvert),
-  configuration: z.object({}),
+  configuration: z.object({
+    omit_headers: z.boolean(),
+    omit_queries: z.boolean(),
+    omit_body: z.boolean(),
+    omit_consumer: z.boolean(),
+  }),
 });
 
 export type RequestConvertFormSchemaType = z.infer<
