a070d0eddf37c59dc60741f90c31257367bc60e8
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-18 11:17:33 +0100

    add new schemas to TargetPluginFormSchema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/target/schema.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/target/schema.ts
@@ -1,8 +1,14 @@
 import { InstantResponseFormSchema } from "./InstantResponse";
 import { TargetFlowFormSchema } from "./TargetFlow";
+import { TargetFlowVarFormSchema } from "./TargetFlowVar";
+import { TargetNamespaceFileFormSchema } from "./TargetNamespaceFile";
+import { TargetNamespaceVarFormSchema } from "./TargetNamespaceVar";
 import { z } from "zod";
 
 export const TargetPluginFormSchema = z.discriminatedUnion("type", [
   InstantResponseFormSchema,
   TargetFlowFormSchema,
+  TargetFlowVarFormSchema,
+  TargetNamespaceFileFormSchema,
+  TargetNamespaceVarFormSchema,
 ]);
