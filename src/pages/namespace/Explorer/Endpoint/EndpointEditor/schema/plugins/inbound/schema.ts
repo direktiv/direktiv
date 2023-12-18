bbe18f1b180cd5e48e6ff85a54117a8adeb9a392
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-18 15:36:35 +0100

    remove HeaderManipulationFormSchema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/schema.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/schema.ts
@@ -1,12 +1,10 @@
 import { AclFormSchema } from "./acl";
-import { HeaderManipulationFormSchema } from "./headerManipulation";
 import { JsInboundFormSchema } from "./jsInbound";
 import { RequestConvertFormSchema } from "./requestConvert";
 import { z } from "zod";
 
 export const InboundPluginFormSchema = z.discriminatedUnion("type", [
   AclFormSchema,
-  HeaderManipulationFormSchema,
   JsInboundFormSchema,
   RequestConvertFormSchema,
 ]);
