8aef342270bc547932a4646bec381c51ab45e988
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-18 15:40:04 +0100

    add AclFormSchema fields
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/acl.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/acl.ts
@@ -3,7 +3,12 @@ import { z } from "zod";
 
 export const AclFormSchema = z.object({
   type: z.literal(inboundPluginTypes.acl),
-  configuration: z.object({}),
+  configuration: z.object({
+    allow_groups: z.array(z.string()),
+    deny_groups: z.array(z.string()),
+    allow_tags: z.array(z.string()),
+    deny_tags: z.array(z.string()),
+  }),
 });
 
 export type AclFormSchemaType = z.infer<typeof AclFormSchema>;
