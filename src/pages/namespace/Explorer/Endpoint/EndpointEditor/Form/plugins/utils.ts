11ade5321c5ae0fe37917bb79b6b8078e56ef0b6
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-19 16:32:26 +0100

    add  getBasicAuthConfigAtIndex, getGithubWebhookAuthConfigAtIndex and getKeyAuthConfigAtIndex,
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/utils.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/utils.ts
@@ -1,8 +1,13 @@
+import { AuthPluginFormSchemaType } from "../../schema/plugins/auth/schema";
+import { BasicAuthFormSchemaType } from "../../schema/plugins/auth/basicAuth";
+import { GithubWebhookAuthFormSchemaType } from "../../schema/plugins/auth/githubWebhookAuth";
 import { InboundPluginFormSchemaType } from "../../schema/plugins/inbound/schema";
 import { JsInboundFormSchemaType } from "../../schema/plugins/inbound/jsInbound";
 import { JsOutboundFormSchemaType } from "../../schema/plugins/outbound/jsOutbound";
+import { KeyAuthFormSchemaType } from "../../schema/plugins/auth/keyAuth";
 import { OutboundPluginFormSchemaType } from "../../schema/plugins/outbound/schema";
 import { RequestConvertFormSchemaType } from "../../schema/plugins/inbound/requestConvert";
+import { authPluginTypes } from "../../schema/plugins/auth";
 import { inboundPluginTypes } from "../../schema/plugins/inbound";
 import { outboundPluginTypes } from "../../schema/plugins/outbound";
 
@@ -39,6 +44,36 @@ export const getJsOutboundConfigAtIndex = (
 ): JsOutboundFormSchemaType["configuration"] | undefined => {
   const plugin = index !== undefined ? fields?.[index] : undefined;
   return plugin?.type === outboundPluginTypes.jsOutbound
+    ? plugin.configuration
+    : undefined;
+};
+
+export const getBasicAuthConfigAtIndex = (
+  fields: AuthPluginFormSchemaType[] | undefined,
+  index: number | undefined
+): BasicAuthFormSchemaType["configuration"] | undefined => {
+  const plugin = index !== undefined ? fields?.[index] : undefined;
+  return plugin?.type === authPluginTypes.basicAuth
+    ? plugin.configuration
+    : undefined;
+};
+
+export const getKeyAuthConfigAtIndex = (
+  fields: AuthPluginFormSchemaType[] | undefined,
+  index: number | undefined
+): KeyAuthFormSchemaType["configuration"] | undefined => {
+  const plugin = index !== undefined ? fields?.[index] : undefined;
+  return plugin?.type === authPluginTypes.keyAuth
+    ? plugin.configuration
+    : undefined;
+};
+
+export const getGithubWebhookAuthConfigAtIndex = (
+  fields: AuthPluginFormSchemaType[] | undefined,
+  index: number | undefined
+): GithubWebhookAuthFormSchemaType["configuration"] | undefined => {
+  const plugin = index !== undefined ? fields?.[index] : undefined;
+  return plugin?.type === authPluginTypes.githubWebhookAuth
     ? plugin.configuration
     : undefined;
 };
