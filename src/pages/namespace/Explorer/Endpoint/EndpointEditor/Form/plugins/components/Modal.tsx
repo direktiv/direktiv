102e54c445212720c1feb8a2a8bcfa0de548f7a1
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-19 14:04:42 +0100

    move components around
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/components/Modal.tsx
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/components/Modal.tsx
@@ -6,12 +6,19 @@ import {
 } from "~/design/Dialog";
 import { FC, PropsWithChildren } from "react";
 
-import { Button } from "react-day-picker";
+import Button from "~/design/Button";
 
-export const ModalFooter = () => (
-  <DialogFooter>
-    <Button type="submit">Save</Button>
-  </DialogFooter>
+type ModalWrapperProps = PropsWithChildren & {
+  title: string;
+};
+
+export const ModalWrapper: FC<ModalWrapperProps> = ({ title, children }) => (
+  <DialogContent className="sm:max-w-xl">
+    <DialogHeader>
+      <DialogTitle>{title}</DialogTitle>
+    </DialogHeader>
+    <div className="my-3 flex flex-col gap-5">{children}</div>
+  </DialogContent>
 );
 
 type ModalPluginSelectorProps = PropsWithChildren & {
@@ -28,15 +35,8 @@ export const ModalPluginSelector: FC<ModalPluginSelectorProps> = ({
   </fieldset>
 );
 
-type ModalWrapperProps = PropsWithChildren & {
-  title: string;
-};
-
-export const ModalWrapper: FC<ModalWrapperProps> = ({ title, children }) => (
-  <DialogContent className="sm:max-w-xl">
-    <DialogHeader>
-      <DialogTitle>{title}</DialogTitle>
-    </DialogHeader>
-    <div className="my-3 flex flex-col gap-5">{children}</div>
-  </DialogContent>
+export const ModalFooter = () => (
+  <DialogFooter>
+    <Button type="submit">Save</Button>
+  </DialogFooter>
 );
