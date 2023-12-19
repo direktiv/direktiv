8c62a41465dc53734b053fe0c5c8a308681261fc
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-19 14:20:29 +0100

    use Fieldset, ModalFooter, PluginWrapper
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/Target/InstantResponseForm.tsx
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/Target/InstantResponseForm.tsx
@@ -1,14 +1,13 @@
 import { Controller, useForm } from "react-hook-form";
 import { FC, FormEvent } from "react";
+import { Fieldset, ModalFooter, PluginWrapper } from "../components/Modal";
 import FormErrors, { errorsType } from "~/componentsNext/FormErrors";
 import {
   InstantResponseFormSchema,
   InstantResponseFormSchemaType,
 } from "../../../schema/plugins/target/instantResponse";
 
-import Button from "~/design/Button";
 import { Card } from "~/design/Card";
-import { DialogFooter } from "~/design/Dialog";
 import Editor from "~/design/Editor";
 import Input from "~/design/Input";
 import { useTheme } from "~/util/store/theme";
@@ -54,18 +53,11 @@ export const InstantResponseForm: FC<FormProps> = ({
 
   return (
     <form onSubmit={submitForm}>
-      {errors?.configuration && (
-        <FormErrors
-          errors={errors?.configuration as errorsType}
-          className="mb-5"
-        />
-      )}
-
-      <div className="my-3 flex flex-col gap-y-5">
-        <fieldset className="flex items-center gap-5">
-          <label className="w-[150px] overflow-hidden text-right text-sm">
-            status code
-          </label>
+      <PluginWrapper>
+        {errors?.configuration && (
+          <FormErrors errors={errors?.configuration as errorsType} />
+        )}
+        <Fieldset label="status code">
           <Input
             {...register("configuration.status_code", {
               valueAsNumber: true,
@@ -73,22 +65,16 @@ export const InstantResponseForm: FC<FormProps> = ({
             type="number"
             placeholder="200"
           />
-        </fieldset>
-        <fieldset className="flex items-center gap-5">
-          <label className="w-[150px] overflow-hidden text-right text-sm">
-            content type (optional)
-          </label>
+        </Fieldset>
+        <Fieldset label="content type (optional)">
           <Input
             {...register("configuration.content_type", {
               setValueAs: (value) => (value === "" ? undefined : value),
             })}
             placeholder="application/json"
           />
-        </fieldset>
-        <fieldset className="flex items-center gap-5">
-          <label className="w-[150px] overflow-hidden text-right text-sm">
-            status message (optional)
-          </label>
+        </Fieldset>
+        <Fieldset label="status message (optional)">
           <Card className="h-[200px] w-full p-5" background="weight-1" noShadow>
             <Controller
               control={control}
@@ -103,11 +89,9 @@ export const InstantResponseForm: FC<FormProps> = ({
               )}
             />
           </Card>
-        </fieldset>
-      </div>
-      <DialogFooter>
-        <Button type="submit">Save</Button>
-      </DialogFooter>
+        </Fieldset>
+      </PluginWrapper>
+      <ModalFooter />
     </form>
   );
 };
