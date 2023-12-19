a5d45bcea3847188746d91bfd134208683f6add7
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-19 09:26:47 +0100

    set a fixed with and height
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/design/Filepicker/index.tsx
+++ src/design/Filepicker/index.tsx
@@ -39,7 +39,7 @@ const FilepickerHeading: FC<PropsWithChildren> = ({ children }) => (
 );
 
 const FilepickerList: FC<PropsWithChildren> = ({ children }) => (
-  <div className="max-h-96 overflow-auto">{children}</div>
+  <div className="h-96 w-72 overflow-auto">{children}</div>
 );
 
 // asChild only works with exactly one child, so when asChild is true, we can not have a loading property
