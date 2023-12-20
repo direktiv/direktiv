d0c43d21f26906539b259b3b529b03ff63f20fca
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-20 14:48:20 +0100

    add defaultValue to Namespace selector and handle invalid
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/NamespaceSelector/index.tsx
+++ src/componentsNext/NamespaceSelector/index.tsx
@@ -13,44 +13,53 @@ import { useNamespace } from "~/util/store/namespace";
 import { useTranslation } from "react-i18next";
 
 export type ButtonProps = {
+  defaultValue?: string;
   onValueChange?: (value: string) => void;
 };
 
-const NamespaceSelector: FC<ButtonProps> = ({ onValueChange }) => {
+const NamespaceSelector: FC<ButtonProps> = ({
+  defaultValue,
+  onValueChange,
+}) => {
   const { t } = useTranslation();
   const namespace = useNamespace();
-  const {
-    data: availableNamespaces,
-    isLoading,
-    isSuccess,
-  } = useListNamespaces();
+  const { data: availableNamespaces, isLoading } = useListNamespaces();
 
   if (!namespace) return null;
 
-  const hasResults = isSuccess && availableNamespaces?.results.length > 0;
+  const defaultDoesNotExist =
+    defaultValue &&
+    !availableNamespaces?.results.some((ns) => ns.name === defaultValue);
 
   return (
-    <Select onValueChange={onValueChange}>
+    <Select onValueChange={onValueChange} defaultValue={defaultValue}>
       <SelectTrigger variant="outline">
         <SelectValue
           placeholder={t("components.namespaceSelector.placeholder")}
         />
       </SelectTrigger>
-      {isLoading && (
-        <SelectContent>
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          {t("components.namespaceSelector.placeholder")}
-        </SelectContent>
-      )}
-      {hasResults && (
-        <SelectContent>
-          {availableNamespaces?.results.map((ns) => (
-            <SelectItem key={ns.name} value={ns.name}>
-              <span>{ns.name}</span>
-            </SelectItem>
-          ))}
-        </SelectContent>
-      )}
+      <SelectContent>
+        {isLoading && (
+          <>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            {t("components.namespaceSelector.placeholder")}
+          </>
+        )}
+        {defaultDoesNotExist && (
+          <SelectItem value={defaultValue}>
+            <span>
+              {t("components.namespaceSelector.optionDoesNotExists", {
+                namespace: defaultValue,
+              })}
+            </span>
+          </SelectItem>
+        )}
+        {availableNamespaces?.results.map((ns) => (
+          <SelectItem key={ns.name} value={ns.name}>
+            <span>{ns.name}</span>
+          </SelectItem>
+        ))}
+      </SelectContent>
     </Select>
   );
 };
