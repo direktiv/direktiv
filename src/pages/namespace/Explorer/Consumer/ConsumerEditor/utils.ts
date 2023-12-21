27851a4947a9e2e3d87631f11c4ea4c025de838b
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-21 14:36:00 +0100

    update utils
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Consumer/ConsumerEditor/utils.ts
+++ src/pages/namespace/Explorer/Consumer/ConsumerEditor/utils.ts
@@ -1,7 +1,43 @@
-import { stringify } from "json-to-pretty-yaml";
+import { ConsumerFormSchema, ConsumerFormSchemaType } from "./schema";
 
-export const consumerHeader = {
+import { ZodError } from "zod";
+import { stringify as jsonToPrettyYamlStringify } from "json-to-pretty-yaml";
+import yamljs from "js-yaml";
+
+type SerializeReturnType =
+  | [ConsumerFormSchemaType, undefined]
+  | [undefined, ZodError<ConsumerFormSchemaType>];
+
+export const serializeConsumerFile = (yaml: string): SerializeReturnType => {
+  let json;
+  try {
+    json = yamljs.load(yaml);
+  } catch (e) {
+    json = null;
+  }
+
+  const jsonParsed = ConsumerFormSchema.safeParse(json);
+  if (jsonParsed.success) {
+    return [jsonParsed.data, undefined];
+  }
+
+  return [undefined, jsonParsed.error];
+};
+
+const defaultConsumerFileJson: ConsumerFormSchemaType = {
   direktiv_api: "consumer/v1",
 };
 
-export const defaultConsumerYaml = stringify(consumerHeader);
+export const defaultConsumerFileYaml = jsonToPrettyYamlStringify(
+  defaultConsumerFileJson
+);
+
+/**
+ * a wrapper around the stringify method of json-to-pretty-yaml
+ * but it will serialize an empty object to an empty string instead
+ * of "{}"
+ *
+ * TODO: merge this function with the one in EndpointEditor/utils.ts
+ */
+export const jsonToYaml = (t: Record<string, unknown>) =>
+  Object.keys(t).length === 0 ? "" : jsonToPrettyYamlStringify(t);
