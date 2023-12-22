3a4860c54dbdaa46d3161e0675f8b7a485f74144
Stefan Kracht <stefan.kracht@direktiv.io>
2023-12-22 15:49:10 +0100

    add back refinement
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/config/env/schema.ts
+++ src/config/env/schema.ts
@@ -12,5 +12,18 @@ export const envVariablesSchema = z.object({
     .string()
     .optional()
     .transform((value) => `${value}`.toLocaleLowerCase() === "true"),
-  VITE_BASE: z.string().optional(),
+  VITE_BASE: z
+    .string()
+    .optional()
+    .refine((value) => {
+      if (value === undefined) {
+        return true;
+      }
+
+      if (!value.startsWith("/") || !value.endsWith("/")) {
+        return false;
+      }
+
+      return true;
+    }),
 });
