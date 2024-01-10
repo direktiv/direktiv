3c29c9c4ca7d7f72b196b5c07615d9e22d5e1969
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-10 14:13:28 +0100

    empty config is valid for target events
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/target/targetEvent.ts
+++ src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/target/targetEvent.ts
@@ -3,9 +3,11 @@ import { z } from "zod";
 
 export const TargetEventFormSchema = z.object({
   type: z.literal(targetPluginTypes.targetEvent),
-  configuration: z.object({
-    namespace: z.string().optional(),
-  }),
+  configuration: z
+    .object({
+      namespace: z.string().optional(),
+    })
+    .nullable(), // since all fields are optional, we need to make the whole object optional
 });
 
 export type TargetEventFormSchemaType = z.infer<typeof TargetEventFormSchema>;
