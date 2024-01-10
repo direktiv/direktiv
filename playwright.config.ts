ad6cfb8383b12555d406adc3ee4727f67572246e
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2024-01-10 16:04:03 +0100

    Parallel test workers can be enabled via ENV
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests. */
-  workers: 1,
+  /* Opt out of parallel tests per default. Enable parallel tests by setting
+     VITE_E2E_PARALLEL to TRUE. */
+  workers: process.env.VITE_E2E_PARALLEL === "TRUE" ? undefined : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
