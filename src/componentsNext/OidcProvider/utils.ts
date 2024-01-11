131785b6aac9eae841ec92d991bda4635fa90349
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-11 10:55:59 +0100

    add getOidcUser
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/OidcProvider/utils.ts
+++ src/componentsNext/OidcProvider/utils.ts
@@ -1,15 +1,17 @@
+import { User, WebStorageStateStore } from "oidc-client-ts";
+
 import { AuthProviderProps } from "react-oidc-context";
-import { WebStorageStateStore } from "oidc-client-ts";
 
 const rootUrl = `${window.location.protocol}//${window.location.host}`;
 const appFolder = process.env.VITE?.VITE_BASE ?? "/";
 
 const realm = "direktiv";
+const authority = `${rootUrl}/realms/${realm}`;
 const resource = "direktiv-ui";
 const client_id = "direktiv-ui";
 
 export const oidcConfig: AuthProviderProps = {
-  authority: `${rootUrl}/realms/${realm}`,
+  authority,
   resource,
   client_id,
   redirect_uri: `${rootUrl}${appFolder}`,
@@ -26,4 +28,15 @@ export const oidcConfig: AuthProviderProps = {
    * edition uses oidc, we would have to conditionally call the hook, which is not possible.
    */
   userStore: new WebStorageStateStore({ store: window.localStorage }),
+};
+
+export const getOidcUser = () => {
+  const oidcStorage = localStorage.getItem(
+    `oidc.user:${authority}:${client_id}`
+  );
+  if (!oidcStorage) {
+    return null;
+  }
+
+  return User.fromStorageString(oidcStorage);
 };
