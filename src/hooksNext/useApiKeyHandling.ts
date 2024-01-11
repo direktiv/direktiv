b0514144a1d8987e1f517c21126a981521ab2e77
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-11 14:36:57 +0100

    update useApiKeyHandling to always require an auth key
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/hooksNext/useApiKeyHandling.ts
+++ src/hooksNext/useApiKeyHandling.ts
@@ -5,16 +5,15 @@ const isEnterprise = !!process.env.VITE?.VITE_IS_ENTERPRISE;
 
 /**
  * Send test request to check if api needs an api key. In enterprise
- * mode, this test will be skipped and will always return false
- * because the api authentication will not be managed by the react
- * app
+ * mode, this test will be skipped and will always return true since
+ * enter price will always require us to send a token
  */
 const useIsApiKeyRequired = () => {
   const { data: testSucceeded, isFetched: isFinished } = useAuthTest({
     enabled: !isEnterprise,
   });
   return isEnterprise
-    ? { isApiKeyRequired: false, isFinished: true }
+    ? { isApiKeyRequired: true, isFinished: true }
     : {
         isApiKeyRequired:
           testSucceeded === undefined ? undefined : !testSucceeded,
@@ -27,8 +26,7 @@ const useIsApiKeyRequired = () => {
  * following properties:
  *
  * isApiKeyRequired: indicates if the api needs an api key to work.
- * In enterprise mode this is always false, because the login in handled
- * by a different layer in front of the api/UI
+ * In enterprise mode this is always true.
  *
  * isCurrentKeyValid: indicates if this stored key from the user can be
  * successfully used to authenticate against the api. The user might not
@@ -42,7 +40,6 @@ const useIsApiKeyRequired = () => {
  * api key handling), in the open source version this is only true if
  * the api is required
  */
-
 const useApiKeyHandling = () => {
   const storedKey = useApiKey();
   const keyIsPresent = !!storedKey;
