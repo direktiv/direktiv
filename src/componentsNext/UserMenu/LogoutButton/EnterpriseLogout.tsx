c6b8e6f1543b407cab8c396ea95ead32ee79c730
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-11 16:31:32 +0100

    use oidc native logout method
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/UserMenu/LogoutButton/EnterpriseLogout.tsx
+++ src/componentsNext/UserMenu/LogoutButton/EnterpriseLogout.tsx
@@ -1,14 +1,19 @@
 import { DropdownMenuItem } from "~/design/Dropdown";
 import { LogOut } from "lucide-react";
-import enterpriseConfig from "~/config/enterprise";
+import { useAuth } from "react-oidc-context";
 import { useTranslation } from "react-i18next";
 
 const EnterpriseLogoutButton = () => {
   const { t } = useTranslation();
+  const auth = useAuth();
+
+  const signOut = () => {
+    auth.signoutRedirect();
+  };
 
   return (
     <DropdownMenuItem className="cursor-pointer" asChild>
-      <a href={enterpriseConfig.logoutPath} className="flex items-center">
+      <a role="button" onClick={signOut} className="flex items-center">
         <LogOut className="mr-2 h-4 w-4" />
         <span>{t("components.userMenu.logout")}</span>
       </a>
