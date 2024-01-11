1f9224d36ab77f87aa7444601e924e7a6d4af650
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-11 16:07:42 +0100

    add another console.log
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- src/componentsNext/OidcProvider/OidcHandler.tsx
+++ src/componentsNext/OidcProvider/OidcHandler.tsx
@@ -14,6 +14,7 @@ export const OidcHandler: FC<PropsWithChildren> = ({ children }) => {
   console.log("🔑 oidc auth object", auth);
   // eslint-disable-next-line no-console
   console.log("👤 user", getOidcUser());
+  console.log("⌚ expires at", getOidcUser()?.expires_at);
 
   useEffect(() => {
     if (
