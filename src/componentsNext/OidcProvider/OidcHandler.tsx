bcd116209a937c6233a78ecda55c70ab69189b20
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-11 10:36:01 +0100

    add OidcHandler
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ src/componentsNext/OidcProvider/OidcHandler.tsx
@@ -0,0 +1,41 @@
+import { FC, PropsWithChildren, useEffect, useState } from "react";
+import { hasAuthParams, useAuth } from "react-oidc-context";
+
+import Alert from "~/design/Alert";
+import { Loader2 } from "lucide-react";
+
+export const OidcHandler: FC<PropsWithChildren> = ({ children }) => {
+  const auth = useAuth();
+  const [hasTriedSignin, setHasTriedSignin] = useState(false);
+
+  useEffect(() => {
+    if (
+      !hasAuthParams() &&
+      !auth.isAuthenticated &&
+      !auth.activeNavigator &&
+      !auth.isLoading &&
+      !hasTriedSignin
+    ) {
+      auth.signinRedirect();
+      setHasTriedSignin(true);
+    }
+  }, [auth, hasTriedSignin]);
+
+  if (auth.error) {
+    return (
+      <div className="flex w-full flex-col items-center p-5">
+        <Alert variant="error">{auth.error.message}</Alert>
+      </div>
+    );
+  }
+
+  if (auth.isLoading) {
+    return (
+      <div className="flex w-full flex-col items-center p-5">
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+};
--- src/componentsNext/OidcProvider/index.tsx
+++ src/componentsNext/OidcProvider/index.tsx
@@ -1,18 +1,33 @@
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 import { FC, PropsWithChildren } from "react";
 
-import { AuthProvider } from "react-oidc-context";
-
-const oidcConfig = {
-  authority: "<your authority>",
-  client_id: "<your client id>",
-  redirect_uri: "<your redirect uri>",
-};
+import { OidcHandler } from "./OidcHandler";
 
 const isEnterprise = !!process.env.VITE?.VITE_IS_ENTERPRISE;
+const rootUrl = `${window.location.protocol}//${window.location.host}`;
+const appFolder = process.env.VITE?.VITE_BASE ?? "/";
+
+const oidcConfig: AuthProviderProps = {
+  authority: `${rootUrl}/realms/direktiv`,
+  resource: "direktiv-ui",
+  client_id: "direktiv-ui",
+  redirect_uri: `${rootUrl}${appFolder}`,
+  /**
+   * removes code and state from url after signin
+   * see https://github.com/authts/react-oidc-context/blob/f175dcba6ab09871b027d6a2f2224a17712b67c5/src/AuthProvider.tsx#L20-L30
+   */
+  onSigninCallback: () => {
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
 
 export const OidcProvider: FC<PropsWithChildren> = ({ children }) => {
   if (!isEnterprise) {
     return <>{children}</>;
   }
-  return <AuthProvider {...oidcConfig}>{children}</AuthProvider>;
+  return (
+    <AuthProvider {...oidcConfig}>
+      <OidcHandler>{children}</OidcHandler>
+    </AuthProvider>
+  );
 };
