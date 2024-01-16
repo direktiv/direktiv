e3b77de7e04ce2eb6424a17e9b27c0ab4f32796d
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2024-01-16 16:24:05 +0100

    Create new services explorer page, add link to service
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Explorer/Service/index.tsx
+++ ui/src/pages/namespace/Explorer/Service/index.tsx
@@ -5,14 +5,14 @@ import { Card } from "~/design/Card";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import { NoPermissions } from "~/design/Table";
-import ServiceEditor from "./ServiceEditor";
 import { analyzePath } from "~/util/router/utils";
 import { pages } from "~/util/router/pages";
 import { useNamespace } from "~/util/store/namespace";
+import { useNamespaceServices } from "~/api/services/query/services";
 import { useNodeContent } from "~/api/tree/query/node";
 import { useTranslation } from "react-i18next";
 
-const Header: FC = () => {
+const ServicePage: FC = () => {
   const { path } = pages.explorer.useParams();
   const namespace = useNamespace();
   const { segments } = analyzePath(path);
@@ -26,10 +26,13 @@ const Header: FC = () => {
     isFetched: isPermissionCheckFetched,
   } = useNodeContent({ path });
 
+  const { data: servicesList } = useNamespaceServices();
+
   if (!namespace) return null;
   if (!path) return null;
   if (!serviceData) return null;
   if (!isPermissionCheckFetched) return null;
+  if (!servicesList) return null;
 
   if (isAllowed === false)
     return (
@@ -38,29 +41,35 @@ const Header: FC = () => {
       </Card>
     );
 
+  const serviceId = servicesList.data.find(
+    (service) => serviceData.node.path === service.filePath
+  )?.id;
+
   return (
     <>
-      <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5  dark:border-gray-dark-5 dark:bg-gray-dark-1">
-        <div className="flex flex-col max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
+      <div className="border-b border-gray-5 bg-gray-1 p-5 dark:border-gray-dark-5 dark:bg-gray-dark-1">
+        <div className="flex flex-col gap-5 max-sm:space-y-4 sm:flex-row sm:items-center sm:justify-between">
           <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
             <Layers className="h-5" />
             {filename?.relative}
           </h3>
-          <Button isAnchor asChild variant="primary">
-            <Link
-              to={pages.services.createHref({
-                namespace,
-              })}
-            >
-              <FileSymlink />
-              {t("pages.explorer.service.goToServices")}
-            </Link>
-          </Button>
+          {serviceId && (
+            <Button isAnchor asChild variant="primary">
+              <Link
+                to={pages.services.createHref({
+                  namespace,
+                  service: serviceId,
+                })}
+              >
+                <FileSymlink />
+                {t("pages.explorer.service.goToService")}
+              </Link>
+            </Button>
+          )}
         </div>
       </div>
-      <ServiceEditor data={serviceData} path={path} />
     </>
   );
 };
 
-export default Header;
+export default ServicePage;
