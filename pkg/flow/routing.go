a259ffba0634bb8d9378b8a9a0d146d21b7bf6ed
sir-hassan <yassir.hassan@direktiv.io>
2024-01-17 14:12:52 +0100

    Fix linter issues
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/flow/routing.go
+++ pkg/flow/routing.go
@@ -7,9 +7,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/direktiv/direktiv/pkg/flow/database"
-
 	"github.com/direktiv/direktiv/pkg/flow/bytedata"
+	"github.com/direktiv/direktiv/pkg/flow/database"
 	"github.com/direktiv/direktiv/pkg/flow/pubsub"
 	"github.com/direktiv/direktiv/pkg/model"
 	enginerefactor "github.com/direktiv/direktiv/pkg/refactor/engine"
