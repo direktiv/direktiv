81e6723aa5ddcc52095bdc55e698a17a143e3a62
Stefan Kracht <stefan.kracht@direktiv.io>
2024-01-17 15:04:37 +0100

    test Service list will list available services
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/e2e/services/list/index.spec.ts
+++ ui/e2e/services/list/index.spec.ts
@@ -1,6 +1,10 @@
 import { createNamespace, deleteNamespace } from "../../utils/namespace";
 import { expect, test } from "@playwright/test";
 
+import { createRedisServiceFile } from "./utils";
+import { createWorkflow } from "~/api/tree/mutate/createWorkflow";
+import { headers } from "e2e/utils/testutils";
+
 let namespace = "";
 
 test.beforeEach(async () => {
@@ -13,6 +17,27 @@ test.afterEach(async () => {
 });
 
 test("Service list is empty by default", async ({ page }) => {
+  await page.goto(`/${namespace}/services`, {
+    waitUntil: "networkidle",
+  });
+
+  await expect(
+    page.getByText("No services exist yet"),
+    "it renders an empy list of services"
+  ).toBeVisible();
+});
+
+test("Service list will list available services", async ({ page }) => {
+  await createWorkflow({
+    payload: createRedisServiceFile(),
+    urlParams: {
+      baseUrl: process.env.VITE_DEV_API_DOMAIN,
+      namespace,
+      name: "redis-service.yaml",
+    },
+    headers,
+  });
+
   await page.goto(`/${namespace}/services`, {
     waitUntil: "networkidle",
   });
