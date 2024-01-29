776d9dca0ea89ce60019d1ce86ef718a9489fe2f
sir-hassan <yassir.hassan@direktiv.io>
2024-01-29 02:13:40 +0100

    Refactor to core pubsub
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/refactor/pubsub/sql/pubsub_pg.go
+++ pkg/refactor/pubsub/sql/pubsub_pg.go
@@ -5,53 +5,28 @@ import (
 	"fmt"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/direktiv/direktiv/pkg/refactor/pubsub"
 	"github.com/google/uuid"
-	"github.com/lib/pq"
 	"go.uber.org/zap"
 )
 
-const globalPostgresChannel = "direktiv_pubsub_events"
-
 type PostgresBus struct {
-	listener    *pq.Listener
-	db          *sql.DB
-	subscribers sync.Map
+	coreBus *postgresCoreBus
 
-	logger *zap.SugaredLogger
+	subscribers sync.Map
+	logger      *zap.SugaredLogger
 }
 
 func NewPostgresBus(logger *zap.SugaredLogger, db *sql.DB, listenConnectionString string) (*PostgresBus, error) {
+	coreBus, err := newPostgresCoreBus(db, listenConnectionString)
+	if err != nil {
+		return nil, err
+	}
+
 	p := &PostgresBus{
-		db:     db,
-		logger: logger,
-	}
-
-	p.listener = pq.NewListener(listenConnectionString, time.Second, time.Second,
-		func(event pq.ListenerEventType, err error) {
-			// do nothing.
-		})
-
-	var err error
-	// try ping up to 10 times.
-	for i := 0; i < 10; i++ {
-		err = p.listener.Ping()
-		if err != nil {
-			time.Sleep(time.Second)
-
-			continue
-		}
-
-		break
-	}
-	if err != nil {
-		return nil, fmt.Errorf("ping connection, err: %w", err)
-	}
-	err = p.listener.Listen(globalPostgresChannel)
-	if err != nil {
-		return nil, fmt.Errorf("listen to direktiv_pubsub_events channel, err: %w", err)
+		coreBus: coreBus,
+		logger:  logger,
 	}
 
 	return p, nil
@@ -61,7 +36,7 @@ func (p *PostgresBus) Start(done <-chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
 		select {
-		case msg := <-p.listener.Notify:
+		case msg := <-p.coreBus.NotifyChannel():
 			channel, data, err := splitNotificationText(msg.Extra)
 			if err != nil {
 				p.logger.Error("parsing notify message", "msg", msg.Extra, "err", err)
@@ -84,15 +59,7 @@ func (p *PostgresBus) Start(done <-chan struct{}, wg *sync.WaitGroup) {
 }
 
 func (p *PostgresBus) Publish(channel string, data string) error {
-	if channel == "" || strings.Contains(channel, " ") {
-		return fmt.Errorf("channel name is empty or has spaces: >%s<", channel)
-	}
-	_, err := p.db.Exec(fmt.Sprintf("NOTIFY %s, '%s %s'", globalPostgresChannel, channel, data))
-	if err != nil {
-		return fmt.Errorf("send notify command, channel: %s, data: %v, err: %w", channel, data, err)
-	}
-
-	return nil
+	return p.coreBus.Publish(channel, data)
 }
 
 func (p *PostgresBus) Subscribe(handler func(data string), channels ...string) {
