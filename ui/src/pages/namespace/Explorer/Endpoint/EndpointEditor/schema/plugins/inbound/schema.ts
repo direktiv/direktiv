4551e95105bca5b3618a7d4df1ec7def075342b4
Stefan Kracht <stefan.kracht@direktiv.io>
2024-02-05 13:31:29 +0100

    add HeaderManipulationFormSchema to InboundPluginFormSchema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/schema.ts
+++ ui/src/pages/namespace/Explorer/Endpoint/EndpointEditor/schema/plugins/inbound/schema.ts
@@ -1,5 +1,6 @@
 import { AclFormSchema } from "./acl";
 import { EventFilterFormSchema } from "./eventFilter";
+import { HeaderManipulationFormSchema } from "./headerManipulation";
 import { JsInboundFormSchema } from "./jsInbound";
 import { RequestConvertFormSchema } from "./requestConvert";
 import { z } from "zod";
@@ -9,6 +10,7 @@ export const InboundPluginFormSchema = z.discriminatedUnion("type", [
   JsInboundFormSchema,
   RequestConvertFormSchema,
   EventFilterFormSchema,
+  HeaderManipulationFormSchema,
 ]);
 
 export type InboundPluginFormSchemaType = z.infer<
