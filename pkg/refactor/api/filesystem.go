39fdb9218f1c1d70af45dcf3f44e79aa0eb1728b
sir-hassan <yassir.hassan@direktiv.io>
2024-02-06 16:38:37 +0100

    Implement filesystem patch v2
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/refactor/api/filesystem.go
+++ pkg/refactor/api/filesystem.go
@@ -20,8 +20,8 @@ type fsController struct {
 func (e *fsController) mountRouter(r chi.Router) {
 	r.Get("/*", e.read)
 	r.Delete("/*", e.delete)
-
 	r.Post("/*", e.createFile)
+	r.Patch("/*", e.updateFile)
 }
 
 func (e *fsController) read(w http.ResponseWriter, r *http.Request) {
@@ -171,4 +171,90 @@ func (e *fsController) createFile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeJSON(w, newFile)
+}
+
+func (e *fsController) updateFile(w http.ResponseWriter, r *http.Request) {
+	//nolint:forcetypeassert
+	ns := r.Context().Value(ctxKeyNamespace{}).(*core.Namespace)
+
+	db, err := e.db.BeginTx(r.Context())
+	if err != nil {
+		writeInternalError(w, err)
+		return
+	}
+	//nolint:errcheck
+	defer db.Rollback()
+
+	fStore := db.FileStore()
+
+	// nolint:tagliatelle
+	req := struct {
+		AbsolutePath string `json:"absolutePath"`
+		Content      string `json:"content"`
+	}{}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeNotJsonError(w, err)
+		return
+	}
+
+	// Validate if content is valid base64 encoded string.
+	decodedBytes, err := base64.StdEncoding.DecodeString(req.Content)
+	if err != nil && req.Content != "" {
+		writeError(w, &Error{
+			Code:    "request_data_invalid",
+			Message: "updated file content has invalid base64 string",
+		})
+
+		return
+	}
+	// Validate if content is valid yaml with direktiv files.
+	var data struct{}
+	if err = yaml.Unmarshal(decodedBytes, &data); req.Content != "" {
+		writeError(w, &Error{
+			Code:    "request_data_invalid",
+			Message: "updated file content has invalid yaml string",
+		})
+
+		return
+	}
+
+	// Fetch file.
+	path := strings.Split(r.URL.Path, "/files-tree")[1]
+	oldFile, err := fStore.ForNamespace(ns.Name).GetFile(r.Context(), path)
+	if err != nil {
+		writeFileStoreError(w, err)
+		return
+	}
+
+	if req.Content != "" {
+		_, err = fStore.ForFile(oldFile).SetData(r.Context(), decodedBytes)
+		if err != nil {
+			writeFileStoreError(w, err)
+			return
+		}
+	}
+
+	if req.AbsolutePath != "" {
+		err = fStore.ForFile(oldFile).SetPath(r.Context(), req.AbsolutePath)
+		if err != nil {
+			writeFileStoreError(w, err)
+			return
+		}
+		oldFile.Path = req.AbsolutePath
+	}
+
+	updatedFile, err := fStore.ForNamespace(ns.Name).GetFile(r.Context(), oldFile.Path)
+	if err != nil {
+		writeFileStoreError(w, err)
+		return
+	}
+
+	err = db.Commit(r.Context())
+	if err != nil {
+		writeInternalError(w, err)
+		return
+	}
+
+	writeJSON(w, updatedFile)
 }
