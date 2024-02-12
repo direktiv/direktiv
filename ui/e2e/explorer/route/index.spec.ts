e3a6361e496cfceb11fc88251f3578951d81e1db
Stefan Kracht <stefan.kracht@direktiv.io>
2024-02-12 13:42:48 +0100

    fill out form and compare yaml
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/e2e/explorer/route/index.spec.ts
+++ ui/e2e/explorer/route/index.spec.ts
@@ -1,8 +1,6 @@
 import { createNamespace, deleteNamespace } from "e2e/utils/namespace";
 import { expect, test } from "@playwright/test";
 
-import { faker } from "@faker-js/faker";
-
 let namespace = "";
 
 test.beforeEach(async () => {
@@ -17,6 +15,21 @@ test.afterEach(async () => {
 test("it is possible to create a route file", async ({ page }) => {
   /* prepare data */
   const filename = "myroute.yaml";
+
+  const expectedYaml = `direktiv_api: "endpoint/v1"
+  path: "path"
+  timeout: 3000
+  methods:
+    - "GET"
+    - "POST"
+  plugins:
+    inbound: []
+    outbound: []
+    auth: []
+    target:
+      type: "instant-response"
+      configuration:
+        status_code: 200`;
 
   /* visit page */
   await page.goto(`/${namespace}/explorer/tree`, { waitUntil: "networkidle" });
@@ -44,4 +57,25 @@ test("it is possible to create a route file", async ({ page }) => {
   await page.getByLabel("timeout").fill("3000");
   await page.getByLabel("GET").click();
   await page.getByLabel("POST").click();
+
+  /* try to save incomplete form */
+  await page.getByRole("button", { name: "Save" }).click();
+
+  await expect(
+    page.getByText("plugins : this field is invalid"),
+    "it can not save the route without a valid plugin"
+  ).toBeVisible();
+
+  await page.getByRole("button", { name: "set target plugin" }).click();
+
+  /* add an empty instant response plugin */
+  await page.getByRole("combobox").click();
+  await page.getByLabel("Instant Response").click();
+  await page.getByRole("button", { name: "Save" }).click();
+
+  const editor = page.locator(".lines-content");
+  await expect(
+    editor,
+    "all entered data is represented in the editor preview"
+  ).toContainText(expectedYaml, { useInnerText: true });
 });
