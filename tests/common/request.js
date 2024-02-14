d89a5906e0ef8faf31ffcf673413ce1098758840
Stefan Kracht <stefan.kracht@direktiv.io>
2024-02-14 13:50:17 +0100

    set auth header if AUTH_TOKEN_KEY and AUTH_TOKEN_VALUE are set
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- tests/common/request.js
+++ tests/common/request.js
@@ -1,17 +1,42 @@
 import request from "supertest";
 
+/**
+ * checks if the environment variables AUTH_TOKEN_KEY and AUTH_TOKEN_VALUE are set and returns
+ * either an array with the key and value or null.
+ */
+const getAuthHeader = () => {
+  if (process.env.AUTH_TOKEN_KEY && process.env.AUTH_TOKEN_VALUE) {
+    return [process.env.AUTH_TOKEN_KEY, process.env.AUTH_TOKEN_VALUE];
+  }
+  return null;
+};
+
 const requestWithHeaders =
   (appConfig, method = "post") =>
-  (args) =>
-    request(appConfig)[method](args).set("Direktiv-Token", "password");
+  (args) => {
+    const authHeader = getAuthHeader();
 
+    if (!authHeader) {
+      return request(appConfig)[method](args);
+    }
+
+    return request(appConfig)
+      [method](args)
+      .set(...authHeader);
+  };
+
+/**
+ * overwrites the http methods (get, head, post, put, delete, patch)
+ * from supertest and injects a custom header if the environment variables
+ * AUTH_TOKEN_KEY and AUTH_TOKEN_VALUE are set.
+ */
 const customRequest = (appConfig) => ({
   get: requestWithHeaders(appConfig, "get"),
   head: requestWithHeaders(appConfig, "head"),
   post: requestWithHeaders(appConfig, "post"),
   put: requestWithHeaders(appConfig, "put"),
   delete: requestWithHeaders(appConfig, "delete"),
-  patch: requestWithHeaders(appConfig, "patch")
+  patch: requestWithHeaders(appConfig, "patch"),
 });
 
 export default customRequest;
