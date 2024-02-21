7257eb9560f4a8078933cd2b83c81cc622a0ed40
sir-hassan <yassir.hassan@direktiv.io>
2024-02-21 22:43:56 +0100

    Reformat engine tests.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/engine/simple-loop.test.js
+++ tests/engine/simple-loop.test.js
@@ -1,34 +1,33 @@
-import request from "../common/request"
+import common from '../common'
+import request from '../common/request'
 
-import common from "../common"
-
-const namespaceName = "simplelooptest"
+const namespaceName = 'simplelooptest'
 
 
 describe('Test a simple loop', () => {
-    beforeAll(common.helpers.deleteAllNamespaces)
+	beforeAll(common.helpers.deleteAllNamespaces)
 
-    it(`should create a namespace`, async () => {
-        var req = await request(common.config.getDirektivHost()).put(`/api/namespaces/${namespaceName}`)
+	it(`should create a namespace`, async () => {
+		const req = await request(common.config.getDirektivHost()).put(`/api/namespaces/${ namespaceName }`)
 
-        expect(req.statusCode).toEqual(200)
-        expect(req.body).toMatchObject({
-            namespace: {
-                createdAt: expect.stringMatching(common.regex.timestampRegex),
-                updatedAt: expect.stringMatching(common.regex.timestampRegex),
-                name: namespaceName,
-            },
-        })
-    })
+		expect(req.statusCode).toEqual(200)
+		expect(req.body).toMatchObject({
+			namespace: {
+				createdAt: expect.stringMatching(common.regex.timestampRegex),
+				updatedAt: expect.stringMatching(common.regex.timestampRegex),
+				name: namespaceName,
+			},
+		})
+	})
 
-    it(`should create a workflow called /simple-loop.yaml`, async () => {
+	it(`should create a workflow called /simple-loop.yaml`, async () => {
 
-        const res = await request(common.config.getDirektivHost())
-            .put(`/api/namespaces/${namespaceName}/tree/simple-loop.yaml?op=create-workflow`)
-            .set({
-                'Content-Type': 'text/plain',
-            })
-            .send(`
+		const res = await request(common.config.getDirektivHost())
+			.put(`/api/namespaces/${ namespaceName }/tree/simple-loop.yaml?op=create-workflow`)
+			.set({
+				'Content-Type': 'text/plain',
+			})
+			.send(`
 states:
 - id: init
   type: noop
@@ -44,19 +43,19 @@ states:
     transition: check
 `)
 
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toMatchObject({
-            namespace: namespaceName,
-        })
-    })
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toMatchObject({
+			namespace: namespaceName,
+		})
+	})
 
-    it(`should invoke the '/simple-loop.yaml' workflow`, async () => {
-        const req = await request(common.config.getDirektivHost()).get(`/api/namespaces/${namespaceName}/tree/simple-loop.yaml?op=wait`)
-        expect(req.statusCode).toEqual(200)
-        expect(req.body).toMatchObject({
-            i: 0,
-            result: [5, 4, 3, 2, 1],
-        })
-    })
+	it(`should invoke the '/simple-loop.yaml' workflow`, async () => {
+		const req = await request(common.config.getDirektivHost()).get(`/api/namespaces/${ namespaceName }/tree/simple-loop.yaml?op=wait`)
+		expect(req.statusCode).toEqual(200)
+		expect(req.body).toMatchObject({
+			i: 0,
+			result: [ 5, 4, 3, 2, 1 ],
+		})
+	})
 
 })
