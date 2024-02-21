dadd312a881ec6f03b2a917a2da8975f698d045f
sir-hassan <yassir.hassan@direktiv.io>
2024-02-21 22:43:11 +0100

    Reformat common tests.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/common/request.js
+++ tests/common/request.js
@@ -1,42 +1,42 @@
-import request from "supertest";
+import request from 'supertest'
 
 /**
  * checks if the environment variables AUTH_TOKEN_KEY and AUTH_TOKEN_VALUE are set and returns
  * either an array with the key and value or null.
  */
 const getAuthHeader = () => {
-  if (process.env.AUTH_TOKEN_KEY && process.env.AUTH_TOKEN_VALUE) {
-    return [process.env.AUTH_TOKEN_KEY, process.env.AUTH_TOKEN_VALUE];
-  }
-  return null;
-};
+	if (process.env.AUTH_TOKEN_KEY && process.env.AUTH_TOKEN_VALUE)
+		return [ process.env.AUTH_TOKEN_KEY, process.env.AUTH_TOKEN_VALUE ]
 
-const requestWithHeaders =
-  (appConfig, method = "post") =>
-  (args) => {
-    const authHeader = getAuthHeader();
+	return null
+}
 
-    if (!authHeader) {
-      return request(appConfig)[method](args);
-    }
+const requestWithHeaders
+  = (appConfig, method = 'post') =>
+  	args => {
+  		const authHeader = getAuthHeader()
 
-    return request(appConfig)
-      [method](args)
-      .set(...authHeader);
-  };
+  		if (!authHeader)
+  			return request(appConfig)[method](args)
+
+
+  		return request(appConfig)
+  			[method](args)
+  			.set(...authHeader)
+  	}
 
 /**
  * overwrites the http methods (get, head, post, put, delete, patch)
  * from supertest and injects a custom header if the environment variables
  * AUTH_TOKEN_KEY and AUTH_TOKEN_VALUE are set.
  */
-const customRequest = (appConfig) => ({
-  get: requestWithHeaders(appConfig, "get"),
-  head: requestWithHeaders(appConfig, "head"),
-  post: requestWithHeaders(appConfig, "post"),
-  put: requestWithHeaders(appConfig, "put"),
-  delete: requestWithHeaders(appConfig, "delete"),
-  patch: requestWithHeaders(appConfig, "patch"),
-});
+const customRequest = appConfig => ({
+	get: requestWithHeaders(appConfig, 'get'),
+	head: requestWithHeaders(appConfig, 'head'),
+	post: requestWithHeaders(appConfig, 'post'),
+	put: requestWithHeaders(appConfig, 'put'),
+	delete: requestWithHeaders(appConfig, 'delete'),
+	patch: requestWithHeaders(appConfig, 'patch'),
+})
 
-export default customRequest;
+export default customRequest
