254bef7d8ab0bf8da04cf76e6c33f4d29bfaedaf
sir-hassan <yassir.hassan@direktiv.io>
2024-02-21 22:48:11 +0100

    Reformat secrets tests.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/secrets/secrets_read.test.js
+++ tests/secrets/secrets_read.test.js
@@ -1,52 +1,52 @@
-import common from "../common";
-import request from "../common/request"
+import common from '../common'
+import request from '../common/request'
 
-const testNamespace = "test-secrets-namespace"
-let testWorkflow = "test-secret"
+const testNamespace = 'test-secrets-namespace'
+const testWorkflow = 'test-secret'
 
 describe('Test secret read operations', () => {
-    beforeAll(common.helpers.deleteAllNamespaces)
+	beforeAll(common.helpers.deleteAllNamespaces)
 
-    it(`should create a new namespace`, async () => {
-        const res = await request(common.config.getDirektivHost()).put(`/api/namespaces/${testNamespace}`)
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toMatchObject({
-            namespace: {
-                name: testNamespace,
-                // regex /^2.*Z$/ matches timestamps like 2023-03-01T14:19:52.383871512Z
-                createdAt: expect.stringMatching(/^2.*Z$/),
-                updatedAt: expect.stringMatching(/^2.*Z$/),
-            }
-        })
-    })
+	it(`should create a new namespace`, async () => {
+		const res = await request(common.config.getDirektivHost()).put(`/api/namespaces/${ testNamespace }`)
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toMatchObject({
+			namespace: {
+				name: testNamespace,
+				// regex /^2.*Z$/ matches timestamps like 2023-03-01T14:19:52.383871512Z
+				createdAt: expect.stringMatching(/^2.*Z$/),
+				updatedAt: expect.stringMatching(/^2.*Z$/),
+			},
+		})
+	})
 
-    it(`should create a new secret`, async () => {
-        const res = await request(common.config.getDirektivHost())
-            .put(`/api/namespaces/${testNamespace}/secrets/key1`)
-            .set({
-                'Content-Type': 'text/plain',
-            })
+	it(`should create a new secret`, async () => {
+		const res = await request(common.config.getDirektivHost())
+			.put(`/api/namespaces/${ testNamespace }/secrets/key1`)
+			.set({
+				'Content-Type': 'text/plain',
+			})
 
-            .send(`value1`)
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toMatchObject({
-            namespace: testNamespace,
-            key: 'key1'
-        })
-    })
+			.send(`value1`)
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toMatchObject({
+			namespace: testNamespace,
+			key: 'key1',
+		})
+	})
 
-    it(`should create the first workflow in a pair to test secret read`, async () => {
+	it(`should create the first workflow in a pair to test secret read`, async () => {
 
-        const res = await request(common.config.getDirektivHost())
-        .put(`/api/namespaces/${testNamespace}/tree/${testWorkflow}-parent.yaml?op=create-workflow`)
-        .set({
-            'Content-Type': 'text/plain',
-        })
+		const res = await request(common.config.getDirektivHost())
+			.put(`/api/namespaces/${ testNamespace }/tree/${ testWorkflow }-parent.yaml?op=create-workflow`)
+			.set({
+				'Content-Type': 'text/plain',
+			})
 
-        .send(`
+			.send(`
 functions:
 - id: echo
-  workflow: ${testWorkflow}-child.yaml
+  workflow: ${ testWorkflow }-child.yaml
   type: subflow
 states:
 - id: echo
@@ -60,38 +60,37 @@ states:
     result: 'jq(.return.secret)'
 `)
 
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toMatchObject({
-        namespace: testNamespace,
-    })
-})
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toMatchObject({
+			namespace: testNamespace,
+		})
+	})
 
-    it(`should create the second workflow in a pair to test secret read`, async () => {
+	it(`should create the second workflow in a pair to test secret read`, async () => {
 
-        const res = await request(common.config.getDirektivHost())
-            .put(`/api/namespaces/${testNamespace}/tree/${testWorkflow}-child.yaml?op=create-workflow`)
-            .set({
-                'Content-Type': 'text/plain',
-            })
+		const res = await request(common.config.getDirektivHost())
+			.put(`/api/namespaces/${ testNamespace }/tree/${ testWorkflow }-child.yaml?op=create-workflow`)
+			.set({
+				'Content-Type': 'text/plain',
+			})
 
-            .send(`
+			.send(`
 states:
 - id: helloworld
   type: noop
 `)
 
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toMatchObject({
-            namespace: testNamespace,
-        })
-    })
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toMatchObject({
+			namespace: testNamespace,
+		})
+	})
 
-    it(`should invoke the '/${testWorkflow}-parent.yaml' workflow`, async () => {
-        const req = await request(common.config.getDirektivHost()).get(`/api/namespaces/${testNamespace}/tree/${testWorkflow}-parent.yaml?op=wait`)
-        expect(req.statusCode).toEqual(200)
-        expect(req.body).toMatchObject({
-            result: 'value1',
-        })
-    })
-});
-
+	it(`should invoke the '/${ testWorkflow }-parent.yaml' workflow`, async () => {
+		const req = await request(common.config.getDirektivHost()).get(`/api/namespaces/${ testNamespace }/tree/${ testWorkflow }-parent.yaml?op=wait`)
+		expect(req.statusCode).toEqual(200)
+		expect(req.body).toMatchObject({
+			result: 'value1',
+		})
+	})
+})
