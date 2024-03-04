e91f80e00de5aecd85779b41a7ee8eb401073d0c
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-04 14:42:07 +0100

    rename VITE_E2E_PARALLEL to PLAYWRIGHT_PARALLEL
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests per default. Enable parallel tests by setting
-     VITE_E2E_PARALLEL to TRUE. */
-  workers: process.env.VITE_E2E_PARALLEL === "TRUE" ? undefined : 1,
+     PLAYWRIGHT_PARALLEL to TRUE. */
+  workers: process.env.PLAYWRIGHT_PARALLEL === "TRUE" ? undefined : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
