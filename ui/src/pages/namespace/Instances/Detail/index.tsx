aa5c87efb1a5926426a2d6d5b59e7f2bd2a3ff53
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-14 14:38:14 +0100

    remove filters
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Instances/Detail/index.tsx
+++ ui/src/pages/namespace/Instances/Detail/index.tsx
@@ -1,25 +1,15 @@
-import {
-  InstanceStateProvider,
-  useFilters,
-  useInstanceId,
-} from "./store/instanceContext";
+import { InstanceStateProvider, useInstanceId } from "./store/instanceContext";
 
 import { InstanceStreamingSubscriber } from "~/api/instances/query/details";
 import InstancesDetail from "./InstanceDetail";
 import { LogStreamingSubscriber } from "~/api/logs/query/logs";
-import { LogStreamingSubscriber as LogStreamingSubscriberDeprecated } from "~/api/logs_DEPRECATED/query/get";
 import { pages } from "~/util/router/pages";
 
 const InstanceStreaming = () => {
   const instanceId = useInstanceId();
-  const filters = useFilters();
   return (
     <>
       <InstanceStreamingSubscriber instanceId={instanceId} />
-      <LogStreamingSubscriberDeprecated
-        filters={filters}
-        instanceId={instanceId}
-      />
       <LogStreamingSubscriber instance={instanceId} />
       <InstancesDetail />
     </>
--- ui/src/pages/namespace/Instances/Detail/store/type.ts
+++ ui/src/pages/namespace/Instances/Detail/store/type.ts
@@ -1,22 +1,3 @@
-import { FiltersObj } from "~/api/logs_DEPRECATED/query/get";
-
 export type StateType = {
   instanceId: string;
-  filters?: FiltersObj;
 };
-
-export type UpdateFilterState = {
-  type: "UPDATE_FILTER_STATE_NAME";
-  payload: {
-    stateName: string | undefined;
-  };
-};
-
-export type UpdateFilterWorkflow = {
-  type: "UPDATE_FILTER_WORKFLOW";
-  payload: {
-    workflowName: string | undefined;
-  };
-};
-
-export type Actions = UpdateFilterState | UpdateFilterWorkflow;
