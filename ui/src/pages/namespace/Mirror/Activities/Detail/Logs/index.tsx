87770cd23ffb3863495d21bc7d4ce6b26f1912ff
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-15 17:29:13 +0100

    make mirrors logs full height
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Mirror/Activities/Detail/Logs/index.tsx
+++ ui/src/pages/namespace/Mirror/Activities/Detail/Logs/index.tsx
@@ -11,6 +11,7 @@ import CopyButton from "~/design/CopyButton";
 import ScrollContainer from "./ScrollContainer";
 import { ScrollText } from "lucide-react";
 import { generateLogEntryForClipboard } from "./utils";
+import { twMergeClsx } from "~/util/helpers";
 import { useLogs } from "~/api/logs/query/logs";
 import { useTranslation } from "react-i18next";
 
@@ -26,43 +27,45 @@ const Logs = ({ activityId }: { activityId: string }) => {
   const copyValue = allLogs.map(generateLogEntryForClipboard).join("\n") ?? "";
 
   return (
-    <Card className="relative m-5 grid h-[500px] grid-rows-[auto,1fr,auto] p-5">
-      <div className="mb-5 flex flex-col gap-5 sm:flex-row">
-        <h3 className="flex grow items-center gap-x-2 font-medium">
-          <ScrollText className="h-5" />
-          {t("pages.mirror.activities.detail.logs.title")}
-        </h3>
-        <ButtonBar>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div className="flex grow">
-                  <CopyButton
-                    value={copyValue}
-                    buttonProps={{
-                      variant: "outline",
-                      size: "sm",
-                      className: "grow",
-                    }}
-                  />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                {t("pages.monitoring.logs.tooltips.copy")}
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </ButtonBar>
-      </div>
-      <ScrollContainer activityId={activityId} />
-      <div className="flex items-center justify-center pt-2 text-sm text-gray-11 dark:text-gray-dark-11">
-        <span className="relative mr-2 flex h-3 w-3">
-          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-gray-11 opacity-75 dark:bg-gray-dark-11"></span>
-          <span className="relative inline-flex h-3 w-3 rounded-full bg-gray-11 dark:bg-gray-dark-11"></span>
-        </span>
-        {t("pages.monitoring.logs.logsCount", { count: numberOfLogs })}
-      </div>
-    </Card>
+    <div className="grid grid-rows-[calc(100vh-10rem)]">
+      <Card className="relative m-5 grid p-5">
+        <div className="mb-5 flex flex-col gap-5 sm:flex-row">
+          <h3 className="flex grow items-center gap-x-2 font-medium">
+            <ScrollText className="h-5" />
+            {t("pages.mirror.activities.detail.logs.title")}
+          </h3>
+          <ButtonBar>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <div className="flex grow">
+                    <CopyButton
+                      value={copyValue}
+                      buttonProps={{
+                        variant: "outline",
+                        size: "sm",
+                        className: "grow",
+                      }}
+                    />
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  {t("pages.monitoring.logs.tooltips.copy")}
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </ButtonBar>
+        </div>
+        <ScrollContainer activityId={activityId} />
+        <div className="flex items-center justify-center pt-2 text-sm text-gray-11 dark:text-gray-dark-11">
+          <span className="relative mr-2 flex h-3 w-3">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-gray-11 opacity-75 dark:bg-gray-dark-11"></span>
+            <span className="relative inline-flex h-3 w-3 rounded-full bg-gray-11 dark:bg-gray-dark-11"></span>
+          </span>
+          {t("pages.monitoring.logs.logsCount", { count: numberOfLogs })}
+        </div>
+      </Card>
+    </div>
   );
 };
 
