8150c5691d0a58a79d921f55cbfbb46f68a89b34
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-15 12:09:45 +0100

    display workflow information
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Monitoring/Logs/utils.ts
+++ ui/src/pages/namespace/Monitoring/Logs/utils.ts
@@ -1,7 +1,14 @@
 import { LogEntryType } from "~/api/logs/schema";
 import { formatLogTime } from "~/util/helpers";
 
-export const generateClipboardLogEntry = (logEntry: LogEntryType) =>
-  `${logEntry.id} - ${formatLogTime(logEntry.time)} - ${logEntry?.level} - ${
-    logEntry.msg
-  }`;
+export const generateClipboardLogEntry = (logEntry: LogEntryType) => {
+  const isWorkflowLog = !!logEntry.workflow;
+
+  const worfklowLogInfo = isWorkflowLog
+    ? ` - ${logEntry.workflow?.workflow} - ${logEntry.workflow?.instance}`
+    : "";
+
+  return `${logEntry.id} - ${formatLogTime(logEntry.time)} - ${
+    logEntry?.level
+  } - ${logEntry.msg}${worfklowLogInfo}`;
+};
