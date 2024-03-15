baf62e12ff2da8793dbb29dd3017613f37c24cc1
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-15 11:27:32 +0100

    update monitoring logs page
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Monitoring/Logs/index.tsx
+++ ui/src/pages/namespace/Monitoring/Logs/index.tsx
@@ -5,29 +5,45 @@ import {
   TooltipTrigger,
 } from "~/design/Tooltip";
 
+import Button from "~/design/Button";
 import { ButtonBar } from "~/design/ButtonBar";
 import CopyButton from "~/design/CopyButton";
-import { NoPermissions } from "~/design/Table";
 import ScrollContainer from "./Scrollcontainer";
 import { ScrollText } from "lucide-react";
 import { formatLogTime } from "~/util/helpers";
-import { useNamespacelogs } from "~/api/namespaces/query/logs";
+import { useLogs } from "~/api/logs/query/logs";
 import { useTranslation } from "react-i18next";
 
 const LogsPanel = () => {
   const { t } = useTranslation();
-  const { data, isFetched, isAllowed, noPermissionMessage } =
-    useNamespacelogs();
 
+  const {
+    data: logData,
+    isFetched,
+    hasPreviousPage,
+    fetchPreviousPage,
+    isFetchingPreviousPage,
+  } = useLogs();
+
+  const pages = logData?.pages.map((page) => page.data ?? []) ?? [];
+  const allLogs = pages.flat();
+  const numberOfLogs = allLogs.length;
+
+  // TODO:
   const copyValue =
-    data?.results
-      .map((logEntry) => `${formatLogTime(logEntry.t)} ${logEntry.msg}`)
+    allLogs
+      .map(
+        (logEntry) =>
+          `${logEntry.id} - ${formatLogTime(logEntry.time)} - ${
+            logEntry?.level
+          } - ${logEntry.msg}`
+      )
       .join("\n") ?? "";
 
-  const resultCount = data?.results.length ?? 0;
-
   if (!isFetched) return null;
-  if (!isAllowed) return <NoPermissions>{noPermissionMessage}</NoPermissions>;
+
+  // TODO:
+  // if (!isAllowed) return <NoPermissions>{noPermissionMessage}</NoPermissions>;
 
   return (
     <>
@@ -36,6 +52,14 @@ const LogsPanel = () => {
           <ScrollText className="h-5" />
           {t("pages.monitoring.logs.title")}
         </h3>
+        <Button
+          size="sm"
+          disabled={!hasPreviousPage}
+          loading={isFetchingPreviousPage}
+          onClick={() => fetchPreviousPage()}
+        >
+          {numberOfLogs} entries on {pages.length} pages
+        </Button>
         <ButtonBar>
           <TooltipProvider>
             <Tooltip>
@@ -64,7 +88,7 @@ const LogsPanel = () => {
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-gray-11 opacity-75 dark:bg-gray-dark-11"></span>
           <span className="relative inline-flex h-3 w-3 rounded-full bg-gray-11 dark:bg-gray-dark-11"></span>
         </span>
-        {t("pages.monitoring.logs.logsCount", { count: resultCount })}
+        {t("pages.monitoring.logs.logsCount", { count: numberOfLogs })}
       </div>
     </>
   );
