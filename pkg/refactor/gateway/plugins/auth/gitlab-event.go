24a26afb592da9d3897a7867d36556eeb75d530c
Jens Gerke <jens.gerke@direktiv.io>
2024-03-15 08:34:44 +0100

    Added Gitlab webhook plugin
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

---
+++ pkg/refactor/gateway/plugins/auth/gitlab-event.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/direktiv/direktiv/pkg/refactor/core"
+	"github.com/direktiv/direktiv/pkg/refactor/gateway/plugins"
+)
+
+const (
+	GitlabWebhookPluginName = "gitlab-webhook-auth"
+	GitlabSecretHeaderName  = "X-Gitlab-Token"
+)
+
+type GitlabWebhookPluginConfig struct {
+	Secret string `mapstructure:"secret" yaml:"secret"`
+}
+
+type GitlabWebhookPlugin struct {
+	config *GitlabWebhookPluginConfig
+}
+
+func ConfigureGitlabWebhook(config interface{}, _ string) (core.PluginInstance, error) {
+	requestConvertConfig := &GitlabWebhookPluginConfig{}
+
+	err := plugins.ConvertConfig(config, requestConvertConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GitlabWebhookPlugin{
+		config: requestConvertConfig,
+	}, nil
+}
+
+func (p *GitlabWebhookPlugin) Config() interface{} {
+	return p.config
+}
+
+func (p *GitlabWebhookPlugin) ExecutePlugin(c *core.ConsumerFile, _ http.ResponseWriter, r *http.Request) bool {
+	secret := r.Header.Get(GitlabSecretHeaderName)
+
+	if secret != p.config.Secret {
+		return false
+	}
+
+	*c = core.ConsumerFile{
+		Username: "gitlab",
+	}
+
+	return true
+}
+
+func (*GitlabWebhookPlugin) Type() string {
+	return GithubWebhookPluginName
+}
+
+//nolint:gochecknoinits
+func init() {
+	plugins.AddPluginToRegistry(plugins.NewPluginBase(
+		GitlabWebhookPluginName,
+		plugins.AuthPluginType,
+		ConfigureGitlabWebhook))
+}
---
+++ pkg/refactor/gateway/plugins/auth/gitlab-event_test.go
@@ -0,0 +1,47 @@
+package auth_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/direktiv/direktiv/pkg/refactor/core"
+	"github.com/direktiv/direktiv/pkg/refactor/gateway/plugins"
+	"github.com/direktiv/direktiv/pkg/refactor/gateway/plugins/auth"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	correctGitlabPwd   = "secret"
+	incorrectGitlabPwd = "incorrectsecret"
+)
+
+func TestGitlabEvent(t *testing.T) {
+	c := &core.ConsumerFile{}
+
+	config := auth.GitlabWebhookPluginConfig{
+		Secret: correctGitlabPwd,
+	}
+
+	assert.True(t, executeGitlab(config, c, correctGitlabPwd))
+
+	// consumer file is gitlab
+	assert.Equal(t, c.Username, "gitlab")
+
+	assert.False(t, executeGitlab(config, c, incorrectGitlabPwd))
+}
+
+func executeGitlab(config auth.GitlabWebhookPluginConfig, c *core.ConsumerFile, secret string) bool {
+	p, _ := plugins.GetPluginFromRegistry(auth.GitlabWebhookPluginName)
+
+	p2, _ := p.Configure(config, core.MagicalGatewayNamespace)
+
+	r, _ := http.NewRequest(http.MethodGet, "/dummy", nil)
+	r.Header.Add(auth.GitlabSecretHeaderName, secret)
+
+	w := httptest.NewRecorder()
+
+	ret := p2.ExecutePlugin(c, w, r)
+
+	return ret
+}
