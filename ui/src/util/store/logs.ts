81b6d3c2fa6ebf0b400c9d94f19151a126055111
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-15 16:57:48 +0100

    remve wordWrap from zustand store
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/util/store/logs.ts
+++ ui/src/util/store/logs.ts
@@ -5,11 +5,9 @@ export type InstanceLayout = "none" | "logs" | "diagram" | "input-output";
 
 interface LogsPreferencesState {
   maximizedPanel: InstanceLayout;
-  wordWrap: boolean;
   verboseLogs: boolean;
   actions: {
     setMaximizedPanel: (layout: LogsPreferencesState["maximizedPanel"]) => void;
-    setWordWrap: (layout: LogsPreferencesState["wordWrap"]) => void;
     setVerboseLogs: (layout: LogsPreferencesState["verboseLogs"]) => void;
   };
 }
@@ -18,11 +16,9 @@ const useLogsPreferencesState = create<LogsPreferencesState>()(
   persist(
     (set) => ({
       maximizedPanel: "none",
-      wordWrap: false,
       verboseLogs: false,
       actions: {
         setMaximizedPanel: (newVal) => set(() => ({ maximizedPanel: newVal })),
-        setWordWrap: (newVal) => set(() => ({ wordWrap: newVal })),
         setVerboseLogs: (newVal) => set(() => ({ verboseLogs: newVal })),
       },
     }),
@@ -31,7 +27,6 @@ const useLogsPreferencesState = create<LogsPreferencesState>()(
       partialize: (state) => ({
         // pick all fields to be persistent and don't persist actions
         maximizedPanel: state.maximizedPanel,
-        wordWrap: state.wordWrap,
         verboseLogs: state.verboseLogs,
       }),
     }
@@ -40,9 +35,6 @@ const useLogsPreferencesState = create<LogsPreferencesState>()(
 
 export const useLogsPreferencesMaximizedPanel = () =>
   useLogsPreferencesState((state) => state.maximizedPanel);
-
-export const useLogsPreferencesWordWrap = () =>
-  useLogsPreferencesState((state) => state.wordWrap);
 
 export const useLogsPreferencesVerboseLogs = () =>
   useLogsPreferencesState((state) => state.verboseLogs);
