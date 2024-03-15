5525d2b1909c10f8635fac2ba933c1019572d58d
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-15 17:04:04 +0100

    update scrollcontainer
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Mirror/Activities/Detail/Logs/ScrollContainer.tsx
+++ ui/src/pages/namespace/Mirror/Activities/Detail/Logs/ScrollContainer.tsx
@@ -1,67 +1,30 @@
-import { useEffect, useRef, useState } from "react";
-
 import { ArrowDown } from "lucide-react";
 import Button from "~/design/Button";
 import Entry from "./Entry";
 import { Logs } from "~/design/Logs";
 import { twMergeClsx } from "~/util/helpers";
-import { useLogsPreferencesWordWrap } from "~/util/store/logs";
-import { useMirrorActivityLog } from "~/api/tree/query/mirrorActivity";
+import { useLogVirtualizer } from "~/hooks/useLogVirtualizer";
 import { useTranslation } from "react-i18next";
-import { useVirtualizer } from "@tanstack/react-virtual";
 
 const ScrollContainer = ({ activityId }: { activityId: string }) => {
-  const wordWrap = useLogsPreferencesWordWrap();
-
   const { t } = useTranslation();
 
-  const { data: logData } = useMirrorActivityLog({ activityId });
-  const [watch, setWatch] = useState(true);
-
-  // The scrollable element for the list
-  const parentRef = useRef<HTMLDivElement | null>(null);
-
-  // The virtualizer
-  const rowVirtualizer = useVirtualizer({
-    count: logData?.results.length ?? 0,
-    getScrollElement: () => parentRef.current,
-    estimateSize: () => 20,
-    /**
-     * overscan is the number of items to render above and below
-     * the visible window. More items = less flickering when
-     * scrolling, but more memory usage and initial load time.
-     * I tested it with around 2000 items and 40 was a good fit
-     * to have no flickering with pretty high scrolling speed.
-     */
-    overscan: 40,
+  const {
+    rowVirtualizer,
+    parentRef,
+    logs,
+    scrolledToBottom,
+    setScrolledToBottom,
+  } = useLogVirtualizer({
+    queryLogsBy: {
+      activity: activityId,
+    },
   });
 
-  useEffect(() => {
-    if (logData?.results.length && watch) {
-      rowVirtualizer.scrollToIndex(logData?.results.length - 1),
-        { align: "end" };
-    }
-  }, [logData?.results.length, rowVirtualizer, watch]);
-
-  if (!logData) return null;
-
-  const items = rowVirtualizer.getVirtualItems();
+  const virtualItems = rowVirtualizer.getVirtualItems();
 
   return (
-    <Logs
-      wordWrap={wordWrap}
-      className="h-full overflow-scroll"
-      ref={parentRef}
-      onScroll={(e) => {
-        const element = e.target as HTMLDivElement;
-        if (element) {
-          const { scrollHeight, scrollTop, clientHeight } = element;
-          const scrollDistanceToBottom =
-            scrollHeight - scrollTop - clientHeight;
-          setWatch(scrollDistanceToBottom < 100);
-        }
-      }}
-    >
+    <Logs className="h-full overflow-scroll" ref={parentRef}>
       <div
         className="relative w-full"
         style={{
@@ -74,11 +37,11 @@ const ScrollContainer = ({ activityId }: { activityId: string }) => {
             top: 0,
             left: 0,
             width: "100%",
-            transform: `translateY(${items[0]?.start}px)`,
+            transform: `translateY(${virtualItems[0]?.start}px)`,
           }}
         >
-          {items.map((virtualItem) => {
-            const logEntry = logData.results[virtualItem.index];
+          {virtualItems.map((virtualItem) => {
+            const logEntry = logs[virtualItem.index];
             if (!logEntry) return null;
             return (
               <Entry
@@ -98,14 +61,14 @@ const ScrollContainer = ({ activityId }: { activityId: string }) => {
           "aria-[hidden=true]:pointer-events-none aria-[hidden=true]:bottom-11 aria-[hidden=true]:opacity-0",
           "aria-[hidden=false]:bottom-16 aria-[hidden=false]:opacity-100"
         )}
-        aria-hidden={watch ? "true" : "false"}
+        aria-hidden={scrolledToBottom ? "true" : "false"}
       >
         <Button
           className="bg-white dark:bg-black"
           variant="outline"
           size="sm"
           onClick={() => {
-            setWatch(true);
+            setScrolledToBottom(true);
           }}
         >
           <ArrowDown />
