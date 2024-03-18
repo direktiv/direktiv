e64b777d58be8252d36bdfc1f565c0947b3dcb30
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-18 13:56:01 +0100

    use LogList for mirror activities and monitoring
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Monitoring/Logs/Scrollcontainer.tsx
+++ ui/src/pages/namespace/Monitoring/Logs/Scrollcontainer.tsx
@@ -1,13 +1,8 @@
-import { ArrowDown } from "lucide-react";
-import Button from "~/design/Button";
 import Entry from "./Entry";
-import { Logs } from "~/design/Logs";
-import { twMergeClsx } from "~/util/helpers";
+import LogList from "~/components/Logs";
 import { useLogVirtualizer } from "~/hooks/useLogVirtualizer";
-import { useTranslation } from "react-i18next";
 
 const ScrollContainer = () => {
-  const { t } = useTranslation();
   const {
     rowVirtualizer,
     parentRef,
@@ -19,59 +14,26 @@ const ScrollContainer = () => {
   const virtualItems = rowVirtualizer.getVirtualItems();
 
   return (
-    <Logs className="h-full overflow-scroll" ref={parentRef}>
-      <div
-        className="relative w-full"
-        style={{
-          height: `${rowVirtualizer.getTotalSize()}px`,
-        }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            transform: `translateY(${virtualItems[0]?.start}px)`,
-          }}
-        >
-          {virtualItems.map((virtualItem) => {
-            const logEntry = logs[virtualItem.index];
-            if (!logEntry) return null;
-            return (
-              <Entry
-                key={virtualItem.key}
-                data-index={virtualItem.key}
-                ref={rowVirtualizer.measureElement}
-                logEntry={logEntry}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <div
-        className={twMergeClsx(
-          "absolute box-border flex w-full pr-10",
-          "justify-center transition-all",
-          "aria-[hidden=true]:pointer-events-none aria-[hidden=true]:bottom-11 aria-[hidden=true]:opacity-0",
-          "aria-[hidden=false]:bottom-16 aria-[hidden=false]:opacity-100"
-        )}
-        aria-hidden={scrolledToBottom ? "true" : "false"}
-      >
-        <Button
-          className="bg-white dark:bg-black"
-          variant="outline"
-          size="sm"
-          onClick={() => {
-            setScrolledToBottom(true);
-          }}
-        >
-          <ArrowDown />
-          {t("pages.monitoring.logs.followLogs")}
-          <ArrowDown />
-        </Button>
-      </div>
-    </Logs>
+    <LogList
+      ref={parentRef}
+      height={rowVirtualizer.getTotalSize()}
+      virtualOffset={virtualItems[0]?.start ?? 0}
+      scrolledToBottom={scrolledToBottom}
+      setScrolledToBottom={setScrolledToBottom}
+    >
+      {virtualItems.map((virtualItem) => {
+        const logEntry = logs[virtualItem.index];
+        if (!logEntry) return null;
+        return (
+          <Entry
+            key={virtualItem.key}
+            data-index={virtualItem.key}
+            ref={rowVirtualizer.measureElement}
+            logEntry={logEntry}
+          />
+        );
+      })}
+    </LogList>
   );
 };
 
