242deb1d87f38d367d54283b984440074c499fda
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-27 11:43:35 +0100

    don't display all http methods
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Gateway/Routes/components/Methods.tsx
+++ ui/src/pages/namespace/Gateway/Routes/components/Methods.tsx
@@ -1,17 +1,57 @@
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "~/design/HoverCard";
+
 import Badge from "~/design/Badge";
 import { FC } from "react";
 import { RouteSchemaType } from "~/api/gateway/schema";
+import { useTranslation } from "react-i18next";
 
 type AllowAnonymousProps = {
   methods: RouteSchemaType["methods"];
 };
 
-export const Methods: FC<AllowAnonymousProps> = ({ methods }) => (
-  <div className="flex w-[190px] flex-wrap gap-1">
-    {methods?.map((method) => (
-      <Badge key={method} variant="outline">
-        {method}
-      </Badge>
-    ))}
-  </div>
-);
+const methodDisplayCount = 2;
+
+export const Methods: FC<AllowAnonymousProps> = ({ methods }) => {
+  const { t } = useTranslation();
+  const numberOfHiddenMethods = (methods?.length ?? 0) - methodDisplayCount;
+  const needsTooltip = numberOfHiddenMethods > 0;
+
+  const methodsToDisplay = methods?.slice(0, methodDisplayCount) ?? [];
+  const methodsBehindTooltip = methods?.slice(methodDisplayCount) ?? [];
+
+  return (
+    <div className="flex gap-1">
+      {methodsToDisplay.map((method) => (
+        <Badge key={method} variant="outline">
+          {method}
+        </Badge>
+      ))}
+      {needsTooltip && (
+        <HoverCard>
+          <HoverCardTrigger>
+            <Badge variant="outline">
+              {t("pages.gateway.routes.row.methods.tooltipLabel", {
+                count: numberOfHiddenMethods,
+              })}
+            </Badge>
+          </HoverCardTrigger>
+          <HoverCardContent
+            align="center"
+            side="right"
+            className="grid gap-2 p-2"
+          >
+            {methodsBehindTooltip.map((method) => (
+              <Badge key={method} variant="outline">
+                {method}
+              </Badge>
+            ))}
+          </HoverCardContent>
+        </HoverCard>
+      )}
+    </div>
+  );
+};
