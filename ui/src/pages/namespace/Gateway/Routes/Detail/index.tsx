0a7ba6c7d356cab3aed95f2927f41709deafb612
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-27 09:27:29 +0100

    handle permission
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Gateway/Routes/Detail/index.tsx
+++ ui/src/pages/namespace/Gateway/Routes/Detail/index.tsx
@@ -1,14 +1,23 @@
+import { Card } from "~/design/Card";
 import { LogStreamingSubscriber } from "~/api/logs/query/LogStreamingSubscriber";
+import { NoPermissions } from "~/design/Table";
 import { pages } from "~/util/router/pages";
 import { useRoute } from "~/api/gateway/query/getRoutes";
 
 const RoutesDetailPage = () => {
   const { routePath } = pages.gateway.useParams();
-
-  const { data } = useRoute({
+  const { data, isAllowed, isFetched, noPermissionMessage } = useRoute({
     routePath: routePath ?? "",
     enabled: !!routePath,
   });
+
+  if (!isFetched) return null;
+  if (!isAllowed)
+    return (
+      <Card className="m-5 flex grow flex-col p-4">
+        <NoPermissions>{noPermissionMessage}</NoPermissions>
+      </Card>
+    );
 
   if (!data) return null;
 
