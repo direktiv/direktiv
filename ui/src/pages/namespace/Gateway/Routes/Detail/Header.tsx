c87154831467e9550cc65420638b24359486c280
Stefan Kracht <stefan.kracht@direktiv.io>
2024-03-27 11:10:47 +0100

    update header
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Gateway/Routes/Detail/Header.tsx
+++ ui/src/pages/namespace/Gateway/Routes/Detail/Header.tsx
@@ -1,43 +1,69 @@
-import { Box, FileSymlink } from "lucide-react";
+import { FileSymlink, Square } from "lucide-react";
 
+import { AllowAnonymous } from "../Table/Row/Anonymous";
 import Button from "~/design/Button";
 import { Link } from "react-router-dom";
+import { Methods } from "../Table/Row/Methods";
+import Plugins from "../Table/Row/Plugins";
+import PublicPathInput from "../Table/Row/PublicPath";
+import { pages } from "~/util/router/pages";
+import { useNamespace } from "~/util/store/namespace";
+import { useRoute } from "~/api/gateway/query/getRoutes";
 import { useTranslation } from "react-i18next";
 
 const Header = () => {
+  const namespace = useNamespace();
+  const { routePath } = pages.gateway.useParams();
+  const { data: route } = useRoute({
+    routePath: routePath ?? "",
+    enabled: !!routePath,
+  });
+
   const { t } = useTranslation();
-  // TODO: update all fields
+
+  if (!route) return null;
+  if (!namespace) return null;
+
   return (
     <div className="space-y-5 border-b border-gray-5 bg-gray-1 p-5 dark:border-gray-dark-5 dark:bg-gray-dark-1">
       <div className="flex flex-col gap-x-7 max-md:space-y-4 md:flex-row md:items-center md:justify-start">
         <div className="flex flex-col items-start gap-2">
           <h3 className="flex items-center gap-x-2 font-bold text-primary-500">
-            <Box className="h-5" /> 111
+            <Square className="h-5" /> {route.file_path}
           </h3>
         </div>
         <div className="text-sm">
           <div className="text-gray-10 dark:text-gray-dark-10">
-            {t("pages.instances.detail.header.invoker")}
+            {t("pages.gateway.routes.columns.methods")}
           </div>
-          111
+          <Methods methods={route.methods} />
         </div>
         <div className="text-sm">
           <div className="text-gray-10 dark:text-gray-dark-10">
-            {t("pages.instances.detail.header.startedAt")}
+            {t("pages.gateway.routes.columns.plugins")}
           </div>
-          dedede
+          <Plugins plugins={route.plugins} />
         </div>
         <div className="text-sm">
           <div className="text-gray-10 dark:text-gray-dark-10">
-            {t("pages.instances.detail.header.updatedAt")}
+            {t("pages.gateway.routes.columns.anonymous")}
           </div>
-          dede
+          <AllowAnonymous allow={route.allow_anonymous} />
         </div>
-        <div className="flex grow justify-end gap-5">
+        <div className="grow text-sm">
+          {route.path && <PublicPathInput path={route.path} />}
+        </div>
+        <div className="flex gap-5">
           <Button asChild isAnchor variant="primary" className="max-md:w-full">
-            <Link to="">
+            <Link
+              to={pages.explorer.createHref({
+                namespace,
+                subpage: "endpoint",
+                path: route.file_path,
+              })}
+            >
               <FileSymlink />
-              {t("pages.instances.detail.header.openWorkflow")}
+              {t("pages.gateway.routes.detail.editRoute")}
             </Link>
           </Button>
         </div>
