ed0f4cbdf68686bf6410972a761c8f5a516d6800
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-02 09:05:15 +0200

    add variables schema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ ui/src/api/variables/schema.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+const VarTypeSchema = z.enum([
+  "namespace-variable",
+  "workflow-variable",
+  "instance-variable",
+]);
+
+/**
+ * example:
+  {
+    "id": "01c9accc-49ab-4acb-a764-551e8ee1eed7",
+    "type": "namespace_variable",
+    "reference": "vars",
+    "name": "variable",
+    "size": 1,
+    "mimeType": "application/json",
+    "createdAt": "2024-04-02T06:22:21.766541Z",
+    "updatedAt": "2024-04-02T06:22:21.766541Z"
+  }
+ */
+export const VarSchema = z.object({
+  id: z.string(),
+  type: VarTypeSchema,
+  reference: z.string(),
+  name: z.string(),
+  size: z.number(),
+  mimeType: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
+/**
+ * example:
+  {
+    "data": [...],
+  }
+ */
+export const VarListSchema = z.object({
+  data: z.array(VarSchema),
+});
