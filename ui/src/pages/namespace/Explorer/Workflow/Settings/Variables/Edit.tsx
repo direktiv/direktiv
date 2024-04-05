1571c5ed7b97f48a9519f9abcb98d5d03988fe7a
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-05 13:07:50 +0200

    update edit form
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Workflow/Settings/Variables/Edit.tsx
+++ ui/src/pages/namespace/Explorer/Workflow/Settings/Variables/Edit.tsx
@@ -2,237 +2,65 @@ import {
   DialogClose,
   DialogContent,
   DialogFooter,
-  DialogHeader,
   DialogTitle,
 } from "~/design/Dialog";
-import Editor, { EditorLanguagesType } from "~/design/Editor";
-import {
-  EditorMimeTypeSchema,
-  MimeTypeType,
-  TextMimeTypeType,
-  getLanguageFromMimeType,
-  mimeTypeToLanguageDict,
-} from "~/pages/namespace/Settings/Variables/utils";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Trans, useTranslation } from "react-i18next";
-import {
-  WorkflowVariableFormSchema,
-  WorkflowVariableFormSchemaType,
-  WorkflowVariableSchemaType,
-} from "~/api/tree/schema/workflowVariable";
-import { useEffect, useState } from "react";
+import { VarFormUpdateSchemaType, VarSchemaType } from "~/api/variables/schema";
 
-import Alert from "~/design/Alert";
 import Button from "~/design/Button";
-import { Card } from "~/design/Card";
-import { FileJson } from "lucide-react";
-import FormErrors from "~/components/FormErrors";
-import Input from "~/design/Input";
-import MimeTypeSelect from "~/pages/namespace/Settings/Variables/MimeTypeSelect";
-import { useSetWorkflowVariable } from "~/api/tree/mutate/setVariable";
-import { useTheme } from "~/util/store/theme";
-import { useWorkflowVariableContent } from "~/api/tree/query/variableContent";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusCircle } from "lucide-react";
+import VariableForm from "~/pages/namespace/Settings/Variables/Form";
+import { useTranslation } from "react-i18next";
+import { useUpdateVar } from "~/api/variables/mutate/update";
+import { useVarDetails } from "~/api/variables/query/details";
 
 type EditProps = {
-  item: WorkflowVariableSchemaType;
-  path: string;
+  item: VarSchemaType;
   onSuccess: () => void;
 };
 
-// mimeType should always be initialized in the form, to avoid the backend
-// setting defaults that may not fit with the options in MimeTypeSelect
-const fallbackMimeType: TextMimeTypeType = "text/plain";
-
-const Edit = ({ item, onSuccess, path }: EditProps) => {
+const Edit = ({ item, onSuccess }: EditProps) => {
   const { t } = useTranslation();
-  const theme = useTheme();
-
-  const {
-    data,
-    isSuccess: isInitialized,
-    isError,
-  } = useWorkflowVariableContent(item.name, path);
-
-  const [body, setBody] = useState<string | File>("");
-  const [mimeType, setMimeType] = useState<MimeTypeType>(fallbackMimeType);
-
-  /**
-   * when the initial loaded content is from a non text mime type
-   * we can't edit or save it, because we have applied res.text()
-   * to the response body, which means we can't save it back to its
-   * original format. Saving would not make much sense anyway, since
-   * nothing would be changed.
-   */
-  const [saveable, setSaveable] = useState(true);
-
-  const [editorLanguage, setEditorLanguage] = useState<EditorLanguagesType>(
-    mimeTypeToLanguageDict[fallbackMimeType]
-  );
-
-  const {
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<WorkflowVariableFormSchemaType>({
-    resolver: zodResolver(WorkflowVariableFormSchema),
-    values: {
-      name: item.name,
-      path,
-      content: body ?? "",
-      mimeType,
-    },
-  });
-
-  const onMimeTypeChange = (value: MimeTypeType) => {
-    setMimeType(value);
-    setSaveable(true);
-    const editorLanguage = getLanguageFromMimeType(value);
-    if (editorLanguage) {
-      setEditorLanguage(editorLanguage);
-    }
-  };
-
-  useEffect(() => {
-    if (isInitialized) {
-      const contentType = data.headers["content-type"];
-      const safeParsedContentType = EditorMimeTypeSchema.safeParse(contentType);
-      setValue("mimeType", contentType ?? "");
-      onMimeTypeChange(contentType ?? "");
-      if (safeParsedContentType.success) {
-        setBody(data.body);
-      } else {
-        setSaveable(false);
-      }
-    }
-  }, [data, isInitialized, setValue]);
-
-  const { mutate: setVariable } = useSetWorkflowVariable({
+  const { data, isSuccess } = useVarDetails(item.id);
+  const { mutate: updateVar } = useUpdateVar({
     onSuccess,
   });
 
-  const onSubmit: SubmitHandler<WorkflowVariableFormSchemaType> = (data) => {
-    setVariable(data);
+  const onMutate = (data: VarFormUpdateSchemaType) => {
+    updateVar({
+      id: item.id,
+      ...data,
+    });
   };
-
-  const onFilepickerChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
-
-    const fileContent = await file.text();
-    const mimeType = file?.type ?? fallbackMimeType;
-
-    const parsedMimetype = EditorMimeTypeSchema.safeParse(mimeType);
-
-    setValue("mimeType", mimeType, { shouldDirty: true });
-    onMimeTypeChange(mimeType);
-
-    if (parsedMimetype.success) {
-      setBody(fileContent);
-    } else {
-      setBody(file);
-    }
-  };
-
-  if (!isInitialized) return null;
-
-  const showEditor = saveable && typeof body === "string";
 
   return (
     <DialogContent>
-      {isError ? (
-        <>
-          <Alert variant="error">
-            {t("pages.settings.variables.edit.fetchError")}
-          </Alert>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="ghost" data-testid="var-edit-cancel">
-                {t("components.button.label.cancel")}
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </>
-      ) : (
-        <form
-          id="edit-variable"
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-5"
-          data-testid="wf-form-edit-variable"
-        >
-          <DialogHeader>
+      {isSuccess && (
+        <VariableForm
+          defaultValues={{
+            name: data.data.name,
+            data: data.data.data,
+            mimeType: data.data.mimeType,
+          }}
+          dialogTitle={
             <DialogTitle>
-              <FileJson />
-              <Trans
-                i18nKey="pages.settings.variables.edit.title"
-                values={{ name: item.name }}
-              />
+              <PlusCircle />
+              {t("pages.settings.variables.edit.title", {
+                name: data.data.name,
+              })}
             </DialogTitle>
-          </DialogHeader>
-
-          <FormErrors errors={errors} className="mb-5" />
-
-          <fieldset className="flex items-center gap-5">
-            <label className="w-[150px] text-right" htmlFor="mimetype">
-              {t("pages.settings.variables.edit.mimeType.label")}
-            </label>
-            <MimeTypeSelect
-              id="mimetype"
-              loading={!isInitialized}
-              mimeType={mimeType}
-              onChange={setMimeType}
-            />
-          </fieldset>
-
-          <fieldset className="flex items-center gap-5">
-            <label className="w-[150px] text-right" htmlFor="file-upload">
-              {t("pages.settings.variables.edit.file.label")}
-            </label>
-            <Input id="file-upload" type="file" onChange={onFilepickerChange} />
-          </fieldset>
-
-          <Card
-            className="grow p-4 pl-0"
-            background="weight-1"
-            data-testid="variable-editor-card"
-          >
-            <div className="flex h-[400px]">
-              {showEditor ? (
-                <Editor
-                  value={body}
-                  onChange={(newData) => {
-                    if (newData) {
-                      setBody(newData);
-                    }
-                  }}
-                  onMount={(editor) => editor.focus()}
-                  theme={theme ?? undefined}
-                  data-testid="variable-editor"
-                  language={editorLanguage}
-                />
-              ) : (
-                <div className="flex grow p-10 text-center">
-                  <div className="flex items-center justify-center text-sm">
-                    {t("pages.settings.variables.edit.noPreview")}
-                  </div>
-                </div>
-              )}
-            </div>
-          </Card>
-
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="ghost" data-testid="var-edit-cancel">
-                {t("components.button.label.cancel")}
-              </Button>
-            </DialogClose>
-            <Button type="submit" disabled={!saveable}>
-              {t("components.button.label.save")}
-            </Button>
-          </DialogFooter>
-        </form>
+          }
+          dialogFooter={
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">
+                  {t("components.button.label.cancel")}
+                </Button>
+              </DialogClose>
+              <Button type="submit">{t("components.button.label.save")}</Button>
+            </DialogFooter>
+          }
+          onMutate={onMutate}
+        />
       )}
     </DialogContent>
   );
