720b37d07154ce16d37f62fc1bfba0fc6d7e2ce5
sir-hassan <yassir.hassan@direktiv.io>
2024-04-09 12:37:02 +0200

    Fix linter issues
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/variables2/no_duplicated.test.js
+++ tests/variables2/no_duplicated.test.js
@@ -7,30 +7,59 @@ import request from '../common/request'
 
 const namespace = basename(__filename)
 
-describe('Test no name duplicated', () => {
+describe('Test no namespace variable name duplicated', () => {
 	beforeAll(helpers.deleteAllNamespaces)
 	helpers.itShouldCreateNamespace(it, expect, namespace)
+
+	let foo = {
+		name: 'foo',
+		data: btoa('bar'),
+		mimeType: 'mime',
+	}
+
+	it(`should create a new namespace variable foo`, async () => {
+		const res = await request(config.getDirektivHost())
+			.post(`/api/v2/namespaces/${ namespace }/variables`)
+			.send(foo)
+		expect(res.statusCode).toEqual(200)
+	})
+
+	it(`should not duplicate a namespace variable foo`, async () => {
+		const res = await request(config.getDirektivHost())
+			.post(`/api/v2/namespaces/${ namespace }/variables`)
+			.send(foo)
+		expect(res.statusCode).toEqual(400)
+		expect(res.body).toEqual(
+			{
+				error: {
+					code: 'resource_already_exists',
+					message: 'resource already exists',
+				},
+			},
+		)
+	})
 
 	helpers.itShouldCreateFileV2(it, expect, namespace, '/', 'wf1.yaml', 'workflow', 'text',
 		btoa(helpers.dummyWorkflow('wf1.yaml')))
 
-	const foo1 = {
-		name: 'foo1',
-		data: btoa('bar1'),
-		mimeType: 'mime1',
+	foo = {
+		name: 'foo',
+		data: btoa('bar'),
+		mimeType: 'mime',
+		workflowPath: '/wf1.yaml'
 	}
 
-	it(`should create a new variable foo1`, async () => {
+	it(`should allow create workflow variable foo`, async () => {
 		const res = await request(config.getDirektivHost())
 			.post(`/api/v2/namespaces/${ namespace }/variables`)
-			.send(foo1)
+			.send(foo)
 		expect(res.statusCode).toEqual(200)
 	})
 
-	it(`should not duplicate a new variable foo1`, async () => {
+	it(`should not duplicate workflow variable foo`, async () => {
 		const res = await request(config.getDirektivHost())
 			.post(`/api/v2/namespaces/${ namespace }/variables`)
-			.send(foo1)
+			.send(foo)
 		expect(res.statusCode).toEqual(400)
 		expect(res.body).toEqual(
 			{
