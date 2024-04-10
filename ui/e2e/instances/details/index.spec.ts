a720b1070a60b908aca8cdfff3e3debb98284d47
julia-zielke <julia.zielke@direktiv.io>
2024-04-10 12:09:23 +0200

    Adding E2E Test for Instances Details - Diagram
    Signed-off-by: julia-zielke <julia.zielke@direktiv.io>

--- ui/e2e/instances/details/index.spec.ts
+++ ui/e2e/instances/details/index.spec.ts
@@ -1,13 +1,17 @@
 import { createNamespace, deleteNamespace } from "../../utils/namespace";
 import { expect, test } from "@playwright/test";
+import {
+  simpleWorkflow as simpleWorkflowContent,
+  workflowWithDelay as workflowWithDelayContent,
+} from "../utils/workflows";
 
 import { createFile } from "e2e/utils/files";
 import { createInstance } from "../utils/index";
 import { faker } from "@faker-js/faker";
-import { simpleWorkflow as simpleWorkflowContent } from "../utils/workflows";
 
 let namespace = "";
 const simpleWorkflowName = faker.system.commonFileName("yaml");
+const workflowWithDelayName = faker.system.commonFileName("yaml");
 
 test.beforeEach(async () => {
   namespace = await createNamespace();
@@ -17,6 +21,13 @@ test.beforeEach(async () => {
     namespace,
     type: "workflow",
     yaml: simpleWorkflowContent,
+  });
+
+  await createFile({
+    name: workflowWithDelayName,
+    namespace,
+    type: "workflow",
+    yaml: workflowWithDelayContent,
   });
 });
 
@@ -29,11 +40,8 @@ test("the header of the instance page shows the relevant data for the workflow",
   page,
 }) => {
   const newInstance = createInstance({ namespace, path: simpleWorkflowName });
-
   await expect(newInstance, "wait until process was completed").toBeDefined();
-
   const instanceId = (await newInstance).instance;
-
   await page.goto(`/${namespace}/instances/${instanceId}`);
 
   const header = page.getByTestId("instance-header-container");
@@ -86,4 +94,157 @@ test("the header of the instance page shows the relevant data for the workflow",
     page,
     "the button 'Open Workflow' is clickable and links to the correct URL"
   ).toHaveURL(editURL);
+});
+
+test("the diagram panel on the instance page responds to user interaction", async ({
+  page,
+}) => {
+  const newInstance = createInstance({ namespace, path: simpleWorkflowName });
+  await expect(newInstance, "wait until process was completed").toBeDefined();
+  const instanceId = (await newInstance).instance;
+  await page.goto(`/${namespace}/instances/${instanceId}`);
+
+  const diagramPanel = page.getByTestId("rf__wrapper");
+  await expect(diagramPanel, "It renders the diagram panel").toBeVisible();
+
+  const startNode = diagramPanel.getByTestId("rf__node-startNode");
+  const endNode = diagramPanel.getByTestId("rf__node-endNode");
+
+  await expect(startNode, "It renders the start node").toBeVisible();
+  await expect(endNode, "It renders the end node").toBeVisible();
+
+  const resizeButton = page.getByTestId("resizeDiagram");
+  await expect(resizeButton, "It renders the maximize button").toBeVisible();
+
+  await resizeButton.hover();
+  await expect(
+    page.getByText("maximize diagram"),
+    "it shows the correct text when hovering over the resize button"
+  ).toBeVisible();
+  // hover over something else to make the overlay disappear
+  page.getByTestId("rf__wrapper").hover();
+
+  await resizeButton.click();
+  await expect(
+    diagramPanel,
+    "It maximizes the diagram to full width of the screen"
+  ).toHaveCSS("width", "1032px");
+  await resizeButton.hover();
+
+  await expect(
+    page.getByText("minimize diagram"),
+    "it shows the correct text when hovering over the resize button"
+  ).toBeVisible();
+  // hover over something else to make the overlay disappear
+  page.getByTestId("rf__wrapper").hover();
+
+  await page.reload();
+  await expect(
+    diagramPanel,
+    "After a page reload the setting for maximizing the width is remembered"
+  ).toHaveCSS("width", "1032px");
+
+  await resizeButton.click();
+  await expect(
+    diagramPanel,
+    "It minimizes the diagram to the initial width of the screen"
+  ).toHaveCSS("width", "506px");
+});
+
+test("the diagram on the instance page changes appearance dynamically", async ({
+  page,
+}) => {
+  const newInstance = createInstance({
+    namespace,
+    path: workflowWithDelayName,
+  });
+  await expect(newInstance, "wait until process was completed").toBeDefined();
+  const instanceId = (await newInstance).instance;
+  await page.goto(`/${namespace}/instances/${instanceId}`);
+
+  const diagramPanel = page.getByTestId("rf__wrapper");
+  await expect(diagramPanel, "It renders the diagram panel").toBeVisible();
+
+  const resizeButton = page.getByTestId("resizeDiagram");
+  await expect(resizeButton, "It renders the maximize button").toBeVisible();
+  // resize screen to see the nodes better
+  await resizeButton.click();
+
+  const startNode = diagramPanel.getByTestId("rf__node-startNode");
+  const endNode = diagramPanel.getByTestId("rf__node-endNode");
+  const actionNode = page.getByText("delay").first();
+  const startLine = page
+    .getByTestId("rf__edge-startNode-delay")
+    .locator("path")
+    .first();
+  const endLine = page
+    .getByTestId("rf__edge-delay-endNode")
+    .locator("path")
+    .first();
+
+  await expect(startNode, "It renders the start node").toBeVisible();
+  await expect(endNode, "It renders the end node").toBeVisible();
+
+  const header = page.getByTestId("instance-header-container");
+  await expect(
+    header.locator("div").first(),
+    "the badge pending is visible"
+  ).toContainText("pending");
+
+  expect(
+    startNode.locator("div").first(),
+    "the start node is grey"
+  ).toHaveClass(/ring-gray-8/);
+
+  expect(endNode.locator("div").first(), "the end node is grey").toHaveClass(
+    /ring-gray-8/
+  );
+
+  await expect(actionNode, "the action node is green").toHaveClass(
+    /text-success-9/
+  );
+
+  await expect(startLine, "the start line is green").toHaveCSS(
+    "stroke",
+    "rgb(48, 164, 108)"
+  );
+
+  await expect(startLine, "the start line is dashed").toHaveCSS(
+    "stroke-dasharray",
+    "5px"
+  );
+
+  await expect(endLine, "the end line is NOT green").not.toHaveCSS(
+    "stroke",
+    "rgb(48, 164, 108)"
+  );
+
+  await expect(endLine, "the end line is NOT dashed").not.toHaveCSS(
+    "stroke-dasharray",
+    "5px"
+  );
+
+  await expect(
+    header.locator("div").first(),
+    "the badge complete is visible"
+  ).toContainText("complete");
+
+  expect(
+    startNode.locator("div").first(),
+    "the start node is green"
+  ).toHaveClass(/ring-success-9/);
+
+  expect(endNode.locator("div").first(), "the end node is green").toHaveClass(
+    /ring-success-9/
+  );
+
+  await expect(endLine, "the end line is green").toHaveCSS(
+    "stroke",
+    "rgb(48, 164, 108)"
+  );
+
+  await expect(endLine, "the end line is dashed").toHaveCSS(
+    "stroke-dasharray",
+    "5px"
+  );
 });
