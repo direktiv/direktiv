1f83878d4aec20d3c6293df19de54e60c193adf0
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-11 13:33:47 +0200

    add a CreateVariableForm
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ ui/src/components/VariableForm/CreateForm.tsx
@@ -0,0 +1,55 @@
+import {
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from "~/design/Dialog";
+
+import Button from "~/design/Button";
+import { PlusCircle } from "lucide-react";
+import { VarFormCreateEditSchemaType } from "~/api/variables/schema";
+import VariableForm from ".";
+import { useTranslation } from "react-i18next";
+
+const defaultMimeType = "application/json";
+
+type CreateFormProps = {
+  unallowedNames: string[];
+  onMutate: (data: VarFormCreateEditSchemaType) => void;
+};
+
+export const CreateVariableForm = ({
+  unallowedNames,
+  onMutate,
+}: CreateFormProps) => {
+  const { t } = useTranslation();
+  return (
+    <DialogContent>
+      <VariableForm
+        unallowedNames={unallowedNames}
+        defaultValues={{
+          name: "",
+          data: "",
+          mimeType: defaultMimeType,
+        }}
+        dialogTitle={
+          <DialogTitle>
+            <PlusCircle />
+            {t("pages.settings.variables.create.title")}
+          </DialogTitle>
+        }
+        dialogFooter={
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="ghost">
+                {t("components.button.label.cancel")}
+              </Button>
+            </DialogClose>
+            <Button type="submit">{t("components.button.label.create")}</Button>
+          </DialogFooter>
+        }
+        onMutate={onMutate}
+      />
+    </DialogContent>
+  );
+};
--- ui/src/pages/namespace/Settings/Variables/Create.tsx
+++ ui/src/pages/namespace/Settings/Variables/Create.tsx
@@ -1,54 +1,15 @@
-import {
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogTitle,
-} from "~/design/Dialog";
-
-import Button from "~/design/Button";
-import { PlusCircle } from "lucide-react";
-import { VariableForm } from "../../../../components/VariableForm";
+import { CreateVariableForm } from "~/components/VariableForm/CreateForm";
 import { useCreateVar } from "~/api/variables/mutate/create";
-import { useTranslation } from "react-i18next";
-
-const defaultMimeType = "application/json";
 
 type CreateProps = { onSuccess: () => void; unallowedNames: string[] };
 
 const Create = ({ onSuccess, unallowedNames }: CreateProps) => {
-  const { t } = useTranslation();
   const { mutate: createVar } = useCreateVar({
     onSuccess,
   });
 
   return (
-    <DialogContent>
-      <VariableForm
-        unallowedNames={unallowedNames}
-        defaultValues={{
-          name: "",
-          data: "",
-          mimeType: defaultMimeType,
-        }}
-        dialogTitle={
-          <DialogTitle>
-            <PlusCircle />
-            {t("pages.settings.variables.create.title")}
-          </DialogTitle>
-        }
-        dialogFooter={
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="ghost">
-                {t("components.button.label.cancel")}
-              </Button>
-            </DialogClose>
-            <Button type="submit">{t("components.button.label.create")}</Button>
-          </DialogFooter>
-        }
-        onMutate={createVar}
-      />
-    </DialogContent>
+    <CreateVariableForm onMutate={createVar} unallowedNames={unallowedNames} />
   );
 };
 
