1e677218e57c95399ae3d5dd7a55ebaff31c6f20
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-11 14:50:08 +0200

    make linter happy
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Settings/Variables/Create.tsx
+++ ui/src/pages/namespace/Settings/Variables/Create.tsx
@@ -1,21 +1,15 @@
 import { CreateVariableForm } from "~/components/VariableForm/CreateForm";
 import { useCreateVar } from "~/api/variables/mutate/create";
-import { useTranslation } from "react-i18next";
 
 type CreateProps = { onSuccess: () => void; unallowedNames: string[] };
 
 const Create = ({ onSuccess, unallowedNames }: CreateProps) => {
-  const { t } = useTranslation();
   const { mutate: createVar } = useCreateVar({
     onSuccess,
   });
 
   return (
-    <CreateVariableForm
-      title={t("pages.settings.variables.create.title")}
-      onMutate={createVar}
-      unallowedNames={unallowedNames}
-    />
+    <CreateVariableForm onMutate={createVar} unallowedNames={unallowedNames} />
   );
 };
 
