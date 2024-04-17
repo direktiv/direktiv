067fc16e842183bff68399c807485b9800bd034d
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-17 14:47:36 +0200

    set isEnterprise from the version endpoints result
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/components/AppInitializer/index.tsx
+++ ui/src/components/AppInitializer/index.tsx
@@ -3,12 +3,12 @@ import { FC, PropsWithChildren } from "react";
 import { useVersion } from "~/api/version/query/get";
 
 export const AppInitializer: FC<PropsWithChildren> = ({ children }) => {
-  const { isFetched } = useVersion();
+  const { isFetched, data } = useVersion();
   if (!isFetched) return null;
 
   window._direktiv = {
     ...window._direktiv,
-    // isEnterprise: true, // TODO: update the window value
+    isEnterprise: data?.data.isEnterprise,
   };
 
   return <>{children}</>;
