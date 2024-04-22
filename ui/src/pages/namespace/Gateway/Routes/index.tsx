148df09779013e04dcc53d9b57b6809c870dd23f
julia-zielke <julia.zielke@direktiv.io>
2024-04-22 17:08:31 +0200

    Added filter to gateway routes list
    Signed-off-by: julia-zielke <julia.zielke@direktiv.io>

--- ui/src/pages/namespace/Gateway/Routes/index.tsx
+++ ui/src/pages/namespace/Gateway/Routes/index.tsx
@@ -1,14 +1,38 @@
+import { useMemo, useState } from "react";
+
 import { Card } from "~/design/Card";
+import Input from "~/design/Input";
 import RefreshButton from "~/design/RefreshButton";
 import RoutesTable from "./Table";
+import { t } from "i18next";
 import { useRoutes } from "~/api/gateway/query/getRoutes";
 
 const RoutesPage = () => {
   const { isFetching, refetch } = useRoutes();
+  const [search, setSearch] = useState("");
+  const { data: routes } = useRoutes();
+
+  const isSearch = search.length > 0;
+
+  const filteredRoutes = useMemo(
+    () =>
+      (routes?.data ?? [])?.filter(
+        (route) => !isSearch || route.file_path.includes(search)
+      ),
+    [isSearch, search, routes?.data]
+  );
 
   return (
     <Card className="m-5">
-      <div className="flex justify-end gap-5 p-2">
+      <div className="flex justify-between gap-5 p-2">
+        <Input
+          className="sm:w-60"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          placeholder={t("pages.gateway.routes.searchPlaceholder")}
+        />
         <RefreshButton
           icon
           variant="outline"
@@ -18,7 +42,7 @@ const RoutesPage = () => {
           }}
         />
       </div>
-      <RoutesTable />
+      <RoutesTable search={search} filteredRoutes={filteredRoutes} />
     </Card>
   );
 };
