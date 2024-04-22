0dedd66fc4c35cb04dfeff4991b7011e6b8a5765
sir-hassan <yassir.hassan@direktiv.io>
2024-04-22 11:08:26 +0200

    Fix api tests
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/api/status.test.js
+++ tests/api/status.test.js
@@ -3,9 +3,9 @@ import { describe, expect, it } from '@jest/globals'
 import config from '../common/config'
 import request from '../common/request'
 
-describe('Test the version information API', () => {
-	it(`should request version information`, async () => {
-		const r = await request(config.getDirektivHost()).get(`/api/v2/version`)
+describe('Test the status information API', () => {
+	it(`should request status information`, async () => {
+		const r = await request(config.getDirektivHost()).get(`/api/v2/status`)
 		expect(r.statusCode).toEqual(200)
 
 		expect(r.body.data).toEqual({
