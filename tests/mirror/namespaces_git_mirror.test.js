3d8c799079abf49896079534261e2e206b72fc9f
sir-hassan <yassir.hassan@direktiv.io>
2024-04-23 12:31:11 +0200

    Fix mirror git namespace test.
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/mirror/namespaces_git_mirror.test.js
+++ tests/mirror/namespaces_git_mirror.test.js
@@ -4,6 +4,7 @@ import { basename } from 'path'
 import common from '../common'
 import request from '../common/request'
 import { retry50 } from '../common/retry'
+import regex from "../common/regex";
 
 const namespace = basename(__filename)
 
@@ -16,7 +17,7 @@ describe('Test namespace git mirroring', () => {
 			.send({
 				name: namespace,
 				mirror: {
-					url: 'https://github.com/direktiv/direktiv-examples.git',
+					url: 'https://github.com/direktiv/direktiv-test-project.git',
 					gitRef: 'main',
 					authType: 'public',
 				},
@@ -30,15 +31,64 @@ describe('Test namespace git mirroring', () => {
 		expect(res.statusCode).toEqual(200)
 	})
 	retry50(`should get the new git namespace`, async () => {
-		const res = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/files/aws`)
+		const res = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/files/listener.yml`)
 		expect(res.statusCode).toEqual(200)
 	})
+
+	it(`should read the workflow variables of '/banana/page-1.yaml'`, async () => {
+		const req = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/variables?workflowPath=/banana/page-1.yaml`)
+		expect(req.statusCode).toEqual(200)
+		expect(req.body.data).toEqual([ {
+			id: expect.stringMatching(common.regex.uuidRegex),
+			type: 'workflow-variable',
+			reference: '/banana/page-1.yaml',
+			name: 'page.html',
+			size: 221,
+			mimeType: 'text/html',
+			createdAt: expect.stringMatching(regex.timestampRegex),
+			updatedAt: expect.stringMatching(regex.timestampRegex),
+		} ])
+	})
+
+	it(`should read the workflow variables of '/banana/page-2.yaml'`, async () => {
+		const req = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/variables?workflowPath=/banana/page-2.yaml`)
+		expect(req.statusCode).toEqual(200)
+		expect(req.body.data).toEqual([ {
+			id: expect.stringMatching(common.regex.uuidRegex),
+			type: 'workflow-variable',
+			reference: '/banana/page-2.yaml',
+			name: 'Page.HTML',
+			size: 233,
+			mimeType: 'text/html',
+			createdAt: expect.stringMatching(regex.timestampRegex),
+			updatedAt: expect.stringMatching(regex.timestampRegex),
+		} ])
+	})
+
+	it(`should check for the expected list of namespace variables`, async () => {
+		const req = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/variables`)
+		expect(req.statusCode).toEqual(200)
+		const reduced = req.body.data.map(i => i.name)
+		expect(reduced.sort()).toEqual([
+			'beta.json',
+			'ALPHA.json',
+			'alp_ha.json',
+			'data.json',
+			'alpha.json',
+			'alp-ha.json',
+			'alpha.csv',
+			'alpha_.json',
+			'gamma.css',
+		].sort())
+	})
+
+
 	it(`should delete the new git namespace`, async () => {
 		const res = await request(common.config.getDirektivHost()).delete(`/api/v2/namespaces/${ namespace }`)
 		expect(res.statusCode).toEqual(200)
 	})
 	it(`should get 404 after the new git namespace deletion`, async () => {
-		const res = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/files/aws`)
+		const res = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ namespace }/files/listener.yml`)
 		expect(res.statusCode).toEqual(404)
 	})
 })
