c169dad9dff4a892c5786335e51eabafedf40e7a
sir-hassan <yassir.hassan@direktiv.io>
2024-04-24 16:22:56 +0200

    Group api e2e tests to reduce spamming github actions
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

---
+++ tests/part2/namespaces/basic_delete.test.js
@@ -0,0 +1,58 @@
+import { beforeAll, describe, expect, it } from '@jest/globals'
+
+import config from '../../common/config'
+import helpers from '../../common/helpers'
+import request from '../../common/request'
+
+describe('Test namespace delete calls', () => {
+	beforeAll(helpers.deleteAllNamespaces)
+
+	let createRes
+	it(`should create a new namespace`, async () => {
+		createRes = await request(config.getDirektivHost())
+			.post(`/api/v2/namespaces`)
+			.send({
+				name: 'foo',
+			})
+		expect(createRes.statusCode).toEqual(200)
+	})
+
+	it(`should delete a namespace`, async () => {
+		const namespaceName = createRes.body.data.name
+		const res = await request(config.getDirektivHost())
+			.delete(`/api/v2/namespaces/${ namespaceName }`)
+		expect(res.statusCode).toEqual(200)
+	})
+})
+
+describe('Test invalid namespace delete calls', () => {
+	beforeAll(helpers.deleteAllNamespaces)
+
+	const testCases = [
+		{
+			// invalid id.
+			name: 'something',
+			wantError: {
+				statusCode: 404,
+				error: {
+					code: 'resource_not_found',
+					message: 'requested resource is not found',
+				},
+			},
+		},
+	]
+
+	for (let i = 0; i < testCases.length; i++) {
+		const testCase = testCases[i]
+
+		it(`should fail delete a namespace case ${ i }`, async () => {
+			const res = await request(config.getDirektivHost())
+				.delete(`/api/v2/namespaces/${ testCase.name }`)
+				.send(testCase.input)
+			expect(res.statusCode).toEqual(testCase.wantError.statusCode)
+			expect(res.body.error).toEqual(
+				testCase.wantError.error,
+			)
+		})
+	}
+})
