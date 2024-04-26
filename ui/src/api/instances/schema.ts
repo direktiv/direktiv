3e7f27d63205ec1f12b071f4efed4a5633895111
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-26 10:03:44 +0200

    update instance schema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/api/instances/schema.ts
+++ ui/src/api/instances/schema.ts
@@ -12,6 +12,23 @@ const InstanceStatusSchema = z.enum([
  * example
  * 
   {
+    "branch": 0,
+    "id": "77f369d9-085a-4a7b-9695-c2ec7a469071",
+    "state": "getter",
+    "step": 9
+  }
+ */
+const parentInstance = z.object({
+  branch: z.number(),
+  id: z.string(),
+  state: z.string(),
+  step: z.number(),
+});
+
+/**
+ * example
+ * 
+  {
     "id": "79310904-929f-4f83-bed6-0bf8c1e49dc1",
     "createdAt": "0001-01-01T00:00:00Z",
     "endedAt": null,
@@ -22,9 +39,10 @@ const InstanceStatusSchema = z.enum([
     "definition": "ZGlyZWt0aXZfYXBpOiB3b3JrZmxvdy92MQpkZXNjcmlwdGlvbjogQSBzaW1wbGUgJ25vLW9wJyBzdGF0ZSB0aGF0IHJldHVybnMgJ0hlbGxvIHdvcmxkIScKc3RhdGVzOgotIGlkOiBoZWxsb3dvcmxkCiAgdHlwZTogbm9vcAogIHRyYW5zZm9ybToKICAgIHJlc3VsdDogSGVsbG8gd29ybGQhCg==",
     "flow": [],
     "traceId": "00000000000000000000000000000000",
-    "lineage": []
+    "lineage": [...]
   }
  */
+
 const InstanceSchema = z.object({
   id: z.string(),
   createdAt: z.string(),
@@ -32,11 +50,17 @@ const InstanceSchema = z.object({
   status: InstanceStatusSchema,
   path: z.string(),
   errorCode: z.string().nullable(),
-  invoker: z.string(), // TODO: check if this is still true "api", "cron", "cloudevent", "complete" (if it's created as a subflow from another instance it's something like instance:%v, where %v is the instance ID of its parent
+  /**
+   * either "api", "cron", "cloudevent" or "complete"
+   * if it's created as a subflow from another instance
+   * it's something like instance:%v, where %v is the
+   * instance ID of its parent
+   */
+  invoker: z.string(),
   definition: z.string(),
   flow: z.array(z.string()),
   traceId: z.string().nullable(),
-  lineage: z.array(z.unknown()), // TODO: this might hold information about its parent instance, refine schema
+  lineage: z.array(parentInstance),
 });
 
 /**
@@ -59,5 +83,5 @@ export type InstanceCancelPayloadType = z.infer<typeof InstanceCancelPayload>;
 export const InstanceCancelSchema = z.null();
 
 export const InstancesListSchema = z.object({
-  data: z.object({}),
+  data: z.array(InstanceSchema),
 });
