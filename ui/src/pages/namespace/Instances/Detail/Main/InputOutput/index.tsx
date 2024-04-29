1f62146db612e3bad7d5bc2455071c18aa17f761
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-29 10:34:44 +0200

    use v2 api to get isntance status
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Instances/Detail/Main/InputOutput/index.tsx
+++ ui/src/pages/namespace/Instances/Detail/Main/InputOutput/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Input from "./Input";
 import Output from "./Output";
 import { t } from "i18next";
-import { useInstanceDetails } from "~/api/instances_obsolete/query/details";
+import { useInstanceDetails } from "~/api/instances/query/details";
 import { useInstanceId } from "../../store/instanceContext";
 import { z } from "zod";
 
@@ -13,8 +13,8 @@ const InputOutput = () => {
   const { data } = useInstanceDetails({ instanceId });
   const tabs = ["input", "output"] as const;
 
-  const instanceWasSuccessfull = data?.instance.status === "complete";
-  const instanceIsFinished = data?.instance.status !== "pending";
+  const instanceWasSuccessfull = data?.status === "complete";
+  const instanceIsFinished = data?.status !== "pending";
 
   const [activeTab, setActiveTab] = useState<(typeof tabs)[number]>(
     instanceWasSuccessfull ? "output" : "input"
