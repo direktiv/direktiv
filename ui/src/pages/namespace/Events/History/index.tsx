fc0894fc63ac502003d7f6cba1d767dd540a8588
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2024-04-29 14:16:46 +0200

    Disable legacy useEventsStream
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Events/History/index.tsx
+++ ui/src/pages/namespace/Events/History/index.tsx
@@ -1,6 +1,5 @@
-import { FiltersObj, useEventsStream } from "~/api/events/query/get";
-
 import EventsList from "./EventsList";
+import { FiltersObj } from "~/api/events/query/get";
 import { useState } from "react";
 
 export const itemsPerPage = 10;
@@ -9,7 +8,7 @@ const History = () => {
   const [filters, setFilters] = useState<FiltersObj>({});
   const [offset, setOffset] = useState(0);
 
-  useEventsStream({ limit: itemsPerPage, offset, filters });
+  // useEventsStream({ limit: itemsPerPage, offset, filters });
 
   return (
     <EventsList
