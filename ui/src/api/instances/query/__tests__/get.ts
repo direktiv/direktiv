b58779bb6c4bbab2c5ac34d7088a0a8e57a4ae16
Stefan Kracht <stefan.kracht@direktiv.io>
2024-04-30 10:23:48 +0200

    add filter
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ ui/src/api/instances/query/__tests__/get.ts
@@ -0,0 +1,87 @@
+import { FiltersObj, getFilterQuery } from "../utils";
+import { describe, expect, test } from "vitest";
+
+const filterByAs: FiltersObj = {
+  AS: { type: "CONTAINS", value: "Findme" },
+};
+
+const filterByStatus: FiltersObj = {
+  STATUS: { type: "MATCH", value: "failed" },
+};
+
+const filterByTrigger: FiltersObj = {
+  TRIGGER: { type: "MATCH", value: "cloudevent" },
+};
+
+const filterByAfter: FiltersObj = {
+  AFTER: { type: "AFTER", value: new Date("2023-04-01T09:24:33.120Z") },
+};
+
+const filterByBefore: FiltersObj = {
+  BEFORE: { type: "BEFORE", value: new Date("2023-05-23T11:11:21.817Z") },
+};
+
+const filterByAsTriggerStatus: FiltersObj = {
+  ...filterByAs,
+  ...filterByTrigger,
+  ...filterByStatus,
+};
+
+const filterByTriggerAfterBefore: FiltersObj = {
+  ...filterByTrigger,
+  ...filterByAfter,
+  ...filterByBefore,
+};
+
+const filterByAsBefore: FiltersObj = {
+  ...filterByAs,
+  ...filterByBefore,
+};
+
+const queryForAs = "&filter.field=AS&filter.type=CONTAINS&filter.val=Findme";
+const queryForStatus =
+  "&filter.field=STATUS&filter.type=MATCH&filter.val=failed";
+const queryForTrigger =
+  "&filter.field=TRIGGER&filter.type=MATCH&filter.val=cloudevent";
+const queryForAfter =
+  "&filter.field=CREATED&filter.type=AFTER&filter.val=2023-04-01T09:24:33.120Z";
+const queryForBefore =
+  "&filter.field=CREATED&filter.type=BEFORE&filter.val=2023-05-23T11:11:21.817Z";
+
+describe("getFilterQuery", () => {
+  test("it returns a query string for filtering by name", () => {
+    expect(getFilterQuery(filterByAs)).toBe(queryForAs);
+  });
+
+  test("it returns a query string for filtering by status", () => {
+    expect(getFilterQuery(filterByStatus)).toBe(queryForStatus);
+  });
+
+  test("it returns a query string for filtering by trigger", () => {
+    expect(getFilterQuery(filterByTrigger)).toBe(queryForTrigger);
+  });
+
+  test("it returns a query string for filtering by created after", () => {
+    expect(getFilterQuery(filterByAfter)).toBe(queryForAfter);
+  });
+
+  test("it returns a query string for filtering by created before", () => {
+    expect(getFilterQuery(filterByBefore)).toBe(queryForBefore);
+  });
+
+  test("it returns a query string for multiple filters: name, trigger, status", () => {
+    expect(getFilterQuery(filterByAsTriggerStatus)).toBe(
+      queryForAs + queryForTrigger + queryForStatus
+    );
+  });
+
+  test("it returns a query string for multiple filters: name, before", () => {
+    expect(getFilterQuery(filterByAsBefore)).toBe(queryForAs + queryForBefore);
+  });
+
+  test("it returns a query string for multiple filters: trigger, after, before", () => {
+    expect(getFilterQuery(filterByTriggerAfterBefore)).toBe(
+      queryForTrigger + queryForAfter + queryForBefore
+    );
+  });
+});
---
+++ ui/src/api/instances/query/utils.ts
@@ -0,0 +1,63 @@
+export const triggerValues = ["api", "cloudevent", "instance", "cron"] as const;
+
+export const statusValues = [
+  "pending",
+  "complete",
+  "cancelled",
+  "failed",
+] as const;
+
+export type TriggerValue = (typeof triggerValues)[number];
+export type StatusValue = (typeof statusValues)[number];
+export type FiltersObj = {
+  AS?: { type: "CONTAINS" | "WORKFLOW"; value: string };
+  STATUS?: {
+    type: "MATCH";
+    value: StatusValue;
+  };
+  TRIGGER?: {
+    type: "MATCH";
+    value: TriggerValue;
+  };
+  AFTER?: { type: "AFTER"; value: Date };
+  BEFORE?: { type: "BEFORE"; value: Date };
+};
+
+export const getFilterQuery = (filters: FiltersObj) => {
+  let query = "";
+  const filterFields = Object.keys(filters) as Array<keyof FiltersObj>;
+
+  filterFields.forEach((field) => {
+    const filterItem = filters[field];
+
+    // Without the guard, TS thinks filterItem may be undefined
+    if (!filterItem) {
+      return console.error("filterItem is not defined");
+    }
+
+    let queryField: string;
+    let queryValue: string;
+
+    if (field === "AFTER" || field === "BEFORE") {
+      const date = filters[field]?.value;
+      if (!date) {
+        throw new Error("date is not defined in date filter");
+      }
+      queryField = "CREATED";
+      queryValue = date.toISOString();
+    } else {
+      const value = filters[field]?.value;
+      if (!value) {
+        throw new Error("filter value is not defined");
+      }
+      queryField = field;
+      queryValue = value;
+    }
+
+    query = query.concat(
+      `&filter.field=${queryField}&filter.type=${filterItem.type}&filter.val=${queryValue}`
+    );
+  });
+
+  return query;
+};
