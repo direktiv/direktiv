507cdcc1b696026a18c588fad2f5634e040f0bc4
Stefan Kracht <stefan.kracht@direktiv.io>
2024-05-02 09:54:53 +0200

    stop streaming
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Instances/Detail/index.tsx
+++ ui/src/pages/namespace/Instances/Detail/index.tsx
@@ -4,12 +4,19 @@ import { InstanceStreamingSubscriber } from "~/api/instances/query/details/strea
 import InstancesDetail from "./InstanceDetail";
 import { LogStreamingSubscriber } from "~/api/logs/query/LogStreamingSubscriber";
 import { pages } from "~/util/router/pages";
+import { useInstanceDetails } from "~/api/instances/query/details";
 
 const InstanceStreaming = () => {
   const instanceId = useInstanceId();
+  const { data, isSuccess } = useInstanceDetails({ instanceId });
+  const stopStreaming = isSuccess && data?.status !== "pending";
+
   return (
     <>
-      <InstanceStreamingSubscriber instanceId={instanceId} />
+      <InstanceStreamingSubscriber
+        instanceId={instanceId}
+        enabled={!stopStreaming}
+      />
       <LogStreamingSubscriber instance={instanceId} />
       <InstancesDetail />
     </>
