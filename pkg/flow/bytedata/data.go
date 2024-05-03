c3285a7f4958f1dfe6083bc61d8254408873d5f8
sir-hassan <yassir.hassan@direktiv.io>
2024-05-03 13:59:09 +0200

    remove unused flow package funcs
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/flow/bytedata/data.go
+++ pkg/flow/bytedata/data.go
@@ -6,9 +6,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"io"
-	"reflect"
-	"strings"
-	"time"
 
 	"github.com/direktiv/direktiv/pkg/flow/grpc"
 	"github.com/direktiv/direktiv/pkg/refactor/datastore"
@@ -83,129 +80,6 @@ func Marshal(x interface{}) string {
 	}
 
 	return string(data)
-}
-
-func convertDataForOutputMapBuilder(v reflect.Value) interface{} {
-	m := make(map[string]interface{})
-
-	iter := v.MapRange()
-
-	for iter.Next() {
-		kv := iter.Key()
-		vv := iter.Value()
-
-		if !vv.CanInterface() {
-			continue
-		}
-
-		key := kv.String()
-
-		val := vv.Interface()
-		m[key] = val
-	}
-
-	x := make(map[string]interface{})
-
-	for k, v := range m {
-		x[k] = convertDataForOutputBuilder(v)
-	}
-
-	return x
-}
-
-func convertDataForOutputStructBuilder(t reflect.Type, v reflect.Value) interface{} {
-	m := make(map[string]interface{})
-
-	for i := 0; i < v.NumField(); i++ {
-		if !v.Field(i).CanInterface() {
-			continue
-		}
-
-		key := t.Field(i).Name
-
-		tag := t.Field(i).Tag.Get("json")
-		if tag != "" {
-			elems := strings.Split(tag, ",")
-			for _, elem := range elems {
-				elem = strings.TrimSpace(elem)
-				if elem != "omitempty" {
-					key = elem
-					break
-				}
-			}
-		}
-
-		if key == "-" {
-			continue
-		}
-
-		val := v.Field(i).Interface()
-		m[key] = val
-	}
-
-	x := make(map[string]interface{})
-
-	for k, v := range m {
-		x[k] = convertDataForOutputBuilder(v)
-	}
-
-	return x
-}
-
-func convertDataForOutputSliceBuilder(v reflect.Value) interface{} {
-	s := make([]interface{}, v.Len())
-
-	for i := 0; i < v.Len(); i++ {
-		if !v.Index(i).CanInterface() {
-			continue
-		}
-
-		val := v.Index(i).Interface()
-		s[i] = val
-	}
-
-	for idx, v := range s {
-		s[idx] = convertDataForOutputBuilder(v)
-	}
-
-	return s
-}
-
-func convertDataForOutputBuilder(a interface{}) interface{} {
-	v := reflect.ValueOf(a)
-
-deref:
-
-	t := v.Type()
-
-	//nolint:exhaustive
-	switch t.Kind() {
-	case reflect.Ptr:
-		if v.IsNil() {
-			return nil
-		}
-		v = v.Elem()
-
-		goto deref
-	case reflect.Slice:
-		return convertDataForOutputSliceBuilder(v)
-	case reflect.Struct:
-		fallthrough
-	case reflect.Map:
-		x := v.Interface()
-		switch y := x.(type) {
-		case time.Time:
-			return timestamppb.New(y)
-		case map[string]string:
-			return convertDataForOutputMapBuilder(v)
-		case map[string]interface{}:
-			return convertDataForOutputMapBuilder(v)
-		default:
-			return convertDataForOutputStructBuilder(t, v)
-		}
-	default:
-		return v.Interface()
-	}
 }
 
 func ConvertEventListeners(in []*datastore.EventListener) []*grpc.EventListener {
