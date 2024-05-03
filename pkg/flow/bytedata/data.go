e1bde6ddf86e1da510b974566f68df1baeec5d40
sir-hassan <yassir.hassan@direktiv.io>
2024-05-03 13:46:52 +0200

    remove unused flow package funcs
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/flow/bytedata/data.go
+++ pkg/flow/bytedata/data.go
@@ -12,8 +12,42 @@ import (
 
 	"github.com/direktiv/direktiv/pkg/flow/grpc"
 	"github.com/direktiv/direktiv/pkg/refactor/datastore"
+	enginerefactor "github.com/direktiv/direktiv/pkg/refactor/engine"
+	"github.com/direktiv/direktiv/pkg/refactor/instancestore"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+func ConvertInstanceToGrpcInstance(instance *enginerefactor.Instance) *grpc.Instance {
+	return &grpc.Instance{
+		CreatedAt:    timestamppb.New(instance.Instance.CreatedAt),
+		UpdatedAt:    timestamppb.New(instance.Instance.UpdatedAt),
+		Id:           instance.Instance.ID.String(),
+		As:           instance.Instance.WorkflowPath,
+		Status:       instance.Instance.Status.String(),
+		ErrorCode:    instance.Instance.ErrorCode,
+		ErrorMessage: string(instance.Instance.ErrorMessage),
+		Invoker:      instance.Instance.Invoker,
+	}
+}
+
+func ConvertInstancesToGrpcInstances(instances []instancestore.InstanceData) []*grpc.Instance {
+	list := make([]*grpc.Instance, 0)
+	for idx := range instances {
+		instance := &instances[idx]
+		list = append(list, &grpc.Instance{
+			CreatedAt:    timestamppb.New(instance.CreatedAt),
+			UpdatedAt:    timestamppb.New(instance.UpdatedAt),
+			Id:           instance.ID.String(),
+			As:           instance.WorkflowPath,
+			Status:       instance.Status.String(),
+			ErrorCode:    instance.ErrorCode,
+			ErrorMessage: string(instance.ErrorMessage),
+			Invoker:      instance.Invoker,
+		})
+	}
+
+	return list
+}
 
 // Checksum is a shortcut to calculate a hash for any given input by first marshalling it to json.
 func Checksum(x interface{}) string {
@@ -28,11 +62,6 @@ func Checksum(x interface{}) string {
 	}
 
 	return hash
-}
-
-func ShortChecksum(x interface{}) string {
-	s := Checksum(x)
-	return s[:8]
 }
 
 // ComputeHash is a shortcut to calculate a hash for a byte slice.
@@ -177,23 +206,6 @@ deref:
 	default:
 		return v.Interface()
 	}
-}
-
-// ConvertDataForOutput converts data from the form returned by the database layer to a form that more closely matches our gRPC APIs output structs.
-func ConvertDataForOutput(a, b interface{}) error {
-	m := convertDataForOutputBuilder(a)
-
-	data, err := json.Marshal(m)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(data, b)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func ConvertEventListeners(in []*datastore.EventListener) []*grpc.EventListener {
