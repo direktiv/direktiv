7bf432cbf7e17be0536a771df3cee4c7ad1a2bff
sir-hassan <yassir.hassan@direktiv.io>
2024-05-03 13:41:19 +0200

    fix linter issues
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/flow/secrets.go
+++ pkg/flow/secrets.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 
 	"github.com/direktiv/direktiv/pkg/model"
-
 	"github.com/direktiv/direktiv/pkg/refactor/database"
 	"github.com/direktiv/direktiv/pkg/refactor/datastore"
 	"github.com/direktiv/direktiv/pkg/refactor/filestore"
