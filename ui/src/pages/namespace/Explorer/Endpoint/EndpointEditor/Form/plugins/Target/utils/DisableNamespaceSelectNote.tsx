38af13d5216bd95118351a460c333058aaa3f597
Stefan Kracht <stefan.kracht@direktiv.io>
2024-05-06 11:09:30 +0200

    add DisableNamespaceSelectNote
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ ui/src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/plugins/Target/utils/DisableNamespaceSelectNote.tsx
@@ -0,0 +1,11 @@
+import Alert from "~/design/Alert";
+import { useTranslation } from "react-i18next";
+
+export const DisableNamespaceSelectNote = () => {
+  const { t } = useTranslation();
+  return (
+    <Alert variant="info">
+      {t("pages.explorer.endpoint.editor.form.plugins.target.namespaceNote")}
+    </Alert>
+  );
+};
