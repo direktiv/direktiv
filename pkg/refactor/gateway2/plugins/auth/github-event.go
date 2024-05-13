08601b7bb02de03ffa7c60253e1f264d91972714
sir-hassan <yassir.hassan@direktiv.io>
2024-05-13 13:53:42 +0200

    migrate auth plugins to gateway2
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

---
+++ pkg/refactor/gateway2/plugins/auth/github-event.go
@@ -0,0 +1,70 @@
+package auth
+
+import (
+	"bytes"
+	"github.com/direktiv/direktiv/pkg/refactor/gateway2"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/direktiv/direktiv/pkg/refactor/core"
+	"github.com/direktiv/direktiv/pkg/refactor/gateway2/plugins"
+	"github.com/google/go-github/v57/github"
+)
+
+const (
+	GithubWebhookPluginName = "github-webhook-auth"
+)
+
+type GithubWebhookPluginConfig struct {
+	Secret string `mapstructure:"secret" yaml:"secret"`
+}
+
+type GithubWebhookPlugin struct {
+	config *GithubWebhookPluginConfig
+}
+
+func NewGithubWebhookPlugin(config core.PluginConfigV2) (core.PluginV2, error) {
+	requestConvertConfig := &GithubWebhookPluginConfig{}
+
+	err := gateway2.ConvertConfig(config, requestConvertConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GithubWebhookPlugin{
+		config: requestConvertConfig,
+	}, nil
+}
+
+func (p *GithubWebhookPlugin) Config() interface{} {
+	return p.config
+}
+
+func (p *GithubWebhookPlugin) ExecutePlugin(_ http.ResponseWriter, r *http.Request) (*http.Request, error) {
+	payload, err := github.ValidatePayload(r, []byte(p.config.Secret))
+	if err != nil {
+		slog.Error("can verify payload", "err", err)
+
+		return r, nil
+	}
+
+	// reset body with payload
+	r.Body = io.NopCloser(bytes.NewBuffer(payload))
+	if c != nil {
+		*c = core.ConsumerFile{
+			Username: "github",
+		}
+	}
+
+	return r, nil
+}
+
+func (*GithubWebhookPlugin) Type() string {
+	return GithubWebhookPluginName
+}
+
+//nolint:gochecknoinits
+func init() {
+	plugins.RegisterPlugin(GithubWebhookPluginName, NewGithubWebhookPlugin)
+}
---
+++ pkg/refactor/gateway2/plugins/auth/gitlab-event.go
@@ -0,0 +1,62 @@
+package auth
+
+import (
+	"github.com/direktiv/direktiv/pkg/refactor/gateway2"
+	"net/http"
+
+	"github.com/direktiv/direktiv/pkg/refactor/core"
+	"github.com/direktiv/direktiv/pkg/refactor/gateway2/plugins"
+)
+
+const (
+	GitlabWebhookPluginName = "gitlab-webhook-auth"
+	GitlabHeaderName        = "X-Gitlab-Token"
+)
+
+type GitlabWebhookPluginConfig struct {
+	Secret string `mapstructure:"secret" yaml:"secret"`
+}
+
+type GitlabWebhookPlugin struct {
+	config *GitlabWebhookPluginConfig
+}
+
+func ConfigureGitlabWebhook(config interface{}, _ string) (core.PluginInstance, error) {
+	gitlabConfig := &GitlabWebhookPluginConfig{}
+
+	err := gateway2.ConvertConfig(config, gitlabConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GitlabWebhookPlugin{
+		config: gitlabConfig,
+	}, nil
+}
+
+func (p *GitlabWebhookPlugin) Config() interface{} {
+	return p.config
+}
+
+func (p *GitlabWebhookPlugin) ExecutePlugin(c *core.ConsumerFile, _ http.ResponseWriter, r *http.Request) bool {
+	secret := r.Header.Get(GitlabHeaderName)
+
+	if secret != p.config.Secret {
+		return false
+	}
+
+	*c = core.ConsumerFile{
+		Username: "gitlab",
+	}
+
+	return true
+}
+
+func (*GitlabWebhookPlugin) Type() string {
+	return GithubWebhookPluginName
+}
+
+//nolint:gochecknoinits
+func init() {
+	plugins.RegisterPlugin(GitlabWebhookPluginName)
+}
