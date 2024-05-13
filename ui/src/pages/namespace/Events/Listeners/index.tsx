dcb786fbc7525ed8e80cf717480bcd7038e34655
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2024-05-13 17:25:56 +0200

    Reimplement server side pagination for listeners list
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Events/Listeners/index.tsx
+++ ui/src/pages/namespace/Events/Listeners/index.tsx
@@ -1,99 +1,72 @@
 import { NoPermissions, NoResult, TableCell, TableRow } from "~/design/Table";
-import { Pagination, PaginationLink } from "~/design/Pagination";
 
 import { Antenna } from "lucide-react";
 import { Card } from "~/design/Card";
 import ListenersTable from "./Table";
-import PaginationProvider from "~/components/PaginationProvider";
+import { Pagination } from "~/components/Pagination";
 import Row from "./Row";
 import { useEventListeners } from "~/api/eventListenersv2/query/get";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const pageSize = 10;
 
 const ListenersList = () => {
+  const [offset, setOffset] = useState(0);
   const { data, isFetched, isAllowed, noPermissionMessage } = useEventListeners(
-    { limit: 10, offset: 0 }
+    { limit: pageSize, offset }
   );
 
   const { t } = useTranslation();
 
   if (!data?.data) return null;
 
-  const noResults = isFetched && data.data.length === 0;
+  const numberOfResults = data?.meta?.total ?? 0;
+  const noResults = isFetched && numberOfResults === 0;
+  const showPagination = numberOfResults > pageSize;
 
   return (
     <div className="flex grow flex-col gap-y-3 p-5">
-      <PaginationProvider items={data.data} pageSize={pageSize}>
-        {({
-          currentItems,
-          goToPage,
-          goToNextPage,
-          goToPreviousPage,
-          currentPage,
-          pagesList,
-          totalPages,
-        }) => (
-          <>
-            <Card>
-              <ListenersTable>
-                {isAllowed ? (
-                  <>
-                    {noResults ? (
-                      <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
-                        <TableCell colSpan={6}>
-                          <NoResult icon={Antenna}>
-                            {t("pages.events.listeners.empty.noResults")}
-                          </NoResult>
-                        </TableCell>
-                      </TableRow>
-                    ) : (
-                      currentItems.map((listener, i) => (
-                        <Row
-                          listener={listener}
-                          key={i}
-                          namespace={listener.namespace}
-                          data-testid={`listener-row-${i}`}
-                        />
-                      ))
-                    )}
-                  </>
-                ) : (
-                  <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
-                    <TableCell colSpan={6}>
-                      <NoPermissions>{noPermissionMessage}</NoPermissions>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </ListenersTable>
-            </Card>
-
-            {totalPages > 1 && (
-              <Pagination>
-                <PaginationLink
-                  data-testid="pagination-btn-left"
-                  icon="left"
-                  onClick={() => goToPreviousPage()}
-                />
-                {pagesList.map((page) => (
-                  <PaginationLink
-                    active={currentPage === page}
-                    key={`${page}`}
-                    onClick={() => goToPage(page)}
-                  >
-                    {page}
-                  </PaginationLink>
-                ))}
-                <PaginationLink
-                  data-testid="pagination-btn-right"
-                  icon="right"
-                  onClick={() => goToNextPage()}
-                />
-              </Pagination>
-            )}
-          </>
-        )}
-      </PaginationProvider>
+      <Card>
+        <ListenersTable>
+          {isAllowed ? (
+            <>
+              {noResults ? (
+                <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
+                  <TableCell colSpan={6}>
+                    <NoResult icon={Antenna}>
+                      {t("pages.events.listeners.empty.noResults")}
+                    </NoResult>
+                  </TableCell>
+                </TableRow>
+              ) : (
+                data.data.map((listener, i) => (
+                  <Row
+                    listener={listener}
+                    key={i}
+                    namespace={listener.namespace}
+                    data-testid={`listener-row-${i}`}
+                  />
+                ))
+              )}
+            </>
+          ) : (
+            <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
+              <TableCell colSpan={6}>
+                <NoPermissions>{noPermissionMessage}</NoPermissions>
+              </TableCell>
+            </TableRow>
+          )}
+        </ListenersTable>
+      </Card>
+      {showPagination && (
+        <Pagination
+          itemsPerPage={pageSize}
+          offset={offset}
+          setOffset={(value) => setOffset(value)}
+          totalItems={numberOfResults}
+        />
+      )}
     </div>
   );
 };
