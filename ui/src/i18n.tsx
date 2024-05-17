7b3cf3393d486acba868dfbc1200c518b43e39a3
Stefan Kracht <stefan.kracht@direktiv.io>
2024-05-17 08:37:58 +0200

    remove som e comments
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/i18n.tsx
+++ ui/src/i18n.tsx
@@ -3,20 +3,9 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationFilePath from "~/assets/locales/en/translation.json?url";
 
-// Example: we could define translations here (or import them as a module)
-// and pass them to the init object below. Our current setup uses the
-// HTTP backend instead, with translation files in /public.
-// const resources = {
-//   en: {
-//     translation: {
-//       welcomeTo: "Welcome to",
-//     }
-//   }
-// };
-
 i18n
   .use(Backend)
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
     backend: {
       loadPath: translationFilePath,
