66335485b75a9189b26a05fe7c1f417fca878a6a
sir-hassan <yassir.hassan@direktiv.io>
2024-05-28 14:57:57 +0200

    migrate send_helper.js to use events v2
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/events/send_helper.js
+++ tests/events/send_helper.js
@@ -42,15 +42,21 @@ async function sendEventAndList (ns, event) {
 	expect(eventObject.id).not.toBeFalsy()
 
 	// post event
-	const workflowEventResponse = await request(common.config.getDirektivHost()).post(`/api/namespaces/${ ns }/broadcast`)
+	const workflowEventResponse = await request(common.config.getDirektivHost()).post(`/api/v2/namespaces/${ ns }/events/broadcast`)
 		.set('Content-Type', 'application/json')
 		.send(event)
 	expect(workflowEventResponse.statusCode).toEqual(200)
 
 	// wait for it to be registered
-	const eventsResponse = await request(common.config.getDirektivHost()).get(`/api/namespaces/${ ns }/events?limit=100&offset=0`)
+	const eventsResponse = await request(common.config.getDirektivHost()).get(`/api/v2/namespaces/${ ns }/events/history?limit=100&offset=0`)
 		.send()
-	return eventsResponse.body.events.results.find(item => item.id === eventObject.id)
+
+
+	for(let i=0; i< eventsResponse.body.data.length; i++) {
+		if(eventsResponse.body.data[i].event.id === eventObject.id) {
+			return  eventsResponse.body.data[i].event
+		}
+	}
 }
 
 export default {
