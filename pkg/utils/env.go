92fba212ca01fc75ad81c5ff4c2fd91e28372ad7
sir-hassan <yassir.hassan@direktiv.io>
2024-05-30 17:58:18 +0200

    remove env.go file
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -4,8 +4,4 @@ const (
 	DirektivDebug         = "DIREKTIV_DEBUG"
 	DirektivOpentelemetry = "DIREKTIV_OTLP"
 	DirektivFlowEndpoint  = "DIREKTIV_FLOW_ENDPOINT"
-
-	DirektivProxyHTTPS = "HTTPS_PROXY"
-	DirektivProxyHTTP  = "HTTP_PROXY"
-	DirektivProxyNO    = "NO_PROXY"
 )
