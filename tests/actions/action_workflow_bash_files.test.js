0911396ec73c0722a10f40d22b00ec4df72984e3
Alexander Ertli <alexander.ertli@direktiv.io>
2024-06-03 15:42:45 +0200

    Apply mv helper method
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- tests/actions/action_workflow_bash_files.test.js
+++ tests/actions/action_workflow_bash_files.test.js
@@ -13,7 +13,7 @@ describe('Test workflow bash commands via action', () => {
 
 	helpers.itShouldCreateNamespace(it, expect, namespace)
 
-	helpers.itShouldCreateFileV2(it, expect, namespace,
+	helpers.itShouldCreateFile(it, expect, namespace,
 		'',
 		testWorkflow,
 		'workflow',
@@ -30,99 +30,100 @@ states:
     function: bash
 `))
 
-	it(`should echo input via bash action from ${ testWorkflow } workflow`, async () => {
+	it(`should echo input via bash action from ${testWorkflow} workflow`, async () => {
 		await helpers.sleep(500)
 		const res = await request(common.config.getDirektivHost())
-		.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
-		.send({
-			commands: [
-				{ 
-					command: "echo '{\"hello\":\"world\"}'"  
-				}
-			]
-		});
+			.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
+			.send({
+				commands: [
+					{
+						command: "echo '{\"hello\":\"world\"}'"
+					}
+				]
+			});
 
 		expect(res.statusCode).toEqual(200)
 		expect(res.body.return.bash).toMatchObject(
-			[{"result": {"hello": "world"}, "success": true}])
+			[{ "result": { "hello": "world" }, "success": true }])
 	})
-	it(`should upload not exec file via bash action from ${ testWorkflow } workflow due to bad permissions`, async () => {
+	it(`should upload not exec file via bash action from ${testWorkflow} workflow due to bad permissions`, async () => {
 		const res = await request(common.config.getDirektivHost())
-		.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
-		.send({
-			files: [
-				{name: "hello.sh", data: "#!/bin/bash\necho 'Hello World'"}
-			],
-			commands: [
-				{ 
-					command: "./hello.sh"
-				}
-			]
-		});
+			.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
+			.send({
+				files: [
+					{ name: "hello.sh", data: "#!/bin/bash\necho 'Hello World'" }
+				],
+				commands: [
+					{
+						command: "./hello.sh"
+					}
+				]
+			});
 		expect(res.statusCode).toEqual(500)
 		expect(res.body.error).toMatchObject({
 			code: 'io.direktiv.command.error',
-			message: 'fork/exec ./hello.sh: permission denied'})
+			message: 'fork/exec ./hello.sh: permission denied'
+		})
 	})
-	it(`should upload and exec file via bash action from ${ testWorkflow } workflow`, async () => {
+	it(`should upload and exec file via bash action from ${testWorkflow} workflow`, async () => {
 		const res = await request(common.config.getDirektivHost())
-		.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
-		.send({
-			files: [
-				{name: "hello.sh", data: "#!/bin/bash\necho 'Hello World'", mode: '0755'}
-			],
-			commands: [
-				{ 
-					command: "./hello.sh"
-				}
-			]
-		});
+			.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
+			.send({
+				files: [
+					{ name: "hello.sh", data: "#!/bin/bash\necho 'Hello World'", mode: '0755' }
+				],
+				commands: [
+					{
+						command: "./hello.sh"
+					}
+				]
+			});
 		expect(res.statusCode).toEqual(200)
 		expect(res.body.return.bash).toMatchObject(
-			[{"result": "Hello World", "success": true}])
+			[{ "result": "Hello World", "success": true }])
 	})
-	it(`should return exported env via bash action from ${ testWorkflow } workflow`, async () => {
+	it(`should return exported env via bash action from ${testWorkflow} workflow`, async () => {
 		const res = await request(common.config.getDirektivHost())
-		.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
-		.send({
-			commands: [
-				{ 
-					command: 'touch executed && cat executed',
-				}
-			]
-		});
+			.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
+			.send({
+				commands: [
+					{
+						command: 'touch executed && cat executed',
+					}
+				]
+			});
 		expect(res.statusCode).toEqual(200)
 		expect(res.body.return.bash).toMatchObject(
-			[{"result": "", "success": true}])
+			[{ "result": "", "success": true }])
 	})
 	it(`files from prior action should not exists`, async () => {
 		const res = await request(common.config.getDirektivHost())
-		.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
-		.send({
-			commands: [
-				{ 
-					command: 'cat executed',
-				}
-			]
-		});
+			.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
+			.send({
+				commands: [
+					{
+						command: 'cat executed',
+					}
+				]
+			});
 
 		expect(res.statusCode).toEqual(500)
 		expect(res.body.error).toMatchObject(
-			{"code": "io.direktiv.command.error", "message": "cat: executed: No such file or directory"})
+			{ "code": "io.direktiv.command.error", "message": "cat: executed: No such file or directory" })
 	})
-	it(`should execute both commands it via bash action from ${ testWorkflow } workflow`, async () => {
+	it(`should execute both commands it via bash action from ${testWorkflow} workflow`, async () => {
 		const res = await request(common.config.getDirektivHost())
-		.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
-		.send({
-			commands: [
-				{ 
-					command: 'touch executed',
-					command: 'sleep 1 && cat executed'
-				}
-			]
-		});
+			.post(`/api/v2/namespaces/${namespace}/instances?path=${testWorkflow}&wait=true`)
+			.send({
+				commands: [
+					{
+						command: 'touch executed',
+						command: 'sleep 1 && cat executed'
+					}
+				]
+			});
 		expect(res.statusCode).toEqual(200)
 		expect(res.body.return.bash).toMatchObject(
-			[{"result": "", "success": true}])
+			[{ "result": "", "success": true }])
 	})
 })
