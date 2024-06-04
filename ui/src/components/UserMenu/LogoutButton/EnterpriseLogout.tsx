0a52ec09e76573fd63bb108a06f2d78871b07520
Stefan Kracht <stefan.kracht@direktiv.io>
2024-06-04 09:50:12 +0200

    make enterorise logout button a button
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/components/UserMenu/LogoutButton/EnterpriseLogout.tsx
+++ ui/src/components/UserMenu/LogoutButton/EnterpriseLogout.tsx
@@ -7,16 +7,14 @@ const EnterpriseLogoutButton = () => {
   const { t } = useTranslation();
   const auth = useAuth();
 
-  const signOut = () => {
+  const logout = () => {
     auth.signoutRedirect();
   };
 
   return (
-    <DropdownMenuItem className="cursor-pointer" asChild>
-      <a role="button" onClick={signOut} className="flex items-center">
-        <LogOut className="mr-2 h-4 w-4" />
-        <span>{t("components.userMenu.logout")}</span>
-      </a>
+    <DropdownMenuItem onClick={logout} className="cursor-pointer">
+      <LogOut className="mr-2 h-4 w-4" />
+      <span>{t("components.userMenu.logout")}</span>
     </DropdownMenuItem>
   );
 };
