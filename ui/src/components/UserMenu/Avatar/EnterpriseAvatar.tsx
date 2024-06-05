4911a696b49f451c8eab43bb7bdbb7bdf30a0ba7
Stefan Kracht <stefan.kracht@direktiv.io>
2024-06-05 10:42:36 +0200

    allow auth to be undefined
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/components/UserMenu/Avatar/EnterpriseAvatar.tsx
+++ ui/src/components/UserMenu/Avatar/EnterpriseAvatar.tsx
@@ -2,7 +2,7 @@ import { useAuth } from "react-oidc-context";
 
 const EnterpriseAvatar = () => {
   const auth = useAuth();
-  const username = auth.user?.profile?.preferred_username ?? "";
+  const username = auth?.user?.profile?.preferred_username ?? "";
   return <>{username.slice(0, 2)}</>;
 };
 
