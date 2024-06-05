e29c1cc293e4e103cd78181b713ff5ae5fe65368
Stefan Kracht <stefan.kracht@direktiv.io>
2024-06-05 10:33:57 +0200

    return jsx
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/components/UserMenu/Avatar/EnterpriseAvatar.tsx
+++ ui/src/components/UserMenu/Avatar/EnterpriseAvatar.tsx
@@ -3,7 +3,7 @@ import { useAuth } from "react-oidc-context";
 const EnterpriseAvatar = () => {
   const auth = useAuth();
   const username = auth.user?.profile?.preferred_username ?? "";
-  return username.slice(0, 2);
+  return <>{username.slice(0, 2)}</>;
 };
 
 export default EnterpriseAvatar;
