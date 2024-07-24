190939050bee13979a1ae4f51cf00b75e42f3e75
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2024-07-24 16:23:49 +0200

    Improve TS signature
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/hooks/useTsWorkflowLibs.ts
+++ ui/src/hooks/useTsWorkflowLibs.ts
@@ -1,16 +1,15 @@
+import * as monaco from "monaco-editor";
+
 import { useMemo } from "react";
 import workflowTsDefinition from "~/assets/ts/workflow.d.ts?raw";
 
+type SetExtraLibsArgument = Parameters<
+  typeof monaco.languages.typescript.typescriptDefaults.setExtraLibs
+>[0];
+
 const useTsWorkflowLibs = (enabled: boolean) =>
-  useMemo(
-    () =>
-      enabled
-        ? [
-            {
-              content: workflowTsDefinition,
-            },
-          ]
-        : [],
+  useMemo<SetExtraLibsArgument>(
+    () => (enabled ? [{ content: workflowTsDefinition }] : []),
     [enabled]
   );
 
