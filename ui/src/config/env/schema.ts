50e98846718dba27e02d671f590ee023982207ce
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2024-07-24 16:09:50 +0200

    Improve variable name
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/config/env/schema.ts
+++ ui/src/config/env/schema.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
-const boolean = z
+const booleanEnv = z
   .string()
   .optional()
   .transform((value) => `${value}`.toLocaleLowerCase() === "true");
 
 export const envVariablesSchema = z.object({
   VITE_DEV_API_DOMAIN: z.string().optional(),
-  VITE_RQ_DEV_TOOLS: boolean,
-  VITE_ENABLE_TS_WORKFLOWS: boolean,
+  VITE_RQ_DEV_TOOLS: booleanEnv,
+  VITE_ENABLE_TS_WORKFLOWS: booleanEnv,
 });
