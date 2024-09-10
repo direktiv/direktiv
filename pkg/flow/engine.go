23a7279d00d4bd2baecd477b4f3e5cd0adc117e7
Alexander Ertli <alexander.ertli@direktiv.io>
2024-09-10 21:17:27 +0200

    Fix ui e2e testcase
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/flow/engine.go
+++ pkg/flow/engine.go
@@ -525,7 +525,7 @@ func (engine *engine) runState(ctx context.Context, im *instanceMemory, wakedata
 	}
 
 	ctx = im.WithTags(ctx)
-	slog.DebugContext(ctx, "Executing state logic.")
+	slog.DebugContext(ctx, "Running state logic.")
 
 	transition, err = im.logic.Run(ctx, wakedata)
 	if err != nil {
