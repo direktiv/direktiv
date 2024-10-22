c32ec1ee80a446301f465b097a326dc4fd264732
sir-hassan <yassir.hassan@direktiv.io>
2024-10-22 14:16:13 +0200

    Fix docker compose targets in make file
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- make_composer.mk
+++ make_composer.mk
@@ -1,34 +1,25 @@
-.PHONY: docker-stop
-docker-stop: ## Stop an existing docker deployment.
-	docker rm -f $$(docker ps -q -f "label=direktiv.io/object-type=container") || true
-	DIREKTIV_UI_IMAGE=direktiv-ui-dev DIREKTIV_IMAGE=direktiv-dev docker compose down --remove-orphans -v
-
-.PHONY: docker-build-api
-docker-build-api:
+.PHONY: docker-start
+docker-start:
+docker-start: ## Create a local docker deployment.
+	docker compose down --remove-orphans -v
 	docker build -t direktiv-dev .
-
-.PHONY: docker-build-ui
-docker-build-ui:
 	cd ui && docker build -t direktiv-ui-dev .
 
-.PHONY: docker-start
-docker-start: docker-build-api docker-build-ui docker-stop
-docker-start: ## Create a local docker deployment.
-	DIREKTIV_UI_IMAGE=direktiv-ui-dev DIREKTIV_IMAGE=direktiv-dev  docker compose up -d --scale e2e-api=0
+	docker compose up -d --scale e2e-api=0
 
 .PHONY: docker-headless
-docker-headless: docker-build-api docker-stop
+docker-headless:
 docker-headless: ## Create a local docker deployment without an included UI container.
-	DIREKTIV_UI_IMAGE=direktiv-ui-dev DIREKTIV_IMAGE=direktiv-dev  docker compose up -d --scale ui=0 --scale e2e-api=0
-
-.PHONY: docker-tail
-docker-tail: ## Tail the logs for the direktiv container in the docker deployment.
-	DIREKTIV_UI_IMAGE=direktiv-ui-dev DIREKTIV_IMAGE=direktiv-dev  docker compose logs -f
+	docker compose down --remove-orphans -v
+	docker build -t direktiv-dev .
+	docker compose up -d --scale ui=0 --scale e2e-api=0
 
 .PHONY: docker-e2e-api
-docker-e2e-api: docker-stop docker-build-api
+docker-e2e-api:
 docker-e2e-api: ## Perform backend end-to-end tests against the docker deployment.
-	DIREKTIV_UI_IMAGE=direktiv-ui-dev DIREKTIV_IMAGE=direktiv-dev  docker compose run e2e-api
+	docker compose down --remove-orphans -v
+	docker build -t direktiv-dev .
+	docker compose run e2e-api
 
 .PHONY: docker-e2e-playwright
 docker-e2e-playwright:
