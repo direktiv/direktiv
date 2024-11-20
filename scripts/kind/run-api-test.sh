c8b87eed7522d720416768d8fb07b03decf87171
Alexander Ertli <alexander.ertli@direktiv.io>
2024-11-20 16:33:12 +0100

    Fix e2e test script
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- scripts/kind/run-api-test.sh
+++ scripts/kind/run-api-test.sh
@@ -7,6 +7,9 @@ CLUSTER_NAME="${1:-$DEFAULT_CLUSTER_NAME}"
 
 kubectl cluster-info --context kind-$CLUSTER_NAME
 
+echo "Waiting for pods to be ready..."
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=direktiv,app.kubernetes.io/name=direktiv --namespace default --timeout=120s
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=direktiv,app.kubernetes.io/name=ingress-nginx --namespace default --timeout=120s
 export DIREKTIV_HOST=(`kubectl get services direktiv-ingress-nginx-controller --output jsonpath='{.status.loadBalancer.ingress[0].ip}'`)
 echo Direktiv is at $DIREKTIV_HOST
 
