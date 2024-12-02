fe5d26c885747d0a2b7f084b5dc871a8bf04b58c
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-02 15:22:22 +0100

    update vite remove vite-plugin-svgr/client
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, loadEnv } from "vite";
 
 import { envVariablesSchema } from "./src/config/env/schema";
 import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default ({ mode }) => {
@@ -58,7 +57,7 @@ export default ({ mode }) => {
         },
       },
     },
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [react(), viteTsconfigPaths()],
     test: {
       globals: true,
       environment: "jsdom",
