825e07b3db6a87a338fe03677e687ad8bd8f844a
Alexander Ertli <alexander.ertli@direktiv.io>
2024-12-09 11:59:24 +0100

    Sort structs
    Signed-off-by: Alexander Ertli <alexander.ertli@direktiv.io>

--- pkg/betterlogger/betterlogger.go
+++ pkg/betterlogger/betterlogger.go
@@ -49,22 +49,10 @@ type GatewayAttributes struct {
 	coreGatewayAttributes
 }
 
-type coreGatewayAttributes struct {
-	coreNamespaceAttributes
-	Plugin string // Optional. Name for the gateway-plugin
-	Route  string // Endpoint of the gateway
-}
-
 // CloudEventBusAttributes holds metadata specific to the cloud event bus.
 type CloudEventBusAttributes struct {
 	coreNamespaceAttributes
 	coreCloudEventBusAttributes
-}
-type coreCloudEventBusAttributes struct {
-	EventID   string // Unique identifier for the event
-	Source    string // Source of the event
-	Subject   string // Subject of the event
-	EventType string // Type of the event
 }
 
 // InstanceMemoryAttributes holds common metadata for an instance, which is helpful for logging.
@@ -87,24 +75,35 @@ type InstanceAttributes struct {
 	coreInstanceAttr
 }
 
+// SyncAttributes holds metadata specific to a Sync component, helpful for logging.
+type SyncAttributes struct {
+	coreSyncAttributes
+	coreNamespaceAttributes
+}
+
 type coreInstanceAttr struct {
 	InstanceID   string // Unique identifier for the instance
 	WorkflowPath string // Path of the workflow the instance belongs to
 	CallPath     string // Identifies the log-stream, legacy feature from the old engine
 }
 
+type coreGatewayAttributes struct {
+	coreNamespaceAttributes
+	Plugin string // Optional. Name for the gateway-plugin
+	Route  string // Endpoint of the gateway
+}
+type coreCloudEventBusAttributes struct {
+	EventID   string // Unique identifier for the event
+	Source    string // Source of the event
+	Subject   string // Subject of the event
+	EventType string // Type of the event
+}
 type coreInstanceActionAttr struct {
 	ActionID string // Unique identifier for the instance action
 }
 
 type coreInstanceMemoryAttributes struct {
 	State string // Memory state of the instance
-}
-
-// SyncAttributes holds metadata specific to a Sync component, helpful for logging.
-type SyncAttributes struct {
-	coreSyncAttributes
-	coreNamespaceAttributes
 }
 
 // SyncAttributes holds metadata specific to a Sync component, helpful for logging.
