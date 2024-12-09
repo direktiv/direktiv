fa6456221063c69f1c334a10f2db63ceaca8c848
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-09 16:06:23 +0100

    add data
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Pages/schema/blocks/modal.ts
+++ ui/src/pages/namespace/Explorer/Pages/schema/blocks/modal.ts
@@ -4,12 +4,16 @@ import { z } from "zod";
 
 export type ModalType = {
   type: "modal";
-  trigger: BlocksType["trigger"];
-  blocks: BlocksType["all"][];
+  data: {
+    trigger: BlocksType["trigger"];
+    blocks: BlocksType["all"][];
+  };
 };
 
 export const Modal = z.object({
   type: z.literal("modal"),
-  trigger: Blocks.trigger,
-  blocks: z.array(Blocks.all),
+  data: z.object({
+    trigger: Blocks.trigger,
+    blocks: z.array(Blocks.all),
+  }),
 }) satisfies z.ZodType<ModalType>;
