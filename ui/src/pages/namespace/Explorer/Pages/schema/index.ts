e3fb38e8d5b98ed97a85239f4a484008139b05bc
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-09 16:01:23 +0100

    use pages/v1 instead of ui/v1
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Pages/schema/index.ts
+++ ui/src/pages/namespace/Explorer/Pages/schema/index.ts
@@ -2,7 +2,7 @@ import { Blocks } from "./blocks";
 import { z } from "zod";
 
 export const DirektivPagesSchema = z.object({
-  direktiv_api: z.literal("ui/v1"),
+  direktiv_api: z.literal("pages/v1"),
   path: z.string().min(1),
   blocks: z.array(Blocks.all),
 });
