e3eb604f606487b9bef598c56450085a2565bad6
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-10 10:06:08 +0100

    allow DynamicString in headline and text
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Pages/schema/blocks/headline.ts
+++ ui/src/pages/namespace/Explorer/Pages/schema/blocks/headline.ts
@@ -1,10 +1,11 @@
+import { DynamicString } from "../primitives/dynamicString";
 import { z } from "zod";
 
 export const Headline = z.object({
   type: z.literal("headline"),
   data: z.object({
-    label: z.string().min(1),
-    description: z.string().optional(),
+    label: DynamicString,
+    description: DynamicString.optional(),
   }),
 });
 
--- ui/src/pages/namespace/Explorer/Pages/schema/blocks/text.ts
+++ ui/src/pages/namespace/Explorer/Pages/schema/blocks/text.ts
@@ -1,9 +1,10 @@
+import { DynamicString } from "../primitives/dynamicString";
 import { z } from "zod";
 
 export const Text = z.object({
   type: z.literal("text"),
   data: z.object({
-    label: z.string().min(1),
+    label: DynamicString,
   }),
 });
 
