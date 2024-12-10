bd6361d1afcf90e4ffffe5a8f96805251e4b6640
julia-zielke <julia.zielke@direktiv.io>
2024-12-10 12:31:31 +0100

    PageEditor is an empty component

--- ui/src/pages/namespace/Explorer/Page/PageEditor/schema/plugins/utils.ts
+++
@@ -1,4 +0,0 @@
-type Plugin = { name: string; enterpriseOnly?: boolean };
-
-export const isPluginAvailable = (plugin: Plugin, isEnterprise: boolean) =>
-  isEnterprise ? true : plugin.enterpriseOnly === false;
