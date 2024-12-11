5cc5b776772818352b44d3fd6adab9fbf8ffdf54
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-11 14:29:11 +0100

    test schema
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

---
+++ ui/src/pages/namespace/Explorer/Page/schema/__tests__/examples/simple.ts
@@ -0,0 +1,33 @@
+import { DirektivPagesType } from "../..";
+
+export default {
+  direktiv_api: "pages/v1",
+  path: "hello/world",
+  blocks: [
+    {
+      type: "headline",
+      label: "Welcome to Direktiv",
+      description: "This is a headline block inside a Direktiv page",
+    },
+    {
+      type: "text",
+      label:
+        "This is a block that contains longer text. You might write some Terms and Conditions here or something similar",
+    },
+    {
+      type: "two-columns",
+      leftBlocks: [
+        {
+          type: "text",
+          label: "Some text goes here",
+        },
+      ],
+      rightBlocks: [
+        {
+          type: "text",
+          label: "Some text goes here",
+        },
+      ],
+    },
+  ],
+} satisfies DirektivPagesType;
