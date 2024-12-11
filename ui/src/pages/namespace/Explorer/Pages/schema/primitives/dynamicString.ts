6d0f8048ef25cb6873769cf94ee105af2e22d6d2
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-11 09:57:52 +0100

    update comment
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Pages/schema/primitives/dynamicString.ts
+++ ui/src/pages/namespace/Explorer/Pages/schema/primitives/dynamicString.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 /**
- * The schema and type of a variable string is just a normal string, but it
- * gets its own file to visualise that this string will get some special
- * processing when it is rendered on the page. It will allow you to include
- * special placeholders to use dynamic data from a parent context, such as
- * form data, data from an API query, etc.
+ * A dynamic string is implemented just a normal string, but it gets its schema
+ * and type to visualise that this string will get some special processing when
+ * they are rendered on the page. It will allow the user to include special
+ * placeholders to use dynamic data from a parent context, such as form data,
+ * data from an API query, etc.
  */
 export const DynamicString = z.string().min(1);
