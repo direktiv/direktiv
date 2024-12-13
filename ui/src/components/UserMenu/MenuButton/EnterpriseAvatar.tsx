8340824b29c6707e93ade1ea83341c9e1cd18fbd
Stefan Kracht <stefan.kracht@direktiv.io>
2024-12-13 11:46:35 +0100

    handle undefined username
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/components/UserMenu/MenuButton/EnterpriseAvatar.tsx
+++ ui/src/components/UserMenu/MenuButton/EnterpriseAvatar.tsx
@@ -1,8 +1,12 @@
+import { User } from "lucide-react";
 import { useAuth } from "react-oidc-context";
 
 const EnterpriseAvatar = () => {
   const auth = useAuth();
   const username = auth?.user?.profile?.preferred_username ?? "";
+
+  if (!username) return <User />;
+
   return <>{username.slice(0, 2)}</>;
 };
 
