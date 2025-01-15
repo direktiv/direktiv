1d6692ec9abea1c3ddda013322a61fef849ae540
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-01-15 13:33:18 +0100

    Add mock data spans
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Instances/Trace/index.tsx
+++ ui/src/pages/namespace/Instances/Trace/index.tsx
@@ -1,5 +1,24 @@
 import { FC } from "react";
+import mock from "./mock.json";
 
-const TracePage: FC = () => <div>Trace</div>;
+type SpanType = { spanId: string; children?: SpanType[] };
+
+const Spans = ({ data }: { data: Array<SpanType> }) => (
+  <>
+    {data.map((span) => (
+      <div className="ml-2" key={span.spanId}>
+        <div>{span.spanId}</div>
+        {span.children && <Spans data={span.children} />}
+      </div>
+    ))}
+  </>
+);
+
+const TracePage: FC = () => (
+  <div>
+    <h1>Mock data:</h1>
+    <Spans data={mock.timeline} />
+  </div>
+);
 
 export default TracePage;
