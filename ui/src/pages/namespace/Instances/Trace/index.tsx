1e09946695b95969eca68361945f3f47c0893a1b
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-01-15 14:44:22 +0100

    Implement basic span components
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Instances/Trace/index.tsx
+++ ui/src/pages/namespace/Instances/Trace/index.tsx
@@ -1,23 +1,85 @@
 import { FC } from "react";
 import mock from "./mock.json";
 
-type SpanType = { spanId: string; children?: SpanType[] };
+type SpanType = {
+  spanId: string;
+  startTimeUnixNano: string;
+  endTimeUnixNano: string;
+  children?: SpanType[];
+};
 
-const Spans = ({ data }: { data: Array<SpanType> }) => (
-  <>
-    {data.map((span) => (
-      <div className="ml-2" key={span.spanId}>
-        <div>{span.spanId}</div>
-        {span.children && <Spans data={span.children} />}
+const flattenSpans = (spans: SpanType[]): SpanType[] =>
+  spans.reduce<SpanType[]>((acc, span) => {
+    acc.push(span);
+    if (span.children) {
+      acc.push(...flattenSpans(span.children));
+    }
+    return acc;
+  }, []);
+
+type SpanRowProps = { span: SpanType; scale: number; start: number };
+
+const SpanRow: FC<SpanRowProps> = ({ span, scale, start }) => {
+  const left = (Number(span.startTimeUnixNano) - start) * scale;
+  const width =
+    (Number(span.endTimeUnixNano) - Number(span.startTimeUnixNano)) * scale;
+
+  return (
+    <div className="relative h-8">
+      <div
+        className="bg-blue-400 rounded h-5 absolute overflow-x-visible text-nowrap"
+        style={{ left: `${left}px`, width: `${width}px` }}
+      ></div>
+      <div
+        className="text-gray-500 absolute px-1"
+        style={{ left: `${left + width}px` }}
+      >
+        {Math.round(width * 100) / 100}
       </div>
-    ))}
-  </>
-);
+    </div>
+  );
+};
+
+type SpansTableProps = { spans: SpanType[]; containerWidth?: number };
+
+const SpansTable: FC<SpansTableProps> = ({ spans, containerWidth = 1000 }) => {
+  if (spans.length === 0) {
+    return <div className="text-gray-500">No spans to display</div>;
+  }
+
+  const allSpans = flattenSpans(spans);
+
+  const timelineStart = Math.min(
+    ...allSpans.map((span) => Number(span.startTimeUnixNano))
+  );
+  const timelineEnd = Math.max(
+    ...allSpans.map((span) => Number(span.endTimeUnixNano))
+  );
+
+  // Scale factor: pixels per millisecond
+  const scale = containerWidth / (timelineEnd - timelineStart);
+
+  return (
+    <div className="space-y-2 p-1">
+      {/* Add vertical spacing between rows */}
+      {allSpans.map((span) => (
+        <SpanRow
+          key={span.spanId}
+          span={span}
+          scale={scale}
+          start={timelineStart}
+        />
+      ))}
+    </div>
+  );
+};
+
+// const Spans = ({ data }: { data: Array<SpanType> }) => <></>;
 
 const TracePage: FC = () => (
   <div>
     <h1>Mock data:</h1>
-    <Spans data={mock.timeline} />
+    <SpansTable spans={mock.timeline} />
   </div>
 );
 
