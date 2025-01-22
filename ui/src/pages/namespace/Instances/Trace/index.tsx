3b3cef5c990b527cbbb3ca99d1118c8478e991fd
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-01-22 14:08:39 +0100

    Use table layout for span viewer
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Instances/Trace/index.tsx
+++ ui/src/pages/namespace/Instances/Trace/index.tsx
@@ -1,7 +1,18 @@
 import { FC, ReactElement } from "react";
+import {
+  Table,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from "~/design/Table";
 
+import { Card } from "~/design/Card";
+import { SquareGanttChartIcon } from "lucide-react";
+import { TableBody } from "@tremor/react";
 import mock from "./mock.json";
 import { twMergeClsx } from "~/util/helpers";
+import { useTranslation } from "react-i18next";
 
 type SpanType = {
   spanId: string;
@@ -10,11 +21,11 @@ type SpanType = {
   children?: SpanType[];
 };
 
-type TreeElementProps = { span: SpanType; depth?: number };
+type TreeElementProps = { id: string; label: string; depth?: number };
 
-const TreeElement: FC<TreeElementProps> = ({ span, depth = 0 }) => (
-  <div className="h-8 w-full" style={{ paddingLeft: `${depth * 12}px` }}>
-    {span.spanId}
+const TreeElement: FC<TreeElementProps> = ({ label, depth = 0 }) => (
+  <div className="w-full" style={{ paddingLeft: `${depth * 12}px` }}>
+    {label}
   </div>
 );
 
@@ -22,7 +33,7 @@ type TimelineElementProps = { start: number; end: number; label: string };
 
 const TimelineElement: FC<TimelineElementProps> = ({ start, end, label }) => (
   <div
-    className="relative h-8 flex flex-row w-full"
+    className="relative flex flex-row w-full"
     style={{ paddingLeft: `${start}%`, paddingRight: `${end}%` }}
   >
     <div
@@ -37,19 +48,8 @@ const TimelineElement: FC<TimelineElementProps> = ({ start, end, label }) => (
   </div>
 );
 
-type TreeProps = { elements: ReactElement[] };
-
-const Tree: FC<TreeProps> = ({ elements }) => (
-  <div className="w-3/12">{...elements}</div>
-);
-
-type TimelineProps = { elements: ReactElement[] };
-
-const Timeline: FC<TimelineProps> = ({ elements }) => (
-  <div className="w-9/12">{...elements}</div>
-);
-
 const SpanViewer: FC = () => {
+  const { t } = useTranslation();
   const { timeline: spans } = mock;
 
   const timelineStart = Math.min(
@@ -62,6 +62,7 @@ const SpanViewer: FC = () => {
   const duration = timelineEnd - timelineStart;
 
   type SpanElement = {
+    id: string;
     tree: ReactElement;
     timeline: ReactElement;
   };
@@ -81,7 +82,10 @@ const SpanViewer: FC = () => {
       const label = `${Math.round(spanLength / labelDivider) / 100} ${labelUnit}`;
 
       acc.push({
-        tree: <TreeElement span={span} depth={depth} />,
+        id: span.spanId,
+        tree: (
+          <TreeElement id={span.spanId} label={span.spanId} depth={depth} />
+        ),
         timeline: <TimelineElement start={start} end={end} label={label} />,
       });
 
@@ -94,9 +98,35 @@ const SpanViewer: FC = () => {
   const spanElements = processSpans(spans);
 
   return (
-    <div className="flex flex-row w-full">
-      <Tree elements={spanElements.map((item) => item.tree)} />
-      <Timeline elements={spanElements.map((item) => item.timeline)} />
+    <div className="flex grow flex-col gap-y-4 p-5">
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <h3 className="flex grow items-center gap-x-2 pb-1 font-bold">
+          <SquareGanttChartIcon className="h-5" />
+          {t("pages.trace.title")}
+        </h3>
+      </div>
+      <Card>
+        <Table className="border-gray-5 dark:border-gray-dark-5">
+          <TableHead>
+            <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
+              <TableHeaderCell className="w-40">
+                {t("pages.trace.tableHeader.spanId")}
+              </TableHeaderCell>
+              <TableHeaderCell>
+                {t("pages.trace.tableHeader.timeline")}
+              </TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {spanElements.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.tree}</TableCell>
+                <TableCell>{item.timeline}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
     </div>
   );
 };
