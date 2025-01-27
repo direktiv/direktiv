e7a6cbe7c89bd12fffba199af75af8b84267faa6
sir-hassan <yassir.hassan@direktiv.io>
2025-01-27 14:21:06 +0100

    remove structs helper from api tests
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/common/index.js
+++ tests/common/index.js
@@ -3,7 +3,6 @@ import filesystem from './filesystem'
 import helpers from './helpers'
 import regex from './regex'
 import request from './request'
-import structs from './structs'
 
 export default {
 	config,
