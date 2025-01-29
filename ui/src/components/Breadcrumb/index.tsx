d8c90f30741e42673b2be88e3304a01c48e27792
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-01-29 17:36:05 +0100

    Add all breadcrumbs
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/components/Breadcrumb/index.tsx
+++ ui/src/components/Breadcrumb/index.tsx
@@ -1,7 +1,15 @@
 import { BreadcrumbRoot } from "~/design/Breadcrumbs";
 import ExplorerBreadcrumb from "./ExplorerBreadcrumb";
 import { FileRoutesById } from "~/routeTree.gen";
+import GatewayBreadcrumb from "./Gateway";
+import HistoryBreadcrumb from "./Events/HistoryBreadcrumb";
+import InstancesBreadcrumb from "./InstancesBreadcrumb";
+import JqPlaygroundBreadcrumb from "./JqPlaygroundBreadcrumb";
+import ListenerBreadcrumb from "./Events/ListenerBreadcrumb";
+import MirrorBreadcrumb from "./MirrorBreadcrumb";
+import MonitoringBreadcrumb from "./MonitoringBreadcrumb";
 import ServicesBreadcrumb from "./ServicesBreadcrumb";
+import SettingsBreadcrumb from "./SettingsBreadcrumb";
 import { useMatches } from "@tanstack/react-router";
 import { useNamespace } from "~/util/store/namespace";
 
@@ -18,16 +26,17 @@ const Breadcrumb = () => {
     <BreadcrumbRoot className="group">
       {/* <NamespaceSelector /> */}
       {match("/n/$namespace/explorer/") && <ExplorerBreadcrumb />}
-      {/* {isInstancePage && <InstancesBreadcrumb />} */}
+      {match("/n/$namespace/instances/") && <InstancesBreadcrumb />}
       {match("/n/$namespace/services/") && <ServicesBreadcrumb />}
-      {/* {isEventsHistoryPage && <EventHistoryBreadcrumb />}
-      {isEventsListenersPage && <EventListenerBreadcrumb />}
-      {isMonitoringPage && <MonitoringBreadcrumb />}
-      {isPermissionsPage && <PermissionsBreadcrumb />}
-      {isSettingsPage && <SettingsBreadcrumb />}
-      {isJqPlaygroundPage && <JqPlaygroundBreadcrumb />}
-      {isMirrorPage && <MirrorBreadcrumb />}
-      {isGatewayPage && <GatewayBreadcrumb />} */}
+      {match("/n/$namespace/events/history") && <HistoryBreadcrumb />}
+      {match("/n/$namespace/events/listeners") && <ListenerBreadcrumb />}
+      {match("/n/$namespace/monitoring") && <MonitoringBreadcrumb />}
+      {/* { && <PermissionsBreadcrumb />} */}
+      {match("/n/$namespace/settings") && <SettingsBreadcrumb />}
+      {match("/n/$namespace/jq") && <JqPlaygroundBreadcrumb />}
+      {match("/n/$namespace/mirror/") && <MirrorBreadcrumb />}
+      {match("/n/$namespace/gateway/routes") && <GatewayBreadcrumb />}
+      {match("/n/$namespace/gateway/consumers") && <GatewayBreadcrumb />}
     </BreadcrumbRoot>
   );
 };
