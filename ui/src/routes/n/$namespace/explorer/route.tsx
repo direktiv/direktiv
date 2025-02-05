43610338a8d1dace2b09dad9a2188eba61121117
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-02-05 12:20:42 +0100

    Fix explorer notfound page
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/routes/n/$namespace/explorer/route.tsx
+++ ui/src/routes/n/$namespace/explorer/route.tsx
@@ -1,6 +1,8 @@
+import ErrorPage from "~/util/router/ErrorPage";
 import ExplorerWrapper from "~/pages/namespace/Explorer";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/n/$namespace/explorer")({
   component: ExplorerWrapper,
+  errorComponent: ErrorPage,
 });
