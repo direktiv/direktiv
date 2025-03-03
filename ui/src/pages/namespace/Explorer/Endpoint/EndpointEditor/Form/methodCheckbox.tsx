364ee9b66f40af93653272d90d4f8ea47d1614f5
Stefan Kracht <stefan.kracht@direktiv.io>
2025-03-03 11:42:01 +0100

    add overlay to move
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- ui/src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/methodCheckbox.tsx
+++ ui/src/pages/namespace/Explorer/Endpoint/EndpointEditor/Form/methodCheckbox.tsx
@@ -1,9 +1,32 @@
+import { ChevronDown, ClipboardPaste } from "lucide-react";
 import { ControllerRenderProps, UseFormReturn } from "react-hook-form";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/design/Dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/design/Dropdown";
+import { RouteMethod, routeMethods } from "~/api/gateway/schema";
 
 import Badge from "~/design/Badge";
+import Button from "~/design/Button";
+import { ButtonBar } from "~/design/ButtonBar";
+import { Card } from "~/design/Card";
 import { Checkbox } from "~/design/Checkbox";
+import { CheckedState } from "@radix-ui/react-checkbox";
+import Editor from "~/design/Editor";
 import { EndpointFormSchemaType } from "../schema";
-import { RouteMethod } from "~/api/gateway/schema";
+import { jsonToYaml } from "../../../utils";
+import { useState } from "react";
+import { useTheme } from "~/util/store/theme";
 
 interface MethodCheckboxProps {
   isChecked: boolean;
@@ -12,23 +35,109 @@ interface MethodCheckboxProps {
   form: UseFormReturn<EndpointFormSchemaType>;
 }
 
+const defaultMethodValue = {
+  responses: { "200": { description: "" } },
+} as const;
+
 export const MethodCheckbox: React.FC<MethodCheckboxProps> = ({
   method,
   field,
   isChecked,
-}) => (
-  <label className="flex items-center gap-2 text-sm" htmlFor={method}>
-    <Checkbox
-      id={method}
-      checked={isChecked}
-      onCheckedChange={(checked) => {
-        if (checked) {
-          field.onChange({ responses: { "200": { description: "" } } });
-        } else {
-          field.onChange(undefined);
-        }
-      }}
-    />
-    <Badge variant={isChecked ? undefined : "secondary"}>{method}</Badge>
-  </label>
-);
+  form,
+}) => {
+  const theme = useTheme();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const previousValue = form.watch(method);
+  const onCheckedChange = (checked: CheckedState) => {
+    if (checked) {
+      field.onChange(defaultMethodValue);
+    } else {
+      const isCustomValue =
+        JSON.stringify(previousValue) !== JSON.stringify(defaultMethodValue);
+      if (isCustomValue) {
+        setDialogOpen(true);
+      } else {
+        field.onChange(undefined);
+      }
+    }
+  };
+
+  const editorContentJson = {
+    connect: form.getValues("connect"),
+    delete: form.getValues("delete"),
+    get: form.getValues("get"),
+    head: form.getValues("head"),
+    options: form.getValues("options"),
+    patch: form.getValues("patch"),
+    post: form.getValues("post"),
+    put: form.getValues("put"),
+    trace: form.getValues("trace"),
+  };
+
+  return (
+    <label className="flex items-center gap-2 text-sm" htmlFor={method}>
+      <Checkbox
+        id={method}
+        checked={isChecked}
+        onCheckedChange={onCheckedChange}
+      />
+      <Badge variant={isChecked ? undefined : "secondary"}>{method}</Badge>
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <DialogContent className="sm:max-w-2xl">
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+          </DialogHeader>
+          You are about to delete the documentation for the {method}.
+          <Card className="h-96 w-full p-4" noShadow background="weight-1">
+            <Editor
+              value={jsonToYaml(editorContentJson)}
+              theme={theme ?? undefined}
+              options={{ readOnly: true }}
+            />
+          </Card>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="ghost">
+                Cancel
+              </Button>
+            </DialogClose>
+            <ButtonBar>
+              <Button
+                type="button"
+                onClick={() => {
+                  form.setValue("patch", previousValue);
+                  field.onChange(undefined);
+                  setDialogOpen(false);
+                }}
+              >
+                Delete Documentation
+              </Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button icon>
+                    <ChevronDown />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-40">
+                  {Array.from(routeMethods).map((method) => (
+                    <DropdownMenuItem
+                      key={method}
+                      onClick={() => {
+                        form.setValue(method, previousValue);
+                        field.onChange(undefined);
+                        setDialogOpen(false);
+                      }}
+                    >
+                      <ClipboardPaste className="mr-2 size-4" /> copy to{" "}
+                      {method}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </ButtonBar>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </label>
+  );
+};
