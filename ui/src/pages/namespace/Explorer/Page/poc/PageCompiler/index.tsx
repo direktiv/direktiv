f24cf96e90965a1c2f40a3eef68c398b0ed4b23e
Stefan Kracht <stefan.kracht@direktiv.io>
2025-04-24 08:33:47 +0200

    render an error when the schema is not valid

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/index.tsx
@@ -1,8 +1,9 @@
+import { DirektivPagesSchema, DirektivPagesType } from "../schema";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import Alert from "~/design/Alert";
 import { Block } from "./Block";
 import { BlocksWrapper } from "./Block/utils/BlocksWrapper";
-import { DirektivPagesType } from "../schema";
 import { addSegmentsToPath } from "./Block/utils/blockPath";
 
 type PageCompilerProps = {
@@ -31,16 +32,31 @@ const queryClient = new QueryClient({
  * [] - add button to switv
  */
 
-export const PageCompiler = ({ page }: PageCompilerProps) => (
-  <QueryClientProvider client={queryClient}>
-    <BlocksWrapper>
-      {page.blocks.map((block, index) => (
-        <Block
-          key={index}
-          block={block}
-          blockPath={addSegmentsToPath("blocks", index)}
-        />
-      ))}
-    </BlocksWrapper>
-  </QueryClientProvider>
-);
+export const PageCompiler = ({ page }: PageCompilerProps) => {
+  const parsedPage = DirektivPagesSchema.safeParse(page);
+
+  if (!parsedPage.success) {
+    return (
+      <div className="p-4 flex flex-col gap-4">
+        <Alert variant="error">
+          The page schema is not valid. Please check the following error:
+        </Alert>
+        <pre>{JSON.stringify(parsedPage.error.issues, null, 2)}</pre>
+      </div>
+    );
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BlocksWrapper>
+        {page.blocks.map((block, index) => (
+          <Block
+            key={index}
+            block={block}
+            blockPath={addSegmentsToPath("blocks", index)}
+          />
+        ))}
+      </BlocksWrapper>
+    </QueryClientProvider>
+  );
+};
