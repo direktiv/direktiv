cb8501255187d962c251c17700d5f314652178fa
Stefan Kracht <stefan.kracht@direktiv.io>
2025-05-05 10:50:37 +0200

    make the loop interate over the variable

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Loop.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Loop.tsx
@@ -1,7 +1,11 @@
 import { BlockPath, addSegmentsToPath } from "./utils/blockPath";
 
 import { Block } from ".";
+import { BlockList } from "./utils/BlockList";
+import { Error } from "../Block/utils/TemplateString/Variable/Error";
 import { LoopType } from "../../schema/blocks/loop";
+import { useTranslation } from "react-i18next";
+import { useVariableArray } from "./utils/TemplateString/Variable/utils/useVariableArray";
 
 type LoopProps = {
   blockProps: LoopType;
@@ -10,17 +14,34 @@ type LoopProps = {
 
 export const Loop = ({ blockProps, blockPath }: LoopProps) => {
   const { blocks, variable } = blockProps;
+  const { t } = useTranslation();
+
+  const [variableContent, error] = useVariableArray(variable);
+
+  if (error) {
+    return (
+      <Error value={variable}>
+        {t(`direktivPage.error.templateString.${error}`)}
+      </Error>
+    );
+  }
 
   return (
-    <>
-      looping over {variable}
-      {blocks.map((block, index) => (
-        <Block
-          key={index}
-          block={block}
-          blockPath={addSegmentsToPath(blockPath, index)}
-        />
+    <BlockList>
+      {variableContent.map((item, variableIndex) => (
+        <BlockList key={variableIndex}>
+          {blocks.map((block, blockIndex) => (
+            <Block
+              key={blockIndex}
+              block={block}
+              blockPath={addSegmentsToPath(blockPath, [
+                `loop#${variableIndex}`,
+                blockIndex,
+              ])}
+            />
+          ))}
+        </BlockList>
       ))}
-    </>
+    </BlockList>
   );
 };
