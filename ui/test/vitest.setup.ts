1853d4bb179862ccd4324ddebdddaea1da8e1abc
Stefan Kracht <stefan.kracht@direktiv.io>
2025-05-12 11:00:09 +0200

    avoid console.error in tests

--- ui/test/vitest.setup.ts
+++ ui/test/vitest.setup.ts
@@ -1,3 +1,5 @@
+import { afterAll, beforeAll, vi } from "vitest";
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translation from "../src/assets/locales/en/translation.json";
@@ -8,4 +10,13 @@ i18n.use(initReactI18next).init({
   ns: ["translation"],
   defaultNS: "translation",
   resources: { en: { translation } },
+});
+
+beforeAll(() => {
+  // avoid console.errors in tests
+  vi.spyOn(console, "error").mockImplementation(() => null);
+});
+
+afterAll(() => {
+  global.console.error = console.error;
 });
