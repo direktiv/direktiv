9f92bc390b846e4020771f4c7592c140158e6e6e
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-05-19 13:57:45 +0200

    Move modal title to translations
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Text.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Text.tsx
@@ -1,12 +1,21 @@
 import { DialogHeader, DialogTitle } from "~/design/Dialog";
 
 import { BlockEditFormProps } from ".";
+import { useTranslation } from "react-i18next";
 
-export const Text = ({ block, path }: BlockEditFormProps) => (
-  <>
-    <DialogHeader>
-      <DialogTitle>Edit Text Block @ {path.join(".")}</DialogTitle>
-    </DialogHeader>
-    <div>{JSON.stringify(block)}</div>
-  </>
-);
+export const Text = ({ block, path }: BlockEditFormProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>
+          {t("direktivPage.blockEditor.Text.modalTitle", {
+            path: path.join("."),
+          })}
+        </DialogTitle>
+      </DialogHeader>
+      <div>{JSON.stringify(block)}</div>
+    </>
+  );
+};
