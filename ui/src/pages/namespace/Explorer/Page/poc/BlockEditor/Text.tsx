c6dc55625ed7c90e28d2747d944890ed3bc463f2
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-05-21 11:25:56 +0200

    Add Textarea to text block edit modal
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Text.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Text.tsx
@@ -1,21 +1,47 @@
-import { DialogHeader, DialogTitle } from "~/design/Dialog";
+import { DialogFooter, DialogHeader, DialogTitle } from "~/design/Dialog";
 
 import { BlockEditFormProps } from ".";
+import Button from "~/design/Button";
+import { TextType } from "../schema/blocks/text";
+import { Textarea } from "~/design/TextArea";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
-export const Text = ({ block, path }: BlockEditFormProps) => {
+type TextBlockEditFormProps = Omit<BlockEditFormProps, "block"> & {
+  block: TextType;
+};
+
+export const Text = ({ block: propBlock, path }: TextBlockEditFormProps) => {
   const { t } = useTranslation();
+
+  const [block, setBlock] = useState<TextType>(structuredClone(propBlock));
 
   return (
     <>
       <DialogHeader>
         <DialogTitle>
+          {" "}
           {t("direktivPage.blockEditor.Text.modalTitle", {
             path: path.join("."),
           })}
         </DialogTitle>
       </DialogHeader>
-      <div>{JSON.stringify(block)}</div>
+      <Textarea
+        value={block.content}
+        onChange={(event) =>
+          setBlock({
+            ...block,
+            content: event.target.value,
+          })
+        }
+      />
+      <div>Debug Info {JSON.stringify(block)}</div>
+
+      <DialogFooter>
+        <Button variant="primary" onClick={() => "TBD"}>
+          {t("direktivPage.blockEditor.generic.saveButton")}
+        </Button>
+      </DialogFooter>
     </>
   );
 };
