431abc5c335df09abf019ffcbf7215413bc4cf4b
Stefan Kracht <stefan.kracht@direktiv.io>
2025-05-27 15:28:53 +0200

    update types

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Button.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Button.tsx
@@ -1,25 +1,42 @@
-import { ElementType, forwardRef } from "react";
+import ButtonDesignComponent, {
+  ButtonProps as ButtonDesignComponentProps,
+} from "~/design/Button";
 
-import ButtonDesignComponent from "~/design/Button";
 import { ButtonType } from "../../schema/blocks/button";
+import { forwardRef } from "react";
 
-type AsComponent = ElementType | typeof ButtonDesignComponent;
+type SpanButtonProps = {
+  as: "span";
+} & React.HTMLAttributes<HTMLSpanElement>;
 
-interface ButtonProps {
-  as?: AsComponent;
+type DefaultButtonProps = {
+  as?: "button";
+} & React.HTMLAttributes<HTMLButtonElement> &
+  ButtonDesignComponentProps;
+
+type ButtonProps<T extends "span" | "button" = "button"> = {
   blockProps: ButtonType;
-}
+} & (T extends "span" ? SpanButtonProps : DefaultButtonProps);
 
-export const Button = forwardRef<HTMLElement, ButtonProps>(
-  ({ as, blockProps, ...props }, ref) => {
-    const { label } = blockProps;
-    const Component = as || ButtonDesignComponent;
+export const Button = forwardRef<
+  HTMLElement,
+  ButtonProps<"span"> | ButtonProps<"button">
+>(({ as, blockProps, ...props }, ref) => {
+  const { label } = blockProps;
+
+  if (as === "span") {
     return (
-      <Component ref={ref} {...props}>
+      <span ref={ref as React.Ref<HTMLSpanElement>} {...props}>
         {label}
-      </Component>
+      </span>
     );
   }
-);
+
+  return (
+    <ButtonDesignComponent ref={ref as React.Ref<HTMLButtonElement>} {...props}>
+      {label}
+    </ButtonDesignComponent>
+  );
+});
 
 Button.displayName = "Button";
