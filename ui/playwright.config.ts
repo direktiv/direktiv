39b1d08b3b7f291fc2cb6b67cca5ed42736fb09b
Stefan Kracht <stefan.kracht@direktiv.io>
2025-06-04 14:39:47 +0200

    make process.env.PLAYWRIGHT_UI_BASE_URL optional

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -6,11 +6,17 @@ import { storageState } from "./e2e/setup/auth";
 
 dotenv.config();
 
-if (process.env && !process.env.PLAYWRIGHT_UI_BASE_URL)
-  throw new Error("PLAYWRIGHT_UI_BASE_URL is not set!!!");
+const {
+  PLAYWRIGHT_CI,
+  PLAYWRIGHT_PARALLEL,
+  PLAYWRIGHT_UI_BASE_URL,
+  PLAYWRIGHT_USE_VITE,
+} = process.env;
 
-const baseURL = process.env.PLAYWRIGHT_UI_BASE_URL
-  ? new URL(`${process.env.PLAYWRIGHT_UI_BASE_URL}`)
+if (!PLAYWRIGHT_UI_BASE_URL) console.error("PLAYWRIGHT_UI_BASE_URL is not set");
+
+const baseURL = PLAYWRIGHT_UI_BASE_URL
+  ? new URL(`${PLAYWRIGHT_UI_BASE_URL}`)
   : undefined;
 
 /**
@@ -21,12 +27,12 @@ export default defineConfig({
   /* Run tests sequentially, since parallel runs are curently failing randomly */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.PLAYWRIGHT_CI,
+  forbidOnly: !!PLAYWRIGHT_CI,
   /* Retry on CI only */
-  retries: process.env.PLAYWRIGHT_CI ? 3 : 0,
+  retries: PLAYWRIGHT_CI ? 3 : 0,
   /* Opt out of parallel tests per default. Enable parallel tests by setting
      PLAYWRIGHT_PARALLEL to TRUE. */
-  workers: process.env.PLAYWRIGHT_PARALLEL === "TRUE" ? undefined : 1,
+  workers: PLAYWRIGHT_PARALLEL === "TRUE" ? undefined : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -73,12 +79,12 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer:
-    process.env.PLAYWRIGHT_USE_VITE === "TRUE"
+    PLAYWRIGHT_USE_VITE === "TRUE"
       ? {
           timeout: 60000,
           command: `pnpm exec vite ${baseURL && `--port ${baseURL.port}`}`,
           url: baseURL?.toString(),
-          reuseExistingServer: !process.env.PLAYWRIGHT_CI,
+          reuseExistingServer: !PLAYWRIGHT_CI,
         }
       : undefined,
 });
