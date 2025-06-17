e6f4de0fbd644b559f8765d3440d52a6f8f454d0
Stefan Kracht <stefan.kracht@direktiv.io>
2025-06-17 13:35:16 +0200

    add a dedicated useReplaceVariables hook

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/procedures/utils.ts
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/procedures/utils.ts
@@ -1,49 +1,25 @@
-import {
-  parseTemplateString,
-  variablePattern,
-} from "../primitives/Variable/utils";
-
 import { KeyValueType } from "../../schema/primitives/keyValue";
 import { MutationType } from "../../schema/procedures/mutation";
 import { QueryType } from "../../schema/procedures/query";
+import { parseTemplateString } from "../primitives/Variable/utils";
 import { useResolveVariableString } from "../primitives/Variable/utils/useResolveVariableString";
 import { useTranslation } from "react-i18next";
 
-// TODO: POC only, needs to be refactored
 export const useGetUrl = () => {
-  const resolveVariableStringFn = useResolveVariableString();
   const getKeyValueArrayFn = useResolveKeyValueArray();
+  const replaceVariablesFn = useReplaceVariables();
 
   return (input: QueryType | MutationType) => {
     const { baseUrl, queryParams } = input;
-
     const queryParamsParsed = getKeyValueArrayFn(queryParams ?? []);
-
     const searchParams = new URLSearchParams();
     queryParamsParsed?.forEach(({ key, value }) => {
       searchParams.append(key, value);
     });
 
     const queryString = searchParams.toString();
-    const url = queryString ? baseUrl.concat("?", queryString) : baseUrl;
-
-    const templateFragments = url.split(variablePattern);
-
-    // TODO: use getKeyValueArrayFn here as well
-    return templateFragments
-      .map((fragment, index) => {
-        const isVariable = index % 2 === 1;
-        if (isVariable) {
-          const a = resolveVariableStringFn(fragment);
-          if (!a.success) {
-            throw new Error("🚀");
-          }
-          return a.data;
-        }
-
-        return fragment;
-      })
-      .join("");
+    const url = replaceVariablesFn(baseUrl);
+    return queryString ? url.concat("?", queryString) : url;
   };
 };
 
@@ -64,8 +40,7 @@ export const useGetUrl = () => {
  *
  */
 export const useResolveKeyValueArray = () => {
-  const resolveVariableStringFn = useResolveVariableString();
-  const { t } = useTranslation();
+  const replaceVariablesFn = useReplaceVariables();
   return (input: KeyValueType[]): KeyValueType[] =>
     // iterate over every KeyValue primitive in the input array
     input.map(({ key, value }) => {
@@ -74,15 +49,21 @@ export const useResolveKeyValueArray = () => {
        * value field and replace it with its resolved value
        * from the React context
        */
-      const parsedValue = parseTemplateString(value, (match) => {
-        const result = resolveVariableStringFn(match);
-        if (!result.success) {
-          throw new Error(
-            t(`direktivPage.error.templateString.${result.error}`)
-          );
-        }
-        return result.data;
-      }).join("");
+      const parsedValue = replaceVariablesFn(value);
       return { key, value: parsedValue };
     });
+};
+
+// TODO: rename
+const useReplaceVariables = () => {
+  const { t } = useTranslation();
+  const resolveVariableStringFn = useResolveVariableString();
+  return (input: string) =>
+    parseTemplateString(input, (match) => {
+      const result = resolveVariableStringFn(match);
+      if (!result.success) {
+        throw new Error(t(`direktivPage.error.templateString.${result.error}`));
+      }
+      return result.data;
+    }).join("");
 };
