4757117f1fc99cd06d31604bf39d5b4baac2bdd5
Stefan Kracht <stefan.kracht@direktiv.io>
2025-06-23 15:34:03 +0200

    make component

---
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/QueryProvider/GenericTable.tsx
@@ -0,0 +1,174 @@
+import { ReactNode, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from "~/design/Table";
+
+import Button from "~/design/Button";
+import { Card } from "~/design/Card";
+import { Dialog } from "~/design/Dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { ListContextMenu } from "~/components/ListContextMenu";
+import { ModalWrapper } from "~/components/ModalWrapper";
+import { Plus } from "lucide-react";
+
+export type GenericTableProps<T> = {
+  data: T[];
+  onChange: (newData: T[]) => void;
+  itemName: string;
+  itemNamePlural: string;
+  renderRow: (item: T, index: number) => ReactNode[];
+  getItemKey: (item: T, index: number) => string;
+  formTitle: string;
+  renderForm: (
+    formId: string,
+    onSubmit: (item: T) => void,
+    defaultValues?: T
+  ) => ReactNode;
+  addButtonText?: string;
+};
+
+const formId = "generic-table-form";
+
+export const GenericTable = <T,>({
+  data,
+  onChange,
+  itemName,
+  itemNamePlural,
+  renderRow,
+  getItemKey,
+  formTitle,
+  renderForm,
+  addButtonText = `add ${itemName}`,
+}: GenericTableProps<T>) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [items, setItems] = useState(data);
+  const [editIndex, setEditIndex] = useState<number>();
+
+  const addItem = (item: T) => {
+    const newItems = [...items, item];
+    setItems(newItems);
+    onChange(newItems);
+  };
+
+  const updateItem = (index: number, item: T) => {
+    const newItems = items.map((i, idx) => (idx === index ? item : i));
+    setItems(newItems);
+    onChange(newItems);
+  };
+
+  const moveItem = (srcIndex: number, targetIndex: number) => {
+    const newItems = [...items];
+    const [movedItem] = newItems.splice(srcIndex, 1);
+    if (!movedItem) throw new Error("Invalid source index");
+    newItems.splice(targetIndex, 0, movedItem);
+    setItems(newItems);
+    onChange(newItems);
+  };
+
+  const deleteItem = (index: number) => {
+    const newItems = items.filter((_, i) => i !== index);
+    setItems(newItems);
+    onChange(newItems);
+  };
+
+  const handleSubmit = (item: T) => {
+    setDialogOpen(false);
+    if (editIndex === undefined) {
+      addItem(item);
+    } else {
+      updateItem(editIndex, item);
+    }
+    setEditIndex(undefined);
+  };
+
+  const formValues = editIndex !== undefined ? items[editIndex] : undefined;
+
+  return (
+    <Dialog
+      open={dialogOpen}
+      onOpenChange={(isOpen) => {
+        if (isOpen === false) setEditIndex(undefined);
+        setDialogOpen(isOpen);
+      }}
+    >
+      <Card noShadow>
+        <Table>
+          <TableHead>
+            <TableRow className="hover:bg-inherit dark:hover:bg-inherit">
+              <TableHeaderCell colSpan={2}>
+                {items.length} {items.length === 1 ? itemName : itemNamePlural}
+              </TableHeaderCell>
+              <TableHeaderCell className="w-60 text-right">
+                <DialogTrigger asChild>
+                  <Button icon variant="outline" size="sm">
+                    <Plus />
+                    {addButtonText}
+                  </Button>
+                </DialogTrigger>
+              </TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((item, index, srcArray) => {
+              const canMoveDown = index < srcArray.length - 1;
+              const canMoveUp = index > 0;
+              const onMoveUp = canMoveUp
+                ? () => {
+                    moveItem(index, index - 1);
+                  }
+                : undefined;
+              const onMoveDown = canMoveDown
+                ? () => {
+                    moveItem(index, index + 1);
+                  }
+                : undefined;
+
+              const onDelete = () => {
+                deleteItem(index);
+              };
+
+              const rowCells = renderRow(item, index);
+
+              return (
+                <TableRow
+                  key={getItemKey(item, index)}
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setDialogOpen(true);
+                    setEditIndex(index);
+                  }}
+                >
+                  {rowCells.map((cell, cellIndex) => (
+                    <TableCell key={cellIndex}>{cell}</TableCell>
+                  ))}
+                  <TableCell className="text-right">
+                    <ListContextMenu
+                      onDelete={onDelete}
+                      onMoveDown={onMoveDown}
+                      onMoveUp={onMoveUp}
+                    />
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Card>
+      <ModalWrapper
+        formId={formId}
+        title={formTitle}
+        onCancel={() => {
+          setDialogOpen(false);
+          setEditIndex(undefined);
+        }}
+      >
+        {renderForm(formId, handleSubmit, formValues)}
+      </ModalWrapper>
+    </Dialog>
+  );
+};
