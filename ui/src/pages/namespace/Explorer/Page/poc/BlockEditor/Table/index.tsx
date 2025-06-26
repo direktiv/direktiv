17ce487bace40a6b4cd86c2df35c7ef0b85d833d
Stefan Kracht <stefan.kracht@direktiv.io>
2025-06-26 08:40:52 +0200

    add actiosn form

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/index.tsx
@@ -1,8 +1,10 @@
 import { Table as TableSchema, TableType } from "../../schema/blocks/table";
 
 import { BlockEditFormProps } from "..";
+import { ColumnForm } from "./ColumnForm";
 import { DialogFooter } from "../components/Footer";
 import { DialogHeader } from "../components/Header";
+import { Table as TableForm } from "../components/FormElements/Table";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -24,10 +26,35 @@ export const Table = ({
   return (
     <>
       <DialogHeader action={action} path={path} type={propBlock.type} />
+      {/* TODO: connect form and display errors */}
       <div className="text-gray-10 dark:text-gray-10">
         {t("direktivPage.blockEditor.blockForms.table.description")}
       </div>
-
+      {/* TODO: loop.id and loop.data */}
+      {/* TODO: actions */}
+      <TableForm
+        data={form.getValues("columns")}
+        onChange={(newValue) => {
+          form.setValue("columns", newValue);
+        }}
+        itemLabel={t(
+          "direktivPage.blockEditor.blockForms.table.column.itemLabel"
+        )}
+        label={(count) =>
+          t("direktivPage.blockEditor.blockForms.table.column.tableLabel", {
+            count,
+          })
+        }
+        renderRow={(query) => [query.label, query.content]}
+        getItemKey={(query, index) => index}
+        renderForm={(formId, onSubmit, defaultValues) => (
+          <ColumnForm
+            formId={formId}
+            onSubmit={onSubmit}
+            defaultValues={defaultValues}
+          />
+        )}
+      />
       <DialogFooter onSubmit={() => onSubmit(form.getValues())} />
     </>
   );
