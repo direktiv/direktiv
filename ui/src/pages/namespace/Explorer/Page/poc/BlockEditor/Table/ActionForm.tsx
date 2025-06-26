53b778b8eb9e0dd61b67d6b5a9db27b16c8c0820
Stefan Kracht <stefan.kracht@direktiv.io>
2025-06-26 09:40:24 +0200

    define action buttons for table

---
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/ActionForm.tsx
@@ -0,0 +1,52 @@
+import FormErrors, { errorsType } from "~/components/FormErrors";
+import { TriggerBlocks, TriggerBlocksType } from "../../schema/blocks";
+
+import { Fieldset } from "~/components/Form/Fieldset";
+import Input from "~/design/Input";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type ActionFormProps = {
+  defaultValues?: TriggerBlocksType;
+  formId: string;
+  onSubmit: (data: TriggerBlocksType) => void;
+};
+
+export const ActionForm = ({
+  defaultValues,
+  formId,
+  onSubmit,
+}: ActionFormProps) => {
+  const { t } = useTranslation();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<TriggerBlocksType>({
+    resolver: zodResolver(TriggerBlocks),
+    defaultValues: {
+      type: "button",
+      label: "",
+      ...defaultValues,
+    },
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} id={formId}>
+      {errors && <FormErrors errors={errors as errorsType} className="mb-5" />}
+      <Fieldset
+        label={t("direktivPage.blockEditor.blockForms.table.action.labelLabel")}
+        htmlFor="label"
+      >
+        <Input
+          {...register("label")}
+          id="label"
+          placeholder={t(
+            "direktivPage.blockEditor.blockForms.table.action.labelPlaceholder"
+          )}
+        />
+      </Fieldset>
+    </form>
+  );
+};
--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/index.tsx
@@ -1,9 +1,12 @@
 import { Table as TableSchema, TableType } from "../../schema/blocks/table";
 
+import { ActionForm } from "./ActionForm";
 import { BlockEditFormProps } from "..";
 import { ColumnForm } from "./ColumnForm";
 import { DialogFooter } from "../components/Footer";
 import { DialogHeader } from "../components/Header";
+import { Fieldset } from "~/components/Form/Fieldset";
+import Input from "~/design/Input";
 import { Table as TableForm } from "../components/FormElements/Table";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -26,12 +29,58 @@ export const Table = ({
   return (
     <>
       <DialogHeader action={action} path={path} type={propBlock.type} />
-      {/* TODO: connect form and display errors */}
+      {/* TODO: connect form and display errors, update schema to not alllow  */}
       <div className="text-gray-10 dark:text-gray-10">
         {t("direktivPage.blockEditor.blockForms.table.description")}
       </div>
+      <Fieldset
+        label={t("direktivPage.blockEditor.blockForms.table.data.idLabel")}
+        htmlFor="data-id"
+      >
+        <Input
+          {...form.register("data.id")}
+          id="data-id"
+          placeholder={t(
+            "direktivPage.blockEditor.blockForms.table.data.idPlaceholder"
+          )}
+        />
+      </Fieldset>
+      <Fieldset
+        label={t("direktivPage.blockEditor.blockForms.table.data.dataLabel")}
+        htmlFor="data-data"
+      >
+        <Input
+          {...form.register("data.data")}
+          id="id"
+          placeholder={t(
+            "direktivPage.blockEditor.blockForms.table.data.dataPlaceholder"
+          )}
+        />
+      </Fieldset>
       {/* TODO: loop.id and loop.data */}
-      {/* TODO: actions */}
+      <TableForm
+        data={form.getValues("actions")}
+        onChange={(newValue) => {
+          form.setValue("actions", newValue);
+        }}
+        itemLabel={t(
+          "direktivPage.blockEditor.blockForms.table.action.itemLabel"
+        )}
+        label={(count) =>
+          t("direktivPage.blockEditor.blockForms.table.action.tableLabel", {
+            count,
+          })
+        }
+        renderRow={(query) => [query.label]}
+        getItemKey={(query, index) => index}
+        renderForm={(formId, onSubmit, defaultValues) => (
+          <ActionForm
+            formId={formId}
+            onSubmit={onSubmit}
+            defaultValues={defaultValues}
+          />
+        )}
+      />
       <TableForm
         data={form.getValues("columns")}
         onChange={(newValue) => {
