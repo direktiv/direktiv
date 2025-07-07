7b5477f30288bd98927dbc2d2f3bbba983551fe1
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-07-07 12:06:58 +0200

    Add moveBlock method to usePageEditor
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/DndContextProvider/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/DndContextProvider/index.tsx
@@ -1,35 +1,26 @@
-import {
-  addBlockToPage,
-  moveBlockWithinPage,
-} from "../PageCompiler/context/utils";
-
 import { AllBlocksType } from "../schema/blocks";
 import { BlockPathType } from "../PageCompiler/Block";
 import { DirektivPagesType } from "../schema";
 import { DndContext } from "~/design/DragAndDropEditor/Context.tsx";
 import { PropsWithChildren } from "react";
+import { usePageEditor } from "../PageCompiler/context/pageCompilerContext";
 
 type DndContextProviderProps = PropsWithChildren & {
   page: DirektivPagesType;
   setPage: (page: DirektivPagesType) => void;
 };
 
-export const DndContextProvider = ({
-  page,
-  setPage,
-  children,
-}: DndContextProviderProps) => {
+export const DndContextProvider = ({ children }: DndContextProviderProps) => {
+  const { addBlock, moveBlock } = usePageEditor();
+
   const onMove = (
     origin: BlockPathType | null,
     target: BlockPathType,
     block: AllBlocksType
   ) => {
-    const updatedPage =
-      origin === null
-        ? addBlockToPage(page, target, block)
-        : moveBlockWithinPage(page, origin, target, block);
-
-    setPage(updatedPage);
+    origin === null
+      ? addBlock(target, block)
+      : moveBlock(origin, target, block);
   };
 
   return <DndContext onMove={onMove}>{children} </DndContext>;
