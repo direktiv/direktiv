0a5ae37696865b50582df4c676c66df8f5a646b6
Stefan Kracht <stefan.kracht@direktiv.io>
2025-07-25 07:32:56 +0200

    don't highlight variables anymore

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/primitives/Variable/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/primitives/Variable/index.tsx
@@ -1,7 +1,5 @@
 import { VariableError } from "./Error";
 import { VariableType } from "../../../schema/primitives/variable";
-import { twMergeClsx } from "~/util/helpers";
-import { usePageEditor } from "../../context/pageCompilerContext";
 import { useTranslation } from "react-i18next";
 import { useVariableStringResolver } from "./utils/useVariableStringResolver";
 
@@ -11,7 +9,6 @@ type VariableProps = {
 
 export const Variable = ({ value }: VariableProps) => {
   const { t } = useTranslation();
-  const { mode } = usePageEditor();
   const resolveVariableString = useVariableStringResolver();
   const variableString = resolveVariableString(value);
 
@@ -23,14 +20,5 @@ export const Variable = ({ value }: VariableProps) => {
     );
   }
 
-  return (
-    <span
-      className={twMergeClsx(
-        mode === "edit" &&
-          "rounded-sm outline-offset-2 outline-gray-8 hover:outline dark:outline-gray-dark-8"
-      )}
-    >
-      {variableString.data}
-    </span>
-  );
+  return <span>{variableString.data}</span>;
 };
