452e52d9a8c568b8528baba64f052b6ec00a84c7
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-04 12:07:27 +0200

    make select placeholder work

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/FormSelect.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/FormSelect.tsx
@@ -8,14 +8,21 @@ import {
 
 import { Fieldset } from "./utils/FieldSet";
 import { FormSelectType } from "../../../schema/blocks/form/select";
+import { useTranslation } from "react-i18next";
 
 type FormSelectProps = {
   blockProps: FormSelectType;
 };
 
 export const FormSelect = ({ blockProps }: FormSelectProps) => {
+  const { t } = useTranslation();
   const { id, label, description, defaultValue, values, optional } = blockProps;
   const htmlID = `id-${id}`;
+
+  const value = values.some((v) => v === defaultValue)
+    ? defaultValue
+    : undefined;
+
   return (
     <Fieldset
       label={label}
@@ -23,9 +30,11 @@ export const FormSelect = ({ blockProps }: FormSelectProps) => {
       htmlFor={htmlID}
       optional={optional}
     >
-      <Select defaultValue={defaultValue}>
+      <Select defaultValue={value}>
         <SelectTrigger variant="outline" id={htmlID}>
-          <SelectValue />
+          <SelectValue
+            placeholder={t("direktivPage.page.blocks.form.selectPlaceholder")}
+          />
         </SelectTrigger>
         <SelectContent>
           {values.map((value) => (
