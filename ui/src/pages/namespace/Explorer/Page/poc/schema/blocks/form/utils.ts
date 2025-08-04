c70be35bbabf4459a9f8ee20c9d243cdd2c0fc81
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-04 11:04:12 +0200

    make label required

--- ui/src/pages/namespace/Explorer/Page/poc/schema/blocks/form/utils.ts
+++ ui/src/pages/namespace/Explorer/Page/poc/schema/blocks/form/utils.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const FormBase = z.object({
   id: z.string().min(1),
-  label: TemplateString,
+  label: TemplateString.min(1),
   description: TemplateString,
   optional: z.boolean(),
 });
