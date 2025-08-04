2453e6da3e8a8fdbaced578d8585ef72bb4ac58b
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-04 11:35:42 +0200

    make checkbox description requried

--- ui/src/pages/namespace/Explorer/Page/poc/schema/blocks/form/checkbox.ts
+++ ui/src/pages/namespace/Explorer/Page/poc/schema/blocks/form/checkbox.ts
@@ -1,8 +1,10 @@
 import { FormBase } from "./utils";
+import { TemplateString } from "../../primitives/templateString";
 import { z } from "zod";
 
 export const FormCheckbox = FormBase.extend({
   type: z.literal("form-checkbox"),
+  description: TemplateString.min(1), // overwrite description from base to be required
   defaultValue: z.boolean(),
 });
 
