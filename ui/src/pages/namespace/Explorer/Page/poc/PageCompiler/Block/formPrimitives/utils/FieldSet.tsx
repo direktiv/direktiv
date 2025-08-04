d57a0d70a66dc64a3c6b7533e75dcec59d707ed9
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-04 11:07:17 +0200

    render optional

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/utils/FieldSet.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/utils/FieldSet.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren } from "react";
 
 import { twMergeClsx } from "~/util/helpers";
+import { useTranslation } from "react-i18next";
 
 type FieldsetProps = PropsWithChildren & {
   label: string;
@@ -16,23 +17,33 @@ export const Fieldset: FC<FieldsetProps> = ({
   children,
   htmlFor,
   horizontal,
-}) => (
-  <fieldset
-    className={twMergeClsx(
-      "mb-2 flex gap-1",
-      horizontal
-        ? "flex-row-reverse flex-wrap items-center gap-2"
-        : "flex-col gap-1"
-    )}
-  >
-    <label className="grow text-sm font-bold" htmlFor={htmlFor}>
-      {label}
-    </label>
-    {children}
-    {description && (
-      <div className="basis-full text-sm text-gray-9 dark:text-gray-dark-9">
-        {description}
-      </div>
-    )}
-  </fieldset>
-);
+  optional,
+}) => {
+  const { t } = useTranslation();
+  return (
+    <fieldset
+      className={twMergeClsx(
+        "mb-2 flex gap-1",
+        horizontal
+          ? "flex-row-reverse flex-wrap items-center gap-2"
+          : "flex-col gap-1"
+      )}
+    >
+      <label className="flex grow text-sm font-bold" htmlFor={htmlFor}>
+        <span className="grow">{label}</span>
+        {optional && (
+          <span className="font-normal text-gray-9 dark:text-gray-dark-9">
+            {" "}
+            {t("direktivPage.page.blocks.form.optional")}
+          </span>
+        )}
+      </label>
+      {children}
+      {description && (
+        <div className="basis-full text-sm text-gray-9 dark:text-gray-dark-9">
+          {description}
+        </div>
+      )}
+    </fieldset>
+  );
+};
