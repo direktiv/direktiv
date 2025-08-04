9dacb7b2db06cf8be0785e72a24e436c6186ea51
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-04 09:46:45 +0200

    add Fieldset for form primitives

---
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/utils/FieldSet.tsx
@@ -0,0 +1,31 @@
+import { FC, PropsWithChildren } from "react";
+
+import { twMergeClsx } from "~/util/helpers";
+
+type FieldsetProps = PropsWithChildren & {
+  label: string;
+  description: string;
+  horizontal?: boolean;
+  htmlFor?: string;
+};
+
+export const Fieldset: FC<FieldsetProps> = ({
+  label,
+  description,
+  children,
+  htmlFor,
+  horizontal,
+}) => (
+  <fieldset
+    className={twMergeClsx(
+      "mb-2 flex gap-2",
+      horizontal ? "flex-row-reverse items-center" : "flex-col"
+    )}
+  >
+    <label className="grow text-sm" htmlFor={htmlFor}>
+      {label}
+    </label>
+    {children}
+    <div>{description}</div>
+  </fieldset>
+);
