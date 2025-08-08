b78691dc5f263bb3eb0e2053f7c7b0beb37a0452
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-08 11:06:07 +0200

    render defautl checkbox value

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/FormCheckbox.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/formPrimitives/FormCheckbox.tsx
@@ -1,14 +1,42 @@
 import { Checkbox } from "~/design/Checkbox";
 import { Fieldset } from "./utils/FieldSet";
 import { FormCheckboxType } from "../../../schema/blocks/form/checkbox";
+import { VariableError } from "../../primitives/Variable/Error";
+import { useTranslation } from "react-i18next";
+import { useVariableBooleanResolver } from "../../primitives/Variable/utils/useVariableBooleanResolver";
 
 type FormCheckboxProps = {
   blockProps: FormCheckboxType;
 };
 
 export const FormCheckbox = ({ blockProps }: FormCheckboxProps) => {
+  const { t } = useTranslation();
   const { id, label, description, defaultValue, optional } = blockProps;
   const htmlID = `form-checkbox-${id}`;
+
+  const resolveVariableBoolean = useVariableBooleanResolver();
+
+  const isVariable = defaultValue.type === "variable";
+
+  let value: boolean;
+  if (isVariable) {
+    const resolvedDefaultValue = resolveVariableBoolean(defaultValue.value);
+    if (!resolvedDefaultValue.success) {
+      return (
+        <VariableError
+          value={defaultValue.value}
+          errorCode={resolvedDefaultValue.error}
+        >
+          {t(`direktivPage.error.templateString.${resolvedDefaultValue.error}`)}
+        </VariableError>
+      );
+    }
+
+    value = resolvedDefaultValue.data;
+  } else {
+    value = defaultValue.value;
+  }
+
   return (
     <Fieldset
       label={label}
@@ -17,7 +45,7 @@ export const FormCheckbox = ({ blockProps }: FormCheckboxProps) => {
       horizontal
       optional={optional}
     >
-      <Checkbox defaultChecked={defaultValue} id={htmlID} />
+      <Checkbox defaultChecked={value} id={htmlID} />
     </Fieldset>
   );
 };
