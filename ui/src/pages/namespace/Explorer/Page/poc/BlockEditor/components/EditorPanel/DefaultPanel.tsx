08148ee625c975635b663b3671cfca962b31041d
julia-zielke <julia.zielke@direktiv.io>
2025-08-20 11:43:03 +0200

    Styling DraggablePalette
    Signed-off-by: julia-zielke <julia.zielke@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/EditorPanel/DefaultPanel.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/EditorPanel/DefaultPanel.tsx
@@ -11,20 +11,20 @@ export const DefaultPanel = () => {
   const { blockTypes } = useBlockTypes();
 
   return (
-    <div className="h-[300px] overflow-y-clip border-b-2 border-gray-4 p-3 dark:border-gray-dark-4 sm:h-[calc(100vh-230px)] sm:border-b-0 sm:border-r-2">
+    <div className="h-[300px] overflow-y-clip border-b-2 border-gray-4 dark:border-gray-dark-4 sm:h-[calc(100vh-230px)] sm:border-b-0 sm:border-r-2">
       <Tabs defaultValue="addBlock">
-        <TabsList className="w-full" variant="boxed">
-          <TabsTrigger className="w-full" variant="boxed" value="addBlock">
+        <TabsList className="w-full rounded-none border-b border-gray-5 bg-gray-1 p-5 pb-0 dark:border-gray-dark-5 dark:bg-gray-dark-1">
+          <TabsTrigger className="w-full" value="addBlock">
             <Blocks size={16} />
             {t("direktivPage.blockEditor.generic.addBlockTab")}
           </TabsTrigger>
-          <TabsTrigger className="w-full" variant="boxed" value="settings">
+          <TabsTrigger className="w-full" value="settings">
             <Settings size={16} />
             {t("direktivPage.blockEditor.generic.settingsTab")}
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="addBlock" asChild>
-          <div className="grid grid-cols-3 gap-2 sm:grid-cols-1">
+        <TabsContent className="p-3" value="addBlock" asChild>
+          <div className="grid grid-cols-2 gap-2">
             {blockTypes.map((type, index) => (
               <DraggablePaletteItem
                 key={index}
