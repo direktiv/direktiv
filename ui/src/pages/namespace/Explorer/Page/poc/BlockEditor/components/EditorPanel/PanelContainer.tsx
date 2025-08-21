e7bf726f2472bedb603b07a6cf8804c0224563b1
julia-zielke <julia.zielke@direktiv.io>
2025-08-21 11:09:13 +0200

    Fixed formatting

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/EditorPanel/PanelContainer.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/EditorPanel/PanelContainer.tsx
@@ -1,5 +1,4 @@
-import { FC, PropsWithChildren } from "react";
-
+import { PropsWithChildren } from "react";
 import { twMergeClsx } from "~/util/helpers";
 
 type PanelContainerProps = PropsWithChildren & {
@@ -9,15 +8,13 @@ type PanelContainerProps = PropsWithChildren & {
 export const PanelContainer = ({
   children,
   className,
-}: PanelContainerProps) => {
-  return (
-    <div
-      className={twMergeClsx(
-        "h-[300px] border-b-2 border-gray-4 dark:border-gray-dark-4 sm:h-[calc(100vh-230px)] sm:border-b-0 sm:border-r-2",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
+}: PanelContainerProps) => (
+  <div
+    className={twMergeClsx(
+      "h-[300px] border-b-2 border-gray-4 dark:border-gray-dark-4 sm:h-[calc(100vh-230px)] sm:border-b-0 sm:border-r-2",
+      className
+    )}
+  >
+    {children}
+  </div>
+);
