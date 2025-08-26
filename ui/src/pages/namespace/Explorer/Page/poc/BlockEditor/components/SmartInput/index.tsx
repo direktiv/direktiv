f13c993d2825ab93e6302c576103ef05e989fbb8
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-08-26 16:21:17 +0200

    Create FakeInput component and add toolbar to pop-out editor
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/SmartInput/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/SmartInput/index.tsx
@@ -1,7 +1,9 @@
 import { Check, SquareArrowOutUpRight } from "lucide-react";
 import { Dialog, DialogContent, DialogTrigger } from "~/design/Dialog";
 import { EditorContent, useEditor } from "@tiptap/react";
+import { FC, PropsWithChildren, useState } from "react";
 
+import Alert from "~/design/Alert";
 import Button from "~/design/Button";
 import Document from "@tiptap/extension-document";
 import { InputWithButton } from "~/design/InputWithButton";
@@ -9,8 +11,43 @@ import Paragraph from "@tiptap/extension-paragraph";
 import Placeholder from "@tiptap/extension-placeholder";
 import Text from "@tiptap/extension-text";
 import { twMergeClsx } from "~/util/helpers";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
+
+type FakeInputProps = PropsWithChildren & {
+  wrap?: boolean;
+  sidebar?: boolean;
+  className?: string;
+};
+
+const FakeInput: FC<FakeInputProps> = ({
+  children,
+  wrap,
+  sidebar,
+  className,
+  ...props
+}) => (
+  <div
+    className={twMergeClsx(
+      // Todo:
+      // - consolidate this with the input styling?
+      // - focus ring does not work as expected
+      sidebar && "max-w-[300px]",
+      !wrap && "h-9",
+      "rounded-md border bg-transparent px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
+      "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4",
+      "dark:focus:ring-offset-gray-dark-1",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </div>
+);
+
+const Toolbar: FC<PropsWithChildren> = ({ children }) => (
+  <div className="border-b pb-2">{children}</div>
+);
 
 export const SmartInput = ({
   onChange,
@@ -43,18 +80,7 @@ export const SmartInput = ({
   return (
     <Dialog open={dialog} onOpenChange={setDialog}>
       <InputWithButton>
-        <div
-          className={twMergeClsx(
-            // Todo:
-            // - consolidate this with the input styling?
-            // - focus ring does not work as expected
-            "max-w-[300px]",
-            "h-9 rounded-md border bg-transparent px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
-            "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1"
-          )}
-          onClick={() => editor?.chain().focus().run()}
-        >
+        <FakeInput sidebar>
           {!dialog && (
             <EditorContent
               id={id}
@@ -69,7 +95,7 @@ export const SmartInput = ({
               )}
             />
           )}
-        </div>
+        </FakeInput>
         <DialogTrigger>
           <Button icon variant="ghost" type="button">
             <SquareArrowOutUpRight
@@ -82,18 +108,28 @@ export const SmartInput = ({
       <DialogContent className="min-w-[600px] max-w-[600px] p-4">
         {dialog && (
           <>
-            <EditorContent
-              id={id}
-              editor={editor}
-              className={twMergeClsx(
-                "max-w-full",
-                "min-h-9 text-sm [&>*]:outline-none",
-                "[&_*.is-empty]:before:absolute",
-                "[&_*.is-empty]:before:pointer-events-none",
-                "[&_*.is-empty]:before:content-[attr(data-placeholder)]",
-                "[&_*.is-empty]:before:text-gray-11"
-              )}
-            />
+            <Alert variant="info" className="text-sm">
+              {t("direktivPage.blockEditor.smartInput.templateHelp")}
+            </Alert>
+            <FakeInput wrap className="flex flex-col gap-2 p-2">
+              <Toolbar>
+                <Button variant="outline" type="button">
+                  Variables
+                </Button>
+              </Toolbar>
+              <EditorContent
+                id={id}
+                editor={editor}
+                className={twMergeClsx(
+                  "max-w-full",
+                  "min-h-9 text-sm [&>*]:outline-none",
+                  "[&_*.is-empty]:before:absolute",
+                  "[&_*.is-empty]:before:pointer-events-none",
+                  "[&_*.is-empty]:before:content-[attr(data-placeholder)]",
+                  "[&_*.is-empty]:before:text-gray-11"
+                )}
+              />
+            </FakeInput>
             <div className="flex justify-end">
               <Button
                 type="button"
@@ -102,7 +138,7 @@ export const SmartInput = ({
                 icon
                 onClick={() => setDialog(false)}
               >
-                <Check size="12" className="text-gray-11" />
+                <Check size="12" />
               </Button>
             </div>
           </>
