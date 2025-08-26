eeb302fed3227cee6b13f63db5ebb97ed458943b
julia-zielke <julia.zielke@direktiv.io>
2025-08-26 14:55:01 +0200

    Added Badge for Loop in edit mode
    Signed-off-by: julia-zielke <julia.zielke@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Loop.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Loop.tsx
@@ -1,12 +1,16 @@
 import { Block, BlockPathType } from ".";
+import { FC, PropsWithChildren } from "react";
 import {
   VariableContextProvider,
   useVariablesContext,
 } from "../primitives/Variable/VariableContext";
 
+import Badge from "~/design/Badge";
 import { BlockList } from "./utils/BlockList";
+import { EyeOff } from "lucide-react";
 import { LoopType } from "../../schema/blocks/loop";
 import { VariableError } from "../primitives/Variable/Error";
+import { usePageStateContext } from "../context/pageCompilerContext";
 import { useTranslation } from "react-i18next";
 import { useVariableArrayResolver } from "../primitives/Variable/utils/useVariableArrayResolver";
 
@@ -18,6 +22,7 @@ type LoopProps = {
 export const Loop = ({ blockProps, blockPath }: LoopProps) => {
   const { blocks, data, id } = blockProps;
   const { t } = useTranslation();
+  const { mode } = usePageStateContext();
   const resolveVariableArray = useVariableArrayResolver();
   const parentVariables = useVariablesContext();
 
@@ -35,6 +40,37 @@ export const Loop = ({ blockProps, blockPath }: LoopProps) => {
         })}{" "}
         ({variableArray.error})
       </VariableError>
+    );
+  }
+
+  if (mode === "edit") {
+    const loopSize = variableArray.data.length - 1;
+    const firstLoopItem = variableArray.data[0];
+
+    return (
+      <VariableContextProvider
+        variables={{
+          ...parentVariables,
+          loop: {
+            ...parentVariables.loop,
+            [id]: firstLoopItem,
+          },
+        }}
+      >
+        <BlockList path={blockPath}>
+          {blocks.map((block, blockIndex) => {
+            const path = [...blockPath, blockIndex];
+            return (
+              <Block key={path.join(".")} block={block} blockPath={path} />
+            );
+          })}
+        </BlockList>
+        <LoopItemBadge>
+          {t(`direktivPage.page.blocks.loop.hiddenItems`, {
+            number: loopSize,
+          })}
+        </LoopItemBadge>
+      </VariableContextProvider>
     );
   }
 
@@ -64,3 +100,12 @@ export const Loop = ({ blockProps, blockPath }: LoopProps) => {
     </BlockList>
   );
 };
+
+const LoopItemBadge: FC<PropsWithChildren> = ({ children }) => (
+  <div className="absolute flex flex-col justify-center items-center mt-1 h-[2px] w-full">
+        <Badge className="bg-gray-4 text-black dark:bg-gray-dark-4 dark:text-white">
+          <EyeOff className="mr-2" size={16} />
+          <span>{children}</span>
+        </Badge>
+  </div>
+);
