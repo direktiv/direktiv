0148bcce055b1b8ebc24bcfd3d69984be074ea04
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2025-08-26 14:03:09 +0200

    Add placeholder and fix fake input styling
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/design/Input/index.tsx
+++ ui/src/design/Input/index.tsx
@@ -1,24 +1,6 @@
 import React from "react";
 import { twMergeClsx } from "~/util/helpers";
 
-export const inputClasses = ({
-  type,
-  className,
-}: {
-  type?: React.HTMLInputTypeAttribute;
-  className?: string;
-}) =>
-  twMergeClsx(
-    "flex h-9 w-full rounded-md border bg-transparent px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-    "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
-    "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1",
-    type === "file" &&
-      "p-0 file:mr-5 file:rounded-l-md file:border-0 file:px-6 file:py-2 file:text-sm file:font-medium",
-    type === "file" &&
-      "file:bg-black file:text-white dark:file:bg-white dark:file:text-black",
-    className
-  );
-
 const Input = React.forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
@@ -26,7 +8,16 @@ const Input = React.forwardRef<
   <input
     ref={ref}
     type={type}
-    className={inputClasses({ type, className })}
+    className={twMergeClsx(
+      "flex h-9 w-full rounded-md border bg-transparent px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
+      "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1",
+      type === "file" &&
+        "p-0 file:mr-5 file:rounded-l-md file:border-0 file:px-6 file:py-2 file:text-sm file:font-medium",
+      type === "file" &&
+        "file:bg-black file:text-white dark:file:bg-white dark:file:text-black",
+      className
+    )}
     {...props}
   />
 ));
--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/SmartInput/index.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/components/SmartInput/index.tsx
@@ -2,8 +2,10 @@ import { EditorContent, useEditor } from "@tiptap/react";
 
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
+import Placeholder from "@tiptap/extension-placeholder";
 import Text from "@tiptap/extension-text";
-import { inputClasses } from "~/design/Input";
+import { twMergeClsx } from "~/util/helpers";
+import { useTranslation } from "react-i18next";
 
 export const SmartInput = ({
   onChange,
@@ -14,18 +16,48 @@ export const SmartInput = ({
   value: string;
   id: string;
 }) => {
+  const { t } = useTranslation();
   const editor = useEditor({
-    extensions: [Document, Text, Paragraph],
+    extensions: [
+      Document,
+      Text,
+      Paragraph,
+      Placeholder.configure({
+        placeholder: t(
+          "direktivPage.blockEditor.blockForms.text.contentPlaceholder"
+        ),
+        showOnlyWhenEditable: true,
+        showOnlyCurrent: false, // show even when not focused
+      }),
+    ],
     content: value,
-    onUpdate: () => {
-      const text = editor.getText();
-      onChange(text);
+    onUpdate: ({ editor }) => {
+      onChange(editor.getText());
     },
   });
 
   return (
-    <div className={inputClasses({})}>
-      <EditorContent id={id} editor={editor} />
+    <div
+      className={twMergeClsx(
+        "max-w-[300px]",
+        "h-9 rounded-md border bg-transparent px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "border-gray-4 placeholder:text-gray-8 focus:ring-gray-4 focus:ring-offset-gray-1",
+        "dark:border-gray-dark-4 dark:placeholder:text-gray-dark-8 dark:focus:ring-gray-dark-4 dark:focus:ring-offset-gray-dark-1"
+      )}
+      onClick={() => editor?.chain().focus().run()}
+    >
+      <EditorContent
+        id={id}
+        editor={editor}
+        className={twMergeClsx(
+          "truncate",
+          "min-h-9 max-w-full text-sm [&>*]:outline-none",
+          "[&_*.is-empty]:before:absolute",
+          "[&_*.is-empty]:before:pointer-events-none",
+          "[&_*.is-empty]:before:content-[attr(data-placeholder)]",
+          "[&_*.is-empty]:before:text-gray-11"
+        )}
+      />
     </div>
   );
 };
