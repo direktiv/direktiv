0d8b8ebc7399d59e9cd4bf8b582f3923541ca77b
Stefan Kracht <stefan.kracht@direktiv.io>
2025-08-28 10:34:43 +0200

    add integration tests

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/__tests__/Form/select.test.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/__tests__/Form/select.test.tsx
@@ -32,6 +32,35 @@ afterEach(() => {
 });
 
 describe("select input", () => {
+  test("values can be sourced from a list of strings", async () => {
+    await act(async () => {
+      render(
+        <PageCompiler
+          setPage={setPage}
+          page={createDirektivPageWithForm([
+            {
+              id: "dynamic-select",
+              label: "dynamic select",
+              description: "default value is always two",
+              optional: false,
+              type: "form-select",
+              values: {
+                type: "string-array",
+                value: ["one", "two", "three"],
+              },
+              defaultValue: "",
+            },
+          ])}
+          mode="live"
+        />
+      );
+    });
+    const select = screen.getByRole("combobox", {
+      name: "dynamic select",
+    }) as HTMLSelectElement;
+    expect(select.parentElement?.textContent).toContain("onetwothree");
+  });
+
   test("values can be sourced from a variable", async () => {
     await act(async () => {
       render(
@@ -59,5 +88,75 @@ describe("select input", () => {
       name: "dynamic select",
     }) as HTMLSelectElement;
     expect(select.parentElement?.textContent).toContain("freeproenterprise");
+  });
+
+  test("shows error when using a non array variable", async () => {
+    await act(async () => {
+      render(
+        <PageCompiler
+          setPage={setPage}
+          page={createDirektivPageWithForm([
+            {
+              id: "dynamic-select",
+              label: "dynamic select",
+              description: "default value is always two",
+              optional: false,
+              type: "form-select",
+              values: {
+                type: "variable",
+                value: "query.user.meta",
+              },
+              defaultValue: "",
+            },
+          ])}
+          mode="live"
+        />
+      );
+    });
+    await screen
+      .getByRole("button", {
+        name: "There was an unexpected error",
+      })
+      .click();
+    expect(
+      screen.getByText(
+        "Variable error (query.user.meta): Pointing to a value that is not an array."
+      )
+    );
+  });
+
+  test("shows error when using an array but the values are not strings", async () => {
+    await act(async () => {
+      render(
+        <PageCompiler
+          setPage={setPage}
+          page={createDirektivPageWithForm([
+            {
+              id: "dynamic-select",
+              label: "dynamic select",
+              description: "default value is always two",
+              optional: false,
+              type: "form-select",
+              values: {
+                type: "variable",
+                value: "query.user.data.recentActivity",
+              },
+              defaultValue: "",
+            },
+          ])}
+          mode="live"
+        />
+      );
+    });
+    await screen
+      .getByRole("button", {
+        name: "There was an unexpected error",
+      })
+      .click();
+    expect(
+      screen.getByText(
+        "Variable error (query.user.data.recentActivity): Pointing to a value that is not an array of strings."
+      )
+    );
   });
 });
