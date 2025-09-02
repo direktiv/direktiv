cd97ee3036f9ccee76891c7dc67d301d7693c658
Stefan Kracht <stefan.kracht@direktiv.io>
2025-09-02 14:10:57 +0200

    test more fields

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/__tests__/Form/requiredFields.test.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/__tests__/Form/requiredFields.test.tsx
@@ -16,6 +16,7 @@ import {
 
 import { PageCompiler } from "../..";
 import { setupFormApi } from "./utils";
+import userEvent from "@testing-library/user-event";
 
 const { apiServer } = setupFormApi();
 
@@ -66,7 +67,7 @@ describe("required fields", () => {
       expect(screen.getAllByText("Some required fields are missing (string)"));
     });
 
-    test("shows an error when a required string input field is missing a value", async () => {
+    test("submits the form when a required string input field is filled in", async () => {
       await act(async () => {
         render(
           <PageCompiler
@@ -80,7 +81,459 @@ describe("required fields", () => {
                   optional: false,
                   type: "form-string-input",
                   variant: "text",
-                  defaultValue: "has default value",
+                  defaultValue: "default value",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+
+      await waitFor(() => {
+        expect(screen.getAllByText("The form has been submitted successfully"));
+      });
+    });
+  });
+
+  describe("number input field", () => {
+    test("shows an error when a required number input field is missing a value", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "number",
+                  label: "number input",
+                  description: "",
+                  optional: false,
+                  type: "form-number-input",
+                  defaultValue: {
+                    type: "number",
+                    value: 0,
+                  },
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      // users clear the input field
+      const user = userEvent.setup();
+      await user.clear(
+        screen.getByRole("spinbutton", { name: "number input" })
+      );
+
+      await screen.getByRole("button", { name: "save" }).click();
+      expect(screen.getAllByText("Some required fields are missing (number)"));
+    });
+
+    test("submits the form when a required number input field is filled in", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "number",
+                  label: "number input",
+                  description: "",
+                  optional: false,
+                  type: "form-number-input",
+                  defaultValue: {
+                    type: "number",
+                    value: 0,
+                  },
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+
+      await waitFor(() => {
+        expect(screen.getAllByText("The form has been submitted successfully"));
+      });
+    });
+  });
+
+  describe("date input field", () => {
+    test("shows an error when a required date input field is missing a value", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "date",
+                  label: "date input",
+                  description: "",
+                  optional: false,
+                  type: "form-date-input",
+                  defaultValue: "",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+      expect(screen.getAllByText("Some required fields are missing (date)"));
+    });
+
+    test("submits the form when a required date input field is filled in", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "date",
+                  label: "date input",
+                  description: "",
+                  optional: false,
+                  type: "form-date-input",
+                  defaultValue: "2025-12-24T00:00:00.000Z",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+
+      await waitFor(() => {
+        expect(screen.getAllByText("The form has been submitted successfully"));
+      });
+    });
+  });
+
+  describe("select field", () => {
+    test("shows an error when a required ‚ field is missing a value", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "select",
+                  label: "select",
+                  description: "",
+                  optional: false,
+                  type: "form-select",
+                  values: {
+                    type: "array",
+                    value: ["one", "two", "three"],
+                  },
+                  defaultValue: "",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+      expect(screen.getAllByText("Some required fields are missing (select)"));
+    });
+
+    test("submits the form when a required select field is filled in", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "select",
+                  label: "select",
+                  description: "",
+                  optional: false,
+                  type: "form-select",
+                  values: {
+                    type: "array",
+                    value: ["one", "two", "three"],
+                  },
+                  defaultValue: "one",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+
+      await waitFor(() => {
+        expect(screen.getAllByText("The form has been submitted successfully"));
+      });
+    });
+  });
+
+  describe("select field", () => {
+    test("shows an error when a required select field is missing a value", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "select",
+                  label: "select",
+                  description: "",
+                  optional: false,
+                  type: "form-select",
+                  values: {
+                    type: "array",
+                    value: ["one", "two", "three"],
+                  },
+                  defaultValue: "",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+      expect(screen.getAllByText("Some required fields are missing (select)"));
+    });
+
+    test("submits the form when a required select field is filled in", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "select",
+                  label: "select",
+                  description: "",
+                  optional: false,
+                  type: "form-select",
+                  values: {
+                    type: "array",
+                    value: ["one", "two", "three"],
+                  },
+                  defaultValue: "one",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+
+      await waitFor(() => {
+        expect(screen.getAllByText("The form has been submitted successfully"));
+      });
+    });
+  });
+
+  describe("checkbox field", () => {
+    test("shows an error when a required checkbox field is missing a value", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "checkbox",
+                  label: "checkbox",
+                  description: "my checkbox",
+                  optional: false,
+                  type: "form-checkbox",
+                  defaultValue: {
+                    type: "boolean",
+                    value: false,
+                  },
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+      expect(
+        screen.getAllByText("Some required fields are missing (checkbox)")
+      );
+    });
+
+    test("submits the form when a required checkbox field is filled in", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "checkbox",
+                  label: "checkbox",
+                  description: "my checkbox",
+                  optional: false,
+                  type: "form-checkbox",
+                  defaultValue: {
+                    type: "boolean",
+                    value: true,
+                  },
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+
+      await waitFor(() => {
+        expect(screen.getAllByText("The form has been submitted successfully"));
+      });
+    });
+  });
+
+  describe("textarea field", () => {
+    test("shows an error when a required textarea field is missing a value", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "textarea",
+                  label: "textarea",
+                  description: "",
+                  optional: false,
+                  type: "form-textarea",
+                  defaultValue: "",
+                },
+              ],
+              {
+                id: "save-user",
+                method: "POST",
+                url: "/save-user",
+                requestBody: [],
+              }
+            )}
+            mode="live"
+          />
+        );
+      });
+
+      await screen.getByRole("button", { name: "save" }).click();
+      expect(
+        screen.getAllByText("Some required fields are missing (textarea)")
+      );
+    });
+
+    test("submits the form when a required textarea field is filled in", async () => {
+      await act(async () => {
+        render(
+          <PageCompiler
+            setPage={setPage}
+            page={createDirektivPageWithForm(
+              [
+                {
+                  id: "textarea",
+                  label: "textarea",
+                  description: "",
+                  optional: false,
+                  type: "form-textarea",
+                  defaultValue: "default value",
                 },
               ],
               {
