8fd54ce52bc7b41ed2d53102f130eada258bda76
Stefan Kracht <stefan.kracht@direktiv.io>
2025-09-17 10:56:38 +0200

    render action buttons

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Table/ActionsCell.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/Table/ActionsCell.tsx
@@ -4,33 +4,42 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/design/Dropdown";
-import { TableActionsType, TableType } from "../../../schema/blocks/table";
 
 import { Button } from "../Button";
 import ButtonDesignComponent from "~/design/Button";
 import { MoreVertical } from "lucide-react";
+import { TableActionsType } from "../../../schema/blocks/table";
 import { TableCell as TableCellDesignComponent } from "~/design/Table";
 
 type ActionsCellProps = {
   actions: TableActionsType;
 };
 
-export const ActionsCell = ({ actions }: ActionsCellProps) => (
-  <TableCellDesignComponent className="w-0">
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <ButtonDesignComponent variant="ghost" size="sm" icon>
-          <MoreVertical />
-        </ButtonDesignComponent>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40" align="end">
-        TODO:
-        {/* {actions.map((action, index) => (
-          <DropdownMenuItem key={index}>
-            <Button blockProps={action} as="text" />
-          </DropdownMenuItem>
-        ))} */}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </TableCellDesignComponent>
-);
+export const ActionsCell = ({ actions }: ActionsCellProps) => {
+  const dialogs = actions.blocks.filter((x) => x.type === "dialog");
+
+  return (
+    <TableCellDesignComponent className="w-0">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <ButtonDesignComponent variant="ghost" size="sm" icon>
+            <MoreVertical />
+          </ButtonDesignComponent>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40" align="end">
+          {dialogs.map((dialog, index) => (
+            <DropdownMenuItem key={index}>
+              <Button
+                blockProps={{
+                  type: "button",
+                  label: dialog.trigger.label,
+                }}
+                as="text"
+              />
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </TableCellDesignComponent>
+  );
+};
