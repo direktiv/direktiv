26ce1725501710c454bf1d41bcb46ede57c52ce7
Stefan Kracht <stefan.kracht@direktiv.io>
2025-09-17 09:21:10 +0200

    add table-actions

--- ui/src/pages/namespace/Explorer/Page/poc/schema/blocks/table/index.ts
+++ ui/src/pages/namespace/Explorer/Page/poc/schema/blocks/table/index.ts
@@ -1,5 +1,5 @@
+import { Block, BlockType } from "..";
 import { BlocklessLoop, BlocklessLoopType } from "../loop";
-import { Dialog, DialogType } from "../dialog";
 import { TableColumn, TableColumnType } from "./tableColumn";
 
 import { z } from "zod";
@@ -10,16 +10,26 @@ import { z } from "zod";
  * It is currently possible to extend the schema without updating the type.
  * The schema needs to get the type input to avoid circular dependencies.
  */
-export type TableType = {
-  type: "table";
-  data: BlocklessLoopType;
-  actions: DialogType[];
-  columns: TableColumnType[];
+export type TableActionsType = {
+  type: "table-actions";
+  blocks: BlockType[];
 };
+
+export const TableActions = z.object({
+  type: z.literal("table-actions"),
+  blocks: z.array(Block),
+}) satisfies z.ZodType<TableActionsType>;
 
 export const Table = z.object({
   type: z.literal("table"),
   data: BlocklessLoop,
-  actions: z.array(z.lazy(() => Dialog)),
+  blocks: z.tuple([TableActions, TableActions]),
   columns: z.array(TableColumn),
 }) satisfies z.ZodType<TableType>;
+
+export type TableType = {
+  type: "table";
+  data: BlocklessLoopType;
+  blocks: [TableActionsType, TableActionsType];
+  columns: TableColumnType[];
+};
