a0476485a85b0dc8f2af2aaecbaad61e400f582a
Stefan Kracht <stefan.kracht@direktiv.io>
2025-09-18 11:24:54 +0200

    reuse component

--- ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/ActionForm.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/BlockEditor/Table/ActionForm.tsx
@@ -1,10 +1,8 @@
 import { Dialog, DialogType } from "../../schema/blocks/dialog";
 import FormErrors, { errorsType } from "~/components/FormErrors";
 
-import { Fieldset } from "~/components/Form/Fieldset";
-import { SmartInput } from "../components/SmartInput";
+import { TriggerLabelFieldset } from "../components/TriggerLabelFieldset";
 import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 type ActionFormProps = {
@@ -13,22 +11,12 @@ type ActionFormProps = {
   onSubmit: (data: DialogType) => void;
 };
 
-// TODO:
-// - may use the Fieldset from the dialog
-// - rename this component
-// - problem: this one uses a SmartInput
 export const ActionForm = ({
   defaultValues,
   formId,
   onSubmit,
 }: ActionFormProps) => {
-  const { t } = useTranslation();
-  const {
-    watch,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<DialogType>({
+  const form = useForm<DialogType>({
     resolver: zodResolver(Dialog),
     defaultValues: {
       type: "dialog",
@@ -43,27 +31,18 @@ export const ActionForm = ({
 
   const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.stopPropagation();
-    handleSubmit(onSubmit)(e);
+    form.handleSubmit(onSubmit)(e);
   };
 
   return (
     <form onSubmit={onFormSubmit} id={formId}>
-      {errors && <FormErrors errors={errors as errorsType} className="mb-5" />}
-      <Fieldset
-        label={t(
-          "direktivPage.blockEditor.blockForms.dialog.triggerLabelLabel"
-        )}
-        htmlFor="label"
-      >
-        <SmartInput
-          value={watch("trigger.label")}
-          onUpdate={(value) => setValue("trigger.label", value)}
-          id="label"
-          placeholder={t(
-            "direktivPage.blockEditor.blockForms.dialog.triggerLabelPlaceholder"
-          )}
+      {form.formState.errors && (
+        <FormErrors
+          errors={form.formState.errors as errorsType}
+          className="mb-5"
         />
-      </Fieldset>
+      )}
+      <TriggerLabelFieldset form={form} />
     </form>
   );
 };
