38d9f3e6b655bc5f71c3ec0173570e1b9a13614e
sir-hassan <yassir.hassan@direktiv.io>
2025-09-30 13:39:31 +0200

    add paginator to instances list
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- internal/engine/databus/cache.go
+++ internal/engine/databus/cache.go
@@ -10,12 +10,12 @@ import (
 
 type StatusCache struct {
 	mu    sync.RWMutex
-	items map[uuid.UUID]engine.InstanceStatus // key: orderID
+	items []engine.InstanceStatus // key: orderID
 }
 
 func NewStatusCache() *StatusCache {
 	return &StatusCache{
-		items: map[uuid.UUID]engine.InstanceStatus{},
+		items: make([]engine.InstanceStatus, 0),
 	}
 }
 
@@ -23,10 +23,18 @@ func (c *StatusCache) Upsert(s *engine.InstanceStatus) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	// keep only the newest by HistorySequence
-	if cur, ok := c.items[s.InstanceID]; !ok || s.HistorySequence >= cur.HistorySequence {
-		cp := s.Clone()
-		c.items[s.InstanceID] = *cp
+	for i, v := range c.items {
+		if v.InstanceID != s.InstanceID {
+			continue
+		}
+		if v.HistorySequence < s.HistorySequence {
+			cp := s.Clone()
+			c.items[i] = *cp
+			return
+		}
 	}
+	cp := s.Clone()
+	c.items = append(c.items, *cp)
 }
 
 func (c *StatusCache) Snapshot(filterNamespace string, filterInstanceID uuid.UUID) []*engine.InstanceStatus {
@@ -37,6 +45,12 @@ func (c *StatusCache) Snapshot(filterNamespace string, filterInstanceID uuid.UUI
 func (c *StatusCache) SnapshotPage(filterNamespace string, filterInstanceID uuid.UUID, limit int, offset int) ([]*engine.InstanceStatus, int) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
+	//line := ""
+	//for k, _ := range c.items {
+	//	line += fmt.Sprintf(" ---> %s", c.items[k].InstanceID.String()[0:3])
+	//}
+	//fmt.Printf("CACHE: %s\n", line)
+
 	out := make([]*engine.InstanceStatus, 0, len(c.items))
 
 	total := 0
@@ -67,9 +81,11 @@ func (c *StatusCache) SnapshotPage(filterNamespace string, filterInstanceID uuid
 func (c *StatusCache) DeleteNamespace(name string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	for k, v := range c.items {
-		if name == v.Namespace {
-			delete(c.items, k)
+	cp := make([]engine.InstanceStatus, 0, len(c.items))
+	for _, v := range c.items {
+		if name != v.Namespace {
+			cp = append(cp, v)
 		}
 	}
+	c.items = cp
 }
