0f0a37fa187e1e6e3443ff43ce3e1b31c565ec72
julia-zielke <julia.zielke@direktiv.io>
2025-10-07 13:58:54 +0200

    render a dropzone before 'firstChild' blocks
    Signed-off-by: julia-zielke <julia.zielke@direktiv.io>

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/BlockWrapper.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/BlockWrapper.tsx
@@ -10,7 +10,12 @@ import {
   useRef,
   useState,
 } from "react";
-import { findAncestor, incrementPath, pathsEqual } from "../../context/utils";
+import {
+  findAncestor,
+  incrementPath,
+  isFirstChild,
+  pathsEqual,
+} from "../../context/utils";
 import {
   usePage,
   usePageStateContext,
@@ -127,6 +132,12 @@ const EditorBlockWrapper = ({
 
   return (
     <>
+      {isFirstChild(blockPath) && (
+        <Dropzone
+          payload={{ ...dropzonePayload, targetPath: blockPath }}
+          validate={validateDropzone}
+        />
+      )}
       <SortableItem
         payload={{
           type: "move",
