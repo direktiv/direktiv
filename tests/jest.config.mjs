b5ec098a561e825f8afd485dcfe56c6a92f61609
sir-hassan <yassir.hassan@direktiv.io>
2025-10-16 11:39:39 +0200

    fix e2e tests format tooling
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- tests/jest.config.mjs
+++ tests/jest.config.mjs
@@ -2,8 +2,4 @@ export default {
 	testEnvironment: 'node',
 	testTimeout: 300000,
 	transform: {}, // disable babel unless you add one
-	watchPlugins: [
-		'jest-watch-typeahead/filename',
-		'jest-watch-typeahead/testname',
-	],
 }
