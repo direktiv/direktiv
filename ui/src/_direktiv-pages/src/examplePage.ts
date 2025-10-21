be6035acc0d641ce364e5acf563dcd8cb34817bd
Stefan Kracht <stefan.kracht@direktiv.io>
2025-10-21 13:55:23 +0200

    update folder structure

--- ui/src/_direktiv-pages/src/examplePage.ts
+++ ui/src/_direktiv-pages/src/examplePage.ts
@@ -1,22 +1,6 @@
-import "../App.css";
-import "../i18n";
+import { DirektivPagesType } from "~/pages/namespace/Explorer/Page/poc/schema";
 
-import { Block } from "../pages/namespace/Explorer/Page/poc/PageCompiler/Block";
-import { BlockList } from "../pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/BlockList";
-import { DirektivPagesType } from "../pages/namespace/Explorer/Page/poc/schema";
-import { EditorPanelLayoutProvider } from "../pages/namespace/Explorer/Page/poc/BlockEditor/EditorPanelProvider";
-import { PageCompilerContextProvider } from "../pages/namespace/Explorer/Page/poc/PageCompiler/context/pageCompilerContext";
-import { QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import { Toaster } from "~/design/Toast";
-import { createRoot } from "react-dom/client";
-import queryClient from "../util/queryClient";
-import { setPage } from "../pages/namespace/Explorer/Page/poc/PageCompiler/__tests__/utils";
-
-const appContainer = document.getElementById("root");
-if (!appContainer) throw new Error("Root element not found");
-
-const page: DirektivPagesType = {
+export const page: DirektivPagesType = {
   direktiv_api: "page/v1",
   type: "page",
   blocks: [
@@ -573,20 +557,3 @@ const page: DirektivPagesType = {
     },
   ],
 };
-
-createRoot(appContainer).render(
-  <React.StrictMode>
-    <PageCompilerContextProvider setPage={setPage} page={page} mode="live">
-      <QueryClientProvider client={queryClient}>
-        <EditorPanelLayoutProvider>
-          <BlockList path={[]}>
-            {page.blocks.map((block, index) => (
-              <Block key={index} block={block} blockPath={[index]} />
-            ))}
-          </BlockList>
-        </EditorPanelLayoutProvider>
-      </QueryClientProvider>
-      <Toaster />
-    </PageCompilerContextProvider>
-  </React.StrictMode>
-);
---
+++ ui/src/_direktiv-pages/src/pages.tsx
@@ -0,0 +1,34 @@
+import "../../App.css";
+import "../../i18n";
+
+import { Block } from "../../pages/namespace/Explorer/Page/poc/PageCompiler/Block";
+import { BlockList } from "../../pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/BlockList";
+import { EditorPanelLayoutProvider } from "../../pages/namespace/Explorer/Page/poc/BlockEditor/EditorPanelProvider";
+import { PageCompilerContextProvider } from "../../pages/namespace/Explorer/Page/poc/PageCompiler/context/pageCompilerContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { Toaster } from "~/design/Toast";
+import { createRoot } from "react-dom/client";
+import { page } from "./examplePage";
+import queryClient from "../../util/queryClient";
+import { setPage } from "../../pages/namespace/Explorer/Page/poc/PageCompiler/__tests__/utils";
+
+const appContainer = document.getElementById("root");
+if (!appContainer) throw new Error("Root element not found");
+
+createRoot(appContainer).render(
+  <React.StrictMode>
+    <PageCompilerContextProvider setPage={setPage} page={page} mode="live">
+      <QueryClientProvider client={queryClient}>
+        <EditorPanelLayoutProvider>
+          <BlockList path={[]}>
+            {page.blocks.map((block, index) => (
+              <Block key={index} block={block} blockPath={[index]} />
+            ))}
+          </BlockList>
+        </EditorPanelLayoutProvider>
+      </QueryClientProvider>
+      <Toaster />
+    </PageCompilerContextProvider>
+  </React.StrictMode>
+);
