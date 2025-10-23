ed1e5f26e24c1f30b6f835546357e5721e278ead
Stefan Kracht <stefan.kracht@direktiv.io>
2025-10-23 09:26:43 +0200

    final version

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/ParsingError.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/ParsingError.tsx
@@ -2,10 +2,11 @@ import { Popover, PopoverContent, PopoverTrigger } from "~/design/Popover";
 
 import Alert from "~/design/Alert";
 import Button from "~/design/Button";
+import { ButtonBar } from "~/design/ButtonBar";
 import { Card } from "~/design/Card";
 import { PropsWithChildren } from "react";
+import { RefreshCcw } from "lucide-react";
 import { StopPropagation } from "~/components/StopPropagation";
-import { TriangleAlert } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
 type ParsingErrorProps = PropsWithChildren<{
@@ -21,31 +22,31 @@ export const ParsingError = ({
   const { t } = useTranslation();
   return (
     <Popover>
-      <StopPropagation>
-        <PopoverTrigger asChild>
-          <Button variant="destructive" size="sm" icon aria-label={title}>
-            <TriangleAlert />
-          </Button>
-        </PopoverTrigger>
-      </StopPropagation>
+      <ButtonBar>
+        <StopPropagation>
+          <PopoverTrigger asChild>
+            <Button variant="destructive" size="sm" aria-label={title}>
+              {t("direktivPage.page.error.label")}
+            </Button>
+          </PopoverTrigger>
+        </StopPropagation>
+        {resetErrorBoundary && (
+          <StopPropagation>
+            <Button
+              variant="destructive"
+              size="sm"
+              icon
+              onClick={resetErrorBoundary}
+            >
+              <RefreshCcw />
+            </Button>
+          </StopPropagation>
+        )}
+      </ButtonBar>
 
       <StopPropagation>
         <PopoverContent className="flex w-[600px] flex-col gap-5 p-5">
-          <Alert variant="error">
-            <div className="flex">
-              <span className="grow">{title}</span>
-
-              {resetErrorBoundary && (
-                <Button
-                  variant="destructive"
-                  size="sm"
-                  onClick={resetErrorBoundary}
-                >
-                  {t("direktivPage.page.error.retry")}
-                </Button>
-              )}
-            </div>
-          </Alert>
+          <Alert variant="error">{title}</Alert>
           {children && <Card className="overflow-scroll p-5">{children}</Card>}
         </PopoverContent>
       </StopPropagation>
