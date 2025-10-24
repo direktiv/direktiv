1b34d1fad9248e53839cb758b994472596dac415
Stefan Kracht <stefan.kracht@direktiv.io>
2025-10-24 14:28:32 +0200

    make error self healing

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/ParsingError.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/ParsingError.tsx
@@ -1,55 +1,70 @@
 import { Popover, PopoverContent, PopoverTrigger } from "~/design/Popover";
+import { PropsWithChildren, memo, useEffect } from "react";
 
 import Alert from "~/design/Alert";
 import Button from "~/design/Button";
 import { ButtonBar } from "~/design/ButtonBar";
 import { Card } from "~/design/Card";
-import { PropsWithChildren } from "react";
-import { RefreshCcw } from "lucide-react";
+import { DirektivPagesType } from "../../../schema";
 import { StopPropagation } from "~/components/StopPropagation";
-import { useTranslation } from "react-i18next";
+import { TriangleAlert } from "lucide-react";
+import { usePage } from "../../context/pageCompilerContext";
 
 type ParsingErrorProps = PropsWithChildren<{
   title: string;
+  page?: DirektivPagesType;
   resetErrorBoundary?: () => void;
 }>;
 
-export const ParsingError = ({
-  title,
-  resetErrorBoundary,
-  children,
-}: ParsingErrorProps) => {
-  const { t } = useTranslation();
-  return (
-    <Popover>
-      <ButtonBar>
-        <StopPropagation>
-          <PopoverTrigger asChild>
-            <Button variant="destructive" size="sm" aria-label={title}>
-              {t("direktivPage.page.error.label")}
-            </Button>
-          </PopoverTrigger>
-        </StopPropagation>
-        {resetErrorBoundary && (
-          <StopPropagation>
-            <Button
-              variant="destructive"
-              size="sm"
-              icon
-              onClick={resetErrorBoundary}
-            >
-              <RefreshCcw />
-            </Button>
-          </StopPropagation>
-        )}
-      </ButtonBar>
+/**
+ * memo() with a custom compare function that always returns true will ensure that this component
+ * never rerenders when the page prop changes meaning that the page prop always contains the page
+ * that caused the error. The usePage hook is used to compare the current page with the page that
+ * caused the error to determine if the error boundary should be reset.
+ *
+ * Resetting in this case means that the component tree beneath the error boundary will be rendered
+ * again which could either solve the error or mount this error boundary again.
+ */
+export const ParsingError = memo(
+  ({
+    title,
+    page: pageWithError,
+    resetErrorBoundary,
+    children,
+  }: ParsingErrorProps) => {
+    const pageWithErrorStringified = JSON.stringify(pageWithError);
+    const currentPage = usePage();
+    const currentPageStringified = JSON.stringify(currentPage);
 
-      <StopPropagation>
-        <PopoverContent className="flex w-[600px] flex-col gap-5 p-5">
-          <Alert variant="error">{title}</Alert>
-          {children && <Card className="overflow-scroll p-5">{children}</Card>}
-        </PopoverContent>
-      </StopPropagation>
-    </Popover>
-  );
-};
+    useEffect(() => {
+      if (pageWithErrorStringified !== currentPageStringified) {
+        resetErrorBoundary?.();
+      }
+    }, [currentPageStringified, pageWithErrorStringified, resetErrorBoundary]);
+
+    return (
+      <Popover>
+        <ButtonBar>
+          <StopPropagation>
+            <PopoverTrigger asChild>
+              <Button variant="destructive" size="sm" icon aria-label={title}>
+                <TriangleAlert />
+              </Button>
+            </PopoverTrigger>
+          </StopPropagation>
+        </ButtonBar>
+        <StopPropagation>
+          <PopoverContent className="flex w-[600px] flex-col gap-5 p-5">
+            <Alert variant="error">{title}</Alert>
+            {children && (
+              <Card className="overflow-scroll p-5">{children}</Card>
+            )}
+          </PopoverContent>
+        </StopPropagation>
+      </Popover>
+    );
+  },
+  () => true
+);
+
+ParsingError.displayName = "ParsingError";
--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/SuspenseBoundary.tsx
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/Block/utils/SuspenseBoundary.tsx
@@ -2,10 +2,12 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Loading } from "./Loading";
 import { ParsingError } from "./ParsingError";
 import { Suspense } from "react";
+import { usePage } from "../../context/pageCompilerContext";
 import { useTranslation } from "react-i18next";
 
 const SuspenseBoundary = ({ children }: { children: React.ReactNode }) => {
   const { t } = useTranslation();
+  const page = usePage();
   return (
     <Suspense fallback={<Loading />}>
       <ErrorBoundary
@@ -13,6 +15,7 @@ const SuspenseBoundary = ({ children }: { children: React.ReactNode }) => {
           <ParsingError
             title={t("direktivPage.error.genericError")}
             resetErrorBoundary={resetErrorBoundary}
+            page={page}
           >
             {error.message}
           </ParsingError>
