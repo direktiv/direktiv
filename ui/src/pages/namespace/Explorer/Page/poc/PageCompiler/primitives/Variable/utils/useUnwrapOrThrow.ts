ccb6de06f5560816de7193a80255b21c8200ecd9
Stefan Kracht <stefan.kracht@direktiv.io>
2025-11-04 15:59:05 +0100

    add comment

--- ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/primitives/Variable/utils/useUnwrapOrThrow.ts
+++ ui/src/pages/namespace/Explorer/Page/poc/PageCompiler/primitives/Variable/utils/useUnwrapOrThrow.ts
@@ -2,6 +2,14 @@ import { AllVariableErrors } from "./errors";
 import { ValidationResult } from "./types";
 import { useTranslation } from "react-i18next";
 
+/**
+ * Hook that returns a function to unwrap validation results or
+ * throw translated errors.
+ *
+ * returns a function that takes a ValidationResult and variable
+ * name, returning the unwrapped data if successful or throwing
+ * a translated error if validation failed.
+ */
 export const useUnwrapOrThrow = () => {
   const { t } = useTranslation();
   return <T, E extends AllVariableErrors>(
