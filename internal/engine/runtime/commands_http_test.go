a9e5e673889156266dd64842e0bb92c0d686b4c1
sir-hassan <yassir.hassan@direktiv.io>
2025-11-14 12:26:37 +0100

    Add tests for actions fetch()
    Signed-off-by: sir-hassan <yassir.hassan@direktiv.io>

--- internal/engine/runtime/commands_http_test.go
+++ internal/engine/runtime/commands_http_test.go
@@ -82,3 +82,65 @@ func TestHttpRequest(t *testing.T) {
 	}, runtime.NoOnFinish, runtime.NoOnTransition, runtime.NoOnAction)
 	require.NoError(t, err)
 }
+
+func TestHttpFetch(t *testing.T) {
+	// Create container request
+	req := testcontainers.ContainerRequest{
+		Image:        "mendhak/http-https-echo:latest",
+		ExposedPorts: []string{"8080/tcp", "8443/tcp"},
+		WaitingFor:   wait.ForListeningPort("8080/tcp"), // wait until port is ready
+	}
+
+	// Start the container
+	container, err := testcontainers.GenericContainer(t.Context(), testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	require.NoError(t, err)
+	defer container.Terminate(t.Context())
+
+	mappedPort, err := container.MappedPort(t.Context(), "8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	script := `
+		function start() {
+			log(now().format("2006-01-02"))
+			var r = fetch("` + fmt.Sprintf("http://localhost:%s", mappedPort.Port()) + `", {
+				 method: "GET",
+				 headers: {
+					"Content-Type": "application/json",
+					"Header1": "whatever",
+					"X-Custom-Header": "customValue",
+				},
+				body: JSON.stringify({
+					key: "value",
+				}),
+			})
+			r.then(data => {
+				data.headers.Etag = undefined
+				data.headers.Date = undefined
+				log('Data:', JSON.stringify(data))
+				return finish(data)
+			})
+			.catch(error => {throw(error)});
+		}
+	`
+	var result []byte
+	onFinish := func(output []byte) error {
+		fmt.Println(">>>>>>>>>", string(output))
+		result = output
+		return nil
+	}
+	err = runtime.ExecScript(context.Background(), &runtime.Script{
+		InstID:   uuid.New(),
+		Text:     script,
+		Mappings: "",
+		Input:    "{}",
+		Fn:       "start",
+	}, onFinish, runtime.NoOnTransition, runtime.NoOnAction)
+
+	require.NoError(t, err)
+	require.Equal(t, "Hello", result)
+}
