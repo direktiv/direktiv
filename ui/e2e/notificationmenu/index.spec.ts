fac586aea8ec1906e11a8cc80b062ac68013070c
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2026-01-22 17:45:34 +0100

    Fix notification menu test
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/e2e/notificationmenu/index.spec.ts
+++ ui/e2e/notificationmenu/index.spec.ts
@@ -36,7 +36,7 @@ test("Notification Bell has an inactive state by default", async ({ page }) => {
   ).toMatch(/You do not have any notifications./);
 });
 
-test("Notification Bell updates depending on the count of Notification Messages", async ({
+test("Notification Bell shows dot for uninitialized secrets", async ({
   page,
 }) => {
   await createFile({
@@ -51,40 +51,36 @@ test("Notification Bell updates depending on the count of Notification Messages"
     waitUntil: "networkidle",
   });
 
-  const notificationBell = page.getByTestId("notification-bell").nth(1);
-
   await expect(
-    notificationBell,
+    page.getByTestId("notification-bell").nth(1),
     "it renders the Notification Bell"
   ).toBeVisible();
+
+  await page.waitForTimeout(10000);
+  await page.reload({ waitUntil: "networkidle" });
 
   expect(
     page.getByTestId("notification-indicator").nth(1),
     "the indicator for new messages is visible"
   ).toBeVisible();
 
-  await notificationBell.click();
-  const notificationText = page.getByTestId("notification-text");
+  await page.getByTestId("notification-bell").nth(1).click();
 
   expect(
-    await notificationText.textContent(),
+    await page.getByTestId("notification-text").textContent(),
     "the modal should now display 'You have 2 uninitialized secrets.'"
   ).toMatch(/You have 2 uninitialized secrets./);
 
   await page.goto(`/n/${namespace}/settings`);
 
-  const initialize_secret1 = page
-    .getByRole("cell", { name: "ACCESS_KEY Initialize secret" })
-    .getByRole("button");
-  const initialize_secret2 = page
-    .getByRole("cell", { name: "ACCESS_SECRET Initialize secret" })
-    .getByRole("button");
-
-  await initialize_secret1.click();
+  await page
+    .getByRole("cell", { name: "one Initialize secret" })
+    .getByRole("button")
+    .click();
 
   await page.locator("textarea").fill("abc");
   await page.getByRole("button", { name: "Save" }).click();
-  await notificationBell.click();
+  await page.getByTestId("notification-bell").nth(1).click();
 
   expect(
     page.getByTestId("notification-indicator").nth(1),
@@ -92,11 +88,14 @@ test("Notification Bell updates depending on the count of Notification Messages"
   ).toBeVisible();
 
   expect(
-    await notificationText.textContent(),
+    await page.getByTestId("notification-text").textContent(),
     "the modal should now display 'You have 1 uninitialized secret.'"
   ).toMatch("You have 1 uninitialized secret.");
 
-  await initialize_secret2.click();
+  await page
+    .getByRole("cell", { name: "two Initialize secret" })
+    .getByRole("button")
+    .click();
 
   await page.locator("textarea").fill("123");
   await page.getByRole("button", { name: "Save" }).click();
@@ -106,10 +105,10 @@ test("Notification Bell updates depending on the count of Notification Messages"
     "the indicator for new messages is NOT visible"
   ).toHaveCount(0);
 
-  await notificationBell.click();
+  await page.getByTestId("notification-bell").nth(1).click();
 
   expect(
-    await notificationText.textContent(),
+    await page.getByTestId("notification-text").textContent(),
     "the modal should now display 'You do not have any notifications.'"
   ).toMatch(/You do not have any notifications./);
 });
