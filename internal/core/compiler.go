18866bc25c62b7d063f054a2125c584086d0b056
RamiFerjaniDev <e.rami.ferjani@gmail.com>
2026-01-28 14:13:05 +0100

    auth added
    # Conflicts:
    #	internal/core/compiler.go
    #	internal/engine/runtime/runtime_action.go

--- internal/core/compiler.go
+++ internal/core/compiler.go
@@ -12,12 +12,11 @@ const (
 )
 
 type ActionConfig struct {
-	Type  string
-	Cmd   string
-	Size  string
-	Image string
-	Envs  []EnvironmentVariable
-
+	Type    string
+	Cmd     string
+	Size    string
+	Image   string
+	Envs    []EnvironmentVariable
 	Retries int
 
 	// service file specific
