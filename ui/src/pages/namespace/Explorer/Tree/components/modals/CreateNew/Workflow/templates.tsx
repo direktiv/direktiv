b02918fdae77ec2d0d0a90f5db246b52ab443126
Sebastian Armbrust <sebastian.armbrust@direktiv.io>
2026-02-10 14:32:24 +0100

    Add workflow examples
    Signed-off-by: Sebastian Armbrust <sebastian.armbrust@direktiv.io>

--- ui/src/pages/namespace/Explorer/Tree/components/modals/CreateNew/Workflow/templates.tsx
+++ ui/src/pages/namespace/Explorer/Tree/components/modals/CreateNew/Workflow/templates.tsx
@@ -1,19 +1,47 @@
-const tsdemo = {
-  name: "tsdemo",
-  data: `// Simple example workflow that validates type of input
+const hello = {
+  name: "hello",
+  data: `const flow: FlowDefinition = {
+  type: "default",
+  timeout: "PT30S",
+  state: "stateFirst",
+};
+
+function stateFirst(): StateFunction<unknown> {
+  return finish({ data: "hello world" })  
+}
+`,
+};
+
+const input = {
+  name: "input",
+  data: `// a simple workflow example demonstrating flow from one state
+// to the next, failing with an error, and typescript evaluation
+// of the input data. Expects an input payload like
+// { "data": "foo" } that it will evaluate, or fail.
+
+// workflow must include a flow definition
 const flow: FlowDefinition = {
   type: "default",
   timeout: "PT30S",
   state: "stateFirst",
 };
 
-const error = 'input must contain { "data": "string" or number }'
+// you can define variables
+const error = 'input must contain { "data": "string" or number }';
 
+// a regular typescript function that you can reference elsewhere
+function formatMessage(data: string | number, type: string) {
+  return { message: \`\${data} is a \${type}\` };
+}
+
+// define states that the workflow will progress through
 function stateFirst(input): StateFunction<unknown> {
   const { data } = input;
   if (!data) {
-    return finish({ error });
+    // this will fail the workflow
+    throw Error(error);
   }
+  // a state must return a transition() or finish()
   return transition(stateSecond, data);
 }
 
@@ -21,14 +49,90 @@ function stateSecond(data): StateFunction<unknown> {
   const type = typeof data;
   if (type === "string" || type === "number") {
     const message = formatMessage(data, type);
-    return finish(message)
+    return finish(message);
   }
-  return finish({ error })
+  // a state must return a transition() or finish()
+  return finish({ error });
 }
-
-function formatMessage(data: string | number, type: string) {
-  return { message: \`\${data} is a \${type}\` }
+`,
 };
+
+const actions = {
+  name: "actions",
+  data: `const flow: FlowDefinition = {
+  type: "default",
+  timeout: "PT30S",
+  state: "stateFirst",
+};
+
+const echo = generateAction({
+  image: "mendhak/http-https-echo",
+  type: "workflow",
+  size: "medium",
+  retries: 3,
+  envs: [
+    {
+      name: "MESSAGE",
+      value: "foo",
+    },
+  ],
+});
+
+function stateFirst(input): StateFunction<unknown> {
+  const response = echo(input);
+
+  return finish({ response });
+}
+`,
+};
+
+const secrets = {
+  name: "secrets",
+  data: `const flow: FlowDefinition = {
+  type: "default",
+  timeout: "PT30S",
+  state: "stateFirst",
+};
+
+function stateFirst(): StateFunction<unknown> {
+  return finish({ data: "hello world" })  
+}
+`,
+};
+
+const variables = {
+  name: "variables",
+  data: `const flow: FlowDefinition = {
+  type: "default",
+  timeout: "PT30S",
+  state: "stateFirst",
+};
+
+function stateFirst(): StateFunction<unknown> {
+  if (true) {
+    throw Error("this was set up to fail");
+  }
+
+  return finish("unreachable");
+}
+`,
+};
+
+const error = {
+  name: "hello",
+  data: `const flow: FlowDefinition = {
+  type: "default",
+  timeout: "PT30S",
+  state: "stateFirst",
+};
+
+function stateFirst(): StateFunction<unknown> {
+  if (true) {
+    throw Error("this was set up to fail");
+  }
+
+  return finish("unreachable");
+}
 `,
 };
 
@@ -57,6 +161,6 @@ states:
 `,
 };
 
-const templates = [tsdemo] as const;
+const templates = [hello, input, actions, secrets, variables, error] as const;
 
 export default templates;
