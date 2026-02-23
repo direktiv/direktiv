476a9e4140358ffb5215ba8411ddc65cd64c3ca9
Stefan Kracht <stefan.kracht@direktiv.io>
2026-02-23 10:21:58 +0100

    add test for service list

--- ui/e2e/explorer/workflow/services.spec.ts
+++ ui/e2e/explorer/workflow/services.spec.ts
@@ -7,6 +7,7 @@ import { simpleWorkflow, workflowWithService } from "e2e/utils/workflows";
 import { createFile } from "e2e/utils/files";
 import { createInstance } from "~/api/instances/mutate/create";
 import { faker } from "@faker-js/faker";
+import { findServiceWithApiRequest } from "e2e/services/utils";
 
 let namespace = "";
 
@@ -36,5 +37,107 @@ test("Workflow service list is empty by default", async ({ page }) => {
   await expect(
     page.getByText("No services exist yet"),
     "it renders an empy list of services"
+  ).toBeVisible();
+});
+
+test("Workflow service list shows all services mounted by the workflow", async ({
+  page,
+}) => {
+  const workflowName = faker.system.commonFileName("wf.ts");
+  const workflowFile = await createFile({
+    name: workflowName,
+    namespace,
+    type: "workflow",
+    content: workflowWithService,
+    mimeType: "application/x-typescript",
+  });
+
+  await createInstance({
+    urlParams: {
+      baseUrl: process.env.PLAYWRIGHT_UI_BASE_URL,
+      namespace,
+      path: workflowName,
+    },
+  });
+
+  await expect
+    .poll(
+      async () =>
+        await findServiceWithApiRequest({
+          namespace,
+          match: (service) =>
+            service.filePath === workflowFile.data.path &&
+            (service.conditions ?? []).some(
+              (c) => c.type === "Available" && c.status === "True"
+            ),
+        }),
+      {
+        timeout: 50000,
+        message: "the service in the backend is in state Available",
+      }
+    )
+    .toBeTruthy();
+
+  await page.goto(
+    `/n/${namespace}/explorer/workflow/services/list/${workflowName}`,
+    {
+      waitUntil: "networkidle",
+    }
+  );
+
+  await expect(
+    page.getByTestId("service-row"),
+    "it renders one row of services"
+  ).toHaveCount(1);
+
+  await expect(
+    page
+      .getByTestId("service-row")
+      .getByRole("link", { name: workflowFile.data.path }),
+    "it renders the link to the service file"
+  ).toBeVisible();
+
+  await expect(
+    page.getByTestId("service-row").filter({ hasText: "Available" }),
+    "it renders the Available status of the service"
+  ).toBeVisible();
+
+  await expect(
+    page
+      .getByTestId("service-row")
+      .locator("a")
+      .filter({ hasText: "1 environment variable" }),
+    "it renders the environment variable count"
+  ).toBeVisible();
+
+  await page
+    .getByTestId("service-row")
+    .locator("a")
+    .filter({ hasText: "1 environment variable" })
+    .hover();
+
+  await expect(
+    page.getByText("MY_ENV_VAR=env-var-value"),
+    "it shows the environment variable in a tooltip"
+  ).toBeVisible();
+
+  await expect(
+    page.getByTestId("service-row").getByRole("cell", {
+      name: "direktiv/request:v4",
+      exact: true,
+    }),
+    "it renders the image name of the service"
+  ).toBeVisible();
+
+  await expect(
+    page
+      .getByTestId("service-row")
+      .getByRole("cell", { name: "0", exact: true }),
+    "it renders the scale of the service"
+  ).toBeVisible();
+
+  await expect(
+    page.getByTestId("service-row").getByRole("cell", { name: "small" }),
+    "it renders the size of the service"
   ).toBeVisible();
 });
