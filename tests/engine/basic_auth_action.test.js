8517635d3a541a150dd47165e5da60034204b203
RamiFerjaniDev <e.rami.ferjani@gmail.com>
2026-02-26 12:52:47 +0100

    added auth test file
    Signed-off-by: RamiFerjaniDev <e.rami.ferjani@gmail.com>

---
+++ tests/engine/basic_auth_action.test.js
@@ -0,0 +1,84 @@
+import {beforeAll, describe, expect, it} from '@jest/globals'
+
+import {basename} from 'path'
+import {btoa} from 'js-base64'
+import common from '../common'
+import {fileURLToPath} from 'url'
+import helpers from '../common/helpers'
+import request from '../common/request'
+
+const namespace = helpers.randomLowercaseString(3) + '-' + basename(fileURLToPath(import.meta.url))
+
+describe('Test js engine', () => {
+	beforeAll(helpers.deleteAllNamespaces)
+	helpers.itShouldCreateNamespace(it, expect, namespace)
+
+	const testCases = [{
+		name: 'basicAction.wf.ts',
+		input: {foo: 'bar'},
+		wantOutput: JSON.stringify({foo: 'bar', input: {foo: 'bar'}}),
+		wantErrorMessage: null,
+		wantStatus: 'complete',
+		file: `
+            var echo = generateAction({
+                type: "workflow",
+                size: "medium",
+                image: "ramiferdocker/auth-echo:latest",
+                auth: { username: "user", password: "pass" },
+            });
+            function stateOne(payload) {
+                let result = echo({ foo: "bar", input: payload });
+                return finish(result);
+            }
+            `,
+	}, {
+		name: 'noAuth.wf.ts',
+		input: {foo: 'bar'},
+		wantOutput: null,
+		wantErrorMessage: expect.stringContaining('giving up after 1 attempt(s)'),
+		wantStatus: 'failed',
+		file: `
+            var echo = generateAction({
+                type: "workflow",
+                size: "medium",
+                image: "ramiferdocker/auth-echo:latest",
+            });
+            function stateOne(payload) {
+                let result = echo({ foo: "bar", input: payload });
+                return finish(result);
+            }
+            `,
+	}, {
+		name: 'wrongAuth.wf.ts',
+		input: {foo: 'bar'},
+		wantOutput: null,
+		wantErrorMessage: expect.stringContaining('giving up after 1 attempt(s)'),
+		wantStatus: 'failed',
+		file: `
+            var echo = generateAction({
+                type: "workflow",
+                size: "medium",
+                image: "ramiferdocker/auth-echo:latest",
+                auth: { username: "wrong", password: "creds" },
+            });
+            function stateOne(payload) {
+                let result = echo({ foo: "bar", input: payload });
+                return finish(result);
+            }
+            `,
+	},]
+
+	for (let i = 0; i < testCases.length; i++) {
+		const testCase = testCases[i]
+		helpers.itShouldCreateFile(it, expect, namespace, '/', testCase.name, 'workflow', 'application/x-typescript', btoa(testCase.file),)
+		it(`should invoke /${testCase.name} workflow with &wait=true&fullOutput=true`, async () => {
+			const res = await request(common.config.getDirektivBaseUrl())
+				.post(`/api/v2/namespaces/${namespace}/instances?path=/${testCase.name}&wait=true&fullOutput=true`,)
+				.send(testCase.input)
+			expect(res.statusCode).toEqual(200)
+			expect(res.body.data.status).toEqual(testCase.wantStatus)
+			expect(res.body.data.errorMessage).toEqual(testCase.wantErrorMessage)
+			expect(res.body.data.output).toEqual(testCase.wantOutput)
+		})
+	}
+})
