5775ef40de6490d39a1558d015c3b6e9d1c78fa3
RamiFerjaniDev <e.rami.ferjani@gmail.com>
2026-03-02 10:15:58 +0100

    fix the ci test
    Signed-off-by: RamiFerjaniDev <e.rami.ferjani@gmail.com>

--- tests/engine/basic_auth_action.test.js
+++ tests/engine/basic_auth_action.test.js
@@ -23,7 +23,7 @@ describe('Test js engine', () => {
             var echo = generateAction({
                 type: "workflow",
                 size: "medium",
-                image: "ramiferdocker/auth-echo:latest",
+                image: "ramiferdocker/auth-echo:v1.1",
                 auth: { username: "user", password: "pass" },
             });
             function stateOne(payload) {
@@ -41,7 +41,7 @@ describe('Test js engine', () => {
             var echo = generateAction({
                 type: "workflow",
                 size: "medium",
-                image: "ramiferdocker/auth-echo:latest",
+                image: "ramiferdocker/auth-echo:v1.1",
             });
             function stateOne(payload) {
                 let result = echo({ foo: "bar", input: payload });
@@ -58,7 +58,7 @@ describe('Test js engine', () => {
             var echo = generateAction({
                 type: "workflow",
                 size: "medium",
-                image: "ramiferdocker/auth-echo:latest",
+                image: "ramiferdocker/auth-echo:v1.1",
                 auth: { username: "wrong", password: "creds" },
             });
             function stateOne(payload) {
